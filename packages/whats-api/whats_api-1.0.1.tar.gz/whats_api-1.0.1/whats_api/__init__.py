0f63a9c3e4414125d23aa5703adc814889c7d0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:35 +0000

    Add package whats-api/1.0.1/whats_api-1.0.1.tar.gz

---
+++ packages/whats-api/whats_api-1.0.1.tar.gz/whats_api-1.0.1/whats_api/__init__.py
@@ -0,0 +1,13 @@
+"""
+:authors: neluckoff
+:email: neluckoff@gmail.com
+:copyright: (c) 2022 neluckoff
+"""
+
+from .user import User
+from .client import Client
+
+
+__author__ = 'neluckoff'
+__version__ = '1.0.1'
+__email__ = 'neluckoff@gmail.com'
