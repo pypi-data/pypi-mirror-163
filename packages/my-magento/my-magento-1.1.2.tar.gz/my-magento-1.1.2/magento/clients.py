bcb0afe3e1e7019ffdf6081f71962cebe6340a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:58 +0000

    Add package my-magento/1.1.2/my-magento-1.1.2.tar.gz

---
+++ packages/my-magento/my-magento-1.1.2.tar.gz/my-magento-1.1.2/magento/clients.py
@@ -0,0 +1,225 @@
+from __future__ import annotations
+import json
+import copy
+import pickle
+import requests
+
+from .utils import MagentoLogger, get_agent
+from .search import SearchQuery, OrderSearch, ProductSearch, InvoiceSearch, CategorySearch
+
+
+class Client(object):
+
+    def __init__(self, domain, username, password, user_agent=None, token=None, log_level='INFO', login=True, **kwargs):
+        self.BASE_URL = f'https://www.{domain}/rest/V1/'
+        self.USER_CREDENTIALS = {
+            'username': username,
+            'password': password
+        }
+        self.ACCESS_TOKEN = token
+        self.domain = domain
+        self.user_agent = user_agent if user_agent else get_agent()
+        self.logger = self.get_logger(
+            stdout_level=log_level,
+            log_file=kwargs.get('log_file', None),
+            log_requests=kwargs.get('log_requests', True)
+        )
+        if login:
+            self.authenticate()
+
+    @property
+    def orders(self):
+        return OrderSearch(self)
+
+    @property
+    def invoices(self):
+        return InvoiceSearch(self)
+
+    @property
+    def categories(self):
+        return CategorySearch(self)
+
+    @property
+    def products(self):
+        return ProductSearch(self)
+
+    def search(self, endpoint: str) -> SearchQuery:
+        """Initializes and returns a SearchQuery object corresponding to the specified endpoint"""
+        if endpoint.lower() == 'orders':
+            return self.orders
+        if endpoint.lower() == 'invoices':
+            return self.invoices
+        if endpoint.lower() == 'categories':
+            return self.categories
+        if endpoint.lower() == 'products':
+            return self.products
+        # Any other endpoint is queried with a general SearchQuery object
+        return SearchQuery(endpoint=endpoint, client=self)
+
+    def authenticate(self) -> bool:
+        """Request access token from the authentication endpoint."""
+        endpoint = self.url_for('integration/admin/token')
+        payload = self.USER_CREDENTIALS
+        headers = {
+            'Content-Type': 'application/json',
+            'User-Agent': self.user_agent
+        }
+        self.logger.info(f'Authenticating {payload["username"]} on {self.domain}...')
+        response = requests.post(
+            url=endpoint,
+            json=payload,
+            headers=headers
+        )
+        if response.ok:
+            self.ACCESS_TOKEN = response.json()
+        else:
+            raise AuthenticationError(self, response=response)
+
+        self.logger.debug('Validating token...')
+        try:
+            self.validate()
+        except AuthenticationError as e:
+            raise AuthenticationError(self, msg='Token validation failed') from e
+
+        self.logger.info('Logged in to {}'.format(payload["username"]))
+        return True
+
+    def request(self, url: str) -> requests.Response:
+        """Sends a request with the access token. Used for all internal requests"""
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 401:
+            self.logger.debug("Attempting to re-authenticate...")
+            self.authenticate()  # Will raise AuthenticationError if unsuccessful (won't recurse infinitely)
+            return self.request(url)
+
+        if response.status_code != 200:  # All other responses are returned; errors are handled by methods
+            self.logger.error("Request to {} failed with status code {} and message: \"{}\"".format(
+                url, response.status_code, response.json().get('message', response.json()))
+            )
+        return response
+
+    def url_for(self, endpoint, scope=''):
+        """Returns the appropriate url for the given endpoint and store scope"""
+        if not scope:
+            return self.BASE_URL + endpoint
+        else:  # Must send POST/PUT requests to a scoped url for some updates
+            return self.BASE_URL.replace('/V1', f'/{scope}/V1') + endpoint
+
+    def validate(self) -> bool:
+        """Sends an authorized request to a base API endpoint"""
+        response = self.request(self.url_for('store/websites'))
+        if response.status_code == 200:
+            self.logger.debug("Token validated for {} on {}".format(
+                self.USER_CREDENTIALS['username'], self.domain)
+            )
+            return True
+        else:
+            msg = "Token validation failed for {} on {}".format(
+                self.USER_CREDENTIALS['username'], self.domain
+            )
+            raise AuthenticationError(self, msg=msg, response=response)
+
+    def get_logger(self, log_file = None, stdout_level='INFO', log_requests = True) -> MagentoLogger:
+        """Retrieve a MagentoLogger for the current username/domain combination. Log files are DEBUG.
+
+        :param log_file: the file to log to
+        :param stdout_level: the logging level for stdout logging
+        :param log_requests: if True, will add the logger's FileHandler to the urllib3.connectionpool logger
+        :rtype MagentoLogger: the Magento logger for the username/domain combination of the Client
+        """
+        logger_name = MagentoLogger.CLIENT_LOG_NAME.format(
+            domain=self.domain.split('.')[0],
+            username=self.USER_CREDENTIALS['username']
+        )  # Note there's only one-way access to the logger
+        return MagentoLogger(
+            name=logger_name,
+            log_file=log_file,
+            stdout_level=stdout_level,
+            log_requests=log_requests
+        )
+
+    @property
+    def headers(self) -> dict:
+        """Any time this is called, the token is validated"""
+        return {
+            'Authorization': f'Bearer {self.token}',
+            'User-Agent': self.user_agent
+        }
+
+    @property
+    def token(self) -> str:
+        """Returns or generates access token"""
+        if not self.ACCESS_TOKEN:
+            self.authenticate()
+        return self.ACCESS_TOKEN
+
+    @classmethod
+    def new(cls) -> Client:
+        return cls(
+            input('Domain: '),
+            input('Username: '),
+            input('Password: '),
+            user_agent=input('User Agent: ')
+        )
+
+    @classmethod
+    def load(cls, pickle_bytes) -> Client:
+        return pickle.loads(pickle_bytes)
+
+    def to_pickle(self, validate=False) -> bytes:
+        """Validates credentials (optional) and returns the Client object as a pickle string"""
+        if validate:
+            self.validate()
+        return pickle.dumps(self)
+
+    def to_json(self, validate=False) -> str:
+        """Validates and saves login credentials for this domain"""
+        if validate:
+            self.validate()
+        data = {    # Add more to this if you want!
+            'domain': self.domain,
+            'user_agent': self.user_agent,
+            'token': self.token,
+            'log_level': self.logger.logger.level
+        }
+        data.update(self.USER_CREDENTIALS)
+        return json.dumps(data)
+
+    @classmethod
+    def from_json(cls, json_str) -> Client:
+        kwargs = json.loads(json_str)
+        return cls(**kwargs)
+
+
+class AuthenticationError(Exception):
+
+    DEFAULT_MSG = 'Failed to authenticate credentials. '
+
+    def __init__(self, client: Client, msg=None, response: requests.Response = None):
+        self.message = msg if msg else AuthenticationError.DEFAULT_MSG
+        self.logger = client.logger
+
+        if response is not None:
+            self.parse(response)
+
+        self.logger.error(self.message)
+        super().__init__(self.message)
+
+    def parse(self, response: requests.Response) -> None:
+        """Parses the error message from the response, including message parameters when available"""
+        msg = response.json().get('message')
+        errors = response.json().get('errors')
+
+        if msg:
+            self.message += ' ' + '\n' + f'Response: {msg}'
+
+        if errors:
+            for error in errors:
+                err_msg = error['message']
+                err_params = error.get('parameters')
+
+                if err_params:
+                    for param in err_params:
+                        err_msg = err_msg.replace(f'%{param}', err_params[param])
+
+                self.message += err_msg + '\n'
---
+++ packages/my-magento/my-magento-1.1.2.tar.gz/my-magento-1.1.2/magento/search.py
@@ -0,0 +1,288 @@
+from __future__ import annotations
+from typing import Union
+from .entities import Order, Invoice
+from .models import Product, Category
+from . import clients
+
+
+class SearchQuery:
+
+    def __init__(self, endpoint: str, client: clients.Client, entity=None):
+        if not isinstance(client, clients.Client):
+            raise TypeError(f'client type must be {clients.Client}')
+
+        self.client = client
+        self.endpoint = endpoint
+        self.Entity = entity
+        self.query = self.client.BASE_URL + endpoint + '/?'
+        self.fields = ''
+        self._result = {}
+
+    def add_criteria(self, field, value, condition='eq', **kwargs) -> SearchQuery:
+        """
+        :param field:       the object attribute to search by
+        :param value:       the value of the attribute to compare
+        :param condition:   the comparison condition
+        :param kwargs:      additional search option arguments
+        :return:            the calling SearchQuery object
+
+        *Options*
+                condition:  condition used to evaluate the attribute value
+
+                            (Default Value)     'eq'            =           (field=value)
+                                                'gt'            >
+                                                'lt'            <
+                                                'gteq'          >=
+                                                'lteq'          <=
+                                                'in'            []
+
+
+                group:      filter group number
+
+                filter:     filter number (within the specified filter group)
+
+
+        *Using Filter Groups*
+
+            Filter groups are filter criteria in the form of { field: value }
+
+                Group 0 Filter 0                        ->      Filter 0
+                Group 0 Filter 0 + Group 0 Filter 1     ->      Filter 0 OR Filter 1
+                Group 0 Filter 0 + Group 1 Filter 0     ->      Filter 0 AND Filter 0
+        """
+
+        options = {
+            'condition': condition,
+            'group': 0,
+            'filter': 0,
+        }
+        options.update(kwargs)
+
+        criteria = (
+                f'searchCriteria[filter_groups][{options["group"]}][filters][{options["filter"]}][field]={field}' +
+                f'&searchCriteria[filter_groups][{options["group"]}][filters][{options["filter"]}][value]={value}' +
+                f'&searchCriteria[filter_groups][{options["group"]}][filters][{options["filter"]}][condition_type]={options["condition"]}'
+        )
+        if not self.query.endswith('?'):
+            self.query += '&'
+        self.query += criteria
+        return self
+
+    # Query -> &fields=items['field1,field2']
+    def restrict_fields(self, fields: Union[str, list, tuple]) -> SearchQuery:
+        if not isinstance(fields, str):
+            if not isinstance(fields, (list, tuple)):
+                raise TypeError('Invalid type for argument "fields". Must be a comma separated string or list/tuple.')
+            fields = ','.join(fields)
+
+        if 'entity_id' not in fields:
+            fields = ','.join([fields, 'entity_id'])
+
+        self.fields = f'&fields=items[{fields}]'
+        return self
+
+    def execute(self):
+        """Sends the search request through the active client."""
+        response = self.client.request(self.query + self.fields)
+        self._result = response.json()
+        return self.result
+
+    def by_id(self, item_id: Union[int, str]) -> {}:
+        self.query = self.query.strip('?') + str(item_id)
+        return self.execute()
+
+    def by_number(self, increment_id: str):
+        return self.add_criteria(
+            field='increment_id',
+            value=increment_id
+        ).execute()
+
+    @property
+    def result(self):
+        result = self.validate_result()
+        if result is None:
+            return result
+        if isinstance(result, list):
+            return [self.parse(item) for item in result]
+        if isinstance(result, dict):
+            return self.parse(result)
+
+    def validate_result(self) -> Union[dict, list[dict]]:
+        """
+        Returns the actual result, regardless of search approach
+        Failed: response will always contain a "message" key
+        Success:
+            Search Query:   response contains up to 3 keys from ["items", "total_count", "search_criteria"]
+            Direct Query:   response is the full entity/model response dict; typically has 20+ keys
+        """
+        if not self._result:
+            return None
+
+        if self._result.get('message'):
+            print(
+                'Search failed with the following message: ' + '\t'
+                + self._result['message']
+            )
+            return None
+
+        if len(self._result.keys()) > 3:    # Direct request of entity by id/sku with full response dict
+            return self._result
+
+        if 'items' in self._result:         # All successful response with search criteria will have items key
+            items = self._result['items']   # Note that some entities (ex. Order) have an items key too. Entity response won't reach this line though
+            if items:                       # Response can still be {'items': None} though
+                if len(items) > 1:
+                    return items
+                return items[0]
+            else:
+                print("No matching {} for this search query".format(self.endpoint))
+                return None
+        else:  # I have no idea what could've gone wrong, sorry :/
+            raise RuntimeError("Unknown Error. Raw Response: {}".format(self._result))
+
+    def parse(self, data):
+        """Parses the API response and returns the corresponding entity/model object"""
+        if self.Entity is not None:
+            return self.Entity(data, self.client)   # SearchQuery subclasses return corresponding Entity/Model
+        # General SearchQuery returns the raw data
+        return data
+
+    def reset(self) -> None:
+        self._result = {}
+        self.fields = ''
+        self.query = self.client.BASE_URL + self.endpoint + '/?'
+
+    @property
+    def result_count(self):
+        return len(self.result) if self._result else 0
+
+    @property
+    def result_type(self):
+        return type(self.result)
+
+
+class OrderSearch(SearchQuery):
+
+    def __init__(self, client):
+        super().__init__(
+            endpoint='orders',
+            client=client,
+            entity=Order
+        )
+
+
+class InvoiceSearch(SearchQuery):
+
+    def __init__(self, client):
+        super().__init__(
+            endpoint='invoices',
+            client=client,
+            entity=Invoice
+        )
+
+    def by_order_number(self, order_number):
+        if order := self.client.orders.by_number(order_number):
+            return self.by_order(order)
+
+    def by_order(self, order):
+        return self.by_order_id(order.id)
+
+    def by_order_id(self, order_id):
+        return self.add_criteria(
+            field='order_id',
+            value=order_id
+        ).execute()
+
+
+class ProductSearch(SearchQuery):
+
+    def __init__(self, client):
+        super().__init__(
+            endpoint='products',
+            client=client,
+            entity=Product
+        )
+
+    def by_id(self, item_id: Union[int, str]) -> {}:
+        return self.add_criteria(
+            field='entity_id',  # Product has no "entity_id" field in API responses, just "id"
+            value=item_id       # But to search by the "id" field, need to use "entity_id"
+        ).execute()
+
+    def by_sku(self, sku) -> Product:
+        self.query = self.client.url_for(
+            endpoint='products/{}'.format(
+                sku.replace('/', '%2F')
+            )
+        )
+        return self.execute()
+
+    def get_stock(self, sku):
+        return self.by_sku(sku).stock
+
+    def get_enabled_simple_skus(self):
+        self.add_criteria(
+            field='type_id',
+            value='simple'
+        ).add_criteria(
+            field='status',
+            value=Product.STATUS_ENABLED,
+            group=1
+        )
+        return self.execute()
+
+
+class CategorySearch(SearchQuery):
+
+    def __init__(self, client):
+        super().__init__(
+            endpoint='categories',
+            client=client,
+            entity=Category
+        )
+
+    def get_parent(self):
+        """The top level/default category. Every other category is a subcategory"""
+        return self.execute()
+
+    def get_all(self):
+        """Retrieve all categories"""
+        parent = self.get_parent()
+        if isinstance(parent, Category):
+            return parent.subcategories
+
+    def products_from_id(self, category_id):
+        return self.by_id(category_id).products
+
+    def order_items_from_id(self, category_id):
+        skus = ','.join(self.products_from_id(category_id))
+        query = self.client.search('orders/items')
+        return query.add_criteria(
+            field='sku',
+            value=','.join(skus),
+            condition = 'in'
+        ).execute()
+
+    def orders_from_id(self, category_id, start, end=None):
+        order_items = self.order_items_from_id(category_id)
+        order_ids = ','.join(set([str(item['order_id']) for item in order_items]))
+        orders = self.client.orders
+
+        orders.add_criteria(  # Criteria to match all order_ids we found
+            field='entity_id',
+            value=order_ids,
+            condition='in'
+        ).add_criteria(  # Criteria to match order date range
+            field='created_at',
+            value=start,
+            condition='gt',
+            group=1
+        )
+        if end:
+            orders.add_criteria(
+                field='created_at',
+                value=end,
+                condition='lteq',
+                group=2
+            )
+        return orders.execute()
