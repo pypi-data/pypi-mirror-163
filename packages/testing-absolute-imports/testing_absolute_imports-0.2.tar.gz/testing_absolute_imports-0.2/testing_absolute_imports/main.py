e27ca74781b772a119edc47b6447dd04058d91f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:38 +0000

    Add package testing-absolute-imports/0.2/testing_absolute_imports-0.2.tar.gz

---
+++ packages/testing-absolute-imports/testing_absolute_imports-0.2.tar.gz/testing_absolute_imports-0.2/testing_absolute_imports/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import testing_absolute_imports.printing
+
+
+def main():
+    module_name = testing_absolute_imports.printing.Print()
+    module_name.print_name()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
