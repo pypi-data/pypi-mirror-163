f0264c25c5941c4808f31d9ace3af6df6a06e050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:31 +0000

    Add package testing-absolute-imports/0.5/testing_absolute_imports-0.5-py3-none-any.whl

---
+++ packages/testing-absolute-imports/testing_absolute_imports-0.5-py3-none-any.whl/testing_absolute_imports/printing.py
@@ -0,0 +1,14 @@
+# import sys
+# import os
+# sys.path.append("testing_absolute_imports")
+
+# from testing_absolute_imports import secondprint
+
+
+class Print:
+
+    def __init__(self):
+        pass
+
+    def print_name(self):
+        print("My name is Yashesh")
---
+++ packages/testing-absolute-imports/testing_absolute_imports-0.5-py3-none-any.whl/testing_absolute_imports/secondprint.py
@@ -0,0 +1,7 @@
+class PrintNew:
+
+    def __init__(self):
+        pass
+
+    def second_print(self):
+        print("This is a second print")
