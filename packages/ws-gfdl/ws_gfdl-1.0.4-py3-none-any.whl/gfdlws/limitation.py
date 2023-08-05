1f7a5b270fac1cb9b9356619aa1420cdb3ba4b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:31 +0000

    Add package ws-gfdl/1.0.4/ws_gfdl-1.0.4-py3-none-any.whl

---
+++ packages/ws-gfdl/ws_gfdl-1.0.4-py3-none-any.whl/gfdlws/limitation.py
@@ -0,0 +1,30 @@
+import asyncio
+import websockets
+import json
+import errno
+import os
+
+
+def get(ws):
+    asyncio.get_event_loop().run_until_complete(
+        mass_subscribe_n_stream(ws))
+    return
+
+
+async def mass_subscribe_n_stream(ws):
+    try:
+        req_msg = str('{"MessageType":"GetLimitation"}')
+        await ws.send(req_msg)
+        print("Request : " + req_msg)
+        await get_msg(ws)
+    except:
+        return "Error"
+
+
+async def get_msg(ws):
+    while True:
+        try:
+            message = await ws.recv()
+        except websockets.ConnectionClosedOK:
+            break
+        print(message)
