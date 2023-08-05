986551bf1a5e9c0127074eaf9724f14e67949008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:31 +0000

    Add package ai-image/0.1/ai_image-0.1.tar.gz

---
+++ packages/ai-image/ai_image-0.1.tar.gz/ai_image-0.1/ai_image/__init__.py
@@ -0,0 +1,1 @@
+from ai_image import *
---
+++ packages/ai-image/ai_image-0.1.tar.gz/ai_image-0.1/ai_image/ai_image.py
@@ -0,0 +1,39 @@
+import requests
+import base64
+
+def image(prompt):
+    url = "https://backend.craiyon.com/generate"
+    
+    s = requests.Session()
+    payload = {
+        "prompt": prompt   
+    }
+    
+    res = s.post(url, json=payload)
+    return res.content
+
+def ai_images(prompt):
+    image_urls = str(image(prompt))
+    image_urls = image_urls.replace('\\\\n', '')
+
+    end_index = int(image_urls.index('],"version') - 1)
+    
+    image_urls = image_urls[14:end_index]
+    image_list = image_urls.split('","')
+    
+    c = 0
+    for i in range(9):
+        c+=1
+        
+        chosen_image = image_list[i]
+        
+        imgdata = base64.b64decode(chosen_image)
+        filename = 'output' + str(c) + '.jpg'
+
+        with open(filename, 'wb') as f:
+            f.write(imgdata)
+    print("success")
+
+
+#Enter a prompt for the AI
+
