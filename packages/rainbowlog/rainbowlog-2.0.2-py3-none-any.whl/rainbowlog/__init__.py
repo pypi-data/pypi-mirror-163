f940392bf16aed14a761b83cfcc67bf1f51a9b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:36 +0000

    Add package rainbowlog/2.0.2/rainbowlog-2.0.2-py3-none-any.whl

---
+++ packages/rainbowlog/rainbowlog-2.0.2-py3-none-any.whl/rainbowlog/__init__.py
@@ -0,0 +1,25 @@
+"""
+.. include:: ../README.md
+"""
+
+
+try:
+    import importlib.metadata as importlib_metadata
+except ImportError:
+    import importlib_metadata as importlib_metadata  # type: ignore
+try:
+    __version__ = importlib_metadata.version(__package__ or __name__)
+except importlib_metadata.PackageNotFoundError:
+    import toml
+
+    __version__ = (
+        toml.load("pyproject.toml")
+        .get("tool", {})
+        .get("poetry", {})
+        .get("version", "unknown")
+        + "-dev"
+    )
+
+from ._formatter import Formatter
+
+__all__ = ("Formatter",)
