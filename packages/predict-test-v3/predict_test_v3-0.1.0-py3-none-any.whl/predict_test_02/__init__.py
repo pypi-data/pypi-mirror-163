452fca00331e2015a9d3fbf03544dade9011c776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:13 +0000

    Add package predict-test-v3/0.1.0/predict_test_v3-0.1.0-py3-none-any.whl

---
+++ packages/predict-test-v3/predict_test_v3-0.1.0-py3-none-any.whl/predict_test_02/__init__.py
@@ -0,0 +1,21 @@
+# importing sys
+import sys
+import os
+cdir = os.getcwd()
+cdir+="/models/"
+# adding src/model/ to the system path
+# sys.path.insert(0, '/home/mosfak/Desktop/Predict-the-Collision-and-Save-the-Earth-master/src/models/')
+# print(cdir)
+sys.path.insert(0, cdir)
+# from src.models.__init__ import *
+
+ 
+# importing the main
+# from __init__ import *
+from models.__init__ import main
+
+# str = "/home/mosfak/Desktop/Predict-the-Collision-and-Save-the-Earth-master/predict_test_01/data/neo.csv"
+# # str = sys.argv[1]
+# # print(str)
+# # readd(str)
+# main(str, 1)
