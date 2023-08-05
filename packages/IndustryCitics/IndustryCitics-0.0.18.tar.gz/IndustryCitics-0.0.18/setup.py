7813b315d96ac97f045ef241a14c6c0f88bc9d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:10 +0000

    Add package IndustryCitics/0.0.18/IndustryCitics-0.0.18.tar.gz

---
+++ packages/IndustryCitics/IndustryCitics-0.0.18.tar.gz/IndustryCitics-0.0.18/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name = "IndustryCitics",
+	version = "0.0.18",
+	author = "Xi",
+	description = "AShare analysis at CITICS industry level1",
+	packages = ["IndustryCitics"]
+	)
