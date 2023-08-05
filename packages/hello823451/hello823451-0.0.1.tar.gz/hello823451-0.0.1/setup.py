3d2a31ebba32d5a826dd16321f48678ef69a3d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:30 +0000

    Add package hello823451/0.0.1/hello823451-0.0.1.tar.gz

---
+++ packages/hello823451/hello823451-0.0.1.tar.gz/hello823451-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+
+
+
+
+print("hi")
+
+VERSION = '0.0.1'
+
+# Setting up
+setup(
+    name="hello823451",
+    version=VERSION,
+    author="stfu",
+    description="",
+    packages=["hello823451"],
+    install_requires=[''],
+    keywords=['niggers'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
