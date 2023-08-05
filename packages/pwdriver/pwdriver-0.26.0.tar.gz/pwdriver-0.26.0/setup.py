939169af339daad51e231f02b310c06034baba48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:17 +0000

    Add package pwdriver/0.26.0/pwdriver-0.26.0.tar.gz

---
+++ packages/pwdriver/pwdriver-0.26.0.tar.gz/pwdriver-0.26.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pwdriver',
+    version='0.26.0',
+    license='MIT',
+    author='Jinmoo Han',
+    author_email='jinmoo21@naver.com',
+    description='It will download a WebDriver, and then set basic configuration automatically.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jinmoo21/pwdriver',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'selenium>=4.4.0',
+        'Appium-Python-Client>=2.6.1'
+    ]
+)
