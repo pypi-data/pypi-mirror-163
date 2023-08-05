6423fa98cccbe2bd4baf8d1919cf3c52acd65b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:28 +0000

    Add package coney/0.1.1/coney-0.1.1.tar.gz

---
+++ packages/coney/coney-0.1.1.tar.gz/coney-0.1.1/coney/__init__.py
@@ -0,0 +1,4 @@
+from .broker import AbstractIncomingMessage, AbstractMessage, BindingType, Broker
+
+__version__ = "0.1.1"
+__all__ = ["Broker", "BindingType", "AbstractMessage", "AbstractIncomingMessage"]
