a109b659216a48d1150e037cf522ac7aca4b5418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:16 +0000

    Add package testing-absolute-imports/0.3/testing_absolute_imports-0.3-py3-none-any.whl

---
+++ packages/testing-absolute-imports/testing_absolute_imports-0.3-py3-none-any.whl/testing_absolute_imports/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from printing import Print
+from secondprint import PrintNew
+
+
+def main():
+    module_name = Print()
+    module_name.print_name()
+
+    module_2 = PrintNew()
+    module_2.second_print()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
