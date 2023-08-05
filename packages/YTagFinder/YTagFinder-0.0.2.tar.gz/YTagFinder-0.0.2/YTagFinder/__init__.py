dfc028eafbb7fec8432b2f41a6cf191fe357f8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:52 +0000

    Add package YTagFinder/0.0.2/YTagFinder-0.0.2.tar.gz

---
+++ packages/YTagFinder/YTagFinder-0.0.2.tar.gz/YTagFinder-0.0.2/YTagFinder/__init__.py
@@ -0,0 +1,1 @@
+from .ytagfinder import YTagFinder, YTagFinderError
---
+++ packages/YTagFinder/YTagFinder-0.0.2.tar.gz/YTagFinder-0.0.2/YTagFinder/ytagfinder.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+class YTagFinder:
+    """
+        YTageFinder is a module to fetch all tags from an youtube videos
+        
+        Methods
+        --------
+        find_tags(self, youtube_video_url:str, return_type='list')
+            youtube_video_url: Url of the youtube to fetch the tags
+            return_type: type of the result ('list' for list, 'string' for str)
+    """
+    
+    def __init__(self):
+        pass
+
+    def find_tags(self, youtube_video_url:str, return_type='list') -> list:
+        
+        if 'list' == return_type or return_type == 'string':
+            page = requests.get(youtube_video_url)
+            b = BeautifulSoup(page.content, features='html.parser')
+            f = b.findAll("meta", {"name":"keywords"})
+            tags = f[0]['content']
+            if return_type == 'list':
+                list_tags = tags.split(', ')
+                return list_tags
+            return tags
+        else:
+            raise YTagFinderError("return type not recognize. All avalaible return type: list (default), string")
+class YTagFinderError(Exception):
+    pass
