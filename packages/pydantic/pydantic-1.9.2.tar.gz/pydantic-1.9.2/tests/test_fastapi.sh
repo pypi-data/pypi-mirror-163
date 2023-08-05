e9acfcf915951c9af37403c828991ad1d630cf56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:27 +0000

    Add package pydantic/1.9.2/pydantic-1.9.2.tar.gz

---
+++ packages/pydantic/pydantic-1.9.2.tar.gz/pydantic-1.9.2/tests/test_fastapi.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+set -x
+set -e
+
+cd fastapi
+git fetch --tags
+latest_tag_commit=$(git rev-list --tags --max-count=1)
+latest_tag=$(git describe --tags "${latest_tag_commit}")
+git checkout "${latest_tag}"
+
+pip install -U flit
+flit install
+
+./scripts/test.sh
