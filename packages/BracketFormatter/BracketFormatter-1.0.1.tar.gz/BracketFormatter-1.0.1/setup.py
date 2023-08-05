7912165339ab9151784178864eb62fab5db7f428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:21 +0000

    Add package BracketFormatter/1.0.1/BracketFormatter-1.0.1.tar.gz

---
+++ packages/BracketFormatter/BracketFormatter-1.0.1.tar.gz/BracketFormatter-1.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+        name="BracketFormatter",
+        author="AJ Poulter",
+        author_email='ajp@auctoris.co.uk',
+        url='https://gitlab.com/Auctoris/bracketformatter',
+        version="1.0.1",
+        description="A custom logging formatter to use a [+] style log comment",
+        packages=["BracketFormatter"])
