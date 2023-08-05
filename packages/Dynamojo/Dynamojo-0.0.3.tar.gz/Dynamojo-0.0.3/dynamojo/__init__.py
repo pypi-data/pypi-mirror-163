e84f2b760b09fef5eb3dff2e8e20b4d40dd2a1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:21 +0000

    Add package Dynamojo/0.0.3/Dynamojo-0.0.3.tar.gz

---
+++ packages/Dynamojo/Dynamojo-0.0.3.tar.gz/Dynamojo-0.0.3/dynamojo/__init__.py
@@ -0,0 +1,12 @@
+from .base import DynamojoBase, JoinedAttribute
+from .config import DynamojoConfig
+from .index import (
+    get_indexes,
+    Gsi,
+    Index,
+    IndexList,
+    IndexMap,
+    Lsi,
+    Mutator,
+    TableIndex,
+)
