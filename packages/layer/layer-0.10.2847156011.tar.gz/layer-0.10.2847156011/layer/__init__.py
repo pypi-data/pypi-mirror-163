bb306e20145ca39809a01a17ebee43ad3efec380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:58 +0000

    Add package layer/0.10.2847156011/layer-0.10.2847156011.tar.gz

---
+++ packages/layer/layer-0.10.2847156011.tar.gz/layer-0.10.2847156011/layer/__init__.py
@@ -0,0 +1,44 @@
+from .context import Context  # noqa
+from .contracts.datasets import Dataset  # noqa
+from .contracts.logged_data import Markdown  # noqa
+from .contracts.logged_data import Image, Video  # noqa
+from .contracts.models import Model  # noqa
+from .contracts.projects import Project  # noqa
+from .contracts.runs import Run  # noqa
+from .decorators import (  # noqa
+    conda,
+    dataset,
+    fabric,
+    model,
+    pip_requirements,
+    resources,
+)
+from .decorators.assertions import (  # noqa
+    assert_not_null,
+    assert_skewness,
+    assert_true,
+    assert_unique,
+    assert_valid_values,
+)
+from .flavors.custom import CustomModel  # noqa
+from .global_context import current_project_full_name  # noqa
+from .logged_data.callbacks import KerasCallback, XGBoostCallback  # noqa
+from .main import clear_cache  # noqa
+from .main import get_dataset  # noqa
+from .main import get_model  # noqa
+from .main import get_version  # noqa
+from .main import init  # noqa
+from .main import log  # noqa
+from .main import login  # noqa
+from .main import login_as_guest  # noqa
+from .main import login_with_access_token  # noqa
+from .main import login_with_api_key  # noqa
+from .main import logout  # noqa
+from .main import run  # noqa
+from .main import show_api_key  # noqa
+from .pandas_extensions import Arrays, Images, _register_type_extensions  # noqa
+
+
+_register_type_extensions()
+
+__version__ = get_version()
