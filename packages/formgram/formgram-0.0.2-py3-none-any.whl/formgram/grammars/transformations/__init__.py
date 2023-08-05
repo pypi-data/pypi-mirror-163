a8659f96bd68b24a891fd8afc75c89945d9434b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:41 +0000

    Add package formgram/0.0.2/formgram-0.0.2-py3-none-any.whl

---
+++ packages/formgram/formgram-0.0.2-py3-none-any.whl/formgram/grammars/transformations/__init__.py
@@ -0,0 +1,16 @@
+
+
+
+"""This package holds modules providing form transformations of grammars
+
+
+These are
+
+#. context_free
+    Provides
+#. montotone.py
+
+#. regular.py
+
+
+"""
---
+++ packages/formgram/formgram-0.0.2-py3-none-any.whl/formgram/machines/finite_automata/__init__.py
@@ -0,0 +1,8 @@
+
+"""This package supplies functionality for finite automata
+
+The subpackages
+
+
+"""
+
