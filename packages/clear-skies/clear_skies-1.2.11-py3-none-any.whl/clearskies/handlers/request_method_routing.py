6052160536384caba58a74006ab859cb5135a79d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:14 +0000

    Add package clear-skies/1.2.11/clear_skies-1.2.11-py3-none-any.whl

---
+++ packages/clear-skies/clear_skies-1.2.11-py3-none-any.whl/clearskies/handlers/request_method_routing.py
@@ -0,0 +1,36 @@
+from .routing import Routing
+from abc import abstractmethod
+class RequestMethodRouting(Routing):
+    def __init__(self, di):
+        super().__init__(di)
+
+    @abstractmethod
+    def method_handler_map(self):
+        pass
+
+    def handler_classes(self, configuration):
+        return self.method_handler_map().values()
+
+    def handle(self, input_output):
+        request_method = input_output.get_request_method()
+        method_handler_map = self.method_handler_map()
+        if not request_method in method_handler_map:
+            return self.error(input_output, 'Invalid request method', 400)
+        handler = self.build_handler(method_handler_map[request_method])
+        return handler(input_output)
+
+    def documentation(self):
+        docs = []
+        for (method, handler) in self.method_handler_map.items():
+            doc = self.build_handler(method_handler_map[request_method]).documentation()
+            if not doc:
+                continue
+            doc.set_request_methods(method)
+            docs.append(doc)
+        return docs
+
+    def documentation_models(self):
+        models = {}
+        for (method, handler) in self.method_handler_map.items():
+            models = {**models, **self.build_handler(method_handler_map[request_method]).documentation_models()}
+        return models
