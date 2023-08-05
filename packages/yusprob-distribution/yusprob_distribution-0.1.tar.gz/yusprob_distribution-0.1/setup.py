60301a1088fefc83808879361f6afc4e1e756258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:54 +0000

    Add package yusprob-distribution/0.1/yusprob_distribution-0.1.tar.gz

---
+++ packages/yusprob-distribution/yusprob_distribution-0.1.tar.gz/yusprob_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='yusprob_distribution',
+      version='0.1',
+      description='Gaussian  and Bionomial distributions',
+      packages=['yusprob_distribution'],
+      author='Yusuf Abubakar',
+      author_email='uoooosuf@gmail.com',
+      zip_safe=False)
