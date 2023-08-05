a20f492a46c5431a717eaf8286e8928233b7d0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:31 +0000

    Add package JsMacrosAC/1.8.2.340005/JsMacrosAC-1.8.2.340005.tar.gz

---
+++ packages/JsMacrosAC/JsMacrosAC-1.8.2.340005.tar.gz/JsMacrosAC-1.8.2.340005/JsMacrosAC/helpers.py
@@ -0,0 +1,42 @@
+from typing import TypeVar
+
+from .EventContainer import EventContainer
+from .BaseEvent import BaseEvent
+from .ScoreboardsHelper import ScoreboardsHelper
+from .NBTElementHelper_NBTCompoundHelper import NBTElementHelper_NBTCompoundHelper
+from .ItemEntityHelper import ItemEntityHelper
+from .BaseHelper import BaseHelper
+from .SuggestionsBuilderHelper import SuggestionsBuilderHelper
+from .BlockDataHelper import BlockDataHelper
+from .ServerInfoHelper import ServerInfoHelper
+from .TextHelper import TextHelper
+from .ItemStackHelper import ItemStackHelper
+from .CommandNodeHelper import CommandNodeHelper
+from .ChatHudLineHelper import ChatHudLineHelper
+from .ScoreboardObjectiveHelper import ScoreboardObjectiveHelper
+from .TextFieldWidgetHelper import TextFieldWidgetHelper
+from .BlockPosHelper import BlockPosHelper
+from .TeamHelper import TeamHelper
+from .PlayerEntityHelper import PlayerEntityHelper
+from .VillagerEntityHelper import VillagerEntityHelper
+from .StyleHelper import StyleHelper
+from .BlockStateHelper import BlockStateHelper
+from .StatusEffectHelper import StatusEffectHelper
+from .LivingEntityHelper import LivingEntityHelper
+from .EntityHelper import EntityHelper
+from .NBTElementHelper_NBTNumberHelper import NBTElementHelper_NBTNumberHelper
+from .ButtonWidgetHelper import ButtonWidgetHelper
+from .OptionsHelper import OptionsHelper
+from .RecipeHelper import RecipeHelper
+from .MerchantEntityHelper import MerchantEntityHelper
+from .ClientPlayerEntityHelper import ClientPlayerEntityHelper
+from .TradeOfferHelper import TradeOfferHelper
+from .BossBarHelper import BossBarHelper
+from .NBTElementHelper_NBTListHelper import NBTElementHelper_NBTListHelper
+from .PlayerListEntryHelper import PlayerListEntryHelper
+from .PlayerAbilitiesHelper import PlayerAbilitiesHelper
+from .NBTElementHelper import NBTElementHelper
+from .BlockHelper import BlockHelper
+
+File = TypeVar["java.io.File"]
+
