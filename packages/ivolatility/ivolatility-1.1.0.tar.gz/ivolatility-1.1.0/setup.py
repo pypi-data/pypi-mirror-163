eb9629906e68f4cd9e93186247a9778c7680c074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:45 +0000

    Add package ivolatility/1.1.0/ivolatility-1.1.0.tar.gz

---
+++ packages/ivolatility/ivolatility-1.1.0.tar.gz/ivolatility-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ivolatility',
+    version='1.1.0',
+    description='IVolatility API wrapper package',
+    url='https://github.com/shuds13/pyexample',
+    author='Vladimir Goldin',
+    author_email='vgoldin@egartech.ru',
+    license='BSD 2-clause',
+    packages=['ivolatility'],
+    install_requires=['pandas',
+                      'requests'
+                      ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',  
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
