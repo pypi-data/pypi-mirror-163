3243177babe93bbbc7fb0d1fcd38da64092d0a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:23 +0000

    Add package weaveio/2022.1.6/weaveio-2022.1.6.tar.gz

---
+++ packages/weaveio/weaveio-2022.1.6.tar.gz/weaveio-2022.1.6/weaveio/__version__.py
@@ -0,0 +1,2 @@
+__version__ = "2022.1.6"
+__changes__ = 'added `split` allowing SQL GROUBPY behaviour to be used, added ability to add tables together, forced singular paths to be found first'
---
+++ packages/weaveio/weaveio-2022.1.6.tar.gz/weaveio-2022.1.6/weaveio/readquery/__init__.py
@@ -0,0 +1,7 @@
+from .objects import Query
+from .functions import *
+from .aggregations import *
+from .results import filtered, masked, apply
+from .uploads import join
+from .helpers import *
+from .split import split
