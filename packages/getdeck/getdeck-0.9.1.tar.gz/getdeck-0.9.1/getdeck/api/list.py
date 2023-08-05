4b61140605e28b8cf37fc05ad8ff9a71d1d384fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:31 +0000

    Add package getdeck/0.9.1/getdeck-0.9.1.tar.gz

---
+++ packages/getdeck/getdeck-0.9.1.tar.gz/getdeck-0.9.1/getdeck/api/list.py
@@ -0,0 +1,17 @@
+import logging
+from typing import List
+
+from getdeck.api.utils import stopwatch
+from getdeck.configuration import default_configuration
+
+logger = logging.getLogger("deck")
+
+
+@stopwatch
+def get_available_decks(deckfile_location: str, config=default_configuration) -> List:
+    from getdeck.utils import read_deckfile_from_location
+
+    deckfile = read_deckfile_from_location(deckfile_location, config)
+    available_decks = deckfile.get_decks()
+    logger.debug(available_decks)
+    return available_decks
