abd3fc85b88173b89683ac6f1c005c833cd6c525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:49 +0000

    Add package pypreroms/0.0.1/pypreroms-0.0.1-py3-none-any.whl

---
+++ packages/pypreroms/pypreroms-0.0.1-py3-none-any.whl/pypreroms/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 12 00:16:57 2022
+
+@author: gsolana
+"""
+
---
+++ packages/pypreroms/pypreroms-0.0.1-py3-none-any.whl/pypreroms/exemple.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 12 00:17:52 2022
+
+@author: gsolana
+"""
+
+def add_one(number):
+    """
+    
+
+    Parameters
+    ----------
+    num : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    return number + 1
