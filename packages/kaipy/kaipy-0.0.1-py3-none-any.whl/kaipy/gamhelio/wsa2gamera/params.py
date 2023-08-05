40245600232cc8fc91409efbb3efea81e1cf1310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:55 +0000

    Add package kaipy/0.0.1/kaipy-0.0.1-py3-none-any.whl

---
+++ packages/kaipy/kaipy-0.0.1-py3-none-any.whl/kaipy/gamhelio/wsa2gamera/params.py
@@ -0,0 +1,33 @@
+import configparser
+
+class params():
+    def __init__(self,ConfigFileName):
+        config = configparser.ConfigParser(inline_comment_prefixes=(';','#'))
+        config.read(ConfigFileName)
+        
+        self.gameraGridFile = config['Gamera']['gameraGridFile']
+        self.GridDir = config['Gamera']['GridDir']
+        self.gameraIbcFile = config['Gamera']['gameraIbcFile'] 
+        self.IbcDir = config['Gamera']['IbcDir']   
+
+        self.wsaFile = config['WSA']['wsafile']
+        self.gaussSmoothWidth = config.getint('WSA','gauss_smooth_width')
+        self.plots = config.getboolean('WSA','plots')
+        self.densTempInfile = config.getboolean('WSA','density_temperature_infile')
+        self.normalized = config.getboolean('WSA','normalized')
+
+        self.gamma = config.getfloat('Constants','gamma')
+        self.NO2   = config.getint('Constants','NO2')
+
+        self.B0 = config.getfloat('Normalization','B0')
+        self.n0 = config.getfloat('Normalization','n0')
+        
+        self.tMin     = config.getfloat('Grid','tMin')
+        self.tMax     = config.getfloat('Grid','tMax')
+        self.Rin      = config.getfloat('Grid','Rin')
+        self.Rout     = config.getfloat('Grid','Rout')
+        self.Ni       = config.getint('Grid','Ni')
+        self.Nj       = config.getint('Grid','Nj')
+        self.Nk       = config.getint('Grid','Nk')
+
+
