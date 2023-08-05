f2d124299c5680eacd5e8a3c695d565313684552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:16 +0000

    Add package py-obsidianmd/0.1.1/py-obsidianmd-0.1.1.tar.gz

---
+++ packages/py-obsidianmd/py-obsidianmd-0.1.1.tar.gz/py-obsidianmd-0.1.1/src/pyobsidianmd/note.py
@@ -0,0 +1,37 @@
+import re
+from pathlib import Path
+from typing import Union
+
+from pyobsidianmd.metadata import NoteMetadata
+
+
+class Note:
+    """Represents a note (text file in obsidian)
+    - frontmatter: the note's frontmatter
+    - body: the note's body (everything except the frontmatter)
+    - content: frontmatter + body
+    """
+
+    def __init__(self, path: Union[Path, str]):
+        self.path: Path = Path(path)
+        with open(self.path, "r") as f:
+            self.content: str = f.read()
+        self.metadata: NoteMetadata = NoteMetadata(self.content)
+
+    def __repr__(self) -> str:
+        return f'Note (path: "{self.path}")\n'
+
+    def update_content(self, how_inline: str = "bottom"):
+        self.content = self.metadata.update_content(self.content, how_inline=how_inline)
+
+    def write(self, path: Union[Path, None] = None):
+        """Write the current content to the note's path"""
+        p = self.path if path is None else path
+        with open(p, "w") as f:
+            f.write(self.content)
+
+    def sub(self, pattern: str, replace: str):
+        self.content = re.sub(pattern, replace, self.content)
+
+    def print(self):
+        print(self.content)
