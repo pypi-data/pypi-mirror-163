f7261a51c5444231823bf301d45c09f09f11fd16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:17 +0000

    Add package starlite/1.10.1/starlite-1.10.1-py3-none-any.whl

---
+++ packages/starlite/starlite-1.10.1-py3-none-any.whl/starlite/cache/simple_cache_backend.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import TYPE_CHECKING, Any, Dict, cast
+
+from starlite.cache.base import CacheBackendProtocol
+
+if TYPE_CHECKING:
+    from typing import Optional
+
+
+class SimpleCacheBackend(CacheBackendProtocol):
+    """
+    This class offers a cache backend that stores values in a dict.
+
+    In a production system you probably should use Redis or MemCached instead.
+    """
+
+    @dataclass()
+    class CacheObject:
+        """
+        A container class
+        """
+
+        value: Any
+        expiration: datetime
+
+    def __init__(self) -> None:
+        self._store: Dict[str, Any] = {}
+
+    def get(self, key: str) -> Any:
+        """
+        Retrieve a value for a given key
+        """
+        cache_obj = cast("Optional[SimpleCacheBackend.CacheObject]", self._store.get(key))
+        if cache_obj:
+            if cache_obj.expiration >= datetime.now():
+                return cache_obj.value
+            self.delete(key)
+        return None
+
+    def set(self, key: str, value: Any, expiration: int) -> None:
+        """
+        Set a value for a given key
+        """
+        self._store[key] = SimpleCacheBackend.CacheObject(
+            value=value, expiration=datetime.now() + timedelta(seconds=expiration)
+        )
+
+    def delete(self, key: str) -> None:
+        """
+        Remove a value for a given key
+        """
+        self._store.pop(key, None)
---
+++ packages/starlite/starlite-1.10.1-py3-none-any.whl/starlite/enums.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+
+class HttpMethod(str, Enum):
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    PATCH = "PATCH"
+    DELETE = "DELETE"
+
+
+class MediaType(str, Enum):
+    JSON = "application/json"
+    HTML = "text/html"
+    TEXT = "text/plain"
+
+
+class OpenAPIMediaType(str, Enum):
+    OPENAPI_YAML = "application/vnd.oai.openapi"
+    OPENAPI_JSON = "application/vnd.oai.openapi+json"
+
+
+class RequestEncodingType(str, Enum):
+    JSON = "application/json"
+    MULTI_PART = "multipart/form-data"
+    URL_ENCODED = "application/x-www-form-urlencoded"
+
+
+class ScopeType(str, Enum):
+    HTTP = "http"
+    WEBSOCKET = "websocket"
+    ASGI = "asgi"
+
+
+class ParamType(str, Enum):
+    PATH = "path"
+    QUERY = "query"
+    COOKIE = "cookie"
+    HEADER = "header"
