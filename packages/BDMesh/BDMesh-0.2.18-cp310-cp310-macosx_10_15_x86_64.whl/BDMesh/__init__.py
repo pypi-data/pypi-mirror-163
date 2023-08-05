667a16ff7caaedfb03fc2a99edd9395f92e3e7ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:47 +0000

    Add package BDMesh/0.2.18/BDMesh-0.2.18-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/BDMesh/BDMesh-0.2.18-cp310-cp310-macosx_10_15_x86_64.whl/BDMesh/__init__.py
@@ -0,0 +1,5 @@
+from ._version import __version__
+from .Mesh1D import Mesh1D
+from .Mesh1DUniform import Mesh1DUniform
+from .TreeMesh1D import TreeMesh1D
+from .TreeMesh1DUniform import TreeMesh1DUniform
