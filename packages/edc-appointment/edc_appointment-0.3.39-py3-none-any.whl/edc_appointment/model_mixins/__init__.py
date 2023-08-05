f1d4864c38a700eac55c016a6df4db2b32b667e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:21 +0000

    Add package edc-appointment/0.3.39/edc_appointment-0.3.39-py3-none-any.whl

---
+++ packages/edc-appointment/edc_appointment-0.3.39-py3-none-any.whl/edc_appointment/model_mixins/__init__.py
@@ -0,0 +1,7 @@
+from .appointment_methods_model_mixin import (
+    AppointmentMethodsModelError,
+    AppointmentMethodsModelMixin,
+)
+from .appointment_model_mixin import AppointmentModelMixin
+from .missed_appointment_model_mixin import MissedAppointmentModelMixin
+from .window_period_model_mixin import AppointmentWindowError, WindowPeriodModelMixin
