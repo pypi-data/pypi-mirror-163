c1686c789fdf2aceae930e691c7e26302b561c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:39 +0000

    Add package soda-sql-redshift/2.2.2/soda-sql-redshift-2.2.2.tar.gz

---
+++ packages/soda-sql-redshift/soda-sql-redshift-2.2.2.tar.gz/soda-sql-redshift-2.2.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup, find_namespace_packages
+
+if sys.version_info < (3, 7):
+    print('Error: Soda SQL requires at least Python 3.7')
+    print('Error: Please upgrade your Python version to 3.7 or later')
+    sys.exit(1)
+
+package_name = "soda-sql-redshift"
+package_version = '2.2.2'
+# TODO Add proper description
+description = "Soda SQL Redshift"
+
+requires = [
+    f'soda-sql-core=={package_version}',
+    f'soda-sql-postgresql=={package_version}',
+    'boto3>=1.15.18, <2.0'
+]
+# TODO Fix the params
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["sodasql*"]),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ]
+)
