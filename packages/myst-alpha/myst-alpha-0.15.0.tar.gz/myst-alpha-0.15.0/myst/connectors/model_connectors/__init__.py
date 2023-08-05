fcb99a2cab9608c415dccf2347a93e1e8212085e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:44 +0000

    Add package myst-alpha/0.15.0/myst-alpha-0.15.0.tar.gz

---
+++ packages/myst-alpha/myst-alpha-0.15.0.tar.gz/myst-alpha-0.15.0/myst/connectors/model_connectors/__init__.py
@@ -0,0 +1,19 @@
+from myst.connectors.model_connectors.elastic_net import ElasticNet
+from myst.connectors.model_connectors.extra_trees_regression import ExtraTreesRegression
+from myst.connectors.model_connectors.lightgbm import LightGBM
+from myst.connectors.model_connectors.linear_regression import LinearRegression
+from myst.connectors.model_connectors.mlp_regression import MLPRegression
+from myst.connectors.model_connectors.ngboost import NGBoost
+from myst.connectors.model_connectors.random_forest_regression import RandomForestRegression
+from myst.connectors.model_connectors.xgboost import XGBoost
+
+__all__ = [
+    "ElasticNet",
+    "ExtraTreesRegression",
+    "LightGBM",
+    "LinearRegression",
+    "MLPRegression",
+    "NGBoost",
+    "RandomForestRegression",
+    "XGBoost",
+]
---
+++ packages/myst-alpha/myst-alpha-0.15.0.tar.gz/myst-alpha-0.15.0/myst/resources/hpo_trial.py
@@ -0,0 +1,25 @@
+from typing import Any, Dict
+
+import pandas as pd
+from pydantic import BaseModel
+
+from myst.openapi.models.hpo_metrics import HPOMetrics
+
+
+class HPOTrial(BaseModel):
+    parameters: Dict[str, Any]
+    metrics: HPOMetrics
+
+    def to_pandas_data_frame(self) -> pd.DataFrame:
+        """Downloads the HPO result and converts the time arrays to pandas data frames.
+
+        Data will be re-indexed against the predictions' natural time index, dropping any target data that doesn't
+        correspond to a prediction.
+
+        Returns:
+            a pandas data frame with the predictions made by the HPO, and their corresponding targets. # noqa: DAR202
+
+        Raises:
+            NotImplementedError: currently is not implemented
+        """
+        raise NotImplementedError
