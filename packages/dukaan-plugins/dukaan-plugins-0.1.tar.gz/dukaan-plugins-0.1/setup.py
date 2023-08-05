5832874b415a12f0b7a95106682df5abd7ed91a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:09 +0000

    Add package dukaan-plugins/0.1/dukaan-plugins-0.1.tar.gz

---
+++ packages/dukaan-plugins/dukaan-plugins-0.1.tar.gz/dukaan-plugins-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dukaan-plugins',
+    version='0.1',
+    author="",
+    author_email='mehboob.shaik@mydukaan.io',
+    url='https://github.com/teamdukaan',
+    keywords='package'
+)
