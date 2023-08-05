10cd392b2d237504ceda5d76c7749b10209420f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:38 +0000

    Add package PySwitchbot/0.18.7/PySwitchbot-0.18.7.tar.gz

---
+++ packages/PySwitchbot/PySwitchbot-0.18.7.tar.gz/PySwitchbot-0.18.7/switchbot/adv_parsers/plug.py
@@ -0,0 +1,11 @@
+"""Library to handle connection with Switchbot."""
+from __future__ import annotations
+
+
+def process_woplugmini(data: bytes, mfr_data: bytes | None) -> dict[str, bool | int]:
+    """Process plug mini."""
+    return {
+        "switchMode": True,
+        "isOn": mfr_data[7] == 0x80,
+        "wifi_rssi": -mfr_data[9],
+    }
