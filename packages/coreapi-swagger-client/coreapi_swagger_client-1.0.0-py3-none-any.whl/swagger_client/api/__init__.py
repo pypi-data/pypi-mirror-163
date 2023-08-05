d609a34976e9c9c111d826e4bb440cfdedd4a64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:30 +0000

    Add package coreapi-swagger-client/1.0.0/coreapi_swagger_client-1.0.0-py3-none-any.whl

---
+++ packages/coreapi-swagger-client/coreapi_swagger_client-1.0.0-py3-none-any.whl/swagger_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.announcements_api import AnnouncementsApi
+from swagger_client.api.people_api import PeopleApi
+from swagger_client.api.projects_api import ProjectsApi
+from swagger_client.api.publications_api import PublicationsApi
+from swagger_client.api.sshkeys_api import SshkeysApi
+from swagger_client.api.version_api import VersionApi
+from swagger_client.api.whoami_api import WhoamiApi
