bc276c2163f5410b0bbee5c72524042f50a44dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:45 +0000

    Add package nandg-distributions/1.0/nandg_distributions-1.0.tar.gz

---
+++ packages/nandg-distributions/nandg_distributions-1.0.tar.gz/nandg_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nandg_distributions',
+      version='1.0',
+      description='Gaussian & Binomial distributions',
+      packages=['nandg_distributions'],
+      author = "Shubham Trivedi",
+      author_email = "trivedi700@gmail.com",
+      zip_safe=False)
