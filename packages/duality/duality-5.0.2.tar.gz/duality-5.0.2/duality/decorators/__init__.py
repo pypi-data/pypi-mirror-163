4f9b77484b4ca98556c380fdab08e08f84d944b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:16 +0000

    Add package duality/5.0.2/duality-5.0.2.tar.gz

---
+++ packages/duality/duality-5.0.2.tar.gz/duality-5.0.2/duality/decorators/__init__.py
@@ -0,0 +1,27 @@
+# imports relevant dependencies.
+from logistics.plugins.metaclass import Meta
+
+# coloring.
+from colorama import (
+    Fore,
+    Back,
+    Style,
+    init,
+)
+
+init()
+
+# imports all data types.
+from logistics.plugins.types import *
+
+# imports all relevant contents.
+from duality.decorators.dualityapp import DualityApp
+from duality.decorators.dualitytrack import DualityTrack
+
+# all relevant contents.
+__all__ = [
+    'VandalTypes',
+    'Meta',
+    'DualityApp',
+    'DualityTrack',
+]
