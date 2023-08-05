e2182b26a0de894d3961addf9807df06136d3b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:45 +0000

    Add package modelfun/0.0.1.3/modelfun-0.0.1.3.tar.gz

---
+++ packages/modelfun/modelfun-0.0.1.3.tar.gz/modelfun-0.0.1.3/modelfun/classify.py
@@ -0,0 +1,37 @@
+from modelfun.response import ModelfunObject
+from typing import List
+
+
+class Confidence(ModelfunObject):
+    def __init__(self, label: str, confidence: float) -> None:
+        self.label = label
+        self.confidence = confidence
+
+
+class Classification(ModelfunObject):
+    def __init__(self, input: str,
+                 prediction: str, confidence: Confidence) -> None:
+        self.input = input
+        self.prediction = prediction
+        self.confidence = confidence
+
+
+class Classifications(ModelfunObject):
+    def __init__(self, classifications: List[Classification]) -> None:
+        self.classifications = classifications
+        self.iterator = iter(classifications)
+
+    def __iter__(self) -> iter:
+        return self.iterator
+
+    def __next__(self) -> next:
+        return next(self.iterator)
+
+    def __len__(self) -> int:
+        return len(self.classifications)
+
+
+class Example(ModelfunObject):
+    def __init__(self, text: str, label: str) -> None:
+        self.text = text
+        self.label = label
