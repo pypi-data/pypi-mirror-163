dd47b9d88b08a6fb84aada81836e274bb0a54fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:28 +0000

    Add package devcloudcli/1.3.17/devcloudcli-1.3.17-py3-none-any.whl

---
+++ packages/devcloudcli/devcloudcli-1.3.17-py3-none-any.whl/src/scripts/dl-model-training-services/training-frameworks/intel-optimized-tensorflow/uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# Copyright (C) 2018-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+docker rmi intel/intel-optimized-tensorflow
+echo -e "\e[1;32mThe tensorflow is uninstalled\e[0m "
+
+
