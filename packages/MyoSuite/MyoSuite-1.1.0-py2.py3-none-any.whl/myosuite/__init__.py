d0618a932daf42910759f949cba146abb804681d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:41 +0000

    Add package MyoSuite/1.1.0/MyoSuite-1.1.0-py2.py3-none-any.whl

---
+++ packages/MyoSuite/MyoSuite-1.1.0-py2.py3-none-any.whl/myosuite/__init__.py
@@ -0,0 +1,9 @@
+""" =================================================
+# Copyright (c) Facebook, Inc. and its affiliates
+Authors  :: Vikash Kumar (vikashplus@gmail.com), Vittorio Caggiano (caggiano@gmail.com)
+================================================= """
+
+import myosuite.envs.myo # noqa
+import myosuite.envs.myo.myochallenge # noqa
+
+__version__ = "1.1.0" #aligned with mj_envs 0.3dev
