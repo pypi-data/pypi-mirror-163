3b9681de412f819a1cfcd0c561d2af362afd5fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:21 +0000

    Add package SolOrder/1.0.1/SolOrder-1.0.1.tar.gz

---
+++ packages/SolOrder/SolOrder-1.0.1.tar.gz/SolOrder-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'SolOrder',
+    packages = ['SolOrder'],
+    version = '1.0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Updated a few..',
+    author = 'Dibyendu Maity',
+    author_email = 'dibyendumaity1999@bose.res.in',
+    url = 'https://github.com/dmighty007/SolOrder',
+    download_url = 'https://github.com/dmighty007/SolOrder',
+    keywords = ['water', 'SOL', 'Order','Parameter'],
+    classifiers = [],
+)
