e03969beab07b43a8270caabbfa70232cc8d5091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:38 +0000

    Add package final-test-04/0.1.0/final_test_04-0.1.0.tar.gz

---
+++ packages/final-test-04/final_test_04-0.1.0.tar.gz/final_test_04-0.1.0/final_test04/untitled0.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 12 04:18:30 2022
+
+@author: mosfak
+"""
+
+# # -*- coding: utf-8 -*-
+# """
+# Spyder Editor
+
+# This is a temporary script file.
+# """
+from __init__ import *
+str = "/home/mosfak/Desktop/Predict-the-Collision-and-Save-the-Earth-master/predict_test_06/data/neo.csv"
+
+main_fun(str, 4)
