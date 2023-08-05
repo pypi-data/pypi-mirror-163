cf3c0175dd28ff62f1bb52a8cb90005e9fdc75e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:05 +0000

    Add package aioutilities/1.0.0/aioutilities-1.0.0.tar.gz

---
+++ packages/aioutilities/aioutilities-1.0.0.tar.gz/aioutilities-1.0.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['aioutilities']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['aioutilities-example = aioutilities.example:run_example']}
+
+setup_kwargs = {
+    'name': 'aioutilities',
+    'version': '1.0.0',
+    'description': 'asyncio-powered coroutine worker pool',
+    'long_description': None,
+    'author': 'Kevin Kirsche',
+    'author_email': 'kev.kirsche@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4',
+}
+
+
+setup(**setup_kwargs)
