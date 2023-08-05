85389b78bcf426df7d440967feb1ca5005e20f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:04 +0000

    Add package XMagics/1.0.4/XMagics-1.0.4.tar.gz

---
+++ packages/XMagics/XMagics-1.0.4.tar.gz/XMagics-1.0.4/README.md
@@ -0,0 +1,18 @@
+# XOne_Magic
+
+Under construction! Not ready for use yet! Currently experimenting and planning!
+
+Developed by Amr Elmenyawy(Mr.Torwada) Egypt (c) 2022
+
+## Examples of How To Use (Buggy Alpha Version)
+
+
+```python
+from XMagXics import Xsms
+
+number = input('Enter Target Number : ')
+text = input('Enter Message : ')
+
+Spam = Xsms.spam(number,text)
+print(spam)
+```
---
+++ packages/XMagics/XMagics-1.0.4.tar.gz/XMagics-1.0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.4'
+DESCRIPTION = 'XOne_Team library'
+LONG_DESCRIPTION = 'The  Best  Library Will Come In THe Future All Code Returned To Amr Elmenyawy'
+
+# Setting up
+setup(
+    name="XMagics",
+    version=VERSION,
+    author="Amr Elmenyawy",
+    author_email="xone.conect@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['Best library'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
