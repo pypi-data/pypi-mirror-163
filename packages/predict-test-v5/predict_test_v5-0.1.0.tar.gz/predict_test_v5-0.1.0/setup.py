04e5f09bf21965708155a5af1df93e37aff41e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:25 +0000

    Add package predict-test-v5/0.1.0/predict_test_v5-0.1.0.tar.gz

---
+++ packages/predict-test-v5/predict_test_v5-0.1.0.tar.gz/predict_test_v5-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='predict_test_v5',
+    packages=["predict_test_04"],
+    version='0.1.0',
+    description='the goal of this project is to predict the collision of nearest objects around the mighty EARTH.',
+    author='Sultan Mahmud Nahian',
+    license='MIT',
+)
