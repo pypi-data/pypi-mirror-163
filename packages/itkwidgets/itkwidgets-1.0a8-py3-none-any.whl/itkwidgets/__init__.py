d0a83d05b162f858ea0ed67e12cbaba1b1069250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:43 +0000

    Add package itkwidgets/1.0a8/itkwidgets-1.0a8-py3-none-any.whl

---
+++ packages/itkwidgets/itkwidgets-1.0a8-py3-none-any.whl/itkwidgets/__init__.py
@@ -0,0 +1,16 @@
+"""itkwidgets: Interactive widgets to visualize images, point sets, and 3D geometry on the web."""
+
+__version__ = "1.0a7"
+
+from imjoy_rpc import register_default_codecs
+register_default_codecs()
+
+from .imjoy import register_itkwasm_imjoy_codecs
+register_itkwasm_imjoy_codecs()
+
+from .viewer import Viewer, view
+
+__all__ = [
+  "Viewer",
+  "view",
+]
