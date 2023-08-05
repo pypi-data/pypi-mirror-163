ca3b1321f83f3bb0c23553d0358cb7de7c5fe2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:11 +0000

    Add package static-deployer/0.1.5/static-deployer-0.1.5.tar.gz

---
+++ packages/static-deployer/static-deployer-0.1.5.tar.gz/static-deployer-0.1.5/setup.py
@@ -0,0 +1,50 @@
+import pathlib
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = 'static-deployer'
+PACKAGE_VER = '0.1.5'
+PACKAGE_DESC = 'Deploy static websites using a single command'
+PACKAGE_URL = 'https://github.com/jweyrich/static-deployer'
+PACKAGE_LICENSE = 'BSD-3-Clause'
+PACKAGE_AUTHOR = 'Jardel Weyrich'
+PACKAGE_AUTHOR_EMAIL = 'jweyrich@gmail.com'
+
+if __name__ == "__main__":
+    # The text of the README file
+    CURRENT_DIR = pathlib.Path(__file__).parent
+    README = (CURRENT_DIR / "README.md").read_text()
+    setup(
+        name=PACKAGE_NAME,
+        version=PACKAGE_VER,
+        description=PACKAGE_DESC,
+        long_description=README,
+        long_description_content_type="text/markdown",
+        url=PACKAGE_URL,
+        author=PACKAGE_AUTHOR,
+        author_email=PACKAGE_AUTHOR_EMAIL,
+        license=PACKAGE_LICENSE,
+        packages=find_packages(exclude=("test",), where='src'),
+        package_dir={"": "src"},
+        py_modules=['cli'],
+        entry_points='''
+            [console_scripts]
+            static-deployer=cli:main
+        ''',
+        install_requires=[
+            'attrs >=21.2.0,<22.0.0',
+            'boto3 >=1.18.39,<2.0.0',
+            'toml >=0.10.2,<1.0.0',
+            'multiprocessing-logging >=0.3.1,<1.0.0',
+        ],
+        python_requires='>=3.7, <4.0',
+        classifiers=[
+            'Development Status :: 1 - Planning',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+        ],
+    )
---
+++ packages/static-deployer/static-deployer-0.1.5.tar.gz/static-deployer-0.1.5/src/static_deployer/common/log.py
@@ -0,0 +1,31 @@
+import logging
+import multiprocessing
+import multiprocessing_logging
+import os
+
+log_level_from_env = os.environ.get('LOGLEVEL', '').upper()
+log_format = '%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(funcName)s %(message)s'
+log_level = logging.DEBUG if log_level_from_env == 'DEBUG' else logging.INFO
+
+logging.basicConfig(format=log_format, level=log_level)
+logger = logging.getLogger(__name__)
+
+mp_logger = multiprocessing.get_logger()
+# mp_handler = logging.StreamHandler()
+# mp_handler.setLevel(log_level)
+# mp_handler.setFormatter(logging.Formatter(log_format))
+# mp_logger.addHandler(mp_handler)
+
+# Handle records from parallel processes to the main process so that they are handled correctly.
+# multiprocessing_logging.install_mp_handler()
+
+def _make_debug_record(message):
+    fn, lno, func, sinfo = logger.findCaller()
+    record = logger.makeRecord(logger.name, logging.DEBUG, fn, lno, message, None, None,
+                               func=func, extra=None, sinfo=sinfo)
+    return record
+
+
+def debug(message: str):
+    record = _make_debug_record(message)
+    logger.handle(record)
