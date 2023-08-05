0b247c6a6e2ac9e8f47888a3f99a57b30069e070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:03 +0000

    Add package final-test-08/0.1.0/final_test_08-0.1.0.tar.gz

---
+++ packages/final-test-08/final_test_08-0.1.0.tar.gz/final_test_08-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='final_test_08',
+    packages=["final_test05"],
+    version='0.1.0',
+    description='the goal of this project is to predict the collision of nearest objects around the mighty EARTH.',
+    author='Sultan Mahmud Nahian',
+    license='MIT',
+)
