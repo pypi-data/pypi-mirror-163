176d71da21456e19e3a7094ce6dfa5e0cffa47ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:13 +0000

    Add package pyseafuel/0.1.1/pyseafuel-0.1.1.tar.gz

---
+++ packages/pyseafuel/pyseafuel-0.1.1.tar.gz/pyseafuel-0.1.1/pyseafuel/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.1.0'
+
+from pyseafuel import degasser
+from pyseafuel import desalinator
+from pyseafuel import dic
+from pyseafuel import electrolyzer
+from pyseafuel import reactor
+from pyseafuel import system
