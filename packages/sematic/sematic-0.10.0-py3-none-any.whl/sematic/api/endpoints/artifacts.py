84a6fbd6b8a8b91859b231a9b2da9706d0db3d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:15 +0000

    Add package sematic/0.10.0/sematic-0.10.0-py3-none-any.whl

---
+++ packages/sematic/sematic-0.10.0-py3-none-any.whl/sematic/api/endpoints/artifacts.py
@@ -0,0 +1,31 @@
+# Standard Library
+from typing import List
+
+# Third-party
+import flask
+import sqlalchemy
+
+# Sematic
+from sematic.api.app import sematic_api
+from sematic.api.endpoints.request_parameters import get_request_parameters
+from sematic.db.db import db
+from sematic.db.models.artifact import Artifact
+
+
+@sematic_api.route("/api/v1/artifacts", methods=["GET"])
+def list_artifacts_endpoint() -> flask.Response:
+    limit, _, _, sql_predicates = get_request_parameters(flask.request.args, Artifact)
+
+    with db().get_session() as session:
+        query = session.query(Artifact)
+
+        if sql_predicates is not None:
+            query = query.filter(sql_predicates)
+
+        query = query.order_by(sqlalchemy.desc(Artifact.created_at))
+
+        artifacts: List[Artifact] = query.limit(limit).all()
+
+    payload = dict(content=[artifacts.to_json_encodable() for artifacts in artifacts])
+
+    return flask.jsonify(payload)
---
+++ packages/sematic/sematic-0.10.0-py3-none-any.whl/sematic/api/server.py
@@ -0,0 +1,106 @@
+# Standard Library
+import argparse
+import os
+import sys
+
+# Third-party
+import eventlet
+from flask import jsonify, send_file
+from flask_socketio import Namespace, SocketIO  # type: ignore
+
+# Sematic
+import sematic.api.endpoints.artifacts  # noqa: F401
+
+# Endpoint modules need to be imported for endpoints
+# to be registered.
+import sematic.api.endpoints.auth  # noqa: F401
+import sematic.api.endpoints.edges  # noqa: F401
+import sematic.api.endpoints.meta  # noqa: F401
+import sematic.api.endpoints.notes  # noqa: F401
+import sematic.api.endpoints.runs  # noqa: F401
+from sematic.api.app import sematic_api
+from sematic.api.wsgi import SematicWSGI
+from sematic.config import get_config, switch_env  # noqa: F401
+
+# Monkey-patching ssl
+# See https://eventlet.net/doc/patching.html
+# google.oauth2.id_token.verify_oauth2_token makes outgoing
+# SSL queries
+eventlet.import_patched("ssl")
+sys.modules["ssl"] = eventlet.green.ssl
+
+
+@sematic_api.route("/data/<file>")
+def data(file: str):
+    """
+    Endpoint to serve images and large payloads stored on disc.
+    """
+    path = os.path.join(get_config().data_dir, file)
+    return send_file(path)
+
+
+@sematic_api.route("/")
+@sematic_api.route("/<path:path>")
+def index(path=""):
+    """
+    Returns the index page of the UI app.
+
+    To build the UI app:
+    $ cd ui
+    $ npm run build
+    """
+    return send_file("../ui/build/index.html")
+
+
+@sematic_api.route("/api/v1/ping")
+def ping():
+    """
+    Basic health ping. Does not include DB liveness check.
+    """
+    return jsonify({"status": "ok"})
+
+
+socketio = SocketIO(sematic_api, cors_allowed_origins="*")
+# This is necessary because starting version 5.7.0 python-socketio does not
+# accept connections to undeclared namespaces
+socketio.on_namespace(Namespace("/pipeline"))
+socketio.on_namespace(Namespace("/graph"))
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser("Sematic API server")
+    parser.add_argument("--env", required=False, default="local", type=str)
+    parser.add_argument("--debug", required=False, default=False, action="store_true")
+    parser.add_argument("--daemon", required=False, default=False, action="store_true")
+    return parser.parse_args()
+
+
+def run_wsgi(daemon: bool):
+    options = {
+        "bind": "{}:{}".format(get_config().server_address, get_config().port),
+        "workers": 1,
+        "worker_class": "eventlet",
+        "daemon": daemon,
+        "pidfile": get_config().server_pid_file_path,
+        "accesslog": os.path.join(get_config().config_dir, "access.log"),
+        "errorlog": os.path.join(get_config().config_dir, "error.log"),
+        "certfile": os.environ.get("CERTIFICATE"),
+        "keyfile": os.environ.get("PRIVATE_KEY"),
+    }
+    SematicWSGI(sematic_api, options).run()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    switch_env(args.env)
+
+    if args.debug:
+        socketio.run(
+            sematic_api,
+            port=get_config().port,
+            host=get_config().server_address,
+            debug=args.debug,
+        )
+
+    else:
+        run_wsgi(args.daemon)
