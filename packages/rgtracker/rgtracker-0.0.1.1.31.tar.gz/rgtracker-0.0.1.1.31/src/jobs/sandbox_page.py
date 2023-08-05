ac42979d701e53fa6469ee0762495638def32492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:38 +0000

    Add package rgtracker/0.0.1.1.31/rgtracker-0.0.1.1.31.tar.gz

---
+++ packages/rgtracker/rgtracker-0.0.1.1.31.tar.gz/rgtracker-0.0.1.1.31/src/jobs/sandbox_page.py
@@ -0,0 +1,17 @@
+import json
+from redisgears import log
+
+desc_json_p = {"name": 'P'}
+GB("StreamReader", desc=json.dumps(desc_json_p)). \
+    filter(lambda record: record['value']['dimension'] == 'P'). \
+    foreach(lambda record: log(f'sandbox-p-{record}')). \
+    register(
+    prefix='ST:1MINUTE:P:::PG',
+    convertToStr=True,
+    collect=True,
+    onFailedPolicy='abort',
+    onFailedRetryInterval=1,
+    batch=1,
+    duration=0,
+    trimStream=False)
+log(f'Register P OK')
