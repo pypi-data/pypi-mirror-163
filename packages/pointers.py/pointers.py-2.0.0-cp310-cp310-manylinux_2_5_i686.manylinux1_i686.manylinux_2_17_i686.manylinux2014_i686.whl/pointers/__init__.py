5ea47a01c4558f216dec2d4ca89ad7d1c368a2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:34 +0000

    Add package pointers.py/2.0.0/pointers.py-2.0.0-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/pointers.py/pointers.py-2.0.0-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/pointers/__init__.py
@@ -0,0 +1,23 @@
+from .base_pointers import (
+    BaseAllocatedPointer, BaseCPointer, BaseObjectPointer, BasePointer,
+    BasicPointer, Dereferencable, IterDereferencable, Sized, Typed
+)
+from .bindings import *
+from .c_pointer import (
+    StructPointer, TypedCPointer, VoidPointer, array, cast, to_c_ptr,
+    to_struct_ptr
+)
+from .c_utils import force_set_attr
+from .calloc import AllocatedArrayPointer, calloc
+from .custom_binding import binding, binds
+from .decay import decay, decay_annotated, decay_wrapped
+from .exceptions import (
+    AllocationError, DereferenceError, FreedMemoryError,
+    InvalidBindingParameter, InvalidSizeError
+)
+from .magic import _
+from .malloc import AllocatedPointer, free, malloc, realloc
+from .object_pointer import Pointer, to_ptr
+from .std_structs import DivT, Lconv, LDivT, Tm
+from .struct import Struct
+from .version import __version__
