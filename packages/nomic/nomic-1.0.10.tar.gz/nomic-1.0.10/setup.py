f220d0bb0c0bf9d6d6be2c5dde1ebb1105786d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:25 +0000

    Add package nomic/1.0.10/nomic-1.0.10.tar.gz

---
+++ packages/nomic/nomic-1.0.10.tar.gz/nomic-1.0.10/setup.py
@@ -0,0 +1,51 @@
+'''Setup file for the Atlas Client'''
+from setuptools import setup, find_packages
+description = 'The offical Nomic python client.'
+setup(
+    name='nomic',
+    version='1.0.10',
+    url='https://github.com/nomic-ai/nomic',
+    description=description,
+    long_description=description,
+    packages=find_packages(),
+    author_email="support@nomic.ai",
+    author="nomic.ai",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[
+        'ruamel.yaml',
+        "click",
+        "jsonlines",
+        'rich',
+        'requests',
+        'numpy',
+        'pydantic',
+        'wonderwords',
+        'tqdm',
+        'cohere',
+        'datasets'
+    ],
+    extras_require={
+        'dev': [
+            'black',
+            'coverage',
+            "pylint",
+            "pytest",
+            "sphinx",
+            "myst-parser",
+            "sphinx_rtd_theme",
+            "furo",
+            "sphinx-copybutton",
+            "twine"
+        ],
+    },
+    entry_points={
+        'console_scripts': ['nomic=nomic.cli:cli'],
+    },
+    include_package_data=True
+)
+
+
+
