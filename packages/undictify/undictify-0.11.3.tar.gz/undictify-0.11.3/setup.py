656618b540463209bff3b009aedff2ea333f141a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:04 +0000

    Add package undictify/0.11.3/undictify-0.11.3.tar.gz

---
+++ packages/undictify/undictify-0.11.3.tar.gz/undictify-0.11.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="undictify",
+    version="0.11.3",
+    author="Tobias Hermann",
+    author_email="editgym@gmail.com",
+    description="Type-checked function calls at runtime",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/Dobiasd/undictify",
+    package_data={"undictify": ["py.typed"]},
+    packages=["undictify"],
+    classifiers=(
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
---
+++ packages/undictify/undictify-0.11.3.tar.gz/undictify-0.11.3/undictify/__init__.py
@@ -0,0 +1,12 @@
+from ._unpack import mandatory_converter as mandatory_converter
+from ._unpack import optional_converter as optional_converter
+from ._unpack import type_checked_call as type_checked_call
+from ._unpack import type_checked_constructor as type_checked_constructor
+
+name = "undictify"
+
+__author__ = "Tobias Hermann"
+__copyright__ = "Copyright 2018, Tobias Hermann"
+__email__ = "editgym@gmail.com"
+__license__ = "MIT"
+__version__ = "0.11.3"
