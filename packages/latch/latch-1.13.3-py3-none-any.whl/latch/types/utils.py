6783ffd88536b61ed26c2774056219b99641026d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:51 +0000

    Add package latch/1.13.3/latch-1.13.3-py3-none-any.whl

---
+++ packages/latch/latch-1.13.3-py3-none-any.whl/latch/types/utils.py
@@ -0,0 +1,17 @@
+from urllib.parse import urlparse
+
+
+def _is_valid_url(raw_url: str) -> bool:
+    """A valid URL (as a source or destination of a LatchFile) must:
+    * contain a latch or s3 scheme
+    * contain an absolute path
+    """
+    try:
+        parsed = urlparse(raw_url)
+    except ValueError:
+        return False
+    if parsed.scheme not in ("latch", "s3"):
+        return False
+    if not parsed.path.startswith("/"):
+        return False
+    return True
