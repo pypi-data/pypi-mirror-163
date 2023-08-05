a22713b6e9d62be98204d1adf634672bd4b3aa71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:58 +0000

    Add package Pragmatic/0.0.19/Pragmatic-0.0.19.tar.gz

---
+++ packages/Pragmatic/Pragmatic-0.0.19.tar.gz/Pragmatic-0.0.19/Pragmatic/Pragmatic.py
@@ -0,0 +1,33 @@
+# -*- coding: ascii -*-
+
+
+"""Pragmatic.Pragmatic: provides entry point main()."""
+
+
+__version__ = "0.0.19"
+
+from . import Stuff
+from .Registry import Registry
+from .ArgParser import ParseArgs, Test
+from .GetRelease import Get
+import os
+import pathlib
+
+def GetDataDir():
+	dir = os.path.dirname(os.path.realpath(__file__))
+	return os.path.join(dir, 'data')
+
+def Update():
+	os.makedirs(GetDataDir())
+	Get('LLVM', 'llvm-0.1')
+	Get('PragmaticPlugin', '0.1')
+
+
+def Main():
+	print(f"Running pragmatic version {__version__}.")
+
+
+	if (not pathlib.Path(GetDataDir()).exists()):
+		Update()
+
+	ParseArgs()
