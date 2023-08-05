403e06f9f55c9d780615912cd72a4f0b6015343a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:09 +0000

    Add package distributions-octo/0.1/distributions-octo-0.1.tar.gz

---
+++ packages/distributions-octo/distributions-octo-0.1.tar.gz/distributions-octo-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-octo',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-octo'],
+      zip_safe=False)
