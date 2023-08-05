c220c372643fc15d6b24e34ad2b9a746749c4093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:04 +0000

    Add package measuring-tools/0.1/measuring-tools-0.1.tar.gz

---
+++ packages/measuring-tools/measuring-tools-0.1.tar.gz/measuring-tools-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='measuring-tools',
+    version='0.1',
+    description='Classes to help with common types of measurements',
+    url='https://github.com/JTBake490/measuring-tools',
+    author='James Baker Jr',
+    license='Apache License 2.0',
+    packages=['measuring_tools'],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10'
+    ]
+)
