344e46cb8acce5f895b5b876e64a7c122d0b0ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:21 +0000

    Add package query-toolkits/1.0.7/query_toolkits-1.0.7.tar.gz

---
+++ packages/query-toolkits/query_toolkits-1.0.7.tar.gz/query_toolkits-1.0.7/query_toolkits/__init__.py
@@ -0,0 +1,8 @@
+# import os,sys
+# os.chdir(sys.path[0])
+from query_toolkits.extractor import Extractor
+
+et = Extractor()
+extract_time = et.extract_time
+extract_number = et.extract_number
+extract_requirement = et.extract_requirement
