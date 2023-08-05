ee63e4545ce428a0fc5c3b8b11470e63d07af806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:36 +0000

    Add package devcloudcli/1.3.19/devcloudcli-1.3.19-py3-none-any.whl

---
+++ packages/devcloudcli/devcloudcli-1.3.19-py3-none-any.whl/src/scripts/dl-model-training-services/training-frameworks/intel-optimized-pytorch/install.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+
+# Copyright (C) 2018-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+docker pull intel/intel-optimized-pytorch
+echo -e "\e[1;32mpytorch is pulled successfully\e[0m"
+echo -e "\e[1;36mTo view the image : docker images\e[0m"
+
+
+echo -e "\e[1;31mFor further queries please follow below URL\e[0m"
+
+echo -e "\e[1;32mhttps://intel.github.io/intel-extension-for-pytorch/1.11.200/\e[0m"
---
+++ packages/devcloudcli/devcloudcli-1.3.19-py3-none-any.whl/src/scripts/dl-model-training-services/training-frameworks/intel-optimized-pytorch/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copyright (C) 2018-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+docker rmi intel/intel-optimized-pytorch
+echo -e "\e[1;32mThe pytorch is uninstalled\e[0m"
+
+
