a3e8a66de1b867462b7d8c2f60d407d2535ff46f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:01 +0000

    Add package dukaan-secrets-manager/0.1/dukaan-secrets_manager-0.1.tar.gz

---
+++ packages/dukaan-secrets-manager/dukaan-secrets_manager-0.1.tar.gz/dukaan-secrets_manager-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dukaan-secrets_manager',
+    version='0.1',
+    author="",
+    author_email='mehboob.shaik@mydukaan.io',
+    url='https://github.com/teamdukaan',
+    keywords='package'
+)
