d9c2896f8b422427cdff2d85b571eefd3433fc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:23 +0000

    Add package crystal-toolkit/2022.8.12/crystal_toolkit-2022.8.12.tar.gz

---
+++ packages/crystal-toolkit/crystal_toolkit-2022.8.12.tar.gz/crystal_toolkit-2022.8.12/crystal_toolkit/apps/examples/structure.py
@@ -0,0 +1,42 @@
+import dash
+from dash import html
+from pymatgen.core.lattice import Lattice
+from pymatgen.core.structure import Structure
+
+# standard Crystal Toolkit import
+import crystal_toolkit.components as ctc
+from crystal_toolkit.settings import SETTINGS
+
+# create Dash app as normal
+app = dash.Dash(assets_folder=SETTINGS.ASSETS_PATH)
+
+# create the Structure object
+structure = Structure(
+    Lattice.cubic(4.2),
+    ["Na", "K"],
+    [[0, 0, 0], [0.5, 0.5, 0.5]],
+    site_properties={"magmom": [-2, 2]},
+)
+
+# create the Crystal Toolkit component
+structure_component = ctc.StructureMoleculeComponent(structure, id="my_structure")
+
+# example layout to demonstrate capabilities of component
+my_layout = html.Div(
+    [
+        html.H1("StructureMoleculeComponent Example"),
+        html.H2("Standard Layout"),
+        structure_component.layout(size="400px"),
+        html.H2("Optional Title Layout"),
+        structure_component.title_layout(),
+    ]
+)
+
+# tell crystal toolkit about your app and layout
+ctc.register_crystal_toolkit(app, layout=my_layout)
+
+# allow app to be run using "python structure.py"
+# in production, deploy behind gunicorn or similar
+# see Dash documentation for more information
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8050)
