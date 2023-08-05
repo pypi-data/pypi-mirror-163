4df4c164e65b96819925d432b45426d7a534fe1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:06 +0000

    Add package das/0.26.8/das-0.26.8.tar.gz

---
+++ packages/das/das-0.26.8.tar.gz/das-0.26.8/tests/test_import.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+def test_import():
+    from das import annot
+    from das import cli
+    from das import data
+    from das import data_hash
+    from das import evaluate
+    from das import event_utils
+    from das import io
+    from das import make_dataset
+    from das import models
+    from das import tracking
+    from das import npy_dir
+    from das import predict
+    from das import pulse_utils
+    from das import segment_utils
+    from das import train
+    from das import train_tune
+    from das import utils
+    from das import utils_plot
+    from das import augmentation
+    from das import block_stratify
+    import das.kapre
+    import das.tcn
