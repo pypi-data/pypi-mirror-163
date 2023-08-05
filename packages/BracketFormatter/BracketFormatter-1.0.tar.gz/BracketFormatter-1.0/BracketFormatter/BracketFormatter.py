f1c286b80e1364e2fbb2807a4607fbb3b2b9b7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:22 +0000

    Add package BracketFormatter/1.0/BracketFormatter-1.0.tar.gz

---
+++ packages/BracketFormatter/BracketFormatter-1.0.tar.gz/BracketFormatter-1.0/BracketFormatter/BracketFormatter.py
@@ -0,0 +1,26 @@
+import logging
+
+class BracketFormatter (logging.Formatter):
+
+    blue = '\x1b[38;5;39m'
+    yellow = '\x1b[38;5;226m'
+    red = '\x1b[38;5;196m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
+
+    def __init__(self):
+        super().__init__()
+        self.fmt = '%(message)s'
+        self.FORMATS = {
+            logging.DEBUG: '[.] ' + self.fmt,
+            logging.INFO: '[' + self.blue + '+' + self.reset + '] ' + self.fmt,
+            logging.WARNING: '[' + self.yellow + '!' + self.reset + '] ' + self.fmt,
+            logging.ERROR: '[' + self.red + '*' + self.reset + '] ' + self.fmt,
+            logging.CRITICAL: self.bold_red + '[*] ' + self.fmt + self.reset
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
---
+++ packages/BracketFormatter/BracketFormatter-1.0.tar.gz/BracketFormatter-1.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+        name="BracketFormatter",
+        author="AJ Poulter",
+        version="1.0",
+        description="A custom logging formatter to use a [+] style log comment",
+        packages=["BracketFormatter"])
