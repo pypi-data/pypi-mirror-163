01902f800aea3b59f5d596a2b2b7b07b5e4e3a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:10 +0000

    Add package cornershop-vertex/0.0.1/cornershop-vertex-0.0.1.tar.gz

---
+++ packages/cornershop-vertex/cornershop-vertex-0.0.1.tar.gz/cornershop-vertex-0.0.1/README.md
@@ -0,0 +1,6 @@
+#Dependency packager
+
+This is a simple warning package. You can use
+[Github-flavored Markdown](https://github.com/leinn32/dependencyConfusion)
+to write your content.
+
---
+++ packages/cornershop-vertex/cornershop-vertex-0.0.1.tar.gz/cornershop-vertex-0.0.1/src/cornershop-vertex/example.py
@@ -0,0 +1,5 @@
+#Warnign create to ensure no abuse is done by the Cornershop Security Team
+def warning(string)
+    result == "You have a security issue, this packege is an incuos warning and you are vulenrable to dependency confussion attacks"
+    print(result)
+    return result
