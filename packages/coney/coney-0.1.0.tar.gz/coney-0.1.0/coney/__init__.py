3c55df555c7b49945a0c4f7486090470540f3ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:28 +0000

    Add package coney/0.1.0/coney-0.1.0.tar.gz

---
+++ packages/coney/coney-0.1.0.tar.gz/coney-0.1.0/coney/__init__.py
@@ -0,0 +1,4 @@
+from broker import AbstractIncomingMessage, AbstractMessage, BindingType, Broker
+
+__version__ = "0.1.0"
+__all__ = ["Broker", "BindingType", "AbstractMessage", "AbstractIncomingMessage"]
---
+++ packages/coney/coney-0.1.0.tar.gz/coney-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['coney']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aio-pika>=8.1.1,<9.0.0']
+
+setup_kwargs = {
+    'name': 'coney',
+    'version': '0.1.0',
+    'description': 'Pika based rabbitmq client library',
+    'long_description': None,
+    'author': 'Teslim Olunlade',
+    'author_email': 'tolunlade@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
