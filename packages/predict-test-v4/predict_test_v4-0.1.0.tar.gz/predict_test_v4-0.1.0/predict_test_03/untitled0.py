99d2bfcc63d69c0021d71750a3507cd13fb6de7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:15 +0000

    Add package predict-test-v4/0.1.0/predict_test_v4-0.1.0.tar.gz

---
+++ packages/predict-test-v4/predict_test_v4-0.1.0.tar.gz/predict_test_v4-0.1.0/predict_test_03/untitled0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 12 03:09:08 2022
+
+@author: mosfak
+"""
+
+from __init__ import *
+str = "/home/mosfak/Desktop/Predict-the-Collision-and-Save-the-Earth-master/predict_test_02/data/neo.csv"
+
+main_func(str, 1)
