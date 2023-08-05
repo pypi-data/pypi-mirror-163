3041e5f4e1875e63d94e3c6d3fecb6543d302d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:31 +0000

    Add package devana/1.0.0/devana-1.0.0-py3-none-any.whl

---
+++ packages/devana/devana-1.0.0-py3-none-any.whl/devana/code_generation/__init__.py
@@ -0,0 +1,7 @@
+"""
+Code generation module provides functionality to modify already parsed C++ code and generate new code without
+manipulation of raw strings.
+"""
+
+
+
---
+++ packages/devana/devana-1.0.0-py3-none-any.whl/devana/code_generation/printers/icodeprinter.py
@@ -0,0 +1,16 @@
+from devana.code_generation.printers.configuration import PrinterConfiguration
+from abc import ABC, abstractmethod
+from typing import Optional
+
+
+
+class ICodePrinter(ABC):
+    """Common interface for all printers."""
+
+    @abstractmethod
+    def print(self, source, config: Optional[PrinterConfiguration] = None, context: Optional = None) -> str:
+        pass
+
+    @property
+    def is_fallback_handler(self) -> bool:
+        return False
