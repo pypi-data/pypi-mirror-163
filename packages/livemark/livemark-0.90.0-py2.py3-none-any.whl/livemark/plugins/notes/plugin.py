77f303d8142e55687567af0e73a4d17fc79d05f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:31 +0000

    Add package livemark/0.90.0/livemark-0.90.0-py2.py3-none-any.whl

---
+++ packages/livemark/livemark-0.90.0-py2.py3-none-any.whl/livemark/plugins/notes/plugin.py
@@ -0,0 +1,30 @@
+import os
+from datetime import datetime
+from ...plugin import Plugin
+
+
+class NotesPlugin(Plugin):
+    identity = "notes"
+    priority = 50
+    validity = {
+        "type": "object",
+        "properties": {
+            "format": {"type": "string"},
+        },
+    }
+
+    # Context
+
+    @property
+    def format(self):
+        return self.config.get("format", "%Y-%m-%d %H:%M")
+
+    @property
+    def current(self):
+        return datetime.fromtimestamp(os.path.getmtime(self.document.source))
+
+    # Process
+
+    def process_markup(self, markup):
+        markup.add_style("style.css")
+        markup.add_markup("markup.html", target="#livemark-main", action="prepend")
