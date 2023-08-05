4d567e05378a5fec874a78be1bd2268e4871f8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:56 +0000

    Add package pyfracgen/0.0.7/pyfracgen-0.0.7.tar.gz

---
+++ packages/pyfracgen/pyfracgen-0.0.7.tar.gz/pyfracgen-0.0.7/pyfracgen/__init__.py
@@ -0,0 +1,10 @@
+from .julia import *
+from .lyapunov import lyapunov
+from .mandelbrot import mandelbrot
+from .buddhabrot import *
+from .random_walk import *
+from .images import images
+from .update_functions import updates
+from .fractal_result import *
+
+__author__ = 'Ryther Anderson'
