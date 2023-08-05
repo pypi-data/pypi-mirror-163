145dc83d384480cbe8cd32b355e36dda0c751f59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:28 +0000

    Add package Flake8-pyproject/1.1.0/flake8_pyproject-1.1.0-py3-none-any.whl

---
+++ packages/Flake8-pyproject/flake8_pyproject-1.1.0-py3-none-any.whl/flake8p/meta.py
@@ -0,0 +1,7 @@
+﻿"""Meta information about the package."""
+
+title     = 'Flake8-pyproject'
+synopsis  = 'Runs Flake8 with configuration from pyproject.toml.'
+version   = '1.1.0'
+author    = 'John Hennig'
+license   = 'MIT'
