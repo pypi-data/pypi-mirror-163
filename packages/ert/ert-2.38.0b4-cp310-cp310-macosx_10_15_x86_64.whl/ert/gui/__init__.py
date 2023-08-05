69f1f0265595ae971705ed62a07e5069e0aa9d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:06 +0000

    Add package ert/2.38.0b4/ert-2.38.0b4-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-2.38.0b4-cp310-cp310-macosx_10_15_x86_64.whl/ert/gui/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+import matplotlib
+
+import ert_shared
+
+
+def headless():
+    return "DISPLAY" not in os.environ
+
+
+if headless():
+    matplotlib.use("Agg")
+else:
+    matplotlib.use("Qt5Agg")
+
+__version__ = ert_shared.__version__
