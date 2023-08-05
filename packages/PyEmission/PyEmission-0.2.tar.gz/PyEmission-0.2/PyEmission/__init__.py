9c399b8b362d4fa477f02a1cde4837c98d24cd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:06 +0000

    Add package PyEmission/0.2/PyEmission-0.2.tar.gz

---
+++ packages/PyEmission/PyEmission-0.2.tar.gz/PyEmission-0.2/PyEmission/__init__.py
@@ -0,0 +1,5 @@
+
+__version__ = "0.2"
+
+from PyEmission.pyemission import Car, GV
+from PyEmission.utility_functions import read_data, calculate_tractive_power, calculate_vsp, vsp_to_op_mod, op_mod_to_emission_rate, joule_to_kwh, convert_to_overall_mpg, energy_kj_to_ml, vsp_coeff, progress_bar
