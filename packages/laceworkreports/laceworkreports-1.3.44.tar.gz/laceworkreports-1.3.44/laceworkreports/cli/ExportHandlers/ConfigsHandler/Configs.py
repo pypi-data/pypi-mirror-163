476caaee8cc92d92a097d1f3e7d8d9a893675ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:56 +0000

    Add package laceworkreports/1.3.44/laceworkreports-1.3.44.tar.gz

---
+++ packages/laceworkreports/laceworkreports-1.3.44.tar.gz/laceworkreports-1.3.44/laceworkreports/cli/ExportHandlers/ConfigsHandler/Configs.py
@@ -0,0 +1,26 @@
+import typer
+
+from laceworkreports import common
+from laceworkreports.cli.ExportHandlers.DataExportHandlers import ConfigsAPIv2Handler
+
+app = typer.Typer()
+
+parent_command = common.ActionTypes.Export.value
+self_command = common.ObjectTypes[__name__.split(".")[-1]].value
+
+commands = []
+
+for t in common.ConfigsTypes:
+    commands.append({"command_name": t.value, "command_type": ConfigsAPIv2Handler.app})
+
+for command in iter(commands):
+    app.add_typer(
+        command["command_type"],
+        name=command["command_name"],
+        help=f"Retrieve lacework activities api {command['command_name']} events",
+        no_args_is_help=True,
+        epilog=f"{common.config.name} {parent_command} {self_command} {command['command_name']} <exporttype> [OPTIONS]",
+    )
+
+if __name__ == "__main__":
+    app()
