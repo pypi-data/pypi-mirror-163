c3908edbbb9fa7c4eb5b3489efea81233c132edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:21 +0000

    Add package mkdocs-custom-fences/0.1.2/mkdocs_custom_fences-0.1.2.tar.gz

---
+++ packages/mkdocs-custom-fences/mkdocs_custom_fences-0.1.2.tar.gz/mkdocs_custom_fences-0.1.2/mkdocs_custom_fences/md_render.py
@@ -0,0 +1,33 @@
+"""Generate markdown embed in code blocks.
+Credit @facelessuser/pydown-extension
+https://github.com/facelessuser/pymdown-extensions/blob/main/tools/pymdownx_md_render.py
+"""
+import markdown
+import yaml
+import re
+from collections import OrderedDict
+
+
+def md_sub_render(src="", language="", class_name=None, options=None, md="", **kwargs):
+    """Formatter wrapper."""
+    try:
+        md = markdown.markdown(
+            src,
+            extensions=[
+                "nl2br",
+                "footnotes",
+                "attr_list",
+                "mdx_breakless_lists",
+                "smarty",
+                "sane_lists",
+                "tables",
+                "admonition",
+                "mdx_wikilink_plus",
+            ],
+        )
+        return md
+    except Exception:
+        import traceback
+
+        print(traceback.format_exc())
+        raise
