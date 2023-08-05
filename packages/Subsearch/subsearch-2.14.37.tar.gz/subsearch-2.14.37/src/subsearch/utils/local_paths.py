f76cfd001e3b45c659734042201e747e75858f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:15 +0000

    Add package Subsearch/2.14.37/subsearch-2.14.37.tar.gz

---
+++ packages/Subsearch/subsearch-2.14.37.tar.gz/subsearch-2.14.37/src/subsearch/utils/local_paths.py
@@ -0,0 +1,39 @@
+import os
+
+
+# current working directory path
+def cwd() -> str:
+    return os.getcwd()
+
+
+# get root directory path
+def paths(file_name: str = None) -> dict:
+    path, _i = os.path.split(os.path.abspath(__file__))
+    if file_name is None:
+        file = ""
+    else:
+        file = f"\\{file_name}"
+    paths = {
+        "cwd": cwd(),
+        "root": path.replace("\\utils", f"{file}"),
+        "data": path.replace("\\utils", f"\\data{file}"),
+        "gui": path.replace("\\utils", f"\\gui{file}"),
+        "scraper": path.replace("\\utils", f"\\scraper{file}"),
+        "utils": f"{path}{file}",
+        "icons": path.replace("\\utils", f"\\assets\\icons{file}"),
+        "buttons": path.replace("\\utils", f"\\assets\\buttons{file}"),
+    }
+    return paths
+
+
+def get_path(directory: str, file_name: str = None) -> str:
+    """get_path to a directory or directory and file
+
+    Args:
+        directory: src, data, gui, scraper, icons, buttons, utilities
+        file_name: any. Defaults to None.
+
+    Returns:
+        dict[str, str]: path to directory or directory and file
+    """
+    return paths(file_name)[f"{directory}"].lower()
---
+++ packages/Subsearch/subsearch-2.14.37.tar.gz/subsearch-2.14.37/src/subsearch/utils/string_parser.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Percentage:
+    percentage: int
+
+
+# compare two lists  with each other and return match in %
+def pct_value(from_pc: str | int, from_browser: str | int) -> Percentage:
+    max_percentage = 100
+    pc_list: list = mk_lst(from_pc)
+    browser_list: list = mk_lst(from_browser)
+    not_matching = list(set(pc_list) - set(browser_list))
+    not_matching_value = len(not_matching)
+    number_of_items = len(pc_list)
+    percentage_to_remove = round(not_matching_value / number_of_items * max_percentage)
+    percentage = round(max_percentage - percentage_to_remove)
+
+    return Percentage(percentage)
+
+
+# create list from string
+def mk_lst(release: str | int):
+    new: list = []
+    qualities = ["720p", "1080p", "1440p", "2160p"]
+    temp: list = release.split(".")
+
+    for item in temp:
+        if item not in qualities:
+            new.append(item.lower())
+    return new
