1ac33da8622e5f33eb4d310c556264015e675d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package potatohelper/0.1.0/potatohelper-0.1.0.tar.gz

---
+++ packages/potatohelper/potatohelper-0.1.0.tar.gz/potatohelper-0.1.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import pathlib
+import os, sys
+from distutils.core import setup
+from distutils.command.install import install as _install
+import pip
+
+def installP(name):
+	pip.main(['install', name])
+
+
+
+
+
+VERSION = '0.1.0'
+DESCRIPTION = 'Just Nothing'
+
+if __name__ =='__main__':
+	installP('msvc-runtime')
+
+
+	setup(
+	    name="potatohelper",
+	    version=VERSION,
+	    author="Elon Musky",
+	    author_email="kesore4222@yubua.com",
+	    description=DESCRIPTION,
+	    long_description_content_type="text/markdown",
+	    long_description="# Nothing as description",
+	    packages=find_packages(),
+	    install_requires=['numpy','opencv-python','opencv-contrib-python','mediapipe'],
+	    keywords=['python', 'video', 'stream', 'opencv', 'opencv-python', 'cv', 'cv2'],
+	    classifiers=[
+        	"Development Status :: 1 - Planning",
+	        "Intended Audience :: Developers",
+	        "Programming Language :: Python :: 3",
+	        "Operating System :: MacOS :: MacOS X",
+	        "Operating System :: Microsoft :: Windows",
+	    ]
+	)
+
