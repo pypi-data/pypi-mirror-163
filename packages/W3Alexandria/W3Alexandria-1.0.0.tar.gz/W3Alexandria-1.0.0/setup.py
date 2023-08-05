f92b956d4de322796b047a12f2a209439f4d39d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:22 +0000

    Add package W3Alexandria/1.0.0/W3Alexandria-1.0.0.tar.gz

---
+++ packages/W3Alexandria/W3Alexandria-1.0.0.tar.gz/W3Alexandria-1.0.0/setup.py
@@ -0,0 +1,34 @@
+# # import setuptools
+
+# from Alexandria import Alexandria
+
+# method_list = [method for method in dir(Alexandria.Book) if method.startswith('__') is False]
+
+# for i in method_list:
+#     print(f'Method: {getattr(Alexandria.Book, i).__name__}()')
+#     print(f'Doc: {getattr(Alexandria.Book, i).__doc__}')
+#     print()
+
+import setuptools
+
+with open("README.md", "r", errors="ignore") as fh:
+    description = fh.read()
+  
+setuptools.setup(
+    name="W3Alexandria",
+    version="1.0.0",
+    author="Obiajulu_M",
+    author_email="oambanefo@outlook.com",
+    packages=setuptools.find_packages(),
+    description="A project to decentralize and distribute literary infrastructure.",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ObiajuluM/Alexandria",
+    license='MIT',
+    python_requires='>=3.8',
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["web3", "ipfshttpclient"])
