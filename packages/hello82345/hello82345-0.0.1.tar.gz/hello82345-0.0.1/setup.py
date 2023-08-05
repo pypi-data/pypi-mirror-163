7d2364ae8d9c33d7e0628beaac8eb7e2b6740f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:28 +0000

    Add package hello82345/0.0.1/hello82345-0.0.1.tar.gz

---
+++ packages/hello82345/hello82345-0.0.1.tar.gz/hello82345-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+VERSION = '0.0.1'
+
+# Setting up
+setup(
+    name="hello82345",
+    version=VERSION,
+    author="stfu",
+    description="",
+    packages=["hello82345"],
+    install_requires=[''],
+    keywords=['niggers'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
