5799e2b2d15130c34ff50b17d3f174a64fc4a1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:33 +0000

    Add package hrthy.core/1.0.92/hrthy.core-1.0.92-py3-none-any.whl

---
+++ packages/hrthy.core/hrthy.core-1.0.92-py3-none-any.whl/hrthy_core/events/events/base_event.py
@@ -0,0 +1,22 @@
+from time import time
+from uuid import UUID
+
+from pydantic import Field, typing
+from pydantic.main import BaseModel
+
+from hrthy_core.security.security import Requester
+
+
+class BaseEvent(BaseModel):
+    type: str = 'BaseEvent'
+    timestamp: int = Field(default_factory=lambda: int(time()))
+    requester: Requester
+    payload: typing.Any
+
+
+class BasePayload(BaseModel):
+    id: UUID
+
+
+class CompanyBasePayload(BasePayload):
+    company_id: UUID
