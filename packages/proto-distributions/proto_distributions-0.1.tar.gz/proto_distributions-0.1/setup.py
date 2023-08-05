7780fb45dad9cf57b03422214119301a040f9a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:13 +0000

    Add package proto-distributions/0.1/proto_distributions-0.1.tar.gz

---
+++ packages/proto-distributions/proto_distributions-0.1.tar.gz/proto_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='proto_distributions',
+      version='0.1',
+      description='Probability distributions',
+      packages=['proto_distributions'],
+      author = 'Emefiele-Konyeri Awinrin',
+      author_email= 'awinrin4bless@gmail.com',
+      zip_safe=False)
