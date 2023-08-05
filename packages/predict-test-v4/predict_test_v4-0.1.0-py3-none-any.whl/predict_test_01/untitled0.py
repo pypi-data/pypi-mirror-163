60ddac3068b0e781e369c8a2b65186fbff49dfe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:13 +0000

    Add package predict-test-v4/0.1.0/predict_test_v4-0.1.0-py3-none-any.whl

---
+++ packages/predict-test-v4/predict_test_v4-0.1.0-py3-none-any.whl/predict_test_01/untitled0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 12 02:33:37 2022
+
+@author: mosfak
+"""
+
+from __init__ import *
+
+main("/home/mosfak/Desktop/Predict-the-Collision-and-Save-the-Earth-master/predict_test/data/neo.csv",2)
---
+++ packages/predict-test-v4/predict_test_v4-0.1.0-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,20 @@
+# importing sys
+import sys
+import os
+cdir = os.getcwd()
+cdir+="/models/"
+# adding src/model/ to the system path
+# sys.path.insert(0, '/home/mosfak/Desktop/Predict-the-Collision-and-Save-the-Earth-master/src/models/')
+sys.path.insert(0, cdir)
+# from src.models.__init__ import *
+
+ 
+# importing the main
+# from __init__ import *
+from __init__ import main 
+
+# str = "/home/mosfak/Desktop/Predict-the-Collision-and-Save-the-Earth-master/src/data/neo.csv"
+str = sys.argv[1]
+# print(str)
+# readd(str)
+main(str)
