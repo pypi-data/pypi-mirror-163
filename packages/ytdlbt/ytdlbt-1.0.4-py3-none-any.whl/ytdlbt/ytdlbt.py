0c0e0d5cd6065c43d059d5112fac6ecde10dbe8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:30 +0000

    Add package ytdlbt/1.0.4/ytdlbt-1.0.4-py3-none-any.whl

---
+++ packages/ytdlbt/ytdlbt-1.0.4-py3-none-any.whl/ytdlbt/ytdlbt.py
@@ -0,0 +1,41 @@
+from youtubesearchpython import *
+from re import match
+from typing import List
+import os
+from pytube import YouTube as yt
+
+def get_url(name,useOpt=False,kw=[],lim=10):
+
+    YOUTUBE_LINK_BASE="https://youtube.com{}"
+
+    kw = [kw_ for kw_ in kw if kw_ is not None]
+
+    if useOpt and len(kw):
+        name += '+' + '+'.join(kw)
+    
+    # Replace all the spaces with +
+    name = name.replace(' ', '+')
+
+    results = CustomSearch(name,VideoSortOrder.viewCount ,limit=lim).result()["result"]
+    stripped_results = []
+    
+    for video in results:
+        data = {}
+        data['title'] = video['title']
+        data['href'] = f"/watch?v={video['id']}"
+        data['author_name'] = video['channel']["name"]
+        data['duration'] = video['duration']
+        stripped_results.append(data)
+    
+    return YOUTUBE_LINK_BASE.format(stripped_results[0]['href'])
+
+def _download(url,path='./'):
+    music = yt(url)
+    music_stream = music.streams.filter(only_audio = True).first()
+    music=music_stream.download(output_path=path)
+    base , ext = os.path.splitext(music)
+    os.rename(music,base+'.mp3')
+
+def download(name,path='./'):
+    url=get_url(name)
+    _download(url,path)
