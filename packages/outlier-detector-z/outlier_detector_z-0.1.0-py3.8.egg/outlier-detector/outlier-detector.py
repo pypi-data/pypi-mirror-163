c4cf22f84c8de0d19798c7a8068be17a4d07a7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:27 +0000

    Add package outlier-detector-z/0.1.0/outlier_detector_z-0.1.0-py3.8.egg

---
+++ packages/outlier-detector-z/outlier_detector_z-0.1.0-py3.8.egg/outlier-detector/outlier-detector.py
@@ -0,0 +1,7 @@
+
+def detect_outlier(dataset:list | tuple) -> list:
+    if type(dataset) == list or type(dataset) == tuple:
+        pass
+    else:
+        raise Exception("Accepts only list or tuple")
+    pass
