fa6f60570720c0045fe2c5cda369e9060696daa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:15 +0000

    Add package notecoin/0.20.14/notecoin-0.20.14.tar.gz

---
+++ packages/notecoin/notecoin-0.20.14.tar.gz/notecoin-0.20.14/notecoin/utils/time.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+
+
+def month_first_datetime(months):
+    today = datetime.today()
+    months = today.month + months - 1
+    years = -int((months % 12 - months) / 12)
+    months = months % 12 + 1
+
+    return datetime(today.year + years, months, 1)
+
+
+def month_during(months):
+    first = month_first_datetime(months)
+    last = month_first_datetime(months + 1) - timedelta(seconds=1)
+    return first, last
+
+
+def week_during(weeks):
+    today = datetime.today()
+    first = today + timedelta(weeks=weeks) - timedelta(days=today.weekday())
+    first = datetime(first.year, first.month, first.day)
+    last = first + timedelta(weeks=1) - timedelta(seconds=1)
+    return first, last
+
+
+def day_during(days):
+    today = datetime.today()
+    first = today + timedelta(days=days)
+    first = datetime(first.year, first.month, first.day)
+    last = first + timedelta(days=1) - timedelta(seconds=1)
+    return first, last
