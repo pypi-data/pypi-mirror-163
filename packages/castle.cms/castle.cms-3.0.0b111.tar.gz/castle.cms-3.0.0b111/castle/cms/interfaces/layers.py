5995280c8046ae6e04e188f63c025a79e6da2157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:31 +0000

    Add package castle.cms/3.0.0b111/castle.cms-3.0.0b111.tar.gz

---
+++ packages/castle.cms/castle.cms-3.0.0b111.tar.gz/castle.cms-3.0.0b111/castle/cms/interfaces/layers.py
@@ -0,0 +1,16 @@
+from wildcard.hps.interfaces import IWildcardHPSLayer
+from plone.app.contenttypes.interfaces import IPloneAppContenttypesLayer
+from plone.app.mosaic.interfaces import IMosaicLayer
+from plone.app.tiles.interfaces import ITilesFormLayer
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+
+class ICastleLayer(IDefaultBrowserLayer, IMosaicLayer, ITilesFormLayer,
+                   IPloneAppContenttypesLayer, IWildcardHPSLayer):
+    pass
+
+
+class IVersionViewLayer(ICastleLayer):
+    """
+    when viewing a version of an object
+    """
