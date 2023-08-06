ab2e0a1ae90e2e757c60b73ba8776a57a94964d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:31 +0000

    Add package distributions-335EFF/0.1/distributions_335EFF-0.1.tar.gz

---
+++ packages/distributions-335EFF/distributions_335EFF-0.1.tar.gz/distributions_335EFF-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_335EFF',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_335EFF'],
+      zip_safe=False)
