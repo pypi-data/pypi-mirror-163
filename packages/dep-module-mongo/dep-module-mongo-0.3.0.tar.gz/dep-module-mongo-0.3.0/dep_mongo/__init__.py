dc86817fdf9e7756d12eae3aab49a63442898e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:08 +0000

    Add package dep-module-mongo/0.3.0/dep-module-mongo-0.3.0.tar.gz

---
+++ packages/dep-module-mongo/dep-module-mongo-0.3.0.tar.gz/dep-module-mongo-0.3.0/dep_mongo/__init__.py
@@ -0,0 +1,97 @@
+"""Mongo module."""
+
+from bson import ObjectId
+from typing import Dict
+from logging import getLogger
+
+from motor.motor_asyncio import (
+    AsyncIOMotorClient as AsyncClient,
+    AsyncIOMotorDatabase as AsyncDatabase,
+)
+
+from spec.types import Module
+
+log = getLogger(__name__)
+
+
+CLIENTS: Dict[str, AsyncClient] = dict()
+COLLECTIONS: Dict[str, AsyncDatabase] = dict()
+
+
+class Mongo(Module):
+    """Mongo module."""
+
+    module_settings = {
+        'uri': 'mongodb://localhost',
+        'collection': 'test_collection',
+        'pool_size': 10,
+    }
+
+    async def middleware(self, scope, receive, send, app=None):  # noqa
+        """Test http middleware."""
+        log.warning(
+            'Mongo middleware',
+            extra={'scope': scope, 'receive': receive, 'send': send},
+        )
+
+        return await app(scope, receive, send)
+
+    async def prepare(self, scope):  # noqa
+        """Test prepare hook."""
+        log.warning('Mongo prepare', extra={'scope': scope})
+
+        _client = AsyncClient(
+            self.module_settings.get('uri'),
+            maxPoolSize=self.module_settings.get('pool_size'),
+            minPoolSize=self.module_settings.get('pool_size'),
+        )
+
+        CLIENTS[self.composite_name] = _client
+        COLLECTIONS[self.composite_name] = _client[self.collection_name]
+
+    async def release(self, scope):  # noqa
+        """Test release hook."""
+        log.warning('Mongo release', extra={'scope': scope})
+        CLIENTS[self.composite_name].close()
+
+    @property
+    def db(self) -> AsyncDatabase:
+        """Async db collection."""
+        return COLLECTIONS[self.composite_name]
+
+    @property
+    def collection_name(self) -> str:
+        """Get collection name."""
+        return self.module_settings.get('collection')
+
+    @property
+    def composite_name(self) -> str:
+        """Composite name."""
+        return f'collection.{self.collection_name}'
+
+
+class Primary(str):
+    """Primary object id."""
+
+    @classmethod
+    def __get_validators__(cls):
+        """Primary validators."""
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        """Validate primary."""
+        if v == '':
+            raise TypeError('ObjectId is empty')
+        if ObjectId.is_valid(v) is False:
+            raise TypeError('ObjectId invalid')
+        return str(v)
+
+
+__all__ = (
+    'AsyncDatabase',
+    'AsyncClient',
+    'ObjectId',
+    'Primary',
+    'Mongo',
+)
