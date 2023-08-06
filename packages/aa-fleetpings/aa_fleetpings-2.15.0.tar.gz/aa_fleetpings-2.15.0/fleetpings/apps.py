c0eab391e150af25d58991af7ce9e4d1b78c07e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:42 +0000

    Add package aa-fleetpings/2.15.0/aa_fleetpings-2.15.0.tar.gz

---
+++ packages/aa-fleetpings/aa_fleetpings-2.15.0.tar.gz/aa_fleetpings-2.15.0/fleetpings/apps.py
@@ -0,0 +1,19 @@
+"""
+app config
+"""
+
+# Django
+from django.apps import AppConfig
+
+# AA Fleet Pings
+from fleetpings import __version__
+
+
+class AaFleetpingsConfig(AppConfig):
+    """
+    Application config
+    """
+
+    name = "fleetpings"
+    label = "fleetpings"
+    verbose_name = f"Fleet Pings v{__version__}"
