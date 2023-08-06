3dc24075ff0228da888015135f905fc40aef2206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:02 +0000

    Add package peaqevcore/5.5.2/peaqevcore-5.5.2.tar.gz

---
+++ packages/peaqevcore/peaqevcore-5.5.2.tar.gz/peaqevcore-5.5.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+setuptools.setup(
+    name="peaqevcore",
+    version="5.5.2",
+    author="Magnus Eldén",
+    description="Core types for peaqev car charging",
+    url="https://github.com/elden1337/peaqev-core",
+    license="MIT",
+    packages=[
+        "peaqevcore", 
+        "peaqevcore.services.hourselection",
+        "peaqevcore.models.hourselection", 
+        "peaqevcore.services.chargecontroller", 
+        "peaqevcore.services.chargertype",
+        "peaqevcore.models.chargertype",
+        "peaqevcore.models", 
+        "peaqevcore.models.hub", 
+        "peaqevcore.hub", 
+        "peaqevcore.services.prediction", 
+        "peaqevcore.services.session",
+        "peaqevcore.services.threshold", 
+        "peaqevcore.services.locale", 
+        "peaqevcore.services.locale.countries", 
+        "peaqevcore.services.locale.querytypes", 
+        "peaqevcore.models.locale",
+        "peaqevcore.services.scheduler",
+        "peaqevcore.services.timer"
+        ],
+    test_requires=['pytest']
+)   
