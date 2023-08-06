4fdc7bfe407fd325e109d195c6ea3513dfbbbd14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:29 +0000

    Add package MMEdu/0.0.1/MMEdu-0.0.1.tar.gz

---
+++ packages/MMEdu/MMEdu-0.0.1.tar.gz/MMEdu-0.0.1/MMEdu/__init__.py
@@ -0,0 +1,16 @@
+from ensurepip import version
+from .Classification import MMClassification
+from .Detection import MMDetection
+# from .Generation_Edu import MMGeneration
+# from .Pose_Edu import MMPose
+# from .Base_Edu import MMBase
+from .version import __version__
+
+__all__ = [
+    'MMClassification',
+    'MMDetection',
+    '__version__',
+    # 'MMGeneration',
+    # 'MMPose'
+    # 'MMBase',
+]
