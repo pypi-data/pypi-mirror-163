6b65e68d87102902924389c01f9d0a43980d6e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:54 +0000

    Add package compatibilityer/0.1.0/compatibilityer-0.1.0-py3-none-any.whl

---
+++ packages/compatibilityer/compatibilityer-0.1.0-py3-none-any.whl/compatibilityer/convert.py
@@ -0,0 +1,24 @@
+from typing import Type
+
+from compatibilityer.converter import Converter
+
+import ast
+
+
+HEAD = """\
+@converted by compatibilityer.convert.convert
+
+"""
+
+
+def convert_ast(node: ast.AST, converter: Type[Converter] = Converter) -> ast.AST:
+    return converter().visit(node)
+
+
+def convert(code: str, converter: Type[Converter] = Converter) -> str:
+    node = ast.parse(code)
+    return ast.unparse(convert_ast(node, converter))
+
+
+def convert_file(code: str, converter: Type[Converter] = Converter, head: str = HEAD) -> str:
+    return head + convert(code, converter)
