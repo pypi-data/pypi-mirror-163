c324490d64932ad6959adb4bc2d799af8f5501f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:39 +0000

    Add package python-glinet/0.3.0/python-glinet-0.3.0.tar.gz

---
+++ packages/python-glinet/python-glinet-0.3.0.tar.gz/python-glinet-0.3.0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("LICENSE", "r", encoding="utf-8") as fh:
+    license = fh.read()
+
+setuptools.setup(
+    name="python-glinet",
+    version="0.3.0",
+    author="Thomas Fontana",
+    author_email="python@fontana.onl",
+    description="This package gives client access to the Gl-Inet json-rpc for firmware >=4.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tomtana/python-glinet",
+    project_urls={
+        "Bug Tracker": "https://github.com/tomtana/python-glinet/issues",
+        "Documentation": "https://tomtana.github.io/python-glinet"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
+    ],
+    python_requires=">=3.6",
+    install_requires=["ipython", "tabulate", "requests"],
+    packages=setuptools.find_packages()
+)
