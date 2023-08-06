191c89d436465e426f19387adb7bdc9845d659bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:06 +0000

    Add package hypernova-orbit/0.1.1/hypernova-orbit-0.1.1.tar.gz

---
+++ packages/hypernova-orbit/hypernova-orbit-0.1.1.tar.gz/hypernova-orbit-0.1.1/hypernova/src/physics/earth_gravity/simplified_gravity.c
@@ -0,0 +1,14 @@
+#include "simplified_gravity.h"
+#include "../../abstractions/spacecraft.h"
+#include <math.h>
+#include <stdlib.h>
+
+void simplified_gravity(double t, double y[VEC_SIZE], double f[3], Spacecraft spacecraft)
+{
+    double r = sqrt(y[0] * y[0] + y[1] * y[1] + y[2] * y[2]);
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        f[i] = -G * MASS_EARTH * spacecraft.mass / (r * r * r) * y[i];
+    }
+}
