be540250fba7eb4c5c8b74953b8e9c7ffc6307cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:39 +0000

    Add package pyoperators/0.14.1/pyoperators-0.14.1.tar.gz

---
+++ packages/pyoperators/pyoperators-0.14.1.tar.gz/pyoperators-0.14.1/setup.py
@@ -0,0 +1,20 @@
+import hooks
+import numpy as np
+from numpy.distutils.core import setup
+from numpy.distutils.extension import Extension
+
+ext_modules = [
+    Extension(
+        'pyoperators.utils.cythonutils',
+        sources=['pyoperators/utils/cythonutils.pyx'],
+        include_dirs=[np.get_include()]),
+    Extension(
+        'pyoperators.utils.ufuncs',
+        sources=["pyoperators/utils/ufuncs.c.src"],
+    )
+]
+
+setup(
+    cmdclass=hooks.cmdclass,
+    ext_modules=ext_modules,
+)
