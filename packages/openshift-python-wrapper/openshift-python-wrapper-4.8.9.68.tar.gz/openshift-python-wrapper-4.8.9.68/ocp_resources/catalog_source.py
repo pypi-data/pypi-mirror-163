a2c402a818e929bc2fbdb35ad1c3180d2df06ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:37 +0000

    Add package openshift-python-wrapper/4.8.9.68/openshift-python-wrapper-4.8.9.68.tar.gz

---
+++ packages/openshift-python-wrapper/openshift-python-wrapper-4.8.9.68.tar.gz/openshift-python-wrapper-4.8.9.68/ocp_resources/catalog_source.py
@@ -0,0 +1,43 @@
+from ocp_resources.logger import get_logger
+from ocp_resources.resource import NamespacedResource
+
+
+LOGGER = get_logger(name=__name__)
+
+
+class CatalogSource(NamespacedResource):
+    api_group = NamespacedResource.ApiGroup.OPERATORS_COREOS_COM
+
+    def __init__(
+        self,
+        name,
+        namespace,
+        client=None,
+        source_type=None,
+        image=None,
+        display_name=None,
+        publisher=None,
+        teardown=True,
+    ):
+        super().__init__(
+            name=name, namespace=namespace, client=client, teardown=teardown
+        )
+        self.source_type = source_type
+        self.image = image
+        self.display_name = display_name
+        self.publisher = publisher
+
+    def to_dict(self):
+        res = super().to_dict()
+        res.update(
+            {
+                "spec": {
+                    "sourceType": self.source_type,
+                    "image": self.image,
+                    "displayName": self.display_name,
+                    "publisher": self.publisher,
+                }
+            }
+        )
+
+        return res
