767904268943d6fad92b00bc8627ae829dee7829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:54 +0000

    Add package anton-cli/0.1.1/anton_cli-0.1.1.tar.gz

---
+++ packages/anton-cli/anton_cli-0.1.1.tar.gz/anton_cli-0.1.1/anton_cli/collect_framework.py
@@ -0,0 +1,16 @@
+from collections import Counter
+from functools import lru_cache
+
+
+@lru_cache(maxsize=500, typed=False)
+def split_letters(input_text):
+    if not isinstance(input_text, str):
+        raise TypeError('Text must be only str type!')
+    result = Counter(Counter(input_text).values())[1]
+    return result
+
+
+def read_from_file(file_path):
+    with open(f'collection_framework/{file_path}', "r") as file:
+        string = file.read().splitlines()
+        return [split_letters(el) for el in string]
