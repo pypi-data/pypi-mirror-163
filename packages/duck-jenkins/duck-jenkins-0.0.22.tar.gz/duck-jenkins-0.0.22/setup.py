7362cdf3fe84d14ccf536ffdd5cd74ef8a8c1660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:40 +0000

    Add package duck-jenkins/0.0.22/duck-jenkins-0.0.22.tar.gz

---
+++ packages/duck-jenkins/duck-jenkins-0.0.22.tar.gz/duck-jenkins-0.0.22/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="duck-jenkins",
+    version="0.0.22",
+    install_requires=[
+        "duckdb",
+        "pandas",
+        "requests",
+        "jsonpath-ng",
+        "pydantic",
+        "aiohttp"
+    ],
+    author="Max Leow",
+    author_email="maxengiu@outlook.com",
+    description="Jenkins build data ETL with DuckDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maxleow/duck-jenkins",
+    project_urls={
+        "Bug Tracker": "https://github.com/maxleow/duck-jenkins/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    python_requires=">=3.7",
+)
