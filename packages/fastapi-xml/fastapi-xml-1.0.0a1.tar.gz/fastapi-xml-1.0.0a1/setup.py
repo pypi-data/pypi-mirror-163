e24407f68252041428c9967606e24735e973733c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:25 +0000

    Add package fastapi-xml/1.0.0a1/fastapi-xml-1.0.0a1.tar.gz

---
+++ packages/fastapi-xml/fastapi-xml-1.0.0a1.tar.gz/fastapi-xml-1.0.0a1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastapi_xml']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.70.0', 'pydantic>=1.9.0', 'xsdata>=22.1']
+
+setup_kwargs = {
+    'name': 'fastapi-xml',
+    'version': '1.0.0a1',
+    'description': 'adds xml support to fastapi',
+    'long_description': 'None',
+    'author': 'Leon Rendel',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
