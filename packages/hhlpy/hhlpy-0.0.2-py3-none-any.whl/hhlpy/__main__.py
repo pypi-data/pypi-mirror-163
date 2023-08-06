268adf05e770ad6fd1e8083a9b5d708d4ce1baa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:34 +0000

    Add package hhlpy/0.0.2/hhlpy-0.0.2-py3-none-any.whl

---
+++ packages/hhlpy/hhlpy-0.0.2-py3-none-any.whl/hhlpy/__main__.py
@@ -0,0 +1,30 @@
+from hhlpy.gui.server import *
+import webbrowser
+from threading import Timer
+
+port = 8000
+
+def open_browser():
+      webbrowser.open_new('http://127.0.0.1:{0}/'.format(port))
+
+def main():
+    websocketServer = WebSocketServer(
+        ('', port),
+        Resource(OrderedDict([('/', HHLPyApplication)]))
+    )
+    try:
+        computationProcess = startComputationProcess()
+        startHttpProcess(port)
+        Timer(3, open_browser).start();
+        print("Running websocket server on ws://localhost:{0}".format(port), flush=True)
+        websocketServer.start()
+        while True:
+            checkComputationProcess() # Check whether there are new computation results
+            gevent.sleep(0) # Pause to allow the server to handle new requests from the client
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+        print("Closing python websocket server")
+        websocketServer.stop()
+
+if __name__ == "__main__":
+    main()
