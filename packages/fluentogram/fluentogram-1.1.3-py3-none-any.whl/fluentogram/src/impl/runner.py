28e2c9257d6f78cf30c3d86fbc6e1418e4f7b438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:30 +0000

    Add package fluentogram/1.1.3/fluentogram-1.1.3-py3-none-any.whl

---
+++ packages/fluentogram/fluentogram-1.1.3-py3-none-any.whl/fluentogram/src/impl/runner.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+"""
+A translator runner by itself
+"""
+from typing import List
+
+from fluentogram.src.impl import AttribTracer, FluentTranslator
+
+
+class TranslatorRunner(AttribTracer):
+    """This is one-shot per Telegram event translator with attrib tracer access way."""
+
+    def __init__(self, translators: List[FluentTranslator]) -> None:
+        super().__init__()
+        self.translators = translators
+        self.request_line = ""
+
+    def get(self, key: str, **kwargs) -> str:
+        """Faster, direct way to use translator, without sugar-like typing supported attribute access way"""
+        return self._get_translation(key, **kwargs)
+
+    def _get_translation(self, key, **kwargs):
+        for translator in self.translators:
+            try:
+                return translator.get(key, **kwargs)
+            except KeyError:
+                continue
+
+    def __call__(self, **kwargs) -> str:
+        text = self._get_translation(self.request_line[:-1], **kwargs)
+        self.request_line = ""
+        return text
+
+    def __getattr__(self, item: str) -> 'TranslatorRunner':
+        self.request_line += f"{item}{self.translators[0].separator}"
+        return self
