f521c397cabb1e7728571223880aabf20a3dae0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:31 +0000

    Add package bucketstore/0.2.2/bucketstore-0.2.2.tar.gz

---
+++ packages/bucketstore/bucketstore-0.2.2.tar.gz/bucketstore-0.2.2/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+pip setup file
+"""
+from setuptools import setup
+
+
+__library__ = "bucketstore"
+__version__ = "0.2.2"
+
+__user__ = "https://github.com/jpetrucciani"
+
+
+with open("README.md", encoding="UTF-8") as readme:
+    LONG_DESCRIPTION = readme.read()
+
+
+REQUIRED = ["boto3"]
+
+setup(
+    name=__library__,
+    version=__version__,
+    description="A simple library for interacting with Amazon S3.",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="Kenneth Reitz, Jacobi Petrucciani",
+    author_email="j@cobi.dev",
+    url=f"{__user__}/{__library__}.git",
+    download_url=f"{__user__}/{__library__}.git",
+    py_modules=[__library__],
+    install_requires=REQUIRED,
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
