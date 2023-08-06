01c2ee719c3e97179c759d14e349a5f280a1b4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:54 +0000

    Add package parkingfunctions/0.1.1/parkingfunctions-0.1.1.tar.gz

---
+++ packages/parkingfunctions/parkingfunctions-0.1.1.tar.gz/parkingfunctions-0.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['parkingfunctions']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['furo>=2022.6.21,<2023.0.0',
+ 'myst-parser>=0.18.0,<0.19.0',
+ 'numpy>=1.23.1,<2.0.0',
+ 'sphinx-rtd-theme>=1.0.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'parkingfunctions',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Seth Bangert',
+    'author_email': 'seth@sbang.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
