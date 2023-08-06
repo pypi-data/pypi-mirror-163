3909848524f5c95e5543c9a9c99433c6c5393a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:25 +0000

    Add package dessia-common/0.9.2/dessia_common-0.9.2-py3.8.egg

---
+++ packages/dessia-common/dessia_common-0.9.2-py3.8.egg/dessia_common/models/workflows/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 17:32:48 2021
+
+@author: masfaraud
+"""
+
+from .power_simulation import simulation_workflow
+from .forms_workflow import workflow_
+from .vectored_workflow import vectored_workflow
