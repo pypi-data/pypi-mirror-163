26b3649c7506fdd3755ab6521e9cd39061d53162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:01 +0000

    Add package gbit/0.1.2/gbit-0.1.2.tar.gz

---
+++ packages/gbit/gbit-0.1.2.tar.gz/gbit-0.1.2/src/gbit/gbit.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from os import path, listdir
+import sys
+from importlib import import_module
+import logging
+
+def main():
+	if len(sys.argv) != 2:
+		logging.error("Insuficient arguments")
+		sys.exit(2)
+
+	gtfobin = sys.argv[1]
+	dir_path = path.dirname(path.realpath(__file__)) # get the path to the current directory
+	binaries = listdir(dir_path) # get the modules
+
+	if gtfobin.lower()+".py" not in binaries: # check if the requested binary is in the modules
+		logging.info(f"{gtfobin} unavailable.")
+		sys.exit(1)
+	
+	module = import_module(f"{gtfobin}") # load the module
+	print(module.gtfobin)
+
+if __name__ == '__main__':
+    main()
