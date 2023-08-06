2b3bdeb9410237c6ea43815d67daf6b759a0f240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:33 +0000

    Add package nb-serverproxy-openrefine/0.0.5/nb-serverproxy-openrefine-0.0.5.tar.gz

---
+++ packages/nb-serverproxy-openrefine/nb-serverproxy-openrefine-0.0.5.tar.gz/nb-serverproxy-openrefine-0.0.5/nb_serverproxy_openrefine/__init__.py
@@ -0,0 +1,19 @@
+__version__="0.0.5"
+
+import os
+from pathlib import Path
+
+# Requires openrefine to be on the path as: refine
+def setup_openrefine():
+    openrefine_path =  os.getenv("REFINE_DIR") if "REFINE_DIR" in os.environ else str(Path.home() / 'openrefine')
+    if not os.path.exists(openrefine_path):
+        os.makedirs(openrefine_path)
+    return {
+        'command': ['refine', '-p', '{port}','-d', openrefine_path],
+        'environment': {},
+        'timeout': 120,
+        'launcher_entry': {
+            'title': 'OpenRefine',
+            'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'openrefine.svg')
+        }
+    }
