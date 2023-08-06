1091289841920e33dfcdecac0dd802fe8616a514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:36 +0000

    Add package hypernova-orbit/0.1.0/hypernova-orbit-0.1.0.tar.gz

---
+++ packages/hypernova-orbit/hypernova-orbit-0.1.0.tar.gz/hypernova-orbit-0.1.0/hypernova/src/solvers/solver_configurations.h
@@ -0,0 +1,12 @@
+#ifndef SOLVER_CONFIGURATIONS_H
+#define SOLVER_CONFIGURATIONS_H
+
+#include "../../abstractions/solver_abstractions.h"
+
+RKSolver rk4();
+RKSolver rk45();
+SymplecticSolver verlet();
+SymplecticSolver yoshida4();
+SymplecticSolver yoshida8();
+
+#endif // SOLVER_CONFIGURATIONS_H
