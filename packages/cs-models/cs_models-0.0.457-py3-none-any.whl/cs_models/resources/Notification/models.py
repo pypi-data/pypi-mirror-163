82acc8ff1a72705e7407cba7c6d93dbb03fa1aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:19 +0000

    Add package cs-models/0.0.457/cs_models-0.0.457-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.457-py3-none-any.whl/cs_models/resources/Notification/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    Boolean,
+    ForeignKey,
+    Text,
+)
+from datetime import datetime
+
+from ...database import Base
+
+
+class NotificationModel(Base):
+    __tablename__ = 'notifications'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(128), nullable=False)
+    search_id = Column(
+        Integer,
+        ForeignKey('user_saved_searches.id'),
+        nullable=False,
+    )
+    source_type = Column(String(50), nullable=False)
+    source_table = Column(String(50), nullable=False)
+    source_id = Column(Integer, nullable=False)
+    source_detail = Column(Text, nullable=False)
+    matches = Column(Text, nullable=True)
+    seen = Column(Boolean, nullable=True)
+    date = Column(DateTime, nullable=False)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
