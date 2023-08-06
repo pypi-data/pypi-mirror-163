4c3c600ff1ef037657ea497c11675882e38e3ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:55 +0000

    Add package eqxvision/0.1.9/eqxvision-0.1.9-py3-none-any.whl

---
+++ packages/eqxvision/eqxvision-0.1.9-py3-none-any.whl/eqxvision/models/classification/__init__.py
@@ -0,0 +1,15 @@
+from . import (
+    alexnet,
+    convnext,
+    densenet,
+    efficientnet,
+    googlenet,
+    mobilenetv2,
+    mobilenetv3,
+    resnet,
+    shufflenetv2,
+    squeezenet,
+    swin,
+    vgg,
+    vit,
+)
