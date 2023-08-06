5b3f34de93c5776a276533a1a121825f26031cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:34 +0000

    Add package pynetsys/0.1.0/pynetsys-0.1.0.tar.gz

---
+++ packages/pynetsys/pynetsys-0.1.0.tar.gz/pynetsys-0.1.0/pynetsys/__init__.py
@@ -0,0 +1,23 @@
+"""pynetsys is a collection of tools and malicious packets."""
+
+from .constants import Protocol, Packets
+from . import _tool, _packet
+from requests import get
+from socket import getfqdn
+
+__version__ = []
+
+tool, packet = _tool, _packet
+
+def isOnline(Address: str):
+    # IMPORT
+    from scapy.layers.inet import IP
+
+    # RUN & RETURN
+    try:
+        if get(getfqdn(Address)).status_code == 200:
+            return True
+        else:
+            return False
+    except:
+        return False
