da8951a5ca9008c06881f23ea7eefaa9a88ac6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:12 +0000

    Add package dbt-materialize/1.1.3/dbt-materialize-1.1.3.tar.gz

---
+++ packages/dbt-materialize/dbt-materialize-1.1.3.tar.gz/dbt-materialize-1.1.3/dbt/include/materialize/macros/adapters.sql
@@ -0,0 +1,89 @@
+-- Copyright 2020 Josh Wills. All rights reserved.
+-- Copyright Materialize, Inc. and contributors. All rights reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License in the LICENSE file at the
+-- root of this repository, or online at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- Most of these macros are direct copies of their PostgreSQL counterparts.
+-- See: https://github.com/dbt-labs/dbt-core/blob/13b18654f/plugins/postgres/dbt/include/postgres/macros/adapters.sql
+
+{% macro materialize__create_view_as(relation, sql) -%}
+  create view {{ relation }}
+  as (
+    {{ sql }}
+  );
+{%- endmacro %}
+
+{% macro materialize__create_materialized_view_as(relation, sql) -%}
+  create materialized view {{ relation }}
+  as (
+    {{ sql }}
+  );
+{%- endmacro %}
+
+{% macro materialize__create_arbitrary_object(sql) -%}
+    {{ sql }}
+{%- endmacro %}
+
+{% macro materialize__rename_relation(from_relation, to_relation) -%}
+  {% set target_name = adapter.quote_as_configured(to_relation.identifier, 'identifier') %}
+  {% call statement('rename_relation') -%}
+    {% if relation.type == 'view' %}
+      alter view {{ from_relation }} rename to {{ target_name }}
+    {% else %}
+      alter materialized view {{ from_relation }} rename to {{ target_name }}
+    {% endif %}
+  {%- endcall %}
+{% endmacro %}
+
+{% macro materialize__drop_relation(relation) -%}
+  {% call statement('drop_relation') -%}
+    {% if relation.type == 'view' %}
+      drop view if exists {{ relation }} cascade
+    {% elif relation.type == 'materializedview' %}
+      drop materialized view if exists {{ relation }} cascade
+    {% elif relation.type == 'sink' %}
+      drop sink if exists {{ relation }}
+    {% elif relation.type == 'source' %}
+      drop source if exists {{ relation }} cascade
+    {% elif relation.type == 'index' %}
+      drop index if exists {{ relation }}
+    {% endif %}
+  {%- endcall %}
+{% endmacro %}
+
+{% macro materialize__get_create_index_sql(relation, index_dict) -%}
+  {%- set index_config = adapter.parse_index(index_dict) -%}
+  {%- set comma_separated_columns = ", ".join(index_config.columns) -%}
+  {%- set index_name = index_config.render(relation) -%}
+    create index if not exists
+      "{{ index_name }}"
+      on {{ relation }} {% if index_config.type -%}
+        using {{ index_config.type }}
+  {%- endif %}
+  ({{ comma_separated_columns }});
+{%- endmacro %}
+
+{% macro materialize__list_relations_without_caching(schema_relation) %}
+  {% call statement('list_relations_without_caching', fetch_result=True) -%}
+    select
+        d.name as database,
+        s.name as schema,
+        o.name,
+        case when o.type = 'materialized view' then 'materializedview' else o.type end as type
+    from mz_objects o
+    join mz_schemas s on o.schema_id = s.id and s.name = '{{ schema_relation.schema }}'
+    join mz_databases d on s.database_id = d.id and d.name = '{{ schema_relation.database }}'
+  {% endcall %}
+  {{ return(load_result('list_relations_without_caching').table) }}
+{% endmacro %}
