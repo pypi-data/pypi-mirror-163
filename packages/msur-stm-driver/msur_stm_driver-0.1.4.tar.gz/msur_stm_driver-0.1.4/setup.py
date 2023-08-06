e17fa8389727761e8deeba86db346024bd0248f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:51 +0000

    Add package msur-stm-driver/0.1.4/msur_stm_driver-0.1.4.tar.gz

---
+++ packages/msur-stm-driver/msur_stm_driver-0.1.4.tar.gz/msur_stm_driver-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='msur_stm_driver',
+    version='0.1.4',
+    license='MIT',
+    author='Photon94',
+    author_email='299792458.photon.94@gmail.com',
+    packages=['msur_stm_driver'],
+    install_requires=['loguru', 'pydantic', 'anyio'],
+    zip_safe=False
+)
