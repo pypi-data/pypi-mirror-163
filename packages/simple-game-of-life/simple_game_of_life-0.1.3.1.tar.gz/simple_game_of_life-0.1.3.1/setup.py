5b97e9ccf167909e76aaf294579078914c0ef4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:16 +0000

    Add package simple-game-of-life/0.1.3.1/simple_game_of_life-0.1.3.1.tar.gz

---
+++ packages/simple-game-of-life/simple_game_of_life-0.1.3.1.tar.gz/simple_game_of_life-0.1.3.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="simple_game_of_life",
+    version="0.1.3.1",
+    author="harpie (Dorian MB)",
+    author_email="<dorian.mariani@gmail.com>",
+    py_modules=["simple_game_of_life","lexicon"],
+    install_requires=["numpy", "matplotlib"],
+    package_dir={"": "src"},
+    description = "Conway's game of life in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
