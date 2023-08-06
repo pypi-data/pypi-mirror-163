ad3a9ceb1db2cc756d0fa48eccf52c2be78e01b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:44 +0000

    Add package async-get-vedio/2.0/async_get_vedio-2.0.tar.gz

---
+++ packages/async-get-vedio/async_get_vedio-2.0.tar.gz/async_get_vedio-2.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name = 'async_get_vedio',
+    version = '2.0',
+    author = 'WUT_ljs',
+    author_email = '3480339804@qq.com',
+    url = 'https://github.com/wutljs/async_get_vedio',
+    description = 'Get vedio',
+    long_description = '此次更新,将async_get_vedio自动调用方法下载一个视频改为:async_get_vedio可以用于用户自定义的函数中被多次调用.'
+                       '具体文档请进入:https://github.com/wutljs/async_get_vedio 查看.',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
