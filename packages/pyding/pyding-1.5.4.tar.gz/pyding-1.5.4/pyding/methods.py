cf24a6634246010a9f635299ee277c666e6328ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:23 +0000

    Add package pyding/1.5.4/pyding-1.5.4.tar.gz

---
+++ packages/pyding/pyding-1.5.4.tar.gz/pyding-1.5.4/pyding/methods.py
@@ -0,0 +1,12 @@
+# methods.py
+from .event_space import global_event_space
+from .structures import EventCall, EventHandler
+
+
+on = global_event_space.on
+call = global_event_space.call
+handlers_registered = global_event_space.handler_registered
+register_handler = global_event_space.register_handler
+unregister_from_module = global_event_space.unregister_from_module
+unregister_handler = global_event_space.unregister_handler
+get_handlers = global_event_space.get_handlers
