cd263bf8ef5a34914619be21b15596d6d7bc8435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:31 +0000

    Add package Random-Word/1.0.10/Random%20Word-1.0.10.tar.gz

---
+++ packages/Random-Word/Random%20Word-1.0.10.tar.gz/Random Word-1.0.10/random_word/__init__.py
@@ -0,0 +1,7 @@
+from random_word.services.wordnik import Wordnik
+from random_word.random_word import RandomWords
+
+__all__ = ['Wordnik', 'RandomWords']
+__name__ = "Random Word"
+__author__ = "Vaibhav Singh <hi@vaibhavsingh97.com>"
+__version__ = "1.0.10"
---
+++ packages/Random-Word/Random%20Word-1.0.10.tar.gz/Random Word-1.0.10/random_word/random_word.py
@@ -0,0 +1,18 @@
+from random_word.services.wordnik import Wordnik
+
+
+class RandomWords(object):
+
+    """Class for generating random words"""
+
+    def __init__(self, api_key=None):
+        self.wordnik_service = Wordnik(api_key)
+
+    def get_random_word(self, **kwargs):
+        return self.wordnik_service.get_random_word(**kwargs)
+
+    def get_random_words(self, **kwargs):
+        return self.wordnik_service.get_random_words(**kwargs)
+
+    def word_of_the_day(self, **kwargs):
+        return self.wordnik_service.word_of_the_day(**kwargs)
