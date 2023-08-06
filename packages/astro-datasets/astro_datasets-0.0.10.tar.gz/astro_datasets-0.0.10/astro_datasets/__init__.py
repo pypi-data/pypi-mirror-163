4576fc73c30979d19920e18285abaf209226e573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:17 +0000

    Add package astro-datasets/0.0.10/astro_datasets-0.0.10.tar.gz

---
+++ packages/astro-datasets/astro_datasets-0.0.10.tar.gz/astro_datasets-0.0.10/astro_datasets/__init__.py
@@ -0,0 +1,18 @@
+# Tensorflow datasets of astronomical time series
+import astro_datasets.time_series.spcc
+import astro_datasets.time_series.plasticc
+
+# Tensorflow datasets of astronomical images
+import astro_datasets.images.mirabest
+import astro_datasets.images.cmd
+import astro_datasets.images.mlsst
+
+builders = [
+    'spcc',
+    'plasticc',
+    'mirabest',
+    'cmd',
+    'mlsst',
+]
+
+__version__ = '0.0.10'
