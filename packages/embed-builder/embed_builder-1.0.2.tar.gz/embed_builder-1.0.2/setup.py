0b90399e4cf4c1c9c72a777af903cc8f45031aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:51 +0000

    Add package embed-builder/1.0.2/embed_builder-1.0.2.tar.gz

---
+++ packages/embed-builder/embed_builder-1.0.2.tar.gz/embed_builder-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from setuptools import setup
+
+setup(
+    version="1.0.2",
+    name="embed_builder",
+    description="Easily build Discord Embed dictionaries",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="depthbomb",
+    license="MIT",
+    url="https://github.com/depthbomb/embed_builder",
+    packages=["embed_builder"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
+    ]
+)
