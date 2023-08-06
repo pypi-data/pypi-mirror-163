011263cfaa653906b1da8fc7a604ff13fcd8460d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:09 +0000

    Add package resoto-cattrs-snapshot-56eda37/22.2.0.dev0/resoto_cattrs_snapshot_56eda37-22.2.0.dev0-py3-none-any.whl

---
+++ packages/resoto-cattrs-snapshot-56eda37/resoto_cattrs_snapshot_56eda37-22.2.0.dev0-py3-none-any.whl/cattr/preconf/json.py
@@ -0,0 +1,4 @@
+"""Preconfigured converters for the stdlib json."""
+from cattrs.preconf.json import configure_converter, make_converter
+
+__all__ = ["make_converter", "configure_converter"]
---
+++ packages/resoto-cattrs-snapshot-56eda37/resoto_cattrs_snapshot_56eda37-22.2.0.dev0-py3-none-any.whl/cattr/preconf/msgpack.py
@@ -0,0 +1,4 @@
+"""Preconfigured converters for msgpack."""
+from cattrs.preconf.msgpack import configure_converter, make_converter
+
+__all__ = ["make_converter", "configure_converter"]
