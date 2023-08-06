1923239afb5915073e8ae4ea2b64195b2c52a8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:49 +0000

    Add package keysec/1.2.0/keysec-1.2.0-py3-none-any.whl

---
+++ packages/keysec/keysec-1.2.0-py3-none-any.whl/keysec/keysec.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# encoding:utf-8
+
+
+import sys
+
+from keysec.converter import convert
+from keysec.edit import edit
+from keysec.generator import gen_private, gen_public
+from keysec.info import info
+from keysec.iokeys import full_process, generate_and_write
+from keysec.parser import ARGS, generate_parser, parser, parse_args
+
+sys.tracebacklimit = 0
+
+
+def main():
+    parse_args()
+    if ARGS.GENERATE:
+        if ARGS.PRIVATE:
+            generate_and_write(output=ARGS.OUT, func=gen_private, algorithm=ARGS.ALGORITHM, dst_format=ARGS.FORMAT, bits=ARGS.BITS)
+        elif ARGS.PUBLIC:
+            full_process(key_str=ARGS.IN, output=ARGS.OUT, func=gen_public)
+        else:
+            generate_parser.print_help()
+    elif ARGS.CONVERT:
+        full_process(key_str=ARGS.IN, output=ARGS.OUT, func=convert, nopass=ARGS.NOPASS)
+    elif ARGS.EDIT:
+        full_process(key_str=ARGS.IN, output=ARGS.OUT, func=edit, comment=ARGS.COMMENT, password=ARGS.PASSWORD)
+    elif ARGS.INFO:
+        full_process(key_str=ARGS.IN, output=ARGS.OUT, func=info)
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
