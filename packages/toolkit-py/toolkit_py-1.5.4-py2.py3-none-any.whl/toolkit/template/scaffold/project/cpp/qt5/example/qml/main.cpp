441f1b6950f4d58f583c31228c52130ff0aabb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:01 +0000

    Add package toolkit-py/1.5.4/toolkit_py-1.5.4-py2.py3-none-any.whl

---
+++ packages/toolkit-py/toolkit_py-1.5.4-py2.py3-none-any.whl/toolkit/template/scaffold/project/cpp/qt5/example/qml/main.cpp
@@ -0,0 +1,29 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "core.h"
+
+int main(int argc, char *argv[]) {
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+  QGuiApplication app(argc, argv);
+
+  Core *core = new Core(&app);
+
+  QQmlApplicationEngine engine;
+
+  engine.rootContext()->setContextProperty("core", core);
+
+  const QUrl url(QStringLiteral("qrc:/main.qml"));
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated,
+      &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+          QCoreApplication::exit(-1);
+      },
+      Qt::QueuedConnection);
+  engine.load(url);
+
+  return QCoreApplication::exec();
+}
