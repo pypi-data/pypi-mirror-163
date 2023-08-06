97bbd5d0070f485745318aca24e550745f101e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:32 +0000

    Add package vos-mjjo/0.0.5/vos_mjjo-0.0.5.tar.gz

---
+++ packages/vos-mjjo/vos_mjjo-0.0.5.tar.gz/vos_mjjo-0.0.5/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vos_mjjo", # Replace with your own username
+    version="0.0.5",
+    author="mjjo",
+    author_email="mj.jo@valueofspace.com",
+    description="vos-mjjo package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jomujin/vos-mjjo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["symspellpy"],
+    entry_points={
+        'console_scripts': [
+            'shortcut1 = package.module:func',
+        ],
+        'gui_scripts': [
+            'shortcut2 = package.module:func',
+        ]
+    },
+    include_package_data=True,
+    package_data={'mjjo': ['mjjo/date_dictionary.txt']},
+    test_suite='tests.test_cordate'
+)
