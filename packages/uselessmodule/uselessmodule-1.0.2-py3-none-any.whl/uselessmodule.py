e852ad14f217688526fc85e08dfbd50c051962af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:28 +0000

    Add package uselessmodule/1.0.2/uselessmodule-1.0.2-py3-none-any.whl

---
+++ packages/uselessmodule/uselessmodule-1.0.2-py3-none-any.whl/uselessmodule.py
@@ -0,0 +1,15 @@
+def printhi():
+    print('hi')
+def pthi():
+    print('hi')
+def delete(a,b):
+    return a.replace(b,'')
+def deletegiho(a):
+    return (a**2)**0.5
+def add(a,b):
+    if 'str' in str(type(a)) or 'str' in str(type(b)):
+        print(str(a)+str(b))
+    elif 'float' in str(type(a)) or 'float' in str(type(b)):
+        print(a+b)
+    elif 'int' in str(type(a)) or 'int' in str(type(b)):
+        print(a+b)
