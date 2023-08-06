30e0deb66a26e78f27f56e056385a594eff53f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:09 +0000

    Add package ajax-views/0.6.0/ajax_views-0.6.0-py2.py3-none-any.whl

---
+++ packages/ajax-views/ajax_views-0.6.0-py2.py3-none-any.whl/tests/app/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class Config(AppConfig):
+    name = 'app'
+    verbose_name = _('App')
+
+    def ready(self):
+        from . import views
