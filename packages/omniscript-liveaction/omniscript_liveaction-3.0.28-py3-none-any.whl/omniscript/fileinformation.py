19fb4ef0f1f312cbafeaeae2e6a641266020afb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:42 +0000

    Add package omniscript-liveaction/3.0.28/omniscript_liveaction-3.0.28-py3-none-any.whl

---
+++ packages/omniscript-liveaction/omniscript_liveaction-3.0.28-py3-none-any.whl/omniscript/fileinformation.py
@@ -0,0 +1,103 @@
+﻿"""FileInformation class.
+"""
+#Copyright (c) LiveAction, Inc. 2022. All rights reserved.
+#Copyright (c) Savvius, Inc. 2013-2019. All rights reserved.
+#Copyright (c) WildPackets, Inc. 2013-2014. All rights reserved.
+
+import xml.etree.ElementTree as ET
+
+from .invariant import FILE_FLAGS_FOLDER
+from .peektime import PeekTime
+
+
+class FileInformation(object):
+    """Information about a file.
+    """
+
+    flags = 0
+    """The attribute flags of the file may be checked against the
+    FILE FLAGS constants.
+    """
+
+    modified = None
+    """The date and time, as
+    :class:`PeekTime <omniscript.peektime.PeekTime>`,
+    that the file was last modified."""
+
+    name = ''
+    """The fully qualified name of the file."""
+
+    size = 0
+    """The size of the file in bytes."""
+
+    #XML Tags
+    _json_partial_path = 'PartialPath'
+    _json_file_name = 'FileName'
+    _json_status = 'Status'
+    _json_size = 'FileSize'
+    _json_time_zone_bias = 'TimeZoneBias'
+    _json_media_type = 'MediaType'
+    _json_media_subtype = 'MediaSubType'
+    _json_link_speed = 'LinkSpeed'
+    _json_count = 'PacketCount'
+    _json_session_start_time = 'SessionStartTime'
+    _json_session_end_time = 'SessionEndTime'
+    _json_capture_name = 'CaptureName'
+    _json_capture_id = 'CaptureID'
+    _json_file_index = 'FileIndex'
+    _json_adapter_name = 'AdapterName'
+    _json_adapter_address = 'AdapterAddr'
+    _json_owner = 'Owner'
+
+    _tag_root_name = "file"
+    _tag_flags = "attr"
+    _tag_modified = "mod"
+    _tag_name = "name"
+    _tag_size = "size"
+
+    def __init__(self, name='', flags=0, props=None):
+        self.flags = flags
+        self.modified = FileInformation.modified
+        self.name = name
+        self.size = FileInformation.size
+        self._load(props)
+
+    def _load(self, props):
+        """Load the File Informaiont from a dictionary."""
+        if isinstance(props, dict):
+            self.modified = props.get(FileInformation._json_session_end_time)
+            self.name = props.get(FileInformation._json_file_name)
+            v = props.get(FileInformation._json_size)
+            self.size = int(v) if v else 0
+
+    def __str__(self):
+        return f'FileInformation: {self.name}'
+
+    def is_folder(self):
+        """Returns True if the information is for a folder."""
+        return (self.flags & FILE_FLAGS_FOLDER) != 0
+
+
+def _create_file_information_list(props):
+    lst = []
+    if isinstance(props, dict):
+        rows = props['rows']
+        if isinstance(rows, list):
+            for row in rows:
+                lst.append(FileInformation(props=row))
+    return lst
+
+
+def file_information_list_to_file_list(fil):
+    """Transform a list of
+    :class:`FileInformation <omniscript.fileinformation.FileInformation>`
+    into a list of strings of fully qualified file names.
+    """
+    fl = []
+    path = ''
+    for f in fil:
+        if f.is_folder():
+            path = f.name
+        else:
+            fl.append(path + f.name)
+    return fl
---
+++ packages/omniscript-liveaction/omniscript_liveaction-3.0.28-py3-none-any.whl/omniscript/packet.py
@@ -0,0 +1,174 @@
+﻿"""Packet class.
+"""
+#Copyright (c) LiveAction, Inc. 2022. All rights reserved.
+#Copyright (c) Savvius, Inc. 2013-2019. All rights reserved.
+#Copyright (c) WildPackets, Inc. 2013-2014. All rights reserved.
+
+import omniscript
+
+from .omnierror import OmniError
+from .peektime import PeekTime
+# from .readstream import ReadStream
+
+# from .omniscript import get_id_protocol_names
+
+
+class Packet(object):
+    """The Packet class has packet information.
+    The 
+    :func:`get_packets() 
+    <omniscript.capture.Capture.get_packets>`
+    function returns a list of Packet objects.
+
+    Packet number vs index: the packet number, which starts at 0, is a
+    counter of the packets as they are captured. When a packet is
+    captured the packet counter becomes the packet number of the packet
+    and then the packet counter is incremented.
+    The packet index is the index into the list of packets called the
+    packet buffer.
+    The first index in the packet buffer is 0. The first packet in the
+    packet buffer, index 0, always contains the packet with the lowest
+    packet number.
+    
+    A capture buffer can only hold a finite number of packets. Once
+    the buffer  has filled and a new packet is captured then the
+    first packet in the packet buffer (index 0) is deleted makeing what
+    was the second packet (index 1) into the first packet (index 0).
+    And the newly captured packet becomes the last packet in the buffer.
+
+    The first_packet attribute of a
+    :class:`Capture <omniscript.capture.Capture>`
+    is the packet number of the first packet in the capture buffer.
+    """
+
+    application = None
+    """The packet's application."""
+
+    data = None
+    """The packet data."""
+
+    flags = 0
+    """The packet's flags."""
+
+    flow_id = 0
+    """The id of the flow the packet belongs to.
+    If there is no flow id then zero (0).
+    """
+
+    index = -1
+    """The packet's index into the capture buffer."""
+
+    number = -1
+    """The packet's number starting from when the capture starts."""
+
+    packet_length = 0
+    """The total length of the packet."""
+
+    proto_spec = 0
+    """The packet's ProtoSpec."""
+
+    status = 0
+    """The packet's status."""
+
+    timestamp = None
+    """The packet's timestamp as
+    :class:`PeekTime <omniscript.peektime.PeekTime>`.
+    """
+
+    def __init__(self, number, data=None):
+        self.number = number if number > 0 else Packet.number
+        self.application = Packet.application
+        self.data = Packet.data
+        self.packet_length = Packet.packet_length
+        self.flags = Packet.flags
+        self.flow_id = Packet.flow_id
+        self.packet_length = Packet.packet_length
+        self.proto_spec = Packet.proto_spec
+        self.status = Packet.status
+        self.timestamp = None
+        self.data = Packet.data
+        self._load_data(data)
+
+    def __str__(self):
+        return f'Packet: {self.index}'
+
+    @property
+    def id(self):
+        """The packet's identifier. (Read Only)"""
+        return self.number
+
+    @property
+    def name(self):
+        """The packet's number. (Read Only)"""
+        return self.number
+
+    def _load(self, props):
+        if not props:
+            return
+        # self.index = stream.read_uint()
+        # self.number = self.index
+        # self.timestamp = PeekTime(stream.read_ulong())
+        # self.proto_spec = stream.read_uint()
+        # self.flags = stream.read_uint()
+        # self.status = stream.read_uint()
+        # self.packet_length = stream.read_ushort()
+        # slice_length = stream.read_ushort()
+        # info_block = (stream.read_uint() != 0)
+        # self.application = stream.read(8).strip('\0')
+        # if version > 1:
+        #     self.flow_id = stream.read_uint()
+        # if info_block:
+        #     # read the media info block.
+        #     pass
+        # if slice_length > 0:
+        #     self.data = stream.read(slice_length)
+        # else:
+        #     self.data = stream.read(self.packet_length)
+
+    def _load_data(self, data):
+        if not data:
+            return
+        self.data = data
+        self.packet_length = len(self.data)
+
+    def data_length(self):
+        """The number of bytes in the packet."""
+        if self.data is not None:
+            return len(self.data)
+        return 0
+
+    def is_sliced(self):
+        """Is the packet sliced."""
+        if self.data is None:
+            return False
+        return len(self.data) < self.packet_length
+
+    def protocol_name(self):
+        """The protocol name of the packet."""
+        protocol_id_names = omniscript.get_id_protocol_names()
+        if self.proto_spec & 0x0FFFF:
+            return protocol_id_names[self.proto_spec & 0x0FFFF]
+        return 'Unknown'
+
+
+# def _create_packet_list(data, first=0):
+#     try:
+#         omniscript._parse_command_response(data)
+#     except omniscript.OmniError as oe:
+#         raise oe
+#     except:
+#         # Only errors are parseable.
+#         # But ET.parse(data) will throw an exception
+#         #   catch the exception and process the success.
+#         pass
+
+#     lst = []
+#     stream = ReadStream(data)
+#     stream.read_uint()      # universal_count
+#     stream.read_ushort()    # magic
+#     version = stream.read_ushort()
+#     packet_count = stream.read_uint()
+#     for _ in range(packet_count):
+#         pkt = Packet(stream, first, version)
+#         lst.append(pkt)
+#     return lst
