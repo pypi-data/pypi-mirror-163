414e06cf01adc7874f1d5f2e35dbe366e5d188b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:45 +0000

    Add package dbpoolpy/0.6.4/dbpoolpy-0.6.4-py3-none-any.whl

---
+++ packages/dbpoolpy/dbpoolpy-0.6.4-py3-none-any.whl/dbpoolpy/config/default_settings.py
@@ -0,0 +1,7 @@
+RECONNECT_TIMES = 3    # 尝试重连次数
+DEBUG = False          # 开启debug
+LOGGER = None          # 日志器
+POOL_TYPE = 'simple'   # 连接池的各类
+DB_TYPE = 'mysql'      # 数据库的种类
+EXT = ''               # 扩展信息
+SSLKEY_PATH = ''       # ssl路径
