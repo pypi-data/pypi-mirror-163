a6d3ed007ec6b47cea87950e450de329d4313eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:01 +0000

    Add package larcoh/0.1.4/larcoh-0.1.4.tar.gz

---
+++ packages/larcoh/larcoh-0.1.4.tar.gz/larcoh-0.1.4/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os
+from os.path import join
+import setuptools
+
+
+setuptools.setup(
+    name='larcoh',
+    version='0.1.4',
+    description='Pipeline for joint calling, sample and variant QC for WGS germline '
+    'variant calling data in large cohorts',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/populationgenomics/joint-calling',
+    license='MIT',
+    packages=['larcoh'],
+    include_package_data=True,
+    zip_safe=False,
+    scripts=[join('scripts', fp) for fp in os.listdir('scripts') if fp.endswith('.py')],
+    keywords='bioinformatics',
+    install_requires=[
+        'cpg-utils',
+        'cpg-pipes',
+        'analysis-runner',
+        'sample_metadata',
+        'hail>=0.2.97',
+        'cpg-gnomad',
+        'pandas',
+        'click',
+        'google-cloud-storage',
+        'google-cloud-secret-manager',
+        'coloredlogs',
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+)
