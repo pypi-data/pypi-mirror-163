72b693c4dd1b65c5f98bc67c087d5d60efab76ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:31 +0000

    Add package scimath/5.0.0/scimath-5.0.0-cp37-cp37m-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/scimath/scimath-5.0.0-cp37-cp37m-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/scimath/interpolate/__init__.py
@@ -0,0 +1,18 @@
+# (C) Copyright 2005-2022 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+""" Specialized interpolation methods for 1-D arrays, much faster than the
+    standards linear interpolation in SciPy. Part of the SciMath project of
+    the Enthought Tool Suite.
+"""
+from .interpolate import linear, block_average_above, window_average
+
+from .fitting import DataFit, Spline, Linear, Logarithmic, BlockAverageAbove, \
+    Block, EndAverage, WindowAverage, FillNaN
