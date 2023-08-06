73f3508da1b3b870bda9b3259938a4bf7dccd8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:19 +0000

    Add package invoke-iife/0.2.0/invoke-iife-0.2.0.tar.gz

---
+++ packages/invoke-iife/invoke-iife-0.2.0.tar.gz/invoke-iife-0.2.0/src/iife/__init__.py
@@ -0,0 +1,77 @@
+"""# iife
+
+## Immediately-invoked function expressions in Python
+
+The `iife` package provides a decorator function `iife` that calls the function/class it decorates and assigns the result to the name of the function/class.
+
+## The entire package:
+
+This is the entire package (`iife.py`):
+
+```python
+from typing import Callable, TypeVar
+
+T = TypeVar("T")
+
+def iife(c: Callable[[], T]) -> T:
+    return c()
+```
+
+That's it.
+
+Some use cases include...
+
+## Creating an anonymous object.
+
+Have you ever written a class that you know will only have one instance? You can use the iife decorator to create that instance immediately.
+
+```python
+@iife
+@dataclass
+class player:
+    x: int = 1
+    y: int = 2
+
+# player is an instance of the player class
+player.x # -> 1
+
+# The class cannot be reinstantiated because the name is shadowed.
+new_player = player(x=3, y=4) # -> SyntaxError
+```
+
+This might also be useful in library development to hide the implementation details of the class from the end user, who can only access the single instance.
+
+## Complex initialization.
+
+Sometimes a variable needs to be initialized by complex logic that cannot be expressed as a single assignment. Traditionally, this can be done with temporarily setting the variable to a default value:
+
+```python
+x = None
+y = [1, 2, 3]
+for i in y:
+    if i == 2:
+        x = i
+```
+
+Why not do it with an IIFE? (To be honest, this isn't the best example, but it's more fun to do it like this.)
+
+```python
+@iife
+def x() -> Optional[int]:
+    y = [1, 2, 3]
+    for i in y:
+        if i == 2:
+            return i
+```
+
+... And a bunch more. Maybe. Tbh this is mostly for fun."""
+
+from typing import Callable, TypeVar
+
+
+T = TypeVar("T")
+
+
+def iife(c: Callable[[], T]) -> T:
+    """Call the function/class and assign the result to the name of the function/class."""
+    return c()
