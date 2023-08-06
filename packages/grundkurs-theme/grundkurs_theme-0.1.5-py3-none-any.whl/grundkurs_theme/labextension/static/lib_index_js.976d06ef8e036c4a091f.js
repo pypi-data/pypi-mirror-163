126dd9462a987fd0dde443a0abab9fca516a0982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:42 +0000

    Add package grundkurs-theme/0.1.5/grundkurs_theme-0.1.5-py3-none-any.whl

---
+++ packages/grundkurs-theme/grundkurs_theme-0.1.5-py3-none-any.whl/grundkurs_theme/labextension/static/lib_index_js.976d06ef8e036c4a091f.js
@@ -0,0 +1,41 @@
+"use strict";
+(self["webpackChunkgrundkurs_theme"] = self["webpackChunkgrundkurs_theme"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @jupyterlab/apputils */ "webpack/sharing/consume/default/@jupyterlab/apputils");
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__);
+
+/**
+ * Initialization data for the grundkurs_theme extension.
+ */
+const plugin = {
+    id: 'grundkurs_theme:plugin',
+    autoStart: true,
+    requires: [_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__.IThemeManager],
+    activate: (app, manager) => {
+        console.log('JupyterLab extension grundkurs_theme is activated!');
+        const style = 'grundkurs_theme/index.css';
+        manager.register({
+            name: 'grundkurs_theme',
+            isLight: true,
+            load: () => manager.loadCSS(style),
+            unload: () => Promise.resolve(undefined)
+        });
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.976d06ef8e036c4a091f.js.map
