334f77a41f0cc61415bbc4b83dedc42de790ffe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:28 +0000

    Add package minetext/0.5.2/minetext-0.5.2.tar.gz

---
+++ packages/minetext/minetext-0.5.2.tar.gz/minetext-0.5.2/minetext/__init__.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+from .domain.es_request import EsRequest
+from .domain.user_auth import UserAuth
+from .mine import Mine
+
+package_root_dir = Path(__file__).parent
+with open(os.path.join(package_root_dir, 'VERSION')) as version_file:
+    __version__ = version_file.read().strip()
+
+__all__ = [
+    'Mine',
+    'EsRequest',
+    'UserAuth'
+]
