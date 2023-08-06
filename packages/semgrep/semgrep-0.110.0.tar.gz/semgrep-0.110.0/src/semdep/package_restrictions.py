461552d61c950b425360a525659199b92f222b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:03 +0000

    Add package semgrep/0.110.0/semgrep-0.110.0.tar.gz

---
+++ packages/semgrep/semgrep-0.110.0.tar.gz/semgrep-0.110.0/src/semdep/package_restrictions.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from typing import Iterator
+from typing import List
+from typing import Tuple
+
+import packaging.version
+from packaging.specifiers import SpecifierSet
+
+from semdep.models import LockfileDependency
+from semdep.models import PackageManagers
+from semgrep.error import SemgrepError
+
+
+@dataclass(eq=True, order=True, frozen=True)
+class ProjectDependsOnEntry:
+    namespace: PackageManagers
+    package_name: str
+    semver_range: str
+
+
+def semver_matches(expression: str, actual_version: str) -> bool:
+
+    try:
+        ss = SpecifierSet(expression)
+        matched = len(list(ss.filter([actual_version]))) > 0
+        # print(f'does {expression} match {actual_version}?: {matched}')
+        return matched
+    except packaging.specifiers.InvalidSpecifier:
+        raise SemgrepError(
+            f"unknown package version comparison expression: {expression}"
+        )
+
+
+# compare vulnerable range to version in lockfile
+def dependencies_range_match_any(
+    search_for_ranges: List[ProjectDependsOnEntry],
+    have_deps: List[LockfileDependency],
+) -> Iterator[Tuple[ProjectDependsOnEntry, LockfileDependency]]:
+    for have_dep in have_deps:
+        for target_range in search_for_ranges:
+            if (
+                target_range.namespace.value.lower() == have_dep.namespace.value.lower()
+                and target_range.package_name == have_dep.name
+                and semver_matches(target_range.semver_range, have_dep.version)
+            ):
+                yield (target_range, have_dep)
