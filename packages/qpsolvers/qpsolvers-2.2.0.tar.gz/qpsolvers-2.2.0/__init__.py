880081d9e7c491355099efd3d86017226a365b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:40 +0000

    Add package qpsolvers/2.2.0/qpsolvers-2.2.0.tar.gz

---
+++ packages/qpsolvers/qpsolvers-2.2.0.tar.gz/qpsolvers-2.2.0/__init__.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2022 Stéphane Caron and the qpsolvers contributors.
+#
+# This file is part of qpsolvers.
+#
+# qpsolvers is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+This file allows the whole repository to act as a Python module when e.g.
+included as a git submodule inside a project. It incurs redundant bookkeeping
+but some users find it useful. (See 4b128db662cfedbe366b8387a59cd18e36c643a3
+and comments related to this commit on GitHub.)
+"""
+
+from .qpsolvers import __version__
+from .qpsolvers import available_solvers
+from .qpsolvers import cvxopt_solve_qp
+from .qpsolvers import cvxpy_solve_qp
+from .qpsolvers import dense_solvers
+from .qpsolvers import ecos_solve_qp
+from .qpsolvers import gurobi_solve_qp
+from .qpsolvers import mosek_solve_qp
+from .qpsolvers import osqp_solve_qp
+from .qpsolvers import print_matrix_vector
+from .qpsolvers import qpoases_solve_qp
+from .qpsolvers import qpswift_solve_qp
+from .qpsolvers import quadprog_solve_qp
+from .qpsolvers import scs_solve_qp
+from .qpsolvers import solve_ls
+from .qpsolvers import solve_qp
+from .qpsolvers import solve_safer_qp
+from .qpsolvers import sparse_solvers
+
+__all__ = [
+    "__version__",
+    "available_solvers",
+    "cvxopt_solve_qp",
+    "cvxpy_solve_qp",
+    "dense_solvers",
+    "ecos_solve_qp",
+    "gurobi_solve_qp",
+    "mosek_solve_qp",
+    "osqp_solve_qp",
+    "print_matrix_vector",
+    "qpoases_solve_qp",
+    "qpswift_solve_qp",
+    "quadprog_solve_qp",
+    "scs_solve_qp",
+    "solve_ls",
+    "solve_qp",
+    "solve_safer_qp",
+    "sparse_solvers",
+]
---
+++ packages/qpsolvers/qpsolvers-2.2.0.tar.gz/qpsolvers-2.2.0/tests/test_solve_ls.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2022 Stéphane Caron and the qpsolvers contributors.
+#
+# This file is part of qpsolvers.
+#
+# qpsolvers is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Tests for the main `solve_qp` function.
+"""
+
+import unittest
+import warnings
+
+from numpy import allclose, array, dot
+from numpy.linalg import norm
+from qpsolvers import available_solvers, solve_ls
+from qpsolvers.exceptions import NoSolverSelected, SolverNotFound
+
+
+class TestSolveLS(unittest.TestCase):
+
+    """
+    Test fixture for the README example problem.
+    """
+
+    def setUp(self):
+        """
+        Prepare test fixture.
+        """
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+        warnings.simplefilter("ignore", category=UserWarning)
+        self.R = array([[1.0, 2.0, 0.0], [2.0, 3.0, 4.0], [0.0, 4.0, 1.0]])
+        self.s = array([3.0, 2.0, 3.0])
+        self.G = array([[1.0, 2.0, 1.0], [2.0, 0.0, 1.0], [-1.0, 2.0, -1.0]])
+        self.h = array([3.0, 2.0, -2.0]).reshape((3,))
+        self.A = array([1.0, 1.0, 1.0])
+        self.b = array([1.0])
+
+    def get_problem(self):
+        """
+        Get problem as a sextuple of values to unpack.
+
+        Returns
+        -------
+        R :
+            Least-squares matrix.
+        s :
+            Least-squares vector.
+        G :
+            Linear inequality matrix.
+        h :
+            Linear inequality vector.
+        A :
+            Linear equality matrix.
+        b :
+            Linear equality vector.
+        """
+        return self.R, self.s, self.G, self.h, self.A, self.b
+
+    @staticmethod
+    def get_test(solver: str):
+        """
+        Get test function for a given solver.
+
+        Parameters
+        ----------
+        solver :
+            Name of the solver to test.
+
+        Returns
+        -------
+        :
+            Test function for that solver.
+        """
+
+        def test(self):
+            R, s, G, h, A, b = self.get_problem()
+            x = solve_ls(R, s, G, h, A, b, solver=solver)
+            x_sp = solve_ls(R, s, G, h, A, b, solver=solver, sym_proj=True)
+            self.assertIsNotNone(x)
+            self.assertIsNotNone(x_sp)
+            known_solution = array([2.0 / 3, -1.0 / 3, 2.0 / 3])
+            sol_tolerance = 1e-5 if solver == "ecos" else 1e-6
+            ineq_tolerance = 2e-7 if solver == "scs" else 1e-9
+            self.assertLess(norm(x - known_solution), sol_tolerance)
+            self.assertLess(norm(x_sp - known_solution), sol_tolerance)
+            self.assertLess(max(dot(G, x) - h), ineq_tolerance)
+            self.assertTrue(allclose(dot(A, x), b))
+
+        return test
+
+    def test_no_solver_selected(self):
+        """
+        Check that NoSolverSelected is raised when applicable.
+        """
+        R, s, G, h, A, b = self.get_problem()
+        with self.assertRaises(NoSolverSelected):
+            solve_ls(R, s, G, h, A, b, solver=None)
+
+    def test_solver_not_found(self):
+        """
+        Check that SolverNotFound is raised when the solver does not exist.
+        """
+        R, s, G, h, A, b = self.get_problem()
+        with self.assertRaises(SolverNotFound):
+            solve_ls(R, s, G, h, A, b, solver="ideal")
+
+
+# Generate test fixtures for each solver
+for solver in available_solvers:
+    setattr(
+        TestSolveLS, "test_{}".format(solver), TestSolveLS.get_test(solver)
+    )
+
+
+if __name__ == "__main__":
+    unittest.main()
