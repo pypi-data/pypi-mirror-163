097891c36fb001712065c0107c739756845b0bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:37 +0000

    Add package Odoo-API-Library/1.0.1.2/Odoo_API_Library-1.0.1.2.tar.gz

---
+++ packages/Odoo-API-Library/Odoo_API_Library-1.0.1.2.tar.gz/Odoo_API_Library-1.0.1.2/Odoo_API_Library/firebase.py
@@ -0,0 +1,24 @@
+from requests.structures import CaseInsensitiveDict
+from requests import post
+
+def send_notifications(sender,receiver,title,body):
+        url = "https://fcm.googleapis.com/fcm/send"
+
+        headers = CaseInsensitiveDict()
+        headers["Accept"] = "application/json"
+        headers[
+            "Authorization"] = "key=AAAAu_exkBc:APA91bGmfBXXUMRHaGVdhNGlmazAQ8WrjvJAFMRhJTD3Lo-61avhPS0EKxUokq08hW0k-w2Gi-mGrUL-C7qr6YdQihqLSMY_xB_W9L9g0w6LjkOg9HptxIKzRuEPdWtmCsCGYIAxQUh6"
+        headers["Content-Type"] = "application/json"
+        to = "\"" + str(receiver.user_id.noti_token) + "\""
+        data = """
+                    {
+                        "to": """ + to + """,
+                        "notification": {
+                            "title": """+title+""",
+                            "body":""" + body + """
+                        }
+                    }
+                    """
+
+        resp = post(url, headers=headers, data=data)
+        return resp
