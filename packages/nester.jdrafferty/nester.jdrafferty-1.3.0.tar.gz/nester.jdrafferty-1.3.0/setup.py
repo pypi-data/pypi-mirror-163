c578abbf05541db08550c6cafba6af4796f2517c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:34 +0000

    Add package nester.jdrafferty/1.3.0/nester.jdrafferty-1.3.0.tar.gz

---
+++ packages/nester.jdrafferty/nester.jdrafferty-1.3.0.tar.gz/nester.jdrafferty-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester.jdrafferty',
+    version = '1.3.0',
+    py_modules = ['nester'],
+    author = 'jdrafferty',
+    description = 'A simple printer of nested lists',
+
+    )
+    
