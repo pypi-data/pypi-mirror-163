7c2f1131d67c3f4375889c70232aa063c9ec1bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:24 +0000

    Add package fastapi-stack-utils/0.2.3/fastapi-stack-utils-0.2.3.tar.gz

---
+++ packages/fastapi-stack-utils/fastapi-stack-utils-0.2.3.tar.gz/fastapi-stack-utils-0.2.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastapi_stack_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['asgi-correlation-id>=3.0.0,<4.0.0', 'fastapi>0.68.0']
+
+setup_kwargs = {
+    'name': 'fastapi-stack-utils',
+    'version': '0.2.3',
+    'description': 'Utils to extend the FastAPI with logging and exception handlers',
+    'long_description': None,
+    'author': 'Jonas Krüger Svensson',
+    'author_email': 'jonas-ks@hotmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
