e2d6ca7bb576c25c06ac70753015b58e1fb4104d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:18 +0000

    Add package dist-kanishka/0.4/dist_kanishka-0.4.tar.gz

---
+++ packages/dist-kanishka/dist_kanishka-0.4.tar.gz/dist_kanishka-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dist_kanishka',
+      version='0.4',
+      description='A package to get the mean and standard deviation of Gaussian and Binomial distributions and their combinations',
+      readme = "README.md",
+      license = "LICENSE",
+      packages=['dist_kanishka'],
+      author = 'Kanishka Dandeniya',
+      author_email = 'kanishka.d@eng.pdn.ac.lk',
+      zip_safe=False)
