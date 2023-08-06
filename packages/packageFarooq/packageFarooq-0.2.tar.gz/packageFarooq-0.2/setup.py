267743161f7639db17363ab257f53a17cd4bcdec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:39 +0000

    Add package packageFarooq/0.2/packageFarooq-0.2.tar.gz

---
+++ packages/packageFarooq/packageFarooq-0.2.tar.gz/packageFarooq-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="packageFarooq"
+      ,version="0.2",
+      description="This is Farooq package",
+      long_description="This is a very very Long Description",
+      author="Farooq",
+      packages=['packageFarooq'],
+      install_requires=[]
+      )
