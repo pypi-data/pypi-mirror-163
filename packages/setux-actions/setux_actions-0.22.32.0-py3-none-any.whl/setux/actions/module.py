4ec91b80d88899d3737eea40c9d4162eb03749cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:52 +0000

    Add package setux-actions/0.22.32.0/setux_actions-0.22.32.0-py3-none-any.whl

---
+++ packages/setux-actions/setux_actions-0.22.32.0-py3-none-any.whl/setux/actions/module.py
@@ -0,0 +1,24 @@
+from setux.core.action import Runner, Actions
+
+
+class Moduler(Runner):
+    @property
+    def label(self):
+        return self.module
+
+    def deploy(self):
+        return self.target.deploy(self.module)
+
+
+class Modules(Actions):
+    @property
+    def label(self):
+        return f'Modules {self.name}'
+
+    @property
+    def actions(self):
+        return [
+            Moduler(self.target, module=module)
+            for module in self.modules
+        ]
+
