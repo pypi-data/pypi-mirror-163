68371ce914af0ec24cf6e4ec4b5ecc4254b8d5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:46 +0000

    Add package pyDataViz/0.0.1/pyDataViz-0.0.1.tar.gz

---
+++ packages/pyDataViz/pyDataViz-0.0.1.tar.gz/pyDataViz-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name = 'pyDataViz',
+  version = '0.0.1',
+  description = 'visualizaing data by Amir H.',
+  py_modules = ['histogram'],
+  package_dir = {'': 'src'},
+)
+
+# name what what you will use for pip install
