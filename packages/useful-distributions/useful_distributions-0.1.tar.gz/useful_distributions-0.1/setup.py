2c34fbbe8e52b31865b2a898aba9098a2940b489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:27 +0000

    Add package useful-distributions/0.1/useful_distributions-0.1.tar.gz

---
+++ packages/useful-distributions/useful_distributions-0.1.tar.gz/useful_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='useful_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['useful_distributions'],
+      author=['Kianna Hendricks'],
+      zip_safe=False)
