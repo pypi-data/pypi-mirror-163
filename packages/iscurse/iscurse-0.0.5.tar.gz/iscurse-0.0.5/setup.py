678b8664c5a464e74590ff9c40dc385c8147482f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:35 +0000

    Add package iscurse/0.0.5/iscurse-0.0.5.tar.gz

---
+++ packages/iscurse/iscurse-0.0.5.tar.gz/iscurse-0.0.5/setup.py
@@ -0,0 +1,35 @@
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.5'
+DESCRIPTION = 'short sentiment analysis'
+LONG_DESCRIPTION = 'A package that allows to simply analyse sentiments based on text inputs using AFINN.'
+
+# Setting up
+setup(
+    name="iscurse",
+    version=VERSION,
+    author="Daniel Yudi",
+    author_email="<danielyudicarvalho@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['curse', 'sentiment', 'analysis'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
