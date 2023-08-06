bb25785927cddd1704adf454c7ae45a4ae8742e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:18 +0000

    Add package Pxucz/0.0.1.3/Pxucz-0.0.1.3-py3-none-any.whl

---
+++ packages/Pxucz/Pxucz-0.0.1.3-py3-none-any.whl/Pxucz/initial/init.py
@@ -0,0 +1,12 @@
+from Pxucz.initial import set_variables
+from Pxucz.utils import global_path
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from Pxucz.initial import opener as px_opener
+
+
+def init(abspath: str, loader_size_x: int, loader_size_y: int):
+    print("ss")
+    global_path.set_abs_path(path=abspath)
+    set_variables.setvar()
+    px_opener.start(size_x=loader_size_x, size_y=loader_size_y)
---
+++ packages/Pxucz/Pxucz-0.0.1.3-py3-none-any.whl/Pxucz/initial/set_variables.py
@@ -0,0 +1,15 @@
+import glfw
+from Pxucz.utils import global_variables
+
+BENCHMARK_PREVIOUSTIME = "PXUCZ_BENCHMARK_FPS_PREVIOUS_TIME_VAR"
+BENCHMARK_FRAMECOUNT = "PXUCZ_BENCHMARK_FPS_COUNT_ER_VAR"
+GRAPHICS_FPS_LIMITER = "PXUCZ_FPS_RATE_LIMITER_TIME_VAR"
+INITIAL_LOADER_TEXT = "PXUCZ_INITIAL_LOADER_LABEL_TEXT_VAR"
+INITIAL_LOADER_CLOSE = "PXUCZ_LOADER_CLOSE_VAR"
+
+
+def setvar():
+    global_variables.set_var(name=GRAPHICS_FPS_LIMITER, value=glfw.get_time())
+    global_variables.set_var(name=BENCHMARK_FRAMECOUNT, value=0)
+    global_variables.set_var(name=BENCHMARK_PREVIOUSTIME, value=glfw.get_time())
+    global_variables.set_var(name=INITIAL_LOADER_TEXT, value="NULL")
