4c8551e12d15609327268eab58c1d2c08d35dab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:37 +0000

    Add package gadgit/0.6.0/gadgit-0.6.0.tar.gz

---
+++ packages/gadgit/gadgit-0.6.0.tar.gz/gadgit-0.6.0/gadgit/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for GADGIT."""
+
+__author__ = """Tyler Collins"""
+__email__ = 'tk11br@sharcnet.ca'
+__version__ = '0.6.0'
+
+from .GAInfo import GAInfo
+from .GeneInfo import GeneInfo
+from .gadgit import ga_single
+from .gadgit import ga_multi
+from .post_run import post_run
