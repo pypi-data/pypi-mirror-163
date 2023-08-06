32c31def3aa5f1feb78dbebc66abd3c8d47114d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:55 +0000

    Add package PyMPDATA/1.0.4/PyMPDATA-1.0.4.tar.gz

---
+++ packages/PyMPDATA/PyMPDATA-1.0.4.tar.gz/PyMPDATA-1.0.4/PyMPDATA/__init__.py
@@ -0,0 +1,23 @@
+"""
+Numba-accelerated Pythonic implementation of Multidimensional Positive Definite
+Advection Transport Algorithm (MPDATA) with examples in Python, Julia and Matlab
+
+PyMPDATA uses staggered grid with the following node placement for
+`PyMPDATA.scalar_field.ScalarField` and
+`PyMPDATA.vector_field.VectorField` elements:
+![](https://github.com/atmos-cloud-sim-uj/PyMPDATA/releases/download/tip/readme_grid.png)
+"""
+# pylint: disable=invalid-name
+from pkg_resources import get_distribution, DistributionNotFound, VersionConflict
+
+from .scalar_field import ScalarField
+from .vector_field import VectorField
+from .options import Options
+from .stepper import Stepper
+from .solver import Solver
+
+try:
+    __version__ = get_distribution(__name__).version
+except (DistributionNotFound, VersionConflict):
+    # package is not installed
+    pass
