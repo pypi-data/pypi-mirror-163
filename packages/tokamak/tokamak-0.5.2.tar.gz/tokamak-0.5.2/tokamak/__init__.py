e4e4707d9d85d9b7fb258e63f8353e30eb80ccf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:09 +0000

    Add package tokamak/0.5.2/tokamak-0.5.2.tar.gz

---
+++ packages/tokamak/tokamak-0.5.2.tar.gz/tokamak-0.5.2/tokamak/__init__.py
@@ -0,0 +1,12 @@
+"""
+Tokamak offers Radix Tree path routing and an experimental web framework.
+
+Module Organization:
+
+- ASGIRouter definition: [`tokamak.router`](/reference/router)
+- Radix Tree implementation: [`tokamak.radix_tree`](reference/radix_tree/__init__/)
+- Experimental Web Framework: [`tokamak.web`](/reference/web/__init__/)
+"""
+
+__version__ = "0.5.2"
+from .router import AsgiRouter, Route
