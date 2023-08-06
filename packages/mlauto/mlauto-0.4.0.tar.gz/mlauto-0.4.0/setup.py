f2ad65001d5a77d45797d8cdbe952c903b35b348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:48 +0000

    Add package mlauto/0.4.0/mlauto-0.4.0.tar.gz

---
+++ packages/mlauto/mlauto-0.4.0.tar.gz/mlauto-0.4.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='mlauto',
+    version='0.4.0',    
+    url='https://github.com/ansariminhaj/mlauto_pip',
+    author='Minhaj Ansari',    
+    license='MIT',
+    packages=['mlauto'],
+    install_requires=[
+                      'numpy',
+                      ],
+
+    classifiers=[
+        'Intended Audience :: Science/Research',      
+        'Programming Language :: Python :: 3',
+    ],
+)
