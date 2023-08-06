2e2249583e4807f905addd499465f98dadd2f2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:43 +0000

    Add package CabFareByKD/0.0.2/CabFareByKD-0.0.2.tar.gz

---
+++ packages/CabFareByKD/CabFareByKD-0.0.2.tar.gz/CabFareByKD-0.0.2/README.txt
@@ -0,0 +1,7 @@
+This is a cab fare calculator that takes four arguments and return the prospective fare.
+
+The four arguments are as follows:
+- City name (string e.g., 'San Francisco')
+- Journey starting time (string, e.g. '13:40:07')
+- Journey starting date (string, e.g. '2017-11-14')
+- Journey duration in hours (int/float e.g., 4/4.5 )
---
+++ packages/CabFareByKD/CabFareByKD-0.0.2.tar.gz/CabFareByKD-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='CabFareByKD',
+  version='0.0.2',
+  description='Cab Fare Calculator',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description_content_type="text/x-rst",
+  url='',  
+  author='Kamal Dobriyal',
+  author_email='kamal.dobriyal.kd@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calculator', 
+  packages=find_packages(),
+  install_requires=['pandas','numpy','datetime','geopy','sklearn'] 
+)
