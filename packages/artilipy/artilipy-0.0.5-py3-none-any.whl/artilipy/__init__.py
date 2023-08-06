dbe79cdb355f4eef1f79aa70354ebf97d339f756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:50 +0000

    Add package artilipy/0.0.5/artilipy-0.0.5-py3-none-any.whl

---
+++ packages/artilipy/artilipy-0.0.5-py3-none-any.whl/artilipy/__init__.py
@@ -0,0 +1,4 @@
+__doc__ = 'Library with Python API wrapper for Artilities.' \
+          'Modules:\n' \
+          '- client (Base module for performing API interactions)\n' \
+          '- exceptions (Module packed with custom exceptions)\n'
