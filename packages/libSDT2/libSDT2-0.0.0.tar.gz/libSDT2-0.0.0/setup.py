d13ecb3b5010e7bc9c9af01e76e4f9a4ea8ee0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:10 +0000

    Add package libSDT2/0.0.0/libSDT2-0.0.0.tar.gz

---
+++ packages/libSDT2/libSDT2-0.0.0.tar.gz/libSDT2-0.0.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+pkg_name = 'libSDT2'
+version = '0.0.0'
+
+setuptools.setup(
+    name=pkg_name,
+    version=version,
+    description='Local packages for development',
+
+    author='hypothesisbase',
+    author_email="support@hypothesisbase.com",
+    url= f'http://pypi.python.org/pypi/{pkg_name}/{version}',
+    license="MIT",
+
+    install_requires=[
+        "bs4",
+        "leveldb",
+        "selenium",
+        "requests"
+    ],
+    packages=setuptools.find_packages(),
+    zip_safe=False
+)
