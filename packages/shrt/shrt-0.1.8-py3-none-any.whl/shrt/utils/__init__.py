d0786f7713f673054507b8117970fdfe198c3661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:52 +0000

    Add package shrt/0.1.8/shrt-0.1.8-py3-none-any.whl

---
+++ packages/shrt/shrt-0.1.8-py3-none-any.whl/shrt/utils/__init__.py
@@ -0,0 +1,8 @@
+from .strings import (
+    random_path,
+)
+from .cache import (
+    redis_init,
+    get_cache,
+    set_cache,
+)
---
+++ packages/shrt/shrt-0.1.8-py3-none-any.whl/shrt/web.py
@@ -0,0 +1,20 @@
+from .app import app, settings
+from .database import database, init_engine
+from .utils import redis_init
+from .views import status, redirect
+
+app.include_router(status.router, prefix='/status', include_in_schema=False)
+app.include_router(redirect.router, prefix='', tags=['redirect'], include_in_schema=True)
+
+
+@app.on_event("startup")
+async def startup():
+    init_engine()
+    await database.connect()
+    if settings.use_cache:
+        redis_init(url=settings.redis_url)
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
