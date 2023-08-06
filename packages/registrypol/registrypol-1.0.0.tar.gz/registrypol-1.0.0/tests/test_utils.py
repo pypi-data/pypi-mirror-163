22f4e252c8fc69c88af546a496f46b64bec216db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:23 +0000

    Add package registrypol/1.0.0/registrypol-1.0.0.tar.gz

---
+++ packages/registrypol/registrypol-1.0.0.tar.gz/registrypol-1.0.0/tests/test_utils.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from registrypol.policy import RegistryPolicy
+from registrypol.utils import dump, load
+
+
+class TestUtils(TestCase):
+    def test_dump(self):
+        pass
+
+    def test_load(self):
+        pass
