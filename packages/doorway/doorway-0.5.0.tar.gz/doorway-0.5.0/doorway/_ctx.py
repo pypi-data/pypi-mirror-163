19c2ed46360ed7c28359a676939652ff6483a2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:19 +0000

    Add package doorway/0.5.0/doorway-0.5.0.tar.gz

---
+++ packages/doorway/doorway-0.5.0.tar.gz/doorway-0.5.0/doorway/_ctx.py
@@ -0,0 +1,152 @@
+
+#  ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+#  MIT License
+#
+#  Copyright (c) 2022 Nathan Juraj Michlo
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+#  ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+
+import os
+import sys
+import contextlib
+from typing import Any
+from typing import Dict
+
+
+# ========================================================================= #
+# context managers -- io streams                                            #
+# ========================================================================= #
+
+
+@contextlib.contextmanager
+def ctx_no_stdout():
+    old_stdout = sys.stdout
+    sys.stdout = open(os.devnull, 'w')
+    try:
+        yield
+    finally:
+        sys.stdout = old_stdout
+
+
+@contextlib.contextmanager
+def ctx_no_stderr():
+    old_stderr = sys.stderr
+    sys.stderr = open(os.devnull, 'w')
+    try:
+        yield
+    finally:
+        sys.stderr = old_stderr
+
+
+# ========================================================================= #
+# context managers -- attr                                                  #
+# ========================================================================= #
+
+
+_DELETE = object()
+
+
+@contextlib.contextmanager
+def ctx_temp_attr(obj, name, value):
+    # if we should delete this or just reset it
+    prev_val = getattr(obj, name, _DELETE)
+    # overwrite the value
+    setattr(obj, name, value)
+    # yield the context
+    try:
+        yield obj
+    finally:
+        # restore the original attr
+        if prev_val is _DELETE:
+            delattr(obj, name)
+        else:
+            setattr(obj, name, prev_val)
+
+
+# ========================================================================= #
+# context managers -- working dir, args, env                                #
+# ========================================================================= #
+
+
+@contextlib.contextmanager
+def ctx_temp_wd(new_wd):
+    old_wd = os.getcwd()
+    os.chdir(new_wd)
+    try:
+        yield
+    finally:
+        os.chdir(old_wd)
+
+
+@contextlib.contextmanager
+def ctx_temp_sys_args(new_argv):
+    old_argv = sys.argv
+    sys.argv = new_argv
+    try:
+        yield
+    finally:
+        sys.argv = old_argv
+
+
+@contextlib.contextmanager
+def ctx_temp_environ(environment: Dict[str, Any] = None, **env_kwargs):
+    # combine the kwargs and the environment dict
+    if environment is None:
+        environment = {}
+    if env_kwargs:
+        assert environment.keys().isdisjoint(env_kwargs.keys())
+        environment.update(env_kwargs)
+    # save the old environment
+    old_env = {}
+    for k in environment:
+        if k in os.environ:
+            old_env[k] = os.environ[k]
+    # update the environment
+    os.environ.update(environment)
+    # run the context
+    try:
+        yield
+    finally:
+        # restore the original environment
+        for k in environment:
+            if k in old_env:
+                os.environ[k] = old_env[k]
+            else:
+                del os.environ[k]
+
+
+# ========================================================================= #
+# export                                                                    #
+# ========================================================================= #
+
+
+__all__ = (
+    'ctx_no_stdout',
+    'ctx_no_stderr',
+    'ctx_temp_attr',
+    'ctx_temp_wd',
+    'ctx_temp_sys_args',
+    'ctx_temp_environ',
+)
+
+
+# ========================================================================= #
+# END                                                                       #
+# ========================================================================= #
---
+++ packages/doorway/doorway-0.5.0.tar.gz/doorway-0.5.0/doorway/_shard.py
@@ -0,0 +1,149 @@
+import os
+from pathlib import Path
+from typing import Callable
+from typing import Iterable
+from typing import Optional
+from typing import Union
+from typing import TypeVar
+
+from doorway._hash import Hash
+from doorway._hash import HashAlgo
+from doorway._hash import hash_str
+
+
+# ========================================================================= #
+# individual shards                                                         #
+# ========================================================================= #
+
+
+T = TypeVar('T')
+
+_SHARD_KEYS = {
+    'basename': os.path.basename,
+    'abspath': os.path.abspath,
+    'input': lambda x: x,
+}
+
+ShardKey = Optional[Union[str, Callable[[T], str]]]
+
+
+def shard_hash(
+    value: T,
+    shard_key: ShardKey[T] = None,
+    hash_algo: Optional[HashAlgo] = None,
+) -> Hash:
+    # get the hash data function
+    if shard_key is None:
+        pass
+    elif callable(shard_key):
+        value = shard_key(value)
+    elif isinstance(shard_key, str):
+        fn = _SHARD_KEYS.get(shard_key, None)  # cannot be None here
+        if fn is None:
+            raise KeyError(f'if shard_key is a str, it must be one of: {list(_SHARD_KEYS.keys())}, got: {repr(shard_key)}')
+        value = fn(value)
+    else:
+        raise ValueError(f'shard_key must be a str, callable or None, got: {repr(shard_key)}')
+    # get the string
+    assert isinstance(value, (str, Path)), f'The value after shard_key is applied must be a str or Path, instead got type: {type(value)}, with value: {repr(value)}'
+    # compute the hash
+    return hash_str(str(value), hash_algo=hash_algo)
+
+
+def shard_idx(
+    value: T,
+    num_shards: int,
+    shard_key: ShardKey[T] = None,
+    hash_algo: Optional[HashAlgo] = None,
+) -> int:
+    assert isinstance(num_shards, int) and (num_shards > 0), f'num_shards must be an integer that is > 0, got: {repr(num_shards)}'
+    # compute the hash for the value
+    hash = shard_hash(value, shard_key=shard_key, hash_algo=hash_algo)
+    # convert hashes to integers, and assign to correct split
+    return int(hash, 16) % num_shards
+
+
+# ========================================================================= #
+# multiples shards                                                          #
+# ========================================================================= #
+
+
+_SHARD_RETURNS = {
+    'pairs':   lambda i, value: (i, value),
+    'indices': lambda i, value: i,
+    'values':  lambda i, value: value,
+}
+
+
+def sharded(
+    values: Iterable[T],
+    num_shards: int,
+    shard_key: ShardKey[T] = None,
+    hash_algo: Optional[HashAlgo] = None,
+    returns: str = 'values'
+) -> list:
+    """
+    Shard files based on their hashes instead of random seeds
+    """
+    # shard functions
+    if returns not in _SHARD_RETURNS:
+        raise KeyError(f'invalid shards returns: {repr(returns)}, must be one of: {sorted(_SHARD_RETURNS.keys())}')
+    value_getter = _SHARD_RETURNS[returns]
+    # create new array of shards
+    shards = [[] for _ in range(num_shards)]
+    # assign paths to shards
+    for i, value in enumerate(values):
+        idx = shard_idx(value, num_shards, shard_key=shard_key, hash_algo=hash_algo)
+        shards[idx].append(value_getter(i, value))
+    # results
+    return shards
+
+
+def sharded_and_grouped(
+    values: Iterable[T],
+    group_sizes: Iterable[int],
+    shard_key: ShardKey[T] = None,
+    hash_algo: Optional[HashAlgo] = None,
+    returns: str = 'values'
+) -> list:
+    """
+    Shard files based on their hashes instead of random seeds
+    -- This is useful if you need to split a dataset, but you expect changes to be made to it,
+       eg. files will always randomly be assigned to the same shared
+    """
+    group_sizes = list(group_sizes)
+    # checks
+    assert all(isinstance(size, int) and (size >= 0) for size in group_sizes), f'values of group_sizes must be integers that are >= 0, got: {repr(group_sizes)}'
+    # get all the shards
+    shards = sharded(
+        values=values,
+        num_shards=sum(group_sizes),
+        shard_key=shard_key,
+        hash_algo=hash_algo,
+        returns=returns,
+    )
+    # group all the shards together
+    splits, i = [], 0
+    for size in group_sizes:
+        splits.append([path for shard in shards[i:i+size] for path in shard])
+        i += size
+    # done!
+    return splits
+
+
+# ========================================================================= #
+# export                                                                    #
+# ========================================================================= #
+
+
+__all__ = (
+    'shard_hash',
+    'shard_idx',
+    'sharded',
+    'sharded_and_grouped',
+)
+
+
+# ========================================================================= #
+# END                                                                       #
+# ========================================================================= #
