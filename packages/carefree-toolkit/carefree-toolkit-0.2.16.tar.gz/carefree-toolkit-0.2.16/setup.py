b07af4badb1ef4bfe839da2c6a77a7dce67c4d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:26 +0000

    Add package carefree-toolkit/0.2.16/carefree-toolkit-0.2.16.tar.gz

---
+++ packages/carefree-toolkit/carefree-toolkit-0.2.16.tar.gz/carefree-toolkit-0.2.16/setup.py
@@ -0,0 +1,37 @@
+import platform
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+VERSION = "0.2.16"
+
+DESCRIPTION = "Some commonly used functions and modules"
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+INSTALL_REQUIRES = [
+    "dill",
+    "tqdm",
+    "future",
+    "psutil",
+    "pathos",
+    "numpy>=1.22.3",
+]
+if platform.system() != "Windows":
+    INSTALL_REQUIRES.append("SharedArray")
+
+setup(
+    name="carefree-toolkit",
+    version=VERSION,
+    packages=find_packages(exclude=("tests",)),
+    install_requires=INSTALL_REQUIRES,
+    author="carefree0910",
+    author_email="syameimaru.saki@gmail.com",
+    url="https://github.com/carefree0910/carefree-toolkit",
+    download_url=f"https://github.com/carefree0910/carefree-toolkit/archive/v{VERSION}.tar.gz",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    keywords="python numpy data-science",
+)
