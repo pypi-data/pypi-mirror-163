4f00d92cad254df4e07d651d5ea08a687c42bc51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:20 +0000

    Add package Subsearch/2.15.46/subsearch-2.15.46.tar.gz

---
+++ packages/Subsearch/subsearch-2.15.46.tar.gz/subsearch-2.15.46/src/subsearch/subsearch.py
@@ -0,0 +1,92 @@
+import ctypes
+import os
+import time
+
+from data import __version__, __video_directory__, __video_name__, __video_path__
+from gui import widget_download, widget_settings
+from scraper import opensubtitles, subscene
+from utils import current_user, file_manager, file_parser, log, raw_config, raw_registry
+
+
+class SubSearch:
+    def __init__(self) -> None:
+        self.start = time.perf_counter()
+        ctypes.windll.kernel32.SetConsoleTitleW(f"SubSearch - {__version__}")
+        if current_user.got_key() is False:
+            raw_config.set_default_json()
+            raw_registry.add_context_menu()
+        self.show_terminal = False if current_user.check_is_exe() else raw_config.get("show_terminal")
+        self.file_exist = True if __video_name__ is not None else False
+        self.lang, self.lang_abbr = raw_config.get("language")
+        self.hi = raw_config.get("hearing_impaired")
+        self.pct = raw_config.get("percentage")
+        self.show_dl_window = raw_config.get("show_download_window")
+        if self.file_exist:
+            self.file_hash = file_manager.get_hash(__video_path__)
+            self.param = file_parser.get_parameters(__video_name__, self.file_hash, self.lang_abbr)
+            log.parameters(self.param, self.lang, self.lang_abbr, self.hi, self.pct)
+            if " " in __video_name__:
+                log.output("[Warning: Filename contains spaces]")
+        if self.file_exist is False:
+            widget_settings.show_widget()
+
+    def opensubtitles_scrape(self):
+        if self.file_exist:
+            log.output("")
+            log.output("[Searching on opensubtitles]")
+            self.scrape_opensubtitles = opensubtitles.scrape(self.param, self.lang, self.hi)
+
+    def subscene_scrape(self):
+        if self.file_exist:
+            log.output("")
+            log.output("[Searching on subscene]")
+            self.scrape_subscene = subscene.scrape(
+                self.param, self.lang, self.lang_abbr, self.hi, self.pct, self.show_dl_window
+            )
+
+    def procsess_files(self):
+        if self.file_exist:
+            if self.scrape_opensubtitles is not None:
+                log.output("")
+                log.output("[Downloading from Opensubtitles]")
+                for item in self.scrape_opensubtitles:
+                    file_manager.download_zip(item)
+
+            if self.scrape_subscene is not None:
+                log.output("")
+                log.output("[Downloading from Subscene]")
+                for item in self.scrape_subscene:
+                    file_manager.download_zip(item)
+
+            if self.scrape_opensubtitles is None and self.scrape_subscene is None:
+                dl_data = f"{__video_directory__}\\__subsearch__dl_data.tmp"
+                if self.show_dl_window is True and os.path.exists(dl_data):
+                    widget_download.show_widget()
+                    file_manager.clean_up(__video_directory__, dl_data)
+
+            log.output("")
+            log.output("[Procsessing files]")
+            file_manager.extract_zips(__video_directory__, ".zip")
+            file_manager.clean_up(__video_directory__, ".zip")
+            file_manager.clean_up(__video_directory__, ").nfo")
+            file_manager.rename_best_match(f"{self.param.release}.srt", __video_directory__, ".srt")
+
+    def _exit_(self):
+        elapsed = time.perf_counter() - self.start
+        log.output("")
+        log.output(f"Finished in {elapsed} seconds")
+
+        if self.show_terminal and current_user.check_is_exe() is False:
+            input("Ctrl + c or Enter to exit")
+
+
+def main():
+    ss = SubSearch()
+    ss.opensubtitles_scrape()
+    ss.subscene_scrape()
+    ss.procsess_files()
+    ss._exit_()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/Subsearch/subsearch-2.15.46.tar.gz/subsearch-2.15.46/src/subsearch/utils/file_parser.py
@@ -0,0 +1,117 @@
+from dataclasses import dataclass
+
+from num2words import num2words
+
+
+@dataclass
+class SearchParameters:
+    url_subscene: str
+    url_opensubtitles: str
+    title: str
+    year: int
+    season: str
+    season_ordinal: str
+    episode: str
+    episode_ordinal: str
+    tv_series: bool
+    release: str
+    group: str
+    file_hash: str
+
+
+def split_last_hyphen(string: str) -> str:
+    """
+    Split last hyphen in a string, keep last part of the string
+
+    Args:
+        string (str): any string with a hyphen
+
+    Returns:
+        str: last item in the list of the string split by hyphen
+    """
+    group = string.rsplit("-", 1)[-1]
+    return group
+
+
+def get_parameters(file_name: str, file_hash: str, lang_abbr: str) -> SearchParameters:
+    """
+    Parse file name and get parameters for searching on subscene and opensubtitles
+
+    Args:
+        file_name (str): name of the file
+        file_hash (str): hash of the file
+        lang_abbr (str): language abbreviation for ordinal numbers
+
+    Returns:
+        SearchParameters: title, year, season, season_ordinal, episode, episode_ordinal, tv_series, release, group
+    """
+    # default values
+    title = "N/A"
+    year = "N/A"
+    season = "N/A"
+    season_ordinal = "N/A"
+    episode = "N/A"
+    episode_ordinal = "N/A"
+    group = "N/A"
+    tv_series = False
+    year_found = False
+
+    if " " in file_name:
+        release = file_name.replace(" ", ".")
+    else:
+        release = file_name
+    # find season, episode, make it ordinal and set tv_series to true if it is a tv-series
+    for item in release.lower().split("."):
+        if item.startswith("s") and item[-1].isdigit() and "e" in item:
+            season, episode = item.replace("s", "").replace("e", " ").split(" ")
+            season_ordinal = num2words(int(season), lang=lang_abbr, to="ordinal")
+            episode_ordinal = num2words(int(episode), lang=lang_abbr, to="ordinal")
+            if season[-1].isdigit():
+                tv_series = True
+                break
+
+    string_list = release.lower().split(".")
+    subtract = []
+    # list is reversed if a year is apart of the title, like "2001: A Foo Bar" from 1991
+    # this is to prevent the year from the title being used as the release year of the movie/show
+    for item in string_list[::-1]:
+        # if year is found everything else in the list should be the release name
+        if year_found or item.startswith("s") and item[-1].isdigit():
+            _title = (i for i in string_list if i not in subtract)
+            break
+        if item.isdigit() and len(item) == 4:
+            year_found = True
+            year = item
+        subtract.append(item)
+
+    if tv_series:
+        title = " ".join(x for x in _title).replace(f"s{season}e{episode}", f"- {season_ordinal} season").strip()
+    else:
+        try:
+            title = " ".join(x for x in _title)
+        except UnboundLocalError:
+            title = file_name
+
+    if "-" in release:
+        group = split_last_hyphen(release)
+
+    subscene = "https://subscene.com/subtitles/searchbytitle?query="
+    opensubtitles = "https://www.opensubtitles.org/en/search/sublanguageid-eng/moviename-"
+    url_subscene = f"{subscene}{title}".replace(" ", "%20")
+    url_opensubtitles = f"{opensubtitles}{file_hash}"
+
+    parameters = {
+        "url_subscene": url_subscene,
+        "url_opensubtitles": url_opensubtitles,
+        "title": title,
+        "year": year,
+        "season": season,
+        "season_ordinal": season_ordinal,
+        "episode": episode,
+        "episode_ordinal": episode_ordinal,
+        "tv_series": tv_series,
+        "release": release,
+        "group": group,
+        "file_hash": file_hash,
+    }
+    return SearchParameters(**parameters)
