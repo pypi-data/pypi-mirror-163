5b48f189a56e0a184b521c4fa732b7cb6f2f061b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:37 +0000

    Add package lhub-cli/0.1.0.2/lhub_cli-0.1.0.2.tar.gz

---
+++ packages/lhub-cli/lhub_cli-0.1.0.2.tar.gz/lhub_cli-0.1.0.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0.2'
+
+setup(
+    name="lhub_cli",
+    version=VERSION,
+    author="Chad Roberts",
+    author_email="chad@logichub.com",
+    description="LogicHub CLI",
+    long_description="A Python package for interacting with LogicHub via shell commands",
+    packages=find_packages(),
+    install_requires=[
+        "lhub ~= 0.2.4",
+        "configobj >= 5.0.6",
+        "dataclasses_json >= 0.5.6",
+        "rsa >= 4.8",
+        "requests",
+        "tabulate",
+        "colorama",
+        "progressbar2",  # Only used in sample scripts for now, and imported not as "progressbar2" but just "progressbar"
+    ],
+    keywords=["python", "lhub", "LogicHub", "api", "security", "cli"],
+
+    url="https://github.com/deathbywedgie/lhub_cli",
+    project_urls={
+        "Bug Tracker": "https://github.com/deathbywedgie/lhub_cli/issues",
+    },
+
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python :: 3.10",
+        "Natural Language :: English",
+    ]
+)
