61081a8d5eadf34f27428e7f2adf144f71b0b37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:41 +0000

    Add package gauss-n-binomial-dist/0.1/gauss_n_binomial_dist-0.1.tar.gz

---
+++ packages/gauss-n-binomial-dist/gauss_n_binomial_dist-0.1.tar.gz/gauss_n_binomial_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss_n_binomial_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss_n_binomial_dist'],
+      author = "Jamal Uddin Mallick",
+      author_email = "jamaluddin10775@gmail.com",
+      zip_safe=False)
