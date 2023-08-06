9b99eff49995b6409a6f65866bc69c7b04778335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:36 +0000

    Add package pacc/0.0.391/pacc-0.0.391-py3-none-any.whl

---
+++ packages/pacc/pacc-0.0.391-py3-none-any.whl/pacc/project/ksjsb/mainloop.py
@@ -0,0 +1,11 @@
+from ...base import run_forever
+
+
+# pylint: disable=too-few-public-methods
+class KsjsbMainloop:
+    """快手极速版主循环类"""
+
+    @classmethod
+    @run_forever
+    def run(cls):
+        """开始运行"""
