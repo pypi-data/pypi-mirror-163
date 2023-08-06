1b137529897a0222522382234fa7ffab1a332a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:36 +0000

    Add package vandal/3.8.11/vandal-3.8.11.tar.gz

---
+++ packages/vandal/vandal-3.8.11.tar.gz/vandal-3.8.11/vandal/__main__.py
@@ -0,0 +1,37 @@
+# coloring.
+from colorama import (
+    Fore,
+    Back,
+    Style,
+    init,
+)
+
+init()
+
+# imports all relevant contents.
+import vandal
+from vandal.objects import (
+    montecarlo,
+    eoq,
+)
+
+# imports plugins.
+from logistics.plugins.metaclass import Meta
+
+# imports all data types.
+from logistics.plugins.types import *
+
+# runs the main CLI client that leads to individual apps.
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--entry', help = 'start the desired application.', type = str,
+    choices = ['montecarlo', 'eoq'])
+    args = parser.parse_args()
+
+    if args.entry == 'montecarlo':
+        print(Fore.YELLOW + '=== ENTERING MONTECARLO CLIENT... ===\n', Fore.RESET)
+        vandal.objects.montecarlo.MCapp()
+    elif args.entry == 'eoq':
+        print(Fore.YELLOW + '=== ENTERING ECONOMIC ORDER QUANTITY CLIENT... ===\n', Fore.RESET)
+        vandal.objects.eoq.EOQapp()
