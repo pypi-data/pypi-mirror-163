37c00e07600fbb2689ff9aa54cd01038bc267da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:25 +0000

    Add package probability-distributions-888/0.2/probability_distributions_888-0.2.tar.gz

---
+++ packages/probability-distributions-888/probability_distributions_888-0.2.tar.gz/probability_distributions_888-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_distributions_888',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_distributions_888'],
+      author = 'Michael Mundell',
+      author_email = 'gbsnguy@gmail.com',
+      zip_safe=False)
