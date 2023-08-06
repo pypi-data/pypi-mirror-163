03a9fc4dedb8fbf7304d0d2c31aa8df693da2a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:46 +0000

    Add package lib-V7/0.0.1/lib-V7-0.0.1.tar.gz

---
+++ packages/lib-V7/lib-V7-0.0.1.tar.gz/lib-V7-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+pkg_name = 'lib-V7'
+version = '0.0.1'
+
+setuptools.setup(
+    name=pkg_name,
+    version=version,
+    description='Local packages for development',
+
+    author='hypothesisbase',
+    author_email="support@hypothesisbase.com",
+    url= f'http://pypi.python.org/pypi/{pkg_name}/{version}',
+    license="MIT",
+
+    install_requires=[
+        "bs4",
+        "leveldb",
+        "selenium",
+        "paramiko",
+    ],
+    packages=setuptools.find_packages(),
+    zip_safe=False
+)
