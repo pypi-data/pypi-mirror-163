5fea7f259c243f07f8d0ffeacdb612945b11a8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:46 +0000

    Add package scyan/1.1.0/scyan-1.1.0-py3-none-any.whl

---
+++ packages/scyan/scyan-1.1.0-py3-none-any.whl/scyan/__init__.py
@@ -0,0 +1,14 @@
+import logging
+import importlib.metadata
+
+import pytorch_lightning as pl
+
+__version__ = importlib.metadata.version("scyan")
+
+logging.basicConfig(level=logging.INFO)
+
+from .model import Scyan
+from . import data, plot, tools, utils
+from .utils import read_fcs, write_fcs
+
+pl.seed_everything(0)
