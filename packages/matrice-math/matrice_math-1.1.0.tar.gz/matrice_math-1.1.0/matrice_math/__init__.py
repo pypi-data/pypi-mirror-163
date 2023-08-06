8fef2cbdbd3b15e749596299839804310b0291a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:33 +0000

    Add package matrice-math/1.1.0/matrice_math-1.1.0.tar.gz

---
+++ packages/matrice-math/matrice_math-1.1.0.tar.gz/matrice_math-1.1.0/matrice_math/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from matrice_math.matrix import add
+from matrice_math.matrix import subtract
+from matrice_math.matrix import scalar_mult
+from matrice_math.matrix import matrix_mult
+from matrice_math.matrix import minor
+from matrice_math.matrix import transpose
+from matrice_math.matrix import cofactor
+from matrice_math.matrix import minor_mat
+from matrice_math.matrix import cofactor_mat
+from matrice_math.matrix import adjoint
+from matrice_math.matrix import determinant
+from matrice_math.matrix import inverse
+
---
+++ packages/matrice-math/matrice_math-1.1.0.tar.gz/matrice_math-1.1.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from distutils.core import setup
+f = setup(
+  name = 'matrice_math',         
+  packages = ['matrice_math'],  
+  version = '1.1.0',      
+  license='MIT',       
+  description = 'Matrice_math is package that can be used for matrix and determinant computing with Python',  
+  readme = 'README.md', 
+  author = 'Ishaan Sawant',                   
+  author_email = 'imsawant05@gmail.com',      
+  url = 'https://github.com/KAMEHAMEHA-05/matrice_math',   
+  download_url = 'https://github.com/KAMEHAMEHA-05/matrice_math/releases/tag/v_0.1.01',    
+  keywords = ['matrix', 'determinants', 'matrices', 'array'],   
+  install_requires=[],           
+  classifiers=['Development Status :: 4 - Beta','Intended Audience :: Developers', 'Topic :: Software Development :: Build Tools','License :: OSI Approved :: MIT License',   'Programming Language :: Python :: 3','Programming Language :: Python :: 3.4','Programming Language :: Python :: 3.5','Programming Language :: Python :: 3.6','Programming Language :: Python :: 3.7']
+)
+
+
+# In[ ]:
+
+
+
+
