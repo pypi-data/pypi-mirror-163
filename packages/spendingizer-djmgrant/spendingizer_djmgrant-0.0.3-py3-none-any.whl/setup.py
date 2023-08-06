1b6fbd15487932d7ee8af82f71d4217deb86e037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:22 +0000

    Add package spendingizer-djmgrant/0.0.3/spendingizer_djmgrant-0.0.3-py3-none-any.whl

---
+++ packages/spendingizer-djmgrant/spendingizer_djmgrant-0.0.3-py3-none-any.whl/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 15:11:46 2022
+
+@author: Dave Grant 
+
+"""
+from setuptools import setup 
+
+if __name__ =="__main__":
+    setup(
+    name='spendingizer_djmgrant',
+    version='0.0.2',
+    description='Personal spending analyasis tool',
+    license='MIT',
+    packages=['spendingizer'],
+    author='David Grant',
+    author_email='djmgrant@gmail.com',
+    keywords=['personal finance'],) 
+      
