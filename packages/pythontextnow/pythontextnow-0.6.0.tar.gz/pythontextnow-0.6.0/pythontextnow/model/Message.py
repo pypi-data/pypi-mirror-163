a998a8b6fd37575c9f1dab9879e923e2fc933d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:30 +0000

    Add package pythontextnow/0.6.0/pythontextnow-0.6.0.tar.gz

---
+++ packages/pythontextnow/pythontextnow-0.6.0.tar.gz/pythontextnow-0.6.0/pythontextnow/model/Message.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import datetime
+
+from pythontextnow.enum import MessageType, MessageDirection
+
+
+@dataclass(kw_only=True)
+class Message:
+    number: str
+    date: datetime
+    first_contact: bool
+    type_: MessageType
+    read: bool
+    id_: str
+    direction: MessageDirection
+    raw: dict
+
+    @classmethod
+    def from_dict(cls, message_dict: dict) -> Message:
+        number = message_dict["contact_value"]
+        date = datetime.fromisoformat(message_dict["date"].replace("Z", "+00:00"))
+        first_contact = message_dict["conversation_filtering"]["first_time_contact"]
+        type_ = MessageType.from_value(message_dict["message_type"])
+        read = message_dict["read"]
+        id_ = message_dict["id"]
+        direction = MessageDirection.from_value(message_dict["message_direction"])
+        raw = message_dict
+        return Message(number=number,
+                       date=date,
+                       first_contact=first_contact,
+                       type_=type_,
+                       read=read,
+                       id_=id_,
+                       direction=direction,
+                       raw=raw)
