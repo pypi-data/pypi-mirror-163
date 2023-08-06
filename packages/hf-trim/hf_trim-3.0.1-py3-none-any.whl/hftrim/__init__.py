ec89b622dba519cb939de3da196092768e0b0fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:01 +0000

    Add package hf-trim/3.0.1/hf_trim-3.0.1-py3-none-any.whl

---
+++ packages/hf-trim/hf_trim-3.0.1-py3-none-any.whl/hftrim/__init__.py
@@ -0,0 +1,17 @@
+"""
+hf-trim
+
+Reduce the size of pretrained Hugging Face models via vocabulary trimming.
+"""
+
+__version__ = "3.0.1"
+__author__ = "Aditya Srivastava"
+__email__ = "adi.srivastava@hotmail.com"
+__url__ = "https://github.com/IamAdiSri/hf-trim"
+
+__supported_architectures__ = [
+    'BartModel', 'BartForConditionalGeneration', 'BartForSequenceClassification', 'BartForQuestionAnswering', 'BartForCausalLM',
+    'MBartModel', 'MBartForConditionalGeneration', 'MBartForSequenceClassification', 'MBartForQuestionAnswering', 'MBartForCausalLM',
+    'T5Model', 'T5ForConditionalGeneration', 'T5EncoderModel',
+    'MT5Model', 'MT5ForConditionalGeneration', 'MT5EncoderModel'
+]
