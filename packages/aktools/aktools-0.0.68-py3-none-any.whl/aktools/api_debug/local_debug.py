9c6186bfd38129bfb4b276721df469ded6933282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:44 +0000

    Add package aktools/0.0.68/aktools-0.0.68-py3-none-any.whl

---
+++ packages/aktools/aktools-0.0.68-py3-none-any.whl/aktools/api_debug/local_debug.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Date: 2021/12/9 19:09
+Desc: HTTP 测试
+"""
+import requests
+import pandas as pd
+
+url = "http://127.0.0.1:8080/api/public/public/fund_em_lcx_rank"
+params = {
+    "symbol": "850335.SI",
+}
+r = requests.get(url, params=params)
+temp_df = pd.DataFrame.from_dict(r.json())
+print(temp_df)
