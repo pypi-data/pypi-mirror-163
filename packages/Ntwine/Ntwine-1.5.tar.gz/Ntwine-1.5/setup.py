6f14f8a38654c69665dd1fc6a2c295263522db88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:37 +0000

    Add package Ntwine/1.5/Ntwine-1.5.tar.gz

---
+++ packages/Ntwine/Ntwine-1.5.tar.gz/Ntwine-1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+long_description = open("README.md").read()
+
+setup(name="Ntwine",
+version="1.5",
+description="Upload pypi package using  Ntwine through termux.",
+long_description=long_description,
+long_description_content_type='text/markdown',
+author="Nishant",
+url="https://github.com/Nishant2009/Ntwine",
+scripts=["Ntwine"],
+install_requires= ['requests', 'setuptools', 'readme-renderer[md]', 'wheel', 'twine==1.12.0'],
+classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+], )
