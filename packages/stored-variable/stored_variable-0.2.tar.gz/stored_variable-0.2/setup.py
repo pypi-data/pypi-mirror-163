9a6ff4a8f044fd3cd2e8d1de86004389c74914e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:35 +0000

    Add package stored-variable/0.2/stored_variable-0.2.tar.gz

---
+++ packages/stored-variable/stored_variable-0.2.tar.gz/stored_variable-0.2/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+long_description = "This is a package that stores value for you in computer memory permanently"
+
+setup(name="stored_variable", version="0.2", description="It creates stored variable", long_description=long_description, author="Saksham Jain",packages=['stored_variable'],install_requires=[])
