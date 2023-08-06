0b8f51203d842ab453f044d776520387a04820cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:27 +0000

    Add package HugeP2G/0.1.3/HugeP2G-0.1.3-py3-none-any.whl

---
+++ packages/HugeP2G/HugeP2G-0.1.3-py3-none-any.whl/hugep2g/versions.py
@@ -0,0 +1,31 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.1.3",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "0.1.2",
+        "features": [
+            "1. debug",
+            "2. modify log file",
+        ],
+    },
+    {
+        "number": "0.1.1",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "0.1.0",
+        "features": [
+            "1. Separate WPGmapper from ToolBiox and rename as HugeP2G",
+        ],
+    },
+]
