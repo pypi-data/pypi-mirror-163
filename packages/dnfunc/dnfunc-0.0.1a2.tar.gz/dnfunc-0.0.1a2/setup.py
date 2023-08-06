abffa774893541666071b5dc8d631871560c057e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:09 +0000

    Add package dnfunc/0.0.1a2/dnfunc-0.0.1a2.tar.gz

---
+++ packages/dnfunc/dnfunc-0.0.1a2.tar.gz/dnfunc-0.0.1a2/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['dnfunc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'havsfunc>=33,<34',
+ 'lvsfunc>=0.4.3,<0.5.0',
+ 'vapoursynth>=59,<60',
+ 'vsutil>=0.8.0,<0.9.0']
+
+extras_require = \
+{'vapoursynth-portable': ['vapoursynth-portable>=59,<60']}
+
+setup_kwargs = {
+    'name': 'dnfunc',
+    'version': '0.0.1a2',
+    'description': 'A collection of Vapoursynth functions and wrapperspoetr',
+    'long_description': '# dnfunc\n\n> A collection of Vapoursynth functions and wrappers\n\n[![PyPI version](https://img.shields.io/pypi/v/dnfunc)](https://pypi.org/project/dnfunc)\n[![CI/CD](https://github.com/DeadNews/dnfunc/actions/workflows/python-vs-app.yml/badge.svg)](https://github.com/DeadNews/dnfunc/actions/workflows/python-vs-app.yml)\n[![pre-commit.ci](https://results.pre-commit.ci/badge/github/DeadNews/dnfunc/main.svg)](https://results.pre-commit.ci/latest/github/DeadNews/dnfunc/main)\n',
+    'author': 'DeadNews',
+    'author_email': 'uhjnnn@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/DeadNews/dnfunc',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
