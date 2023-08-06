3fc5f7344bb7bc12879e0282bcba9ba76f02b4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:40 +0000

    Add package eqxvision/0.1.8/eqxvision-0.1.8.tar.gz

---
+++ packages/eqxvision/eqxvision-0.1.8.tar.gz/eqxvision-0.1.8/eqxvision/models/classification/__init__.py
@@ -0,0 +1,14 @@
+from . import (
+    alexnet,
+    convnext,
+    densenet,
+    efficientnet,
+    googlenet,
+    mobilenetv2,
+    mobilenetv3,
+    resnet,
+    shufflenetv2,
+    squeezenet,
+    vgg,
+    vit,
+)
