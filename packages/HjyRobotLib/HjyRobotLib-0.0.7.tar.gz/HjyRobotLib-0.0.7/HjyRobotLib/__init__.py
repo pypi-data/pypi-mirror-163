627c8f8e1363e69005f97b297d3ef798226a9010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:27 +0000

    Add package HjyRobotLib/0.0.7/HjyRobotLib-0.0.7.tar.gz

---
+++ packages/HjyRobotLib/HjyRobotLib-0.0.7.tar.gz/HjyRobotLib-0.0.7/HjyRobotLib/__init__.py
@@ -0,0 +1,29 @@
+"""Main library."""
+from robotlibcore import DynamicCore, keyword
+from HjyRobotLib.mystuff import Library1, Library2
+
+__version__ = "0.0.1"
+
+
+class HjyRobotLib(DynamicCore):
+    """General library documentation."""
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+    ROBOT_LIBRARY_VERSION = __version__
+
+    def __init__(self):
+        libraries = [Library1(), Library2()]
+        DynamicCore.__init__(self, libraries)
+
+    @keyword
+    def keyword_in_main(self):
+        pass
+
+
+def hello_world():
+    print("hello world")
+    return "hello world"
+
+
+if __name__ == '__main__':
+    hello_world()
+    pass
