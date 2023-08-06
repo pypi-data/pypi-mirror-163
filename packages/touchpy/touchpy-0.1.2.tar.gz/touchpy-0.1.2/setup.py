63220da7a6e66a8b1ec00fc6bc5477d5c716e5a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:58 +0000

    Add package touchpy/0.1.2/touchpy-0.1.2.tar.gz

---
+++ packages/touchpy/touchpy-0.1.2.tar.gz/touchpy-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+__version__ = '0.1.2'
+setuptools.setup(
+    name="touchpy",
+    version=__version__,
+    author="Talha Asghar",
+    author_email="talhaasghar.contact@simplelogin.fr",
+    description="A simple script which creates .py files with necessary docstrings.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamtalhaasghar/touchpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[i for i in open('requirements.txt').readlines() if len(i)!=0],
+    entry_points={'console_scripts': ['touchpy = touchpy:touchpy.main']},
+)
+
