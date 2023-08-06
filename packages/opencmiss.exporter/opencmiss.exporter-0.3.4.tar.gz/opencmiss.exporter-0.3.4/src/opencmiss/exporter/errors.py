e43c7a601a07e7ffe893728c7d36b1aa296ecbe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:50 +0000

    Add package opencmiss.exporter/0.3.4/opencmiss.exporter-0.3.4.tar.gz

---
+++ packages/opencmiss.exporter/opencmiss.exporter-0.3.4.tar.gz/opencmiss.exporter-0.3.4/src/opencmiss/exporter/errors.py
@@ -0,0 +1,11 @@
+
+class OpenCMISSExportError(Exception):
+    pass
+
+
+class OpenCMISSExportWebGLError(OpenCMISSExportError):
+    pass
+
+
+class OpenCMISSExportThumbnailError(OpenCMISSExportError):
+    pass
