123a2ef4a1daea4d4d849aa0a1e3d21201a558b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:02 +0000

    Add package sfdutf7/0.1.0/sfdutf7-0.1.0.tar.gz

---
+++ packages/sfdutf7/sfdutf7-0.1.0.tar.gz/sfdutf7-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sfdutf7",
+    version="0.1.0",
+    author="Fredrick Brennan",
+    author_email="copypaste@kittens.ph",
+    description="SFDUTF7 encoding parsing library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ctrlcctrlv/sfdutf7.py",
+    entry_points={
+        "console_scripts": ["sfdutf7 = sfdutf7.__main__:main"],
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
