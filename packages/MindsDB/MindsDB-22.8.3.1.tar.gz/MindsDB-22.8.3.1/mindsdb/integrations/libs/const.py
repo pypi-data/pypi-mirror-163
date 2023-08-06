07be5787bca14544c268e7ee983a466a12d6bcde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:09 +0000

    Add package MindsDB/22.8.3.1/MindsDB-22.8.3.1.tar.gz

---
+++ packages/MindsDB/MindsDB-22.8.3.1.tar.gz/MindsDB-22.8.3.1/mindsdb/integrations/libs/const.py
@@ -0,0 +1,18 @@
+class HANDLER_TYPE:
+    __slots__ = ()
+    DATA = 'data'
+    ML = 'ml'
+
+
+HANDLER_TYPE = HANDLER_TYPE()
+
+
+class HANDLER_CONNECTION_ARG_TYPE:
+    __slots__ = ()
+    STR = 'str'
+    INT = 'int'
+    BOOL = 'bool'
+    URL = 'url'
+
+
+HANDLER_CONNECTION_ARG_TYPE = HANDLER_CONNECTION_ARG_TYPE()
