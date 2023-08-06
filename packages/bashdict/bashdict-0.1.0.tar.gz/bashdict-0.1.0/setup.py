c7837ea32b495b251638b250c26085bf49393d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:14 +0000

    Add package bashdict/0.1.0/bashdict-0.1.0.tar.gz

---
+++ packages/bashdict/bashdict-0.1.0.tar.gz/bashdict-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['bashdict']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'bashdict',
+    'version': '0.1.0',
+    'description': 'A simple commandline dictionary',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '==3.10.5',
+}
+
+
+setup(**setup_kwargs)
