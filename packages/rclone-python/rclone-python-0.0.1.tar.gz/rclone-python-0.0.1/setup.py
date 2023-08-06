6132bb2ca7c9a637ba66b3ea62a26204f388ba55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:36 +0000

    Add package rclone-python/0.0.1/rclone-python-0.0.1.tar.gz

---
+++ packages/rclone-python/rclone-python-0.0.1.tar.gz/rclone-python-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+from py_rclone.rclone import get_version
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='rclone-python',
+    version=get_version(),
+    description='A python wrapper for rclone.',
+    author='Johannes Gundlach',
+    install_requires=['simple_term_menu', 'alive-progress', ],
+    packages=['py_rclone'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['rclone', 'wrapper', 'cloud sync'],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
