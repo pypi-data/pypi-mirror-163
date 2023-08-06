b86a710cddea61ba9d1f8d986a299dc2b0dc5c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:23 +0000

    Add package AVOScript/0.2.3/AVOScript-0.2.3.tar.gz

---
+++ packages/AVOScript/AVOScript-0.2.3.tar.gz/AVOScript-0.2.3/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+
+setup(
+    name="AVOScript",
+    description="little language just4fun",
+    author="Ethosa",
+    author_email="social.ethosa@gmail.com",
+    version="0.2.3",
+    url="https://github.com/ethosa/avoscript",
+    install_requires=[
+        "colorama",
+        "equality",
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    python_requires='>=3.10',
+    keywords=['language', 'avocat', 'avoscript', 'script language'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development',
+    ]
+)
