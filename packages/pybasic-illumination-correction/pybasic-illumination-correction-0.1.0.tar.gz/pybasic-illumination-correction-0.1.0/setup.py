14d8316db50bec04bda250a60e5037f1c5b5f10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:43 +0000

    Add package pybasic-illumination-correction/0.1.0/pybasic-illumination-correction-0.1.0.tar.gz

---
+++ packages/pybasic-illumination-correction/pybasic-illumination-correction-0.1.0.tar.gz/pybasic-illumination-correction-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pybasic']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.23.2,<2.0.0',
+ 'opencv-python>=4.6.0,<5.0.0',
+ 'scipy>=1.9.0,<2.0.0',
+ 'tqdm>=4.64.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'pybasic-illumination-correction',
+    'version': '0.1.0',
+    'description': 'Python Implementation of the BaSiC shading correction algorithm',
+    'long_description': None,
+    'author': 'Joël Lefebvre',
+    'author_email': 'lefebvre.joel@uqam.ca',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/linum-uqam/PyBaSiC',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<3.12',
+}
+
+
+setup(**setup_kwargs)
