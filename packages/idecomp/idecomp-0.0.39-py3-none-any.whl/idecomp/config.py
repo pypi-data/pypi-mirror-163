0e3178481854626f26b415b7e78ce354db573c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:10 +0000

    Add package idecomp/0.0.39/idecomp-0.0.39-py3-none-any.whl

---
+++ packages/idecomp/idecomp-0.0.39-py3-none-any.whl/idecomp/config.py
@@ -0,0 +1,65 @@
+SUBSISTEMAS = ["SE", "S", "NE", "N", "FC"]
+REES = [
+    "SUDESTE",
+    "SUL",
+    "NORDESTE",
+    "NORTE",
+    "ITAIPU",
+    "MADEIRA",
+    "TPIRES",
+    "BMONTE",
+    "MAN-AP",
+    "PARANA",
+    "IGUACU",
+    "PRNPANEMA",
+]
+MAX_PATAMARES = 5
+MAX_SEMANAS_PRE = 10
+MAX_ESTAGIOS = 10
+MAX_SUBSISTEMAS = 10
+MAX_UTES = 300
+MAX_UHES = 300
+MAX_REES = 20
+MAX_CENARIOS = 999
+MESES = [
+    "JANEIRO",
+    "FEVEREIRO",
+    "MARCO",
+    "ABRIL",
+    "MAIO",
+    "JUNHO",
+    "JULHO",
+    "AGOSTO",
+    "SETEMBRO",
+    "OUTUBRO",
+    "NOVEMBRO",
+    "DEZEMBRO",
+]
+MESES_DF = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+]
+MESES_ABREV = [
+    "JAN",
+    "FEV",
+    "MAR",
+    "ABR",
+    "MAI",
+    "JUN",
+    "JUL",
+    "AGO",
+    "SET",
+    "OUT",
+    "NOV",
+    "DEZ",
+]
