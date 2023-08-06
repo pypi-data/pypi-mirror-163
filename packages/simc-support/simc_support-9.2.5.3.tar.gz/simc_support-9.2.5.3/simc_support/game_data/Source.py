2d551f7dd71e4f966cc1a9b3431850954ac46093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:48 +0000

    Add package simc-support/9.2.5.3/simc_support-9.2.5.3.tar.gz

---
+++ packages/simc-support/simc_support-9.2.5.3.tar.gz/simc_support-9.2.5.3/simc_support/game_data/Source.py
@@ -0,0 +1,17 @@
+import enum
+
+
+class Source(enum.Enum):
+    CALLING = "Calling"
+    DUNGEON = "Dungeon"
+    KORTHIA = "Korthia"
+    ZERETH_MORTIS = "Zereth Mortis"
+    MISSION = "Mission"
+    PROFESSION = "Profession"
+    PVP = "PvP"
+    RAID = "Raid"
+    RARE_MOB = "Rare Mob"
+    UNKNOWN = "Unknown"
+    WORLD_BOSS = "World Boss"
+    WORLD_DROP = "World Drop"
+    WORLD_QUEST = "World Quest"
