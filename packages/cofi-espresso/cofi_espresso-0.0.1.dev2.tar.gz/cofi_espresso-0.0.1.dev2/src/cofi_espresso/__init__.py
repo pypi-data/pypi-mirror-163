67f8657b75ee68a07f8bee3bb65825812978597c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:19 +0000

    Add package cofi-espresso/0.0.1.dev2/cofi_espresso-0.0.1.dev2.tar.gz

---
+++ packages/cofi-espresso/cofi_espresso-0.0.1.dev2.tar.gz/cofi_espresso-0.0.1.dev2/src/cofi_espresso/__init__.py
@@ -0,0 +1,24 @@
+from .espresso_problem import EspressoProblem
+
+
+try:
+    from . import _version
+
+    __version__ = _version.__version__
+except ImportError:
+    pass
+
+
+__all__ = [
+    "EspressoProblem",
+]
+
+# from .example_name import ExampleName
+# __all__.append("ExampleName")
+
+from .gravity_density import GravityDensity
+
+__additional_all__ = [
+	'GravityDensity',
+]
+__all__ += __additional_all__
