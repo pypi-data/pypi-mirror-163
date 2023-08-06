f89cc602f4efa4c4530d22fa7c5c30a127f1dc13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:42 +0000

    Add package hydropandas/0.6.1/hydropandas-0.6.1.tar.gz

---
+++ packages/hydropandas/hydropandas-0.6.1.tar.gz/hydropandas-0.6.1/hydropandas/__init__.py
@@ -0,0 +1,20 @@
+from .obs_collection import ObsCollection
+from .observation import (
+    Obs,
+    GroundwaterObs,
+    GroundwaterQualityObs,
+    PrecipitationObs,
+    EvaporationObs,
+    MeteoObs,
+    ModelObs,
+    WaterlvlObs,
+)
+from .extensions import geo as _geo
+from .extensions import gwobs as _gwobs
+from .extensions import plots as _plots
+from .extensions import stats as _stats
+from .version import __version__
+
+import logging
+
+logging.getLogger("hydropandas").addHandler(logging.NullHandler())
