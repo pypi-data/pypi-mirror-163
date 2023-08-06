2ac7e81ae7fa3575c141e5dcd56581fe209d8e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:29 +0000

    Add package scimath/5.0.0/scimath-5.0.0-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/scimath/scimath-5.0.0-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/scimath/_version.py
@@ -0,0 +1,17 @@
+# (C) Copyright 2005-2022 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+# This file is generated from setup.py
+version = '5.0.0'
+full_version = '5.0.0'
+git_revision = 'a5cdf56bed164a2095acbbd67611f028e1606e78'
+is_released = True
+if not is_released:
+    version = full_version
