033fe1b5b7a75772371cc0ab678fc2cd4018ea69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:30 +0000

    Add package mplchart/0.0.2/mplchart-0.0.2-py3-none-any.whl

---
+++ packages/mplchart/mplchart-0.0.2-py3-none-any.whl/mplchart/primitives/markers.py
@@ -0,0 +1,121 @@
+""" Markers """
+
+import numpy as np
+
+from ..model import Primitive
+
+from matplotlib.collections import LineCollection
+
+
+def export(func):
+    globals().setdefault('__all__', []).append(func.__name__)
+    return func
+
+
+class PosMarker(Primitive):
+    """ Base class for position based markers """
+
+    candidate_items = ('pos', 'macdhist', 'ppohist')
+    indicator = None
+    expr = None
+
+    def __init__(self, expr=None):
+        self.expr = expr
+
+    def __ror__(self, indicator):
+        if not callable(indicator):
+            raise ValueError(f"{indicator!r} not callable!")
+
+        return self.clone(indicator=indicator)
+
+    def default_item(self, prices):
+        items = [c for c in prices.columns if c in self.candidate_items]
+        if items:
+            return items[0]
+        else:
+            raise ValueError("No valid position column!")
+
+    def process(self, prices, chart):
+        """ adds indicator result and position to prices """
+
+        indicator = self.indicator or chart.last_indicator
+
+        if indicator:
+            data = indicator(prices)
+            prices = prices.join(data)
+
+        expr = self.expr or self.default_item(prices)
+        pos = prices.eval(expr)
+        pos = np.where(pos > 0.0, 1.0, 0.0)
+        prices = prices.assign(pos=pos)
+
+        return prices
+
+
+@export
+class TradeMarker(PosMarker):
+    """ Trade Marker Primitive """
+
+    def plot_handler(self, data, chart, ax=None):
+        """ main plot handler from raw prices """
+
+        if ax is None:
+            ax = chart.main_axes()
+
+        data = self.process(data, chart)
+        data = chart.extract_df(data)
+
+        mask = data.pos.diff().fillna(0).ne(0)
+
+        xv = data.index[mask]
+        yv = data.close[mask]
+        pos = data.pos[mask]
+
+        cv = np.where(pos > 0, 'g', 'r')
+
+        ax.scatter(xv, yv, c=cv, s=12 * 12, alpha=0.6, marker=".")
+
+        points = np.array([xv, yv]).T.reshape(-1, 1, 2)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+        pos_color = (0.0, 0.0, 0.0, 1.0)
+        neg_color = (0.0, 0.0, 0.0, 0.0)
+
+        colors = [pos_color if pos.iloc[i] else neg_color for i in range(len(segments))]
+
+        lc = LineCollection(segments, colors=colors, linestyles='solid', linewidths=1.0)
+
+        ax.add_collection(lc)
+
+
+@export
+class TradeSpan(PosMarker):
+    """ Trade Span Primitive """
+
+    def plot_handler(self, data, chart, ax=None):
+        """ main plot handler from raw prices """
+
+        if ax is None:
+            ax = chart.root_axes()
+
+        data = self.process(data, chart)
+        data = chart.extract_df(data)
+        pos = data.pos
+
+        mask = pos.diff().fillna(0).ne(0)
+        color = 'green'
+        alpha = 0.1
+
+        xv = data.index[mask]
+        sv = data.pos[mask]
+        x = s = px = None
+
+        for x, s in zip(xv, sv):
+            if s > 0 and px is None:
+                px = x
+            if s <= 0 and px is not None:
+                ax.axvspan(px, x, color=color, alpha=alpha)
+                px = None
+
+        if s <= 0 and px is not None:
+            ax.axvspan(px, x, color=color, alpha=alpha)
