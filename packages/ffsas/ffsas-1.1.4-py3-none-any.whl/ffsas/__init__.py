5cfc5b1d32c0e1b0c4928ba26f7e17ab2f4568e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:15 +0000

    Add package ffsas/1.1.4/ffsas-1.1.4-py3-none-any.whl

---
+++ packages/ffsas/ffsas-1.1.4-py3-none-any.whl/ffsas/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# __init__.py
+# ffsas: free-form inversion for small-angle scattering
+# Copyright © 2021 SciML, STFC, UK. All rights reserved.
+
+
+""" ffsas module """
+
+import torch
+
+# data type
+torch_dtype = torch.float64
+torch.set_default_dtype(torch_dtype)
+
+
+def set_torch_dtype(dtype):
+    """ set torch dtype """
+    global torch_dtype
+    torch_dtype = dtype
+    torch.set_default_dtype(torch_dtype)
+
+
+# specify version here; it will picked up by pip
+__version__ = '1.1.4'
