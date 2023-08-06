38e5c97f29dc157d77d0889c9404fbf05a2df0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:36 +0000

    Add package vortexdm/2022.1.0/vortexdm-2022.1.0-py3-none-any.whl

---
+++ packages/vortexdm/vortexdm-2022.1.0-py3-none-any.whl/vdm/__main__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    Vortex Download Manager (VDM)
+
+    Multi-connection internet download manager, based on "LibCurl", and "youtube_dl". Original project, FireDM, by Mahmoud Elshahat.
+    :copyright: (c) 2022 by Sixline
+    :copyright: (c) 2019-2021 by Mahmoud Elshahat.
+    :license: GNU GPLv3, see LICENSE.md for more details.
+"""
+# main module executed when run command
+# python -m vdm
+
+import sys
+
+if __package__ is None and not hasattr(sys, 'frozen'):
+    # direct call of __main__.py
+    import os.path
+    path = os.path.realpath(os.path.abspath(__file__))
+    sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
+
+from vdm import VDM
+
+if __name__ == '__main__':
+    VDM.main()
