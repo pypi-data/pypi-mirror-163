971d4095c31d80763f543ea05658ec5c12925ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:42 +0000

    Add package bgetlib/3.2.6/bgetlib-3.2.6.tar.gz

---
+++ packages/bgetlib/bgetlib-3.2.6.tar.gz/bgetlib-3.2.6/bgetlib/__init__.py
@@ -0,0 +1,13 @@
+from .__metainfo__ import __title__
+from .__metainfo__ import __description__
+from .__metainfo__ import __source__
+from .__metainfo__ import __url__
+from .__metainfo__ import __author__
+from .__metainfo__ import __author_email__
+from .__metainfo__ import __license__
+
+from .api import BilibiliAPI
+from . import codec
+from . import utils
+from . import models
+from . import streamer
