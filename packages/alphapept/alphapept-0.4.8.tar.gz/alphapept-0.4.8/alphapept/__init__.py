fc661f6939c462e059cf43734670d47fd8dbb16d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:40 +0000

    Add package alphapept/0.4.8/alphapept-0.4.8.tar.gz

---
+++ packages/alphapept/alphapept-0.4.8.tar.gz/alphapept-0.4.8/alphapept/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.4.8"
+
+__requirements__ = {
+    "": "requirements/requirements.txt",
+    "develop": "requirements/requirements_develop.txt",
+    "gui": "requirements/requirements_gui.txt",
+}
