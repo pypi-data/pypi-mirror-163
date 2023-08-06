f94e3a643e7abad6199aae5385dcc01a7a20e6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:41 +0000

    Add package pysimulators/1.2.0/pysimulators-1.2.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pysimulators/pysimulators-1.2.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pysimulators/__init__.py
@@ -0,0 +1,25 @@
+from pyoperators.utils.mpi import MPI
+
+# force gfortran's read statement to always use the dot sign as fraction
+# separator (PR47007)
+import locale
+locale.setlocale(locale.LC_NUMERIC, 'POSIX')
+del locale
+from importlib.metadata import version as _version
+
+from . import _flib
+from . import geometry
+from .beams import *
+from .operators import *
+from .datatypes import *
+from .packedtables import *
+from .instruments import *
+#from .mpiutils import *
+from .acquisitions import *
+#from .processing import *
+from .quantities import *
+from .datautils import *
+from .sparse import *
+from .wcsutils import *
+
+__version__ = _version('pysimulators')
