e853938426c569020bd622bbe40633c0d4c4fb5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:46 +0000

    Add package robyn/0.17.3/robyn-0.17.3-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/robyn/robyn-0.17.3-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/robyn/dev_event_handler.py
@@ -0,0 +1,26 @@
+import subprocess
+
+from watchdog.events import FileSystemEventHandler
+
+
+class EventHandler(FileSystemEventHandler):
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.processes = []
+
+    def start_server_first_time(self):
+        if self.processes:
+            raise Exception("Something wrong with the server")
+        self.processes.append(subprocess.Popen(["python3", self.file_name], start_new_session=False))
+
+    def on_any_event(self, event):
+        """
+        This function is a callback that will start a new server on every even change
+
+        :param event FSEvent: a data structure with info about the events
+        """
+
+        if len(self.processes) > 0:
+            for process in self.processes:
+                process.terminate()
+        self.processes.append(subprocess.Popen(["python3", self.file_name], start_new_session=False))
