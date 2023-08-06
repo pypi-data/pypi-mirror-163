362b54975e4ed35a0204eb28948f4a47f0227cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:59 +0000

    Add package atoti-azure/0.7.0/atoti_azure-0.7.0-0_23f0cd4-py3-none-any.whl

---
+++ packages/atoti-azure/atoti_azure-0.7.0-0_23f0cd4-py3-none-any.whl/atoti_azure/client_side_encryption.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+from atoti_core import keyword_only_dataclass
+
+from atoti import KeyPair
+from atoti.client_side_encryption_config import ClientSideEncryptionConfig
+
+
+@keyword_only_dataclass
+@dataclass(frozen=True)
+class AzureKeyPair(KeyPair, ClientSideEncryptionConfig):
+    """Key pair to use for client side encryption.
+
+    Warning:
+
+        Each encrypted blob must have the metadata attribute ``unencrypted_content_length`` with the unencrypted file size.
+        If this is not set, an :guilabel:`Issue while downloading` error will occur.
+
+    Example:
+
+        >>> from atoti_azure import AzureKeyPair
+        >>> azure_client_side_encryption = (
+        ...     AzureKeyPair(
+        ...         key_id="key_id",
+        ...         public_key="public_key",
+        ...         private_key="private_key",
+        ...     ),
+        ... )
+    """
+
+    key_id: str
+    """The ID of the key used to encrypt the blob."""
