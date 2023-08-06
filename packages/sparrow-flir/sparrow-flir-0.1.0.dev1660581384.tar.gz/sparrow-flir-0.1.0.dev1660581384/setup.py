c87dbdd864c5dabeb7854110bb11d13ca308b45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:07 +0000

    Add package sparrow-flir/0.1.0.dev1660581384/sparrow-flir-0.1.0.dev1660581384.tar.gz

---
+++ packages/sparrow-flir/sparrow-flir-0.1.0.dev1660581384.tar.gz/sparrow-flir-0.1.0.dev1660581384/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sparrow_flir']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=9.2.0,<10.0.0', 'numpy>=1.23.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'sparrow-flir',
+    'version': '0.1.0.dev1660581384',
+    'description': '',
+    'long_description': None,
+    'author': 'Sparrow Computing',
+    'author_email': 'ben@sparrow.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
