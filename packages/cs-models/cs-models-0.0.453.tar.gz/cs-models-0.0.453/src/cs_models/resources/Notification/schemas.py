303da5da441505082c6a2e1693c75fcfead76fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:31 +0000

    Add package cs-models/0.0.453/cs-models-0.0.453.tar.gz

---
+++ packages/cs-models/cs-models-0.0.453.tar.gz/cs-models-0.0.453/src/cs_models/resources/Notification/schemas.py
@@ -0,0 +1,21 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class NotificationResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    search_id = fields.Integer(required=True)
+    source_type = fields.String(required=True)
+    source_id = fields.Integer(required=True)
+    company_matches = fields.String(required=True)
+    drug_matches = fields.String(required=True)
+    disease_matches = fields.String(required=True)
+    target_matches = fields.String(required=True)
+    seen = fields.Boolean(allow_none=True)
+    date = fields.DateTime(required=True)
+    updated_at = fields.DateTime()
