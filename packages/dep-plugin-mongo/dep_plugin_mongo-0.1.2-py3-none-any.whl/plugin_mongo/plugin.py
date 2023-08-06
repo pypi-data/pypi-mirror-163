df7e9415bbf83b891136de7ad537c4a79fee0ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:42 +0000

    Add package dep-plugin-mongo/0.1.2/dep_plugin_mongo-0.1.2-py3-none-any.whl

---
+++ packages/dep-plugin-mongo/dep_plugin_mongo-0.1.2-py3-none-any.whl/plugin_mongo/plugin.py
@@ -0,0 +1,33 @@
+"""Mongo plugin."""
+
+from typing import Dict
+from spec.types import App, Plugin, Spec
+
+
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+
+
+CLIENTS: Dict[str, AsyncIOMotorClient] = {}
+CONNECTIONS: Dict[str, AsyncIOMotorDatabase] = {}
+
+
+class MongoPlugin(Plugin):
+    """Mongo plugin."""
+
+    type_name = 'mongo'
+
+    async def prepare(self, app: App, spec: Spec):
+        """Prepare mongo."""
+        uri = self.env.get('uri')
+        pool_size = int(self.env.get('pool_size', spec.policy.db_pool_size))
+
+        client_options = {'minPoolSize': pool_size, 'maxPoolSize': pool_size}
+
+        if self.fqdn not in CLIENTS:
+            CLIENTS[self.fqdn] = AsyncIOMotorClient(uri, **client_options)
+
+        CONNECTIONS[self.fqdn] = CLIENTS[self.fqdn][self.alias]
+
+    async def release(self, app: App, spec: Spec):
+        """Release mongo."""
+        CLIENTS[self.fqdn].close()
