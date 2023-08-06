c86c5f6c97c1d66beebb32632084407c3ecf02c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:50 +0000

    Add package kolorz/0.2.2/kolorz-0.2.2-py3-none-any.whl

---
+++ packages/kolorz/kolorz-0.2.2-py3-none-any.whl/kolorz/cli.py
@@ -0,0 +1,15 @@
+from kolorz.colors import colors
+from kolorz.kolor import make_kolorz
+import random
+
+def kolor():
+    """
+    A stand alone function to print all available colors
+    """
+    print("Supported colorschemes: \n")
+
+    for color, values in colors.items():
+        kol = make_kolorz(color)
+        col = random.choice(list(values.keys()))
+        print(f"{getattr(kol, col)}{color}{kol.end}")
+
