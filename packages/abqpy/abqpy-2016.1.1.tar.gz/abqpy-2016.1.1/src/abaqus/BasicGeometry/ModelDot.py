3b6fce6a5daf031c77487b5541f997fe2a1c490a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:43 +0000

    Add package abqpy/2016.1.1/abqpy-2016.1.1.tar.gz

---
+++ packages/abqpy/abqpy-2016.1.1.tar.gz/abqpy-2016.1.1/src/abaqus/BasicGeometry/ModelDot.py
@@ -0,0 +1,10 @@
+class ModelDot:
+    """The ModelDot object can be used to access an actual MeshNode, ReferencePoint, or ConstrainedSketchVertex
+    object.
+
+    Notes
+    -----
+    This object can be accessed by:
+    """
+
+    ...
