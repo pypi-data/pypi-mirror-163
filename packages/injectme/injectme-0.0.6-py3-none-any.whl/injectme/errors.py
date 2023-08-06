066df890fb23291b30b8549f372b3e64b27b7017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:03 +0000

    Add package injectme/0.0.6/injectme-0.0.6-py3-none-any.whl

---
+++ packages/injectme/injectme-0.0.6-py3-none-any.whl/injectme/errors.py
@@ -0,0 +1,45 @@
+from typing import Any
+
+
+class InjectmeException(Exception):
+    """
+    Base class for all injectme's exceptions.
+    """
+
+
+class DependencyNotFound(InjectmeException):
+    """
+    Requested dependency has not been found in the registry.
+    """
+
+    def __init__(self, dependency: type):
+        super().__init__(
+            f"Dependency {dependency} has not been found in the registry"
+        )
+
+
+class DependencyAlreadyRegistered(InjectmeException):
+    """
+    Dependency has already been registered in selected registry.
+    """
+
+    def __init__(self, dependency: type):
+        super().__init__(f"Dependency {dependency} already registered")
+
+
+class InjectionNotSupported(InjectmeException):
+    """
+    Decorated target is not valid for injection.
+    """
+
+    def __init__(self, target: Any):
+        super().__init__(f"Unsopported injection target {target}")
+
+
+class InjectionFailure(InjectmeException):
+    """
+    Failed to inject the dependency.
+    """
+
+    def __init__(self, cls: type):
+        super().__init__(f"Failed to inject dependencies into {cls}")
---
+++ packages/injectme/injectme-0.0.6-py3-none-any.whl/injectme/injector.py
@@ -0,0 +1,80 @@
+import logging
+from typing import Optional
+
+from .errors import (
+    DependencyNotFound,
+    InjectionFailure,
+    InjectionNotSupported,
+)
+from .registry import DependenciesRegistry
+
+logger = logging.getLogger(__name__)
+
+
+class Injector:
+    """
+    Class responsible for the injection of dependencies.
+    """
+
+    def __init__(self, registry: Optional[DependenciesRegistry] = None):
+        """
+        Initialize the Injector.
+
+        :param registry: dependencies registry
+            to be used with this ``Injector`` instance. If not sepcified,
+            an instance of registry will be created automatically.
+        """
+        if registry is None:
+            registry = DependenciesRegistry()
+
+        self._registry = registry
+
+    @property
+    def registry(self) -> DependenciesRegistry:
+        """
+        Dependencies registry associated with this ``Injector``.
+
+        :return: instance of registry.
+        """
+        return self._registry
+
+    def __call__(self, cls: type) -> type:
+        """
+        Mark class to be the target of injection performed with this instance
+        of ``Injector``.
+
+        :param cls: class to be marked for injection.
+        :raises InjectionFailure: raised if any of the required dependencies
+            can't be found in the registry associated with this ``Injector``.
+        :return: class passed as a param.
+        """
+        logger.debug("marking %s as target for %s", cls, self)
+        if not isinstance(cls, type):
+            raise InjectionNotSupported(cls)
+
+        injector = self
+
+        def __init_deps__(self):
+            annotations = cls.__dict__.get("__annotations__", {})
+            registry = injector.registry
+
+            logger.debug(
+                "injecting into %s by %s using %s", self, injector, registry
+            )
+
+            try:
+                for name, dependency in annotations.items():
+                    instance = registry.get(dependency)
+                    setattr(self, name, instance)
+            except DependencyNotFound as err:
+                raise InjectionFailure(cls) from err
+
+        def injectme_init(self, *args, **kwargs):
+            cls.__init_deps__(self)
+            cls.__original_init__(self, *args, **kwargs)
+
+        cls.__original_init__ = cls.__init__
+        cls.__init_deps__ = __init_deps__
+        cls.__init__ = injectme_init
+
+        return cls
