9da9e4435f4055e1a5d082c1f729c9b8635e0ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:06 +0000

    Add package rir-generator/0.2.0/rir-generator-0.2.0.tar.gz

---
+++ packages/rir-generator/rir-generator-0.2.0.tar.gz/rir-generator-0.2.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import setuptools
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="rir-generator",
+        version="0.2.0",
+        description="Room Impulse Response Generator.",
+        author="Nils Werner",
+        author_email="nils.werner@fau.de",
+        license="MIT",
+        packages=setuptools.find_packages(),
+        setup_requires=["cffi>=1.1.0"],
+        install_requires=[
+            "numpy>=1.6",
+            "scipy>=0.13.0",
+            "cffi>=1.1.0",
+        ],
+        cffi_modules=[
+            "rir_generator/_rir/build.py:rir_generator",
+        ],
+        extras_require={
+            "tests": [
+                "pytest",
+                "pytest-cov",
+                "pytest-black",
+                "sphinx",
+                "sphinxcontrib-napoleon",
+                "sphinx_rtd_theme",
+                "numpydoc",
+            ],
+            "docs": [
+                "sphinx",
+                "sphinxcontrib-napoleon",
+                "sphinxcontrib-bibtex",
+                "sphinx_rtd_theme",
+                "numpydoc",
+            ],
+        },
+        zip_safe=False,
+    )
