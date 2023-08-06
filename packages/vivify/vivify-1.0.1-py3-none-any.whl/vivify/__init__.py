bfc5d79f0d5a1cffd5d289863afdf63cf14f5082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:22 +0000

    Add package vivify/1.0.1/vivify-1.0.1-py3-none-any.whl

---
+++ packages/vivify/vivify-1.0.1-py3-none-any.whl/vivify/__init__.py
@@ -0,0 +1,29 @@
+from vivify.vivify import (
+    Config,
+    Vivifiable,
+    VivificationError,
+    Vivifier,
+    __author__,
+    __copyright__,
+    __credits__,
+    __email__,
+    __license__,
+    __maintainer__,
+    __status__,
+    __version__,
+)
+
+__all__ = [
+    "Config",
+    "Vivifier",
+    "Vivifiable",
+    "VivificationError",
+    "__author__",
+    "__copyright__",
+    "__credits__",
+    "__email__",
+    "__license__",
+    "__maintainer__",
+    "__status__",
+    "__version__",
+]
