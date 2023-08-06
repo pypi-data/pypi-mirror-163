604258b2bf59439ce545597667667919d893dc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:48 +0000

    Add package pybound/0.1.3/pyBound-0.1.3.tar.gz

---
+++ packages/pybound/pyBound-0.1.3.tar.gz/pyBound-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pybound']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pybound',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': "Xhaiden D'Souza",
+    'author_email': 'xhaidendsouza@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
