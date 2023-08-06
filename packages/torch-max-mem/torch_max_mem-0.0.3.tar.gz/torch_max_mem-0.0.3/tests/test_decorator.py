cf53a1712937ea7f24fa2e1c4db4101e102b9022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:15 +0000

    Add package torch-max-mem/0.0.3/torch_max_mem-0.0.3.tar.gz

---
+++ packages/torch-max-mem/torch_max_mem-0.0.3.tar.gz/torch_max_mem-0.0.3/tests/test_decorator.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+"""Tests."""
+
+import unittest
+
+import numpy.testing
+import torch
+
+from torch_max_mem import maximize_memory_utilization
+from torch_max_mem.api import maximize_memory_utilization_decorator
+
+
+def knn(x, y, batch_size, k: int = 3):
+    """Compute k-nearst neigbors via batched brute-force distance calculation."""
+    return torch.cat(
+        [
+            torch.cdist(x[start : start + batch_size], y).topk(k=k, dim=1, largest=False).indices
+            for start in range(0, x.shape[0], batch_size)
+        ],
+        dim=0,
+    )
+
+
+wrapped_knn = maximize_memory_utilization_decorator(parameter_name="batch_size")(knn)
+wrapped_knn_stateful = maximize_memory_utilization()(knn)
+
+
+class TestDecorator(unittest.TestCase):
+    """Test the decorator."""
+
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    def test_knn(self):
+        """Test consistent results between original and wrapped method."""
+        x = torch.rand(100, 100, device=self.device)
+        y = torch.rand(200, 100, device=self.device)
+        for batch_size in [1, 10, x.shape[0]]:
+            numpy.testing.assert_array_equal(
+                knn(x, y, batch_size).numpy(),
+                wrapped_knn(x, y, batch_size=x.shape[0])[0].numpy(),
+            )
+
+    def test_knn_stateful(self):
+        """Test consistent results between original and wrapped method for stateful wrapper."""
+        x = torch.rand(100, 100, device=self.device)
+        y = torch.rand(200, 100, device=self.device)
+        for batch_size in [1, 10, x.shape[0]]:
+            numpy.testing.assert_array_equal(
+                knn(x, y, batch_size).numpy(),
+                wrapped_knn_stateful(x, y, batch_size=x.shape[0]).numpy(),
+            )
+
+
+def test_parameter_types():
+    """Test decoration for various parameter types."""
+
+    @maximize_memory_utilization()
+    def positional_or_keyword_only_func(a, batch_size: int):
+        """Evaluate a function where batch_size is a positional or keyword parameter."""
+
+    @maximize_memory_utilization()
+    def keyword_only_func(*a, batch_size: int):
+        """Evaluate a function where batch_size is a keyword-only parameter."""
