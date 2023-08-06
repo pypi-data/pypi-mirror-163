d96637311468a950e76a0cf2ae0dd784bb1379ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:30 +0000

    Add package yaex/0.1.2/yaex-0.1.2.tar.gz

---
+++ packages/yaex/yaex-0.1.2.tar.gz/yaex-0.1.2/README.md
@@ -0,0 +1,42 @@
+# Yet Another EX command library
+
+This is a library based on the ex command. So, it works the same way :)
+
+## Usage
+
+```python
+from yaex import *
+
+result = yaex(
+    append("Hello"),
+    append("World"),
+)
+print(result)
+# >Hello
+# >World
+# >
+
+result = yaex(
+    append("first line"),
+    append("second line"),
+    append("third line"),
+    go_to(2),
+    delete(),
+)
+print(result)
+# >first line
+# >third line
+# >
+
+result = yaex(
+    append("# yaex\n\nThis is a library based on the ex command.\n"),
+    go_to_first_line(),
+    delete(),
+    insert("# Yet Another EX command library"),
+)
+print(result)
+# ># Yet Another EX command library
+# >
+# >This is a library based on the ex command.
+# >
+```
---
+++ packages/yaex/yaex-0.1.2.tar.gz/yaex-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['yaex']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'yaex',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': '# Yet Another EX command library\n\nThis is a library based on the ex command. So, it works the same way :)\n\n## Usage\n\n```python\nfrom yaex import *\n\nresult = yaex(\n    append("Hello"),\n    append("World"),\n)\nprint(result)\n# >Hello\n# >World\n# >\n\nresult = yaex(\n    append("first line"),\n    append("second line"),\n    append("third line"),\n    go_to(2),\n    delete(),\n)\nprint(result)\n# >first line\n# >third line\n# >\n\nresult = yaex(\n    append("# yaex\\n\\nThis is a library based on the ex command.\\n"),\n    go_to_first_line(),\n    delete(),\n    insert("# Yet Another EX command library"),\n)\nprint(result)\n# ># Yet Another EX command library\n# >\n# >This is a library based on the ex command.\n# >\n```\n',
+    'author': 'Emerson Max de Medeiros Silva',
+    'author_email': 'emersonmx@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/emersonmx/yaex',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
