192ad6732a50b757a8d337cd2318353a006bbce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:19 +0000

    Add package joringels/0.0.2/joringels-0.0.2.tar.gz

---
+++ packages/joringels/joringels-0.0.2.tar.gz/joringels-0.0.2/joringels/src/connectors/scp.py
@@ -0,0 +1,34 @@
+# ps_upload.py
+
+import os, sys, getpass
+import subprocess
+import joringels.src.settings as sts
+import joringels.src.get_soc as soc
+
+
+class SCPPS:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def upload(self, serverCreds, localPath=None, *args, safeName, **kwargs):
+        localPath = localPath if localPath is not None else sts.prep_path(safeName)
+        if os.name == "nt":
+            bash = "powershell.exe"
+            script = os.path.join(sts.settingsPath, "connectors", "scp.ps1")
+
+        cmds = [
+            bash,
+            script,
+            serverCreds["rmUserName"],
+            serverCreds["rmHost"],
+            localPath,
+            serverCreds["rmPath"].replace(f"C:\\Users\\{getpass.getuser()}", "~"),
+            serverCreds["rmKey"],
+        ]
+        p = subprocess.Popen(cmds, stdout=sys.stdout)
+        p.communicate()
+
+
+def main(*args, **kwargs):
+    scp = SCPPS(*args, **kwargs)
+    return scp
