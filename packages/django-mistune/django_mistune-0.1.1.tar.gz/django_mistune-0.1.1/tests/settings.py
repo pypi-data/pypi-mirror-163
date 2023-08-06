f73b74120ec36c8129f488a69748fa603deb88b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:33 +0000

    Add package django-mistune/0.1.1/django_mistune-0.1.1.tar.gz

---
+++ packages/django-mistune/django_mistune-0.1.1.tar.gz/django_mistune-0.1.1/tests/settings.py
@@ -0,0 +1,19 @@
+from django_mistune.plugins import HeaderLevels, AddClasses
+
+USE_TZ = True  # Avoid deprecation warning
+
+INSTALLED_APPS = [
+    "django_mistune",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+    },
+]
+
+MISTUNE_STYLES = {
+    "noescape": {"escape": False},
+    "headers": {"plugins": [HeaderLevels(3)]},
+    "classes": {"plugins": [AddClasses({"p": ("a", "b"), "em": "c"})]},
+}
