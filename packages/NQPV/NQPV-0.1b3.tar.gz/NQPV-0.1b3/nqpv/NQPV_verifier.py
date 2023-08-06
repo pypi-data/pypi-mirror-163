e1f85f10ed765fb2a8032ceb995bddc493be6436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:06 +0000

    Add package NQPV/0.1b3/NQPV-0.1b3.tar.gz

---
+++ packages/NQPV/NQPV-0.1b3.tar.gz/NQPV-0.1b3/nqpv/NQPV_verifier.py
@@ -0,0 +1,224 @@
+# ------------------------------------------------------------
+# NQPV_verifier.py
+#
+# method 'verify' provides the user interface of the whole verifier
+# ------------------------------------------------------------
+
+import os
+import numpy as np
+
+from .tools import msg, ver_label, lineno_added
+
+from .NQPV_ast import *
+from . import NQPV_lexer, NQPV_parser, NQPV_la
+
+from . import semantics_analyser
+from .semantics_analyser import check
+from . import backward_transformer
+from .backward_transformer import wlp_verify
+
+def verifiy_reset():
+    # clean up the error info
+    NQPV_la.error_info = ""
+    NQPV_la.silent = True
+    NQPV_lexer.error_info = ""
+    NQPV_lexer.silent = True
+    NQPV_parser.error_info = ""
+    NQPV_parser.silent = True
+    semantics_analyser.error_info = ""
+    semantics_analyser.silent = True
+    backward_transformer.error_info = ""
+    backward_transformer.silent = True
+
+
+def verify(folder_path, lib_path = "", silent = False, total_correctness = False, preserve_pre = False, opt_in_output = False, save_opt = False):
+
+    if total_correctness:
+        print("total correctness not supported yet")
+        return
+
+    verifiy_reset()
+
+    msg("\n= Nondeterministic Quantum Program Verifier Output = \n\n", silent, None)
+
+    msg("Version: " + ver_label + "\n\n", silent, None)
+
+    # Prompt the running path
+    msg("running path: " + os.getcwd() +"\n\n", silent, None)
+
+    # detect the file
+    if folder_path[-1] == '/' or folder_path[-1] == '\\':
+        folder_path = folder_path[:-1]
+    if lib_path != "":
+        if lib_path[-1] == '/' or lib_path[-1] == '\\':
+            lib_path += lib_path[:-1]
+
+    
+    try:
+        p_prog = open(folder_path + '/prog', 'r')
+        prog_str = p_prog.read()
+        p_prog.close()
+    except:
+        print("Error: program file '" + folder_path + "/prog' not found.")
+        return
+
+    
+    # create the output file
+    try:
+        p_output = open(folder_path + '/output.txt', 'w')
+    except:
+        print("Error: cannot create output file '" + folder_path + "/output.txt'.")
+        return
+
+    # add the beginning of output.txt
+    p_output.write("\n= Nondeterministic Quantum Program Verifier Output = \n\n")
+    p_output.write("Version: " + ver_label + "\n\n")
+    p_output.write("running path: " + os.getcwd() +"\n\n")
+
+    msg("folder path: "+ folder_path + "\n\n", silent, p_output)
+
+    if total_correctness:
+        msg("property to verify: Total Correctness\n\n", silent, p_output)
+    else:
+        msg("property to verify: Partial Correctness\n\n", silent, p_output)
+
+    msg("intermediate preconditions preserved: "+("Yes" if preserve_pre else "No") + "\n\n", silent, p_output)
+    msg("show operators in this output: " + ("Yes" if opt_in_output else "No") + "\n\n", silent, p_output)
+    msg("operators saved in running paht: "+ ("Yes" if save_opt else "No") + "\n\n", silent, p_output)
+
+    msg("--------------------------------------------\n", silent, p_output)
+    msg("<prog> \n\n", silent, p_output)
+    msg(lineno_added(prog_str), silent, p_output)
+    msg("\n\n--------------------------------------------\n\n", silent, p_output)
+
+    
+    # syntactic analysis, produce the abstract syntax tree
+    msg("syntactic analysis ...\n\n", silent, None)
+    ast = NQPV_parser.parser.parse(prog_str)
+    if NQPV_lexer.error_info != "" or NQPV_parser.error_info != "":
+        if NQPV_lexer.error_info != "":
+            msg(NQPV_lexer.error_info, False, p_output)
+        else:
+            msg(NQPV_parser.error_info, False, p_output)
+        msg("\nAbort: syntactic analysis not passed.\n", False, p_output)
+        p_output.close()
+        return
+    msg("syntactic analysis passed.\n\n", silent, p_output)
+
+    # semantic analysis
+    msg("semantic analysis ...\n\n", silent, None)
+    pinfo = check(ast, folder_path, lib_path)
+    if pinfo is None:
+        # it means there is error in the semantic analysis
+        if semantics_analyser.error_info != "":
+            msg(semantics_analyser.error_info, False, p_output)
+        elif NQPV_la.error_info != "":
+            msg(NQPV_la.error_info, False, p_output)
+
+        msg("\nAbort: semantic analysis not passed.\n", False, p_output)
+        p_output.close()
+        return
+    msg("semantic analysis passed.\n\n", silent, p_output)
+
+
+    # start verification
+
+    msg("verification starts, calculating weakest (liberal) preconditions...\n\n", silent, None)
+
+    v_result = wlp_verify(ast, pinfo, preserve_pre)
+
+    if backward_transformer.error_info != "":
+        msg(backward_transformer.error_info, False, p_output)
+
+    # declare the result
+    if v_result:
+        msg("Verification Result: Property holds.\n\n", False, p_output)
+    else:
+        # check whether there is while structures
+        if pinfo['while_exists']:
+            msg("Verification Result: Property cannot be determined. A suitable loop invariant may be sufficient.\n\n", False, p_output)
+        else:
+            msg("Verification Result: Property does not hold.\n\n", False, p_output)
+
+    # show the proof outline
+    
+    msg("(proof outline shown in 'output.txt)\n\n", silent, None)
+    msg("--------------------------------------------\n", True, p_output)
+    msg("<prog proof outline> \n\n", True, p_output)
+    msg(lineno_added(NQPV_parser.prog_to_code(ast, "")), True, p_output)
+
+
+
+    # show the operators in the output
+    if opt_in_output:
+
+        # prompt that the operator output is hidden in cmd
+        if not silent:
+            print("(operators shown in 'output.txt')\n\n", end='')
+        
+        msg("\n\n--------------------------------------------\n", True, p_output)
+        msg("<unitary operators> \n\n", True, p_output)
+
+        for id in pinfo['unitary']:
+            msg(id + "\n", True, p_output)
+            msg(str(NQPV_la.tensor_to_matrix(pinfo['unitary'][id])), True, p_output)
+            msg("\n\n", True, p_output)
+        
+        msg("--------------------------------------------\n", True, p_output)
+
+        msg("<Hermitian operators> \n\n", True, p_output)
+
+        for id in pinfo['herm']:
+            msg(id + "\n", True, p_output)
+            msg(str(NQPV_la.tensor_to_matrix(pinfo['herm'][id])), True, p_output)
+            msg("\n\n", True, p_output)
+        
+        msg("--------------------------------------------\n", True, p_output)
+
+        msg("<measurement operators> \n\n", True, p_output)
+
+        for id in pinfo['measure']:
+            msg(id + " RESULT0 \n", True, p_output)
+            msg(str(NQPV_la.tensor_to_matrix(pinfo['measure'][id][0])), True, p_output)
+            msg("\n\n", True, p_output)
+            msg(id + " RESULT1 \n", True, p_output)
+            msg(str(NQPV_la.tensor_to_matrix(pinfo['measure'][id][1])), True, p_output)
+            msg("\n\n", True, p_output)
+        
+        msg("--------------------------------------------\n", True, p_output)
+
+        if preserve_pre:
+            msg("<Hermitians for intermediate preconditions> \n\n", True, p_output)
+
+            for id in pinfo['im_pre-cond']:
+                msg(id + "\n", True, p_output)
+                msg(str(NQPV_la.tensor_to_matrix(pinfo['im_pre-cond'][id])), True, p_output)
+                msg("\n\n", True, p_output)
+            
+            msg("--------------------------------------------\n", True, p_output)
+
+    # save the related operaters
+    if save_opt:
+        msg("Saving operators...\n\n", silent, None)
+
+        for id in pinfo['unitary']:
+            msg("unitary: " + id + ".npy ... ", silent, None)
+            np.save(folder_path + "/" + id + ".npy", pinfo['unitary'][id])
+            msg("done\n", silent, None)
+        for id in pinfo['measure']:
+            msg("measurement: " + id + ".npy ... ", silent, None)
+            np.save(folder_path + "/" + id + ".npy", pinfo['measure'][id])
+            msg("done\n", silent, None)
+        for id in pinfo['herm']:
+            msg("Hermitian Operator: " + id + ".npy ... ", silent, None)
+            np.save(folder_path + "/" + id + ".npy", pinfo['herm'][id])
+            msg("done\n", silent, None)
+        if preserve_pre:
+            for id in pinfo['im_pre-cond']:
+                msg("Hermitian Operator: " + id + ".npy ... ", silent, None)
+                np.save(folder_path + "/" + id + ".npy", pinfo['im_pre-cond'][id])
+                msg("done\n", silent, None)
+
+
+    # close the output file
+    p_output.close()
---
+++ packages/NQPV/NQPV-0.1b3.tar.gz/NQPV-0.1b3/nqpv/semantics_analyser.py
@@ -0,0 +1,226 @@
+# ------------------------------------------------------------
+# semantics_analyser.py
+#
+# semantics analyser for nondeterministic quantum programs
+#
+# verification include: 
+#   - whether the variables are predefined
+#   - whether the unitary/measurement/hermitian operator referred in the program exists in the folder
+#   - whether the unitary/measurement/hermitian operator provided is valid
+#   - whether the unitary/measurement/hermitian operataor matches the variable lists
+#
+# ------------------------------------------------------------
+
+import numpy as np
+from . import NQPV_la
+
+# store the error information
+from .tools import err
+error_info = ""
+silent = False
+
+#check whether the variables in check_ls are predefined in var_ls
+def check_variable(var_ls : list, check_ls : list):
+    global error_info, silent
+
+    used_qvar = []
+    for var in check_ls:
+        # check whether the variable is defined in qvar
+        if var not in var_ls:
+            error_info += err("Error: variable '" + var + "' not defined in qvar\n", silent)
+            return False
+        # check whether the variable has been used
+        if var in used_qvar:
+            error_info += err("Error: Variable '" + var + "' appeared more than once in the variable list "+str(check_ls) + ".\n", silent)
+            return False
+        used_qvar.append(var)
+    return True
+
+def check_unitary(id, unitary_dict, var_ls, run_path, lib_path):
+    global error_info, silent
+
+    if id in unitary_dict:
+        pass
+    else:
+        try:
+            loaded = np.load(lib_path + "/unitary/" + id + ".npy")
+        except:
+            try:
+                loaded = np.load(run_path + "/" + id + ".npy")
+            except:
+                error_info += err("Error: unitary '" + id +".npy' not found\n", silent)
+                return False
+
+        # check for valid unitary
+        if not NQPV_la.check_unity(loaded, id):
+            return False
+
+        unitary_dict[id] = loaded
+
+    # check the dimension informaion
+    if (len(var_ls) * 2 == len(unitary_dict[id].shape)):
+        return True
+    else:
+        error_info += err("Error: The dimensions of unitary '" + id + "' and qvars " + str(var_ls) + " do not match.\n", silent)
+        return False
+
+
+
+def check_measure(id, measure_dict, var_ls, run_path, lib_path):
+    global error_info, silent
+
+    if id in measure_dict:
+        pass
+    else:
+        try:
+            loaded = np.load(lib_path + "/measure/" + id + ".npy")
+        except:
+            try:
+                loaded = np.load(run_path + "/" + id + ".npy")
+            except:
+                error_info += err("Error: measurement '" + id +".npy' not found\n", silent)
+                return False
+
+        # check for valid measurement
+        if not NQPV_la.check_measure(loaded, id):
+            return False
+    
+        measure_dict[id] = loaded
+
+    # check the dimension information
+    # + 1 is for the extra dimension of 0-result and 1-result
+    if (len(var_ls) * 2 + 1 == len(measure_dict[id].shape)):
+        return True
+    else:
+        error_info += err("Error: The dimensions of measurement '" + id + "' and qvars " + str(var_ls) + " do not match.\n", silent)
+        return False
+
+
+def check_hermitian(id, herm_dict, var_ls, run_path, lib_path):
+    # check whether the hermitian is suitable
+    global error_info, silent
+
+    if id in herm_dict:
+        pass
+    else:
+        try:
+            loaded = np.load(lib_path + "/herm/" + id + ".npy")
+        except:
+            try:
+                loaded = np.load(run_path + "/" + id + ".npy")
+            except:
+                error_info += err("Error: hermitian operator '" + id +".npy' not found\n", silent)
+                return False
+
+        # check for valid unitary
+        if not NQPV_la.check_hermitian_predicate(loaded, id):
+            return False
+
+        herm_dict[id] = loaded
+
+    # check the dimension informaion
+    if (len(var_ls) * 2 == len(herm_dict[id].shape)):
+        return True
+    else:
+        error_info += err("Error: The dimensions of hermitian '" + id + "' and qvars " + str(var_ls) + " do not match.\n", silent)
+        return False
+
+
+# check the semantics of prog. load and return the operators
+def check(prog : dict, run_path, lib_path):
+    global error_info, silent
+
+    if prog == None:
+        error_info += err("Error: The input abstract syntax tree is invalid.\n", silent)
+        return None
+
+    # check the declared qvar, ensure the uniqueness
+    if not check_variable(prog['qvar'], prog['qvar']):
+        return None
+
+    pinfo = {
+        'unitary' : {},
+        'measure' : {},
+        'herm' : {},
+        'while_exists' : False
+    }
+    
+    # check the pre-condition and post-condition
+    for herm_tuple in prog['pre-cond']:
+        if not check_variable(prog['qvar'], herm_tuple[1]):
+            return None
+        if not check_hermitian(herm_tuple[0], pinfo['herm'], herm_tuple[1], run_path, lib_path):
+            return None
+    for herm_tuple in prog['post-cond']:
+        if not check_variable(prog['qvar'], herm_tuple[1]):
+            return None
+        if not check_hermitian(herm_tuple[0], pinfo['herm'], herm_tuple[1], run_path, lib_path):
+            return None
+
+    if check_iter(prog['qvar'], prog['sequence'], pinfo, run_path, lib_path) :
+        return pinfo
+    else:
+        return None
+    
+
+def check_iter(var_ls : list, sequence : list, pinfo : dict, run_path, lib_path):
+    for sentence in sequence:
+        if sentence.label == 'SKIP':
+            pass
+        elif sentence.label == 'ABORT':
+            pass
+        elif sentence.label == 'INIT':
+            # check the variables
+            if not check_variable(var_ls, sentence.vls):
+                return False
+        elif sentence.label == 'UNITARY':
+            # check the variables
+            if not check_variable(var_ls, sentence.vls):
+                return False
+            # check the unitary
+            if not check_unitary(sentence.unitary, pinfo['unitary'], sentence.vls, run_path, lib_path):
+                return False
+
+        elif sentence.label == 'IF':
+            # check the variables
+            if not check_variable(var_ls, sentence.measure_vls):
+                return False
+            # check the measure existence
+            if not check_measure(sentence.measure, pinfo['measure'], sentence.measure_vls, run_path, lib_path):
+                return False
+            # check the two subprograms
+            if not check_iter(var_ls, sentence.S0, pinfo, run_path, lib_path):
+                return False
+            if not check_iter(var_ls, sentence.S1, pinfo, run_path, lib_path):
+                return False
+        
+        elif sentence.label == 'WHILE':
+            pinfo['while_exists'] = True
+            # check the hermitian operators
+            for herm_tuple in sentence.inv:
+                if not check_variable(var_ls, herm_tuple[1]):
+                    return False
+                if not check_hermitian(herm_tuple[0], pinfo['herm'], herm_tuple[1], run_path, lib_path):
+                    return False
+            # check the variables
+            if not check_variable(var_ls, sentence.measure_vls):
+                return False
+            # check the measure existence
+            if not check_measure(sentence.measure, pinfo['measure'], sentence.measure_vls, run_path, lib_path):
+                return False
+            # check the subprogram
+            if not check_iter(var_ls, sentence.S, pinfo, run_path, lib_path):
+                return False
+        
+        elif sentence.label == 'NONDET_CHOICE':
+            # check the two subprograms
+            if not check_iter(var_ls, sentence.S0, pinfo, run_path, lib_path):
+                return False
+            if not check_iter(var_ls, sentence.S1, pinfo, run_path, lib_path):
+                return False
+        
+        else:
+            raise Exception("Unknown program structure.")
+
+    # after the examination of all programs in the sequence
+    return True
