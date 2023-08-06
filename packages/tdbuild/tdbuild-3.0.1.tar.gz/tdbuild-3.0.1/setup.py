1d46daa0dc4b8b3840271a9fd8d7f638d5ecf5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:43 +0000

    Add package tdbuild/3.0.1/tdbuild-3.0.1.tar.gz

---
+++ packages/tdbuild/tdbuild-3.0.1.tar.gz/tdbuild-3.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tdbuild']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama>=0.4.5,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['tdbuild = tdbuild.cli:main']}
+
+setup_kwargs = {
+    'name': 'tdbuild',
+    'version': '3.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'spaderthomas',
+    'author_email': 'thomas.spader@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
