e4024fdc79f0faf8be5b1978ba00ed6e07b8019c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:19 +0000

    Add package docker-composition/0.0.2/docker_composition-0.0.2-py2.py3-none-any.whl

---
+++ packages/docker-composition/docker_composition-0.0.2-py2.py3-none-any.whl/composition/composer.py
@@ -0,0 +1,42 @@
+import logging
+import os
+
+import fire
+
+
+class CommandLineInterface:
+    """
+    Composer - a docker-compose package manager.
+
+    install - Installs a given template
+    list - Lists installed applications
+    log_level - Sets the log level for the application
+    """
+
+    def install(self, template="template.yaml"):
+        """
+
+        :param template: str
+            The name of the template file to install (defaults to template.yaml)
+        :return:
+        """
+        logging.debug(f"Installing template {template}")
+        logging.info(f"Hello, {os.getcwd()}, template {template}")
+
+    def list(self):
+        logging.debug("Listing...")
+
+    def log_level(self, level=None):
+        if level is None:
+            logging.info(f"Current log level is {logging.root.level}")
+        logging.debug(f"Setting log level to {level}")
+        logging.root.setLevel(level)
+
+
+def entrypoint():
+    logging.basicConfig(level=logging.INFO)
+    fire.Fire(CommandLineInterface)
+
+
+if __name__ == "__main__":
+    entrypoint()
