bbb404eeb622268c5ce02384121fda0e6a547a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:34 +0000

    Add package spaceone-config/1.10.1rc1/spaceone_config-1.10.1rc1-py3-none-any.whl

---
+++ packages/spaceone-config/spaceone_config-1.10.1rc1-py3-none-any.whl/spaceone/config/model/user_config_model.py
@@ -0,0 +1,36 @@
+from mongoengine import *
+
+from spaceone.core.model.mongo_model import MongoModel
+
+
+class UserConfig(MongoModel):
+    name = StringField(max_length=255, unique_with=['user_id', 'domain_id'])
+    data = DictField()
+    tags = DictField()
+    user_id = StringField(max_length=40)
+    domain_id = StringField(max_length=40)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    meta = {
+        'updatable_fields': [
+            'name',
+            'data',
+            'tags',
+            'updated_at'
+        ],
+        'minimal_fields': [
+            'name'
+        ],
+        'change_query_keys': {
+            'user_self': 'user_id'
+        },
+        'ordering': [
+            'name'
+        ],
+        'indexes': [
+            'name',
+            'user_id',
+            'domain_id',
+        ]
+    }
