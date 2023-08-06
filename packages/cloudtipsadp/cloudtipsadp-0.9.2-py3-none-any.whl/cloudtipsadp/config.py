040689554a939c692f90a9a77a3b7c4a0b96db31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:39 +0000

    Add package cloudtipsadp/0.9.2/cloudtipsadp-0.9.2-py3-none-any.whl

---
+++ packages/cloudtipsadp/cloudtipsadp-0.9.2-py3-none-any.whl/cloudtipsadp/config.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+
+from loguru import logger
+
+logger.add('file.log', format='{time} {level} {message}', level='WARNING',
+           rotation='1 MB', compression='zip')
+
+
+class ConfigurationError(Exception):
+    pass
+
+
+@dataclass(frozen=True)
+class Token:
+    access_token: str
+    expires_in: int
+    token_type: str
+    refresh_token: str
+    scope: str
+
+    def refresh(self):
+        return self.refresh_token
+
+
+class Filter:
+    dateFrom: str
+    dateTo: str
+    phoneNumber: str
+    layoutId: str
+    id: str
+    status: str
+    userId: str
+    page: str
+    limit: str
+
+    def __init__(self, date_from: str = None,
+                 date_to: str = None, phone: str = None,
+                 layout_id: str = None, id: str = None, status: str = None,
+                 user_id: str = None, page: str = None, limit: str = None):
+        self.dateFrom = date_from
+        self.dateTo = date_to
+        self.phoneNumber = phone
+        self.layoutId = layout_id
+        self.id = id
+        self.status = status
+        self.userId = user_id
+        self.page = page
+        self.limit = limit
+
+
+filters = Filter(date_from='2022-05-01',
+                 date_to='2022-08-15',
+                 phone='+79062047500')
+if __name__ == '__main__':
+    print(filters.__dict__)
