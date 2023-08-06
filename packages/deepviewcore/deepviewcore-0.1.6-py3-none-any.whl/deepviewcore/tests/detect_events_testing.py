840513596cea9e2de173cdae20c449cf4c9ebb12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:41 +0000

    Add package deepviewcore/0.1.6/deepviewcore-0.1.6-py3-none-any.whl

---
+++ packages/deepviewcore/deepviewcore-0.1.6-py3-none-any.whl/deepviewcore/tests/detect_events_testing.py
@@ -0,0 +1,20 @@
+from deepviewcore.Video import Video
+
+
+videoPath = "C:/Users/migue/Documents/U/4to/TFG/DeepView/static/videos/Pez1.mp4"
+
+v = Video(videoPath)
+
+events = {}
+
+def action(data):
+  global events
+  events.update(data[1])
+
+v.process(action=action, showContours=False, options = {
+  "events": {"minArea": 200}
+})
+
+print("Se han detectado " + str(len(events)) + " eventos en los segundos: " + str(events.keys()))
+
+
