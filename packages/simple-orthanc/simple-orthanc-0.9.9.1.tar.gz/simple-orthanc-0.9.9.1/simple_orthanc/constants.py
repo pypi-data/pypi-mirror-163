11060d5e81244223637c66766c61cab58f0319cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:12 +0000

    Add package simple-orthanc/0.9.9.1/simple-orthanc-0.9.9.1.tar.gz

---
+++ packages/simple-orthanc/simple-orthanc-0.9.9.1.tar.gz/simple-orthanc-0.9.9.1/simple_orthanc/constants.py
@@ -0,0 +1,16 @@
+import logging
+
+# json keys in orthanc response
+MAINDICOMTAGS =                 'MainDicomTags'
+TYPE =                          'Type'
+ID =                            'ID'
+PARENT  =                       'Parent'
+
+UNKNOWN =                       '!UNKNOWN!'
+LOG_LEVEL =                     logging.ERROR
+PRIVATE =                       'private'
+ASCENDING =                     'ascending'
+DESCENDING =                    'descendig'
+
+# when requesting more than this number of items a progressbar will be shown
+SHOW_PROGRESS_BAR_COUNT =       1000
