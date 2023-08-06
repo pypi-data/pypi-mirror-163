7bb5635c5d7f6fbcfbf91aba09e702a3f5d433bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:45 +0000

    Add package sina-seifouri/0.1/sina_seifouri-0.1.tar.gz

---
+++ packages/sina-seifouri/sina_seifouri-0.1.tar.gz/sina_seifouri-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sina_seifouri',
+    version='0.1',
+    license='MIT',
+    author="Sina Seifouri",
+    author_email='sinasox@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/sinasox/sina_seifouri',
+    keywords='This is just an introduction.',
+    install_requires=[
+      ],
+
+)
---
+++ packages/sina-seifouri/sina_seifouri-0.1.tar.gz/sina_seifouri-0.1/src/sina_seifouri/__init__.py
@@ -0,0 +1,1 @@
+print("Hi im Sina Seifouri")
