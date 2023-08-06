c876e4acf04ec005243cc8b8ccb218f7f26a08c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:38 +0000

    Add package pytest-csv-params/0.3.0/pytest_csv_params-0.3.0-py3-none-any.whl

---
+++ packages/pytest-csv-params/pytest_csv_params-0.3.0-py3-none-any.whl/pytest_csv_params/types.py
@@ -0,0 +1,19 @@
+"""
+Types to ease the usage of the API
+"""
+import csv
+from typing import Any, Callable, Dict, Optional, Type
+
+DataCast = Callable[[str], Any]
+DataCastDict = Dict[str, DataCast]
+DataCasts = Optional[DataCastDict]
+
+BaseDir = Optional[str]
+IdColName = Optional[str]
+
+DataFile = str
+
+CsvDialect = Type[csv.Dialect]
+
+HeaderRenamesDict = Dict[str, str]
+HeaderRenames = Optional[HeaderRenamesDict]
