818f9ac78beb1d578d917482942b7340aae498a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:35 +0000

    Add package vos-mjjo/0.0.3/vos_mjjo-0.0.3.tar.gz

---
+++ packages/vos-mjjo/vos_mjjo-0.0.3.tar.gz/vos_mjjo-0.0.3/tests/test_cordate.py
@@ -0,0 +1,31 @@
+import unittest
+from mjjo import cordate
+
+
+class TestClass(unittest.TestCase):
+    def setUp(self):
+        self.instance = cordate.CorDate()
+        self.test_date = '19880416'
+
+    def tearDown(self):
+        del self.instance
+
+    def test_get_correct_array(self):
+        res = self.instance.get_correct_array(self.test_date)
+        self.assertEqual(res, ['19880416'])
+        
+    def test_get_correct_one(self):
+        res = self.instance.get_correct_one(self.test_date)
+        self.assertEqual(res, '19880416')
+
+    # def test_load_date_dictionary(self):
+    #     res = self.instance.load_date_dictionary()
+    #     self.assertEqual(res, True)
+
+    def test_look_up_array(self):
+        res = self.instance.look_up_array(self.test_date)
+        self.assertEqual(res[0].term, '19880416')
+
+    def test_look_up_one(self):
+        res = self.instance.look_up_one(self.test_date)
+        self.assertEqual(res.term, '19880416')
