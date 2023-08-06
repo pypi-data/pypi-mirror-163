42444cf80f839d07a9a5a57de965262e59e8f2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:53 +0000

    Add package netbox-secretstore/1.3.0/netbox-secretstore-1.3.0.tar.gz

---
+++ packages/netbox-secretstore/netbox-secretstore-1.3.0.tar.gz/netbox-secretstore-1.3.0/netbox_secretstore/__init__.py
@@ -0,0 +1,28 @@
+from extras.plugins import PluginConfig
+
+
+try:
+    from importlib.metadata import metadata
+except ModuleNotFoundError:
+    from importlib_metadata import metadata
+
+metadata = metadata('netbox_secretstore')
+
+
+class NetBoxSecretStore(PluginConfig):
+    name = metadata.get('Name').replace('-', '_')
+    verbose_name = metadata.get('Summary')
+    description = metadata.get('Description')
+    version = metadata.get('Version')
+    author = metadata.get('Author')
+    author_email = metadata.get('Author-email')
+    base_url = 'netbox_secretstore'
+    min_version = '3.3.0beta1'
+    max_version = '3.3.9'
+    required_settings = []
+    default_settings = {
+        'public_key_size': 2048
+    }
+
+
+config = NetBoxSecretStore
