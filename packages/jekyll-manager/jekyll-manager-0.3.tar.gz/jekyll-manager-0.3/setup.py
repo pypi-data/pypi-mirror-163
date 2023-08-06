6e8de56f129ff9ede3607eef33baf055bc088723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:47 +0000

    Add package jekyll-manager/0.3/jekyll-manager-0.3.tar.gz

---
+++ packages/jekyll-manager/jekyll-manager-0.3.tar.gz/jekyll-manager-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='jekyll-manager',
+    description='A command line interface for managing your Jekyll Blog',
+    author = "Kludron",
+    author_email = "kludron@lukewarmsecurityinfo.com",
+    url='https://github.com/kludron/jekyll-manager',
+    packages=['jekyll_manager'],
+    version='0.3',
+    install_requires=[
+        'colorama',
+        'pyyaml',
+    ],
+    scripts=['bin/jekyll-manager'],
+)
