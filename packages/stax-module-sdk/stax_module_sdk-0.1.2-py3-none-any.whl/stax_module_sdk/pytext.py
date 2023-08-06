ed4d3ec1d7648edeea97dff5b1c3dd737374f2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:33 +0000

    Add package stax-module-sdk/0.1.2/stax_module_sdk-0.1.2-py3-none-any.whl

---
+++ packages/stax-module-sdk/stax_module_sdk-0.1.2-py3-none-any.whl/stax_module_sdk/pytext.py
@@ -0,0 +1,21 @@
+# See LICENSE.md file in project root directory
+import re
+from typing import Dict
+from .helpers import getValueByKey
+
+def renderPyText(doc: Dict, body: str):
+    out = body
+    
+    # Link to document
+    link = "https://my.stax.ai/stack/" + (str(doc["stack"]) if doc["stack"] else "unclassified") + "/document/" + str(doc["_id"])
+    
+    # Field value extractor
+    def field(key):
+        return getValueByKey(doc["metadata"], key)
+    
+    for m in re.finditer(r"\{(.*?)\}", body, re.MULTILINE | re.DOTALL):
+        for g in range(0, len(m.groups())):
+            pattern = m.group(g)
+            out = out.replace(pattern, str(eval(pattern[1:-1])))
+    
+    return out
