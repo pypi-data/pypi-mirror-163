61b59aae8743648b6f35e6397aec67a17184e0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:14 +0000

    Add package merdaccia/0.0.2/merdaccia-0.0.2.tar.gz

---
+++ packages/merdaccia/merdaccia-0.0.2.tar.gz/merdaccia-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='merdaccia',
+  version='0.0.2',
+  description='Bella merdaccia aia vista',
+  long_description='Niente mi dispiace',
+  url='',  
+  author='Andrea Primo',
+  author_email='josh@edublocks.org',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='calculator', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
