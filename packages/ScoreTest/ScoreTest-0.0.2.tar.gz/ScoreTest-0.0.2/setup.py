945ad934cf48de16eeff6909f88f7132295c71ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:59 +0000

    Add package ScoreTest/0.0.2/ScoreTest-0.0.2.tar.gz

---
+++ packages/ScoreTest/ScoreTest-0.0.2.tar.gz/ScoreTest-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools #导入setuptools打包工具
+ 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requirements = ["numpy", "scipy"] # 这里填依赖包信息
+
+setuptools.setup(
+    name="ScoreTest",
+    version="0.0.2",    #包版本号，便于维护版本
+    author="WangJing",    #作者，可以写自己的姓名
+    author_email="615964325@qq.com",    #作者联系方式，可写自己的邮箱地址
+    description="异常值检验方法——score检验统计量",#包的简述
+    long_description=long_description,    #包的详细介绍，一般在README.md文件内
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bob-Annette/ScoreTest",    #自己项目地址，比如github的项目地址
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',    #对python的最低版本要求
+)
