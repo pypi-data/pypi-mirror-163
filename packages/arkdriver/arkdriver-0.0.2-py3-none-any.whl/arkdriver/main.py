0216f4d62225092f9aa71d22914c800c9906f633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:47 +0000

    Add package arkdriver/0.0.2/arkdriver-0.0.2-py3-none-any.whl

---
+++ packages/arkdriver/arkdriver-0.0.2-py3-none-any.whl/arkdriver/main.py
@@ -0,0 +1,32 @@
+from arkclient import GameBotClient
+from arkdriver.lib import Ini
+from arkdriver import Admin
+from pathlib import Path
+from time import sleep
+
+__testing__ = False
+
+
+def run():
+    user_config = Path().cwd() / Path('config.ini')
+    default_config = Path(__file__).parent / Path('config.ini')
+    config = Ini(user_config) if user_config.exists() else Ini(default_config)
+    host = config['NETWORK']['host']
+    port = int(config['NETWORK']['port'])
+    password = config['ADMIN']['password']
+    admin = Admin(password=password)
+    admin.enable_admin()
+    admin.execute()
+
+    if not __testing__:
+        with GameBotClient('bot', host=host, port=port) as bot:
+            while True:
+                response = bot.send("ping")
+                data = response.decode()
+                admin.command_list += data
+                admin.execute()
+                sleep(5)
+
+
+if __name__ == "__main__":
+    run()
