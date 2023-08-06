5a88f20389eef2afa70f3a508bde0f9103e3773b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:22 +0000

    Add package asrassessment/0.0.2/asrassessment-0.0.2.tar.gz

---
+++ packages/asrassessment/asrassessment-0.0.2.tar.gz/asrassessment-0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Science/Research',
+    'Operating System :: MacOS',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name = 'asrassessment',
+    version = '0.0.2',
+    description = 'Provides Phoneme Error Rate & Visualisation Assessment',
+    long_description = open('README.md').read(),
+    url='',
+    author = 'Jonathan Lim',
+    author_email = 'Jonathanlimws@gmail.com',
+    license = 'MIT',
+    classifiers = classifiers,
+    keywords = 'ASR',
+    packages = find_packages(),
+    install_requires = ['glob',
+                        'tqdm',
+                        'librosa',
+                        'scipy.io.wavfile',
+                        'numpy',
+                        'pandas',
+                        'sklearn',
+                        'pydub',
+                        's5oundfile',
+                        'subprocess',
+                        'copy',
+                        'difflib',
+                        'eng_to_ipa']
+)
