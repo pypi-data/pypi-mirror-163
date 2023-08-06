062d7688c49caf90455ee25f4573095adaa52078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:11 +0000

    Add package violetear/0.4.0/violetear-0.4.0-py3-none-any.whl

---
+++ packages/violetear/violetear-0.4.0-py3-none-any.whl/violetear/style.py
@@ -0,0 +1,145 @@
+from .selector import Selector
+from .units import Unit, pc, pt, px, em, rem
+from .color import Color
+import textwrap
+
+
+class Style:
+    def __init__(self, selector: Selector = None, *, parent:"Style"=None) -> None:
+        self.selector = selector
+        self._rules = {}
+        self.parent = parent
+
+    def rule(self, attr: str, value) -> "Style":
+        self._rules[attr] = value
+        return self
+
+    def font(self, size: Unit = None, *, weight: str = None) -> "Style":
+        if size:
+            self.rule("font-size", Unit.infer(size))
+
+        if weight:
+            self.rule("font-weight", weight)
+
+        return self
+
+    def color(
+        self, color: Color = None, *, rgb=None, hsv=None, hls=None, alpha: float = None
+    ) -> "Style":
+        if color is None:
+            if rgb is not None:
+                r, g, b = rgb
+                color = Color(r, g, b, alpha)
+            elif hsv is not None:
+                h, s, v = hsv
+                color = Color.from_hsv(h, s, v, alpha)
+            elif hls is not None:
+                h, l, s = hls
+                color = Color.from_hls(h, l, s, alpha)
+
+        return self.rule("color", color)
+
+    def background(
+        self, color: Color = None, *, rgb=None, hsv=None, hls=None, alpha: float = None
+    ) -> "Style":
+        if color is None:
+            if rgb is not None:
+                r, g, b = rgb
+                color = Color(r, g, b, alpha)
+            elif hsv is not None:
+                h, s, v = hsv
+                color = Color.from_hsv(h, s, v, alpha)
+            elif hls is not None:
+                h, l, s = hls
+                color = Color.from_hls(h, l, s, alpha)
+
+        return self.rule("background-color", color)
+
+    def display(self, display: str) -> "Style":
+        self.rule("display", display)
+        return self
+
+    def apply(self, *others: "Style") -> "Style":
+        for other in others:
+            for attr, value in other._rules.items():
+                self.rule(attr, value)
+
+        return self
+
+    def margin(self, all=None, *, left=None, right=None, top=None, bottom=None):
+        if all is not None:
+            self.rule("margin", Unit.infer(all))
+        if left is not None:
+            self.rule("margin-left", Unit.infer(left))
+        if right is not None:
+            self.rule("margin-right", Unit.infer(right))
+        if top is not None:
+            self.rule("margin-top", Unit.infer(top))
+        if bottom is not None:
+            self.rule("margin-bottom", Unit.infer(bottom))
+
+        return self
+
+    def padding(self, all=None, *, left=None, right=None, top=None, bottom=None):
+        if all is not None:
+            self.rule("padding", Unit.infer(all))
+        if left is not None:
+            self.rule("padding-left", Unit.infer(left))
+        if right is not None:
+            self.rule("padding-right", Unit.infer(right))
+        if top is not None:
+            self.rule("padding-top", Unit.infer(top))
+        if bottom is not None:
+            self.rule("padding-bottom", Unit.infer(bottom))
+
+        return self
+
+    def absolute(self, *, left:int=None, right:int=None, top:int=None, bottom:int=None) -> "Style":
+        self.rule("position", "absolute")
+
+        if left is not None:
+            self.rule("left", Unit.infer(left))
+        if right is not None:
+            self.rule("right", Unit.infer(right))
+        if top is not None:
+            self.rule("top", Unit.infer(top))
+        if bottom is not None:
+            self.rule("bottom", Unit.infer(bottom))
+
+        return self
+
+    def rounded(self, radius:Unit=None):
+        if radius is None:
+            radius = 0.25
+
+        return self.rule("border-radius", Unit.infer(radius))
+
+    def width(self, value):
+        return self.rule("width", Unit.infer(value, on_float=pc))
+
+    def height(self, value):
+        return self.rule("height", Unit.infer(value, on_float=pc))
+
+    def css(self, inline: bool = False) -> str:
+        separator = "" if inline else "\n"
+
+        rules = separator.join(
+            f"{attr}: {value};" for attr, value in self._rules.items()
+        )
+
+        if inline:
+            return rules
+
+        return f"{self.selector.css()} {{\n{textwrap.indent(rules, 4*' ')}\n}}"
+
+    def inline(self) -> str:
+        return f'style="{self.css(inline=True)}"'
+
+    def markup(self) -> str:
+        return self.selector.markup()
+
+    def on(self, state) -> "Style":
+        return Style(self.selector.on(state))
+
+    def __str__(self):
+        return self.markup()
