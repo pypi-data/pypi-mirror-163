28555509094a87df80f4fa50aa4ab0b0a283853d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:15 +0000

    Add package quickstats/0.6.3.6/quickstats-0.6.3.6.tar.gz

---
+++ packages/quickstats/quickstats-0.6.3.6.tar.gz/quickstats-0.6.3.6/quickstats/interface/root/helper.py
@@ -0,0 +1,25 @@
+import ROOT
+
+def set_multithread(multithread:bool):
+    if (not ROOT.IsImplicitMTEnabled()) and (multithread):
+        ROOT.EnableImplicitMT()
+    elif (ROOT.IsImplicitMTEnabled()) and (not multithread):
+        ROOT.DisableImplicitMT()
+
+class RMultithreadEnv(object):
+    def __init__(self, enable_multithread:bool):
+        self.original_multithread_state = ROOT.IsImplicitMTEnabled()
+        self.new_multithread_state = enable_multithread
+      
+    def __enter__(self):
+        if (not self.original_multithread_state) and self.new_multithread_state:
+            ROOT.EnableImplicitMT()
+        elif (self.original_multithread_state) and (not self.new_multithread_state):
+            ROOT.DisableImplicitMT()
+        return self
+  
+    def __exit__(self, *args):
+        if (not self.original_multithread_state) and self.new_multithread_state:
+            ROOT.DisableImplicitMT()
+        elif (self.original_multithread_state) and (not self.new_multithread_state):
+            ROOT.EnableImplicitMT()
