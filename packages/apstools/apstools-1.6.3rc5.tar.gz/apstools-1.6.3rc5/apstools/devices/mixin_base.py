b4a66f3442460a20cca4ef4409d40a1fe9c39e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:20 +0000

    Add package apstools/1.6.3rc5/apstools-1.6.3rc5.tar.gz

---
+++ packages/apstools/apstools-1.6.3rc5.tar.gz/apstools-1.6.3rc5/apstools/devices/mixin_base.py
@@ -0,0 +1,29 @@
+"""
+Base class for Device Mixins
++++++++++++++++++++++++++++++++++++++++
+
+.. autosummary::
+
+   ~DeviceMixinBase
+"""
+
+from ophyd import Device
+
+
+class DeviceMixinBase(Device):
+    """
+    Base class for apstools Device mixin classes
+
+    .. index:: Ophyd Device Mixin; DeviceMixinBase
+    """
+
+
+# -----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     jemian@anl.gov
+# :copyright: (c) 2017-2022, UChicago Argonne, LLC
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# -----------------------------------------------------------------------------
