e057f5a825f4e99a71e54a126986258f2ecc7875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:35 +0000

    Add package cs-models/0.0.455/cs_models-0.0.455-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.455-py3-none-any.whl/cs_models/resources/ArtifactQueue/models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    Text,
+    String,
+)
+
+from ...database import Base
+
+
+class ArtifactQueueModel(Base):
+    __tablename__ = "artifact_queue"
+
+    id = Column(Integer, primary_key=True)
+    source_type = Column(String(50), nullable=False)
+    source_id = Column(Integer, nullable=False)
+    artifact_mappings = Column(Text, nullable=True)
+    date = Column(DateTime, nullable=True)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
