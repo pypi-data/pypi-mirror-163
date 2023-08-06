930cd42e699894053f418c2374f56fe34ebdd4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:26 +0000

    Add package pytwoway/0.3.13/pytwoway-0.3.13.tar.gz

---
+++ packages/pytwoway/pytwoway-0.3.13.tar.gz/pytwoway-0.3.13/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open('README.rst', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pytwoway',
+    version='0.3.13',
+    author='Thibaut Lamadon',
+    author_email='thibaut.lamadon@gmail.com',
+    description='Estimate two way fixed effect labor models',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/tlamadon/pytwoway',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'numba',
+        'bipartitepandas>=1.0.35',
+        'scipy',
+        'statsmodels',
+        'pyamg',
+        'qpsolvers',
+        'quadprog',
+        # 'ConfigArgParse',
+        'matplotlib',
+        'tqdm'
+      ],
+    entry_points = {
+        'console_scripts': ['pytw=pytwoway.command_line:main'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
