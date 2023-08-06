20a245d04fc303f90905e30f22bda5e464b25734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:46 +0000

    Add package vacc/0.1.1/vacc-0.1.1.tar.gz

---
+++ packages/vacc/vacc-0.1.1.tar.gz/vacc-0.1.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(name='vacc',
+      version='0.1.1',
+      description="Helper package for running code on UVM's VACC",
+      url='http://github.com/erikweis/vacc',
+      author='Erik Weis',
+      author_email='erik.weis@uvm.edu',
+      license='UVM',
+      packages=setuptools.find_packages(),
+      include_package_data=True
+  )
