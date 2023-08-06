54731ffab52734ed5eac9f33bff0323ef3032411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:23 +0000

    Add package text2sentences/0.0.3/text2sentences-0.0.3.tar.gz

---
+++ packages/text2sentences/text2sentences-0.0.3.tar.gz/text2sentences-0.0.3/text2sentences/sentence.py
@@ -0,0 +1,22 @@
+
+from appPublic.uniqueID import getID
+from .detectlang import detect_lang
+
+class Sentence:
+	def __init__(self, text, start_pos=0, 
+						dialog=False, 
+						semi_sentence=False, 
+						new_paragraph=True):
+		self.sentence_id = getID()
+		self.start_pos = start_pos
+		self.text = text
+		self.dialog = dialog
+		self.semi_sentence=semi_sentence
+		self.new_paragraph = new_paragraph
+		self.language = detect_lang(text)
+		self.lang = self.language['lang']
+
+	def set_lang(self, lang):
+		if self.language['score'] < 0.5:
+			self.lang = lang
+
