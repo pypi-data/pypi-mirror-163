c7f6e2f509a79e7954d82dfb3d58cbed7384874b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:28 +0000

    Add package run-matlab/1.0.6/run_matlab-1.0.6.tar.gz

---
+++ packages/run-matlab/run_matlab-1.0.6.tar.gz/run_matlab-1.0.6/run_matlab/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Requirement: unzip, libncurses5
+
+installationdir="TOREPLACE_INSTALLATION_DIR"
+matlabver="TOREPLACE_MATLAB_VER"
+runtimever="TOREPLACE_RUNTIME_VER"
+functiondir="TOREPLACE_FUNCTION_DIR"
+functionname="TOREPLACE_FUNCTION_NAME"
+functionargs="TOREPLACE_FUNCTION_ARGS"
+
+echo $functionargs
+
+libmwmclmcrrt=v${runtimever::${#runtimever}-2}${runtimever:(-1)}
+MCRver=MCR_${matlabver}_glnxa64
+
+chmod u+x ${functiondir}/files/bin/run_${functionname}.sh
+chmod u+x ${functiondir}/files/bin/${functionname}
+${functiondir}/files/bin/./run_${functionname}.sh ${installationdir}/$MCRver/$libmwmclmcrrt $functionargs
+
