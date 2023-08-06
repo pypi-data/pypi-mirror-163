f1bdee6c31986b6df2bea877ab39fa63d6dd2897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:17 +0000

    Add package cbra/0.92.0/cbra-0.92.0.tar.gz

---
+++ packages/cbra/cbra-0.92.0.tar.gz/cbra-0.92.0/cbra/ext/oauth2/types/iauthorizeendpoint.py
@@ -0,0 +1,54 @@
+"""Declares :class:`IAuthorizeEndpoint`."""
+from cbra.types import IPrincipal
+from .authorizationrequest import AuthorizationRequest
+from .authorizationrequestparameters import AuthorizationRequestParameters
+from .iclient import IClient
+from .istorage import IStorage
+from .isubject import ISubject
+from .isubjectrepository import ISubjectRepository
+
+
+class IAuthorizeEndpoint:
+    __module__: str = 'cbra.ext.oauth2.types'
+    error_url: str | None
+    queryable: bool = True
+    redirect_uri: str | None
+    client: IClient
+    issuer: str
+    storage: IStorage
+    subjects: ISubjectRepository
+
+    async def create_redirect(
+        self,
+        dto: AuthorizationRequestParameters
+    ) -> str:
+        raise NotImplementedError
+
+    async def get_subject(
+        self,
+        client: IClient,
+        principal: IPrincipal,
+        dto: AuthorizationRequest
+    ) -> ISubject | None:
+        """Return a :class:`~cbra.ext.oauth2.types.ISubject` instance
+        representing the currently authenticated principal.
+        """
+        raise NotImplementedError
+
+    async def persist(
+        self,
+        dto: AuthorizationRequest
+    ) -> None:
+        """Inspect the request parameters and persist the appropriate
+        objects to the transient storage.
+        """
+        raise NotImplementedError
+
+    async def validate_request(
+        self,
+        client: IClient,
+        subject: ISubject,
+        dto: AuthorizationRequest
+    ) -> None:
+        """Validates an OAuth 2.0 authorization request."""
+        raise NotImplementedError
