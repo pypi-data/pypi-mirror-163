35341506ada1c11b37fca0de65442e22cc4faed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:26 +0000

    Add package outlier-detector-z/0.0.8/outlier-detector-z-0.0.8.tar.gz

---
+++ packages/outlier-detector-z/outlier-detector-z-0.0.8.tar.gz/outlier-detector-z-0.0.8/README.md
@@ -0,0 +1,7 @@
+# outlier-detector-z
+Outlier detector using Z-score. application accepts only list or tuple. 
+By default threshold set as 3 std. Any element z-score more than threshold will be considered as outlier
+
+## run pip command 
+## from outlier_detector import outlier_detector
+## outlier = outlier_detector.detect_outlier([1,2,3,4,5,6,7,8,9,200]) 
---
+++ packages/outlier-detector-z/outlier-detector-z-0.0.8.tar.gz/outlier-detector-z-0.0.8/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup,find_packages
+from typing import List
+import os
+
+CWD = os.getcwd()
+#Declaring variables for setup functions
+PROJECT_NAME="outlier-detector-z"
+VERSION="0.0.8"
+AUTHOR="Mohamed Naji Aboo"
+DESRCIPTION="Application is used to detect the outliers in a list or tuple"
+
+REQUIREMENT_FILE_NAME= os.path.join(CWD, "requirements.txt")
+                       
+ 
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements_list() -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(), 
+install_requires= ["numpy"]
+)
