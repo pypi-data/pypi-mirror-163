c4b5892e5e4d63343391002e9abf5c0f4e59cc5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:13 +0000

    Add package dist-kanishka/0.2/dist_kanishka-0.2.tar.gz

---
+++ packages/dist-kanishka/dist_kanishka-0.2.tar.gz/dist_kanishka-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_kanishka',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_kanishka'],
+      author = 'Kanishka Dandeniya',
+      author_email = 'kanishka.d@eng.pdn.ac.lk',
+      zip_safe=False)
