07c26c7ee3a971d7229efe852946f75406a51ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:52 +0000

    Add package pyrcache/1.1.0/pyrcache-1.1.0.tar.gz

---
+++ packages/pyrcache/pyrcache-1.1.0.tar.gz/pyrcache-1.1.0/rcache/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+import requests
+
+# GET one object
+def rget(key: str, host: str = "localhost", port: int = 8080, db: int = 0) -> any:
+    return rsend("get", key, "", host=host, port=port, db=db)
+
+# POST one object
+def rset(key: str, value: str, host: str = "localhost", port: int = 8080, db: int = 0):
+    return rsend("set", key, value, host=host, port=port, db=db)
+
+# GET all keys
+def rkeys(host: str = "localhost", port: int = 8080, db: int = 0) -> list[str]:
+    return rsend("get", "", "", host=host, port=port, db=db)
+
+# Send a request
+def rsend(method: str, key: str, value: str, host: str = "localhost", port: int = 8080, db: int = 0):
+    params = {}
+    if key != "":
+        params.update({"key": key})
+    if value != "":
+        params.update({"value": value})
+    resp = requests.get(f"http://{host}:{port}/{db}/{method}", params=params)
+    return resp.json()
