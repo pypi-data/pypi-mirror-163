4f4137afefd9464c39c03b0931c281ddb94e40d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:45 +0000

    Add package one-team-one-dream/0.0.2/one-team-one-dream-0.0.2.tar.gz

---
+++ packages/one-team-one-dream/one-team-one-dream-0.0.2.tar.gz/one-team-one-dream-0.0.2/README.md
@@ -0,0 +1,4 @@
+Test Environment
+
+
+Author: **Amandeep Saluja**
---
+++ packages/one-team-one-dream/one-team-one-dream-0.0.2.tar.gz/one-team-one-dream-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="one-team-one-dream",
+    version="0.0.2",
+    author="Amandeep Saluja",
+    author_email="asaluja@whitehorseliquidity.com",
+    description="A package to consolidate all the commonly used functions within Whitehorse Liquidity Partners",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.linkedin.com/in/salujaamandeep/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
