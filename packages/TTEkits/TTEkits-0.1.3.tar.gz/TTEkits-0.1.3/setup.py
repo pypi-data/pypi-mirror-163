39f132894c0c00a3b75ca906688a05a627432207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:11 +0000

    Add package TTEkits/0.1.3/TTEkits-0.1.3.tar.gz

---
+++ packages/TTEkits/TTEkits-0.1.3.tar.gz/TTEkits-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="TTEkits",
+    version='0.1.3',
+    description="This is a travel time estimation Python Library!",
+    py_modules=["model"],
+    package_dir={'': 'TTEkits'},
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    extras_require = {"dev":[],},
+    url="https://github.com/Elon-Lau/TTEkits",
+    author="Elon Lau",
+    author_email="weitinglau1999@gmail.com",
+    setup_requires=['wheel']
+)
