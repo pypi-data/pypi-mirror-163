4b73a6f5ca7fd324a5e96042496a7b554578c9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:55 +0000

    Add package CrawlersTools/1.2.1/CrawlersTools-1.2.1.tar.gz

---
+++ packages/CrawlersTools/CrawlersTools-1.2.1.tar.gz/CrawlersTools-1.2.1/CrawlersTools/piplines/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Project : CrawlersTools
+# @Time    : 2022/8/15 11:29
+# @Author  : MuggleK
+# @File    : __init__.py
+
+from CrawlersTools.piplines.mongo_pipeline import MongoPipeline
+from CrawlersTools.piplines.mysql_pipeline import MysqlPipeline
+from CrawlersTools.piplines.redis_pipeline import RedisPipeline
