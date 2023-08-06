d306248645eaa1d9a7d53ae359dc423b8e35a07f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:09 +0000

    Add package async-get-vedio/1.2/async_get_vedio-1.2.tar.gz

---
+++ packages/async-get-vedio/async_get_vedio-1.2.tar.gz/async_get_vedio-1.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name = 'async_get_vedio',
+    version = '1.2',
+    author = 'wut_2021_dx_ljs',
+    author_email = '3480339804@qq.com',
+    description = 'Get vedio',
+    long_description = '解决了由pyautogui引起的输入法混乱问题.',
+    url = 'https://github.com/wutljs/async_get_vedio',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
