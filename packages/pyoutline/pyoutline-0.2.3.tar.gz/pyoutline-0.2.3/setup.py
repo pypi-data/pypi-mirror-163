3470f971494d71b1658a9d0aba41e635d07c81aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:01 +0000

    Add package pyoutline/0.2.3/pyoutline-0.2.3.tar.gz

---
+++ packages/pyoutline/pyoutline-0.2.3.tar.gz/pyoutline-0.2.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="pyoutline",
+    version='0.2.3',
+    license='MIT',
+    description = 'A simple CLI app to start Outline Proxy',
+    long_description = open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author = 'NonProjects',
+    author_email = 'thenonproton@pm.me',
+    url = 'https://github.com/NonProjects/pyoutline',
+    download_url = 'https://github.com/NonProjects/pyoutline/archive/refs/tags/v0.2.3.tar.gz',
+    py_modules=['pyoutline', 'pyoutline_tools'],
+    install_requires=['click'],
+    entry_points='''
+        [console_scripts]
+        pyoutline=pyoutline:safe_cli
+    ''',
+)
