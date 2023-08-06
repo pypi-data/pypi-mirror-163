d7db3e458d873b4f60aa689541fae04e69896c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:53 +0000

    Add package onlinecsat/0.1.0/onlinecsat-0.1.0.tar.gz

---
+++ packages/onlinecsat/onlinecsat-0.1.0.tar.gz/onlinecsat-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='onlinecsat',
+    version='0.1.0',    
+    description='OnlineCSAT use case',
+    author='John Harney',
+    author_email='John.Harney@Dell.com',
+    packages=['onlinecsat'],
+    install_requires=[
+                      'pandas',
+                      'numpy',    
+                      'scipy',
+                      'nltk',
+                      'sklearn'
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
