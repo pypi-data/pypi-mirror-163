bdbab0a77d27b8b9c5b4e47dd45c337c63f392e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:22 +0000

    Add package openapi-django/1.1.3/openapi_django-1.1.3.tar.gz

---
+++ packages/openapi-django/openapi_django-1.1.3.tar.gz/openapi_django-1.1.3/openapi_django/openapi_utils/decorators.py
@@ -0,0 +1,16 @@
+def openapi(return_class=None, description=None, path_parameters=None, parameters=None, body=None, answer_content=None):
+    def decorate(f):
+        def wrapper(*args, **kwargs):
+            if len(args) == 1:
+                if args[0] == 'openapi':
+                    return {
+                        'return_class': return_class,
+                        'description': description,
+                        'path_parameters': path_parameters,
+                        "parameters": parameters,
+                        "body": body,
+                        "answer_content": answer_content
+                    }
+            return f(*args, **kwargs)
+        return wrapper
+    return decorate
