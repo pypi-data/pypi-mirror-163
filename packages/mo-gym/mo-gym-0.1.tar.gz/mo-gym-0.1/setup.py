4247fe35a00170bbadfa04079f780fe1c494d1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:48 +0000

    Add package mo-gym/0.1/mo-gym-0.1.tar.gz

---
+++ packages/mo-gym/mo-gym-0.1.tar.gz/mo-gym-0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+# Environment-specific dependencies.
+extras = {
+    "mario": ["nes-py", "gym-super-mario-bros"],
+    "minecart" : ["scipy"]
+}
+extras["all"] = extras["mario"] + extras["minecart"]
+
+
+setup(
+    name="mo-gym",
+    version="0.1",
+    description="Environments for Multi-Objective RL.",
+    url="https://www.github.com/LucasAlegre/mo-gym",
+    author="LucasAlegre",
+    author_email="lnalegre@inf.ufrgs.br",
+    license="MIT",
+    packages=["mo_gym"],
+    install_requires=[
+        "gym==0.24.1", # 0.25 has breaking changes
+        "numpy",
+        "pygame",
+        "scipy",
+        "pymoo",
+    ],
+    extras_require=extras,
+    tests_require=["pytest", "mock"],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
