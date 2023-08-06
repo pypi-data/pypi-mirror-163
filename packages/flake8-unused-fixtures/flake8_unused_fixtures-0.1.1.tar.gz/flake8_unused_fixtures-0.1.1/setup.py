1849f4e111ad42b1882a6bbdf35b46032d8c4d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:44 +0000

    Add package flake8-unused-fixtures/0.1.1/flake8_unused_fixtures-0.1.1.tar.gz

---
+++ packages/flake8-unused-fixtures/flake8_unused_fixtures-0.1.1.tar.gz/flake8_unused_fixtures-0.1.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['flake8_unused_fixtures']
+install_requires = \
+['astpretty>=3.0.0,<4.0.0', 'flake8>=5.0.4,<6.0.0']
+
+entry_points = \
+{'flake8.extension': ['FUF = flake8_unused_fixtures:Plugin']}
+
+setup_kwargs = {
+    'name': 'flake8-unused-fixtures',
+    'version': '0.1.1',
+    'description': 'Warn about unnecesary fixtures in tests definition.',
+    'long_description': None,
+    'author': 'Marcin Binkowski',
+    'author_email': 'binq661@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/MarcinBinkowski/flake8_unused_fixtures',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
