ac5e2d622ecfb3a5c7c4d348889889ce4323472f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:48 +0000

    Add package handyscikit/0.0.7.3/handyscikit-0.0.7.3.tar.gz

---
+++ packages/handyscikit/handyscikit-0.0.7.3.tar.gz/handyscikit-0.0.7.3/setup.py
@@ -0,0 +1,19 @@
+"""
+Type this in terminal to upgrade the package. (Write here for remember)
+>> python setup.py sdist bdist_wheel
+>> twine upload dist/*
+"""
+from setuptools import find_packages, setup
+
+setup(name="handyscikit",
+      version="0.0.7.3",
+      description="Unstructured grid online.",
+      author="Hong Peng",
+      python_requires=">=3.7.0",
+      url="https://github.com/minho-hong/handyscikit.git",
+      package_data={"handyscikit.plot":["*.ttc"]},
+      packages=find_packages(),
+      install_requires=["cython", "gmsh", "numpy", "vtk"],
+      license="GPL")
+
+# todo: commit了但是没有上传pypi。
