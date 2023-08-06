37293559c7c4ff187bbef77bd3a2f195b5b5ea5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:55 +0000

    Add package dilsorting/1.0.0/dilsorting-1.0.0-py3-none-any.whl

---
+++ packages/dilsorting/dilsorting-1.0.0-py3-none-any.whl/dilsorting/insertionsort.py
@@ -0,0 +1,10 @@
+def insertion_sort(array):
+    for step in range(1, len(array)):
+        key = array[step]
+        j = step - 1
+
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j = j - 1
+
+        array[j + 1] = key
