be1e18d06ce987a5b44665c3b90d39994e07d6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:13 +0000

    Add package pytest-csv-params/0.0.3/pytest_csv_params-0.0.3-py3-none-any.whl

---
+++ packages/pytest-csv-params/pytest_csv_params-0.0.3-py3-none-any.whl/pytest_csv_params/plugin.py
@@ -0,0 +1,15 @@
+"""
+Pytest Plugin Entrypoint
+"""
+
+from _ptcsvp.configure import pytest_configure as _pytest_configure
+from _ptcsvp.configure import pytest_unconfigure as _pytest_unconfigure
+from _ptcsvp.version import check_pytest_version, check_python_version
+
+# Fist at all, check if the python & pytest version matches
+check_python_version()
+check_pytest_version()
+
+# Basic config
+pytest_configure = _pytest_configure
+pytest_unconfigure = _pytest_unconfigure
