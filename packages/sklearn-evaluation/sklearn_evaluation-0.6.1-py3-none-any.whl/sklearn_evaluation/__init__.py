369682011f24492ce7c52f7fed90c6d53538a66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:05 +0000

    Add package sklearn-evaluation/0.6.1/sklearn_evaluation-0.6.1-py3-none-any.whl

---
+++ packages/sklearn-evaluation/sklearn_evaluation-0.6.1-py3-none-any.whl/sklearn_evaluation/__init__.py
@@ -0,0 +1,25 @@
+__version__ = '0.6.1'
+
+from ploomber_core.telemetry.telemetry import Telemetry
+
+from .evaluator import ClassifierEvaluator
+from .nb.NotebookIntrospector import NotebookIntrospector
+from .nb.NotebookCollection import NotebookCollection
+from .nb.NotebookDatabase import NotebookDatabase
+from .SQLiteTracker import SQLiteTracker
+
+__all__ = [
+    'ClassifierEvaluator',
+    'NotebookIntrospector',
+    'SQLiteTracker',
+    'NotebookCollection',
+    'NotebookDatabase',
+]
+
+telemetry = Telemetry(
+    api_key="phc_3Of87HTxtAH0GnqU5ApdJnUWUcDhaR11LuEHtVCV41Z",
+    package_name="sklearn-evaluation",
+    version=__version__,
+)
+
+telemetry.log_api('imported')
