819251ace1c2bcd4c0dff29b29e074965f34d260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:04 +0000

    Add package gdastudio/0.3.1/gdastudio-0.3.1.tar.gz

---
+++ packages/gdastudio/gdastudio-0.3.1.tar.gz/gdastudio-0.3.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.3.1'
+DESCRIPTION = 'SQL Server,CSV & JSON Connection Objects and Tools'
+
+# Setting up
+setup(
+    name="gdastudio",
+    version=VERSION,
+    author="Kamil Martenczuk",
+    author_email="<kamil.martenczuk@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pypyodbc', 'tqdm', 'colorama', 'psycopg2'],
+    keywords=['python', 'sql', 'csv', 'json'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
