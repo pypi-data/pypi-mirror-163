556adb4e6bc73c96accfc7c428f0ebb478a6de0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:43 +0000

    Add package hatch/1.4.1/hatch-1.4.1.tar.gz

---
+++ packages/hatch/hatch-1.4.1.tar.gz/hatch-1.4.1/docs/next-steps.md
@@ -0,0 +1,11 @@
+# Next steps
+
+-----
+
+At this point you should have a basic understanding of how to use Hatch.
+
+Now you may want to check out advanced configuration for [environments](config/environment/overview.md) or [builds](config/build.md), set up your [preferred shell](config/hatch.md#shell), or read more about Hatch's [CLI](cli/about.md).
+
+After that, check out the [Hatch Showcase](https://github.com/ofek/hatch-showcase) project to see examples of what is possible.
+
+Finally, if you see a need, feel free to write a [plugin](plugins/about.md) for extended functionality.
---
+++ packages/hatch/hatch-1.4.1.tar.gz/hatch-1.4.1/tests/cli/version/test_version.py
@@ -0,0 +1,88 @@
+from hatch.project.core import Project
+
+
+def test_show_dynamic(hatch, temp_dir):
+    project_name = 'My.App'
+
+    with temp_dir.as_cwd():
+        hatch('new', project_name)
+
+    path = temp_dir / 'my-app'
+
+    with path.as_cwd():
+        result = hatch('version')
+
+    assert result.exit_code == 0, result.output
+    assert result.output == '0.0.1\n'
+
+
+def test_set_dynamic(hatch, helpers, temp_dir):
+    project_name = 'My.App'
+
+    with temp_dir.as_cwd():
+        hatch('new', project_name)
+
+    path = temp_dir / 'my-app'
+
+    with path.as_cwd():
+        result = hatch('version', 'minor,rc')
+
+    assert result.exit_code == 0, result.output
+    assert result.output == helpers.dedent(
+        """
+        Old: 0.0.1
+        New: 0.1.0rc0
+        """
+    )
+
+    with path.as_cwd():
+        result = hatch('version')
+
+    assert result.exit_code == 0, result.output
+    assert result.output == '0.1.0rc0\n'
+
+
+def test_show_static(hatch, temp_dir):
+    project_name = 'My.App'
+
+    with temp_dir.as_cwd():
+        hatch('new', project_name)
+
+    path = temp_dir / 'my-app'
+
+    project = Project(path)
+    config = dict(project.raw_config)
+    config['project']['version'] = '1.2.3'
+    config['project']['dynamic'].remove('version')
+    project.save_config(config)
+
+    with path.as_cwd():
+        result = hatch('version')
+
+    assert result.exit_code == 0, result.output
+    assert result.output == '1.2.3\n'
+
+
+def test_set_static(hatch, helpers, temp_dir):
+    project_name = 'My.App'
+
+    with temp_dir.as_cwd():
+        hatch('new', project_name)
+
+    path = temp_dir / 'my-app'
+
+    project = Project(path)
+    config = dict(project.raw_config)
+    config['project']['version'] = '1.2.3'
+    config['project']['dynamic'].remove('version')
+    project.save_config(config)
+
+    with path.as_cwd():
+        result = hatch('version', 'minor,rc')
+
+    assert result.exit_code == 1, result.output
+    assert result.output == helpers.dedent(
+        """
+        Cannot set version when it is statically defined by the `project.version` field
+        """
+    )
