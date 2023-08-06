a15d02b4cc256c82c3f6ac6ef07da0cb92cb8106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:52 +0000

    Add package debuglater/1.4.2/debuglater-1.4.2.tar.gz

---
+++ packages/debuglater/debuglater-1.4.2.tar.gz/debuglater-1.4.2/src/debuglater/__init__.py
@@ -0,0 +1,6 @@
+from debuglater.pydump import run, excepthook_factory
+from debuglater.ipython import patch_ipython
+
+__all__ = ['run', 'excepthook_factory', 'patch_ipython']
+
+__version__ = "1.4.2"
