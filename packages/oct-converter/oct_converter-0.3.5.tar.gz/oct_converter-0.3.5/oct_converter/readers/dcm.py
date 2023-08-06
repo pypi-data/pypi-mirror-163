bad80bb50721375cbf83e1bcd29884099c634cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:47 +0000

    Add package oct-converter/0.3.5/oct_converter-0.3.5.tar.gz

---
+++ packages/oct-converter/oct_converter-0.3.5.tar.gz/oct_converter-0.3.5/oct_converter/readers/dcm.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from oct_converter.image_types import OCTVolumeWithMetaData
+
+
+class Dicom(object):
+    def __init__(self, filepath):
+        self.filepath = Path(filepath)
+        if not self.filepath.exists():
+            raise FileNotFoundError(self.filepath)
+
+    def read_oct_volume(self):
+        """Reads OCT data.
+
+        Returns:
+            obj:OCTVolumeWithMetaData
+        """
+        import pydicom
+
+        dicom_data = pydicom.dcmread(self.filepath)
+        pixel_data = dicom_data.pixel_array
+        oct_volume = OCTVolumeWithMetaData(volume=pixel_data)
+        return oct_volume
