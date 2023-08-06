9ca233d07df8d4e6f7192504aa07c922d20f5c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:43 +0000

    Add package untitleoop/0.0.1/untitleoop-0.0.1.tar.gz

---
+++ packages/untitleoop/untitleoop-0.0.1.tar.gz/untitleoop-0.0.1/untitleoop/School.py
@@ -0,0 +1,35 @@
+# School.py
+
+# -*-coding: utf-8 -*-
+
+import Untitled
+from Untitled import Student,SpecialStudent
+from Untitled import *
+
+def Test():
+    '''
+    ตัวอย่างการเขียนโปรแกรมโดยใช้แพคเกจนี้
+    st1 = Untitled.Student('Alberto','Einsteino')
+    print(st1)
+
+    st2 = Student('Steven','Joops')
+    print(st2)
+
+    stp1 = SpecialStudent('Baron','Zemo','Zeop')
+    print(stp1)
+
+    teacher1 = Teacher('Alana Hayes')
+    print(teacher1)
+    
+    '''
+st1 = Untitled.Student('Alberto','Einsteino')
+print(st1)
+
+st2 = Student('Steven','Joops')
+print(st2)
+
+stp1 = SpecialStudent('Baron','Zemo','Zeop')
+print(stp1)
+
+teacher1 = Teacher('Alana Hayes')
+print(teacher1)
---
+++ packages/untitleoop/untitleoop-0.0.1.tar.gz/untitleoop-0.0.1/untitleoop/__init__.py
@@ -0,0 +1,3 @@
+#-*- coding: utf-8 -*-
+from untitleoop.Untitled import Student,Tesla,SpecialStudent,Teacher
+from untitleoop.School import Test
