2b51d2c383514a8e170f2656bb8ebeddad7a3d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:18 +0000

    Add package simple-slurm/0.2.5/simple_slurm-0.2.5.tar.gz

---
+++ packages/simple-slurm/simple_slurm-0.2.5.tar.gz/simple_slurm-0.2.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='simple_slurm',
+    version='0.2.5',
+    author='Arturo Mendoza',
+    description='A simple Python wrapper for Slurm with flexibility in mind.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/amq92/simple_slurm',
+    packages=find_packages(),
+    package_data={'': ['*.txt']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    license='GNU Affero General Public License v3',
+)
