158f60780921afaa22235e63fb594f516a8fba43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:30 +0000

    Add package wenxin-api/0.0.1.4/wenxin_api-0.0.1.4-py3-none-any.whl

---
+++ packages/wenxin-api/wenxin_api-0.0.1.4-py3-none-any.whl/wenxin_api/variable.py
@@ -0,0 +1,6 @@
+""" wenxin global variables """
+# http request
+TIMEOUT_SECS = 600
+MAX_CONNECTION_RETRIES = 2
+REQUEST_SLEEP_TIME = 20
+ACCESS_TOKEN_URL = "https://wenxin.baidu.com/younger/portal/api/oauth/token"
