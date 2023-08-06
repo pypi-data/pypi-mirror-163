7c8a0fd5c41c7d0b1d83255373a3f45f291fac5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:14 +0000

    Add package distributions-mango/0.1/distributions_mango-0.1.tar.gz

---
+++ packages/distributions-mango/distributions_mango-0.1.tar.gz/distributions_mango-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_mango',
+      version='0.1',
+      description='Gaussian distributions and Binomial Distributions',
+      packages=['distributions_mango'],
+      zip_safe=False)
