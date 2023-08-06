b0173330cee62d354b68d3304b029dab7b71d049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:38 +0000

    Add package pygicord/1.1.3/pygicord-1.1.3.tar.gz

---
+++ packages/pygicord/pygicord-1.1.3.tar.gz/pygicord-1.1.3/pygicord/enums.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+__all__ = ("StopAction", "Config")
+
+
+class StopAction(Enum):
+    DO_NOTHING = 0
+    DELETE_MESSAGE = 1
+    CLEAR_REACTIONS = 2
+
+
+class Config(Enum):
+    DEFAULT = 0  # controller: first, previous, stop, next, last, input
+    MINIMAL = 1  # controller: previous, stop, next
+    PLAIN = 2  # controller: first, previous, stop, next, last
+    RICH = 3  # controller: all defaults + lock
---
+++ packages/pygicord/pygicord-1.1.3.tar.gz/pygicord-1.1.3/setup.py
@@ -0,0 +1,46 @@
+import os
+import re
+
+from setuptools import setup
+
+version = ""
+with open("pygicord/__init__.py") as fp:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fp.read(), re.MULTILINE).group(1)
+
+
+if not version:
+    raise RuntimeError("Cannot set the version.")
+
+
+def read(fp):
+    return open(os.path.join(os.path.dirname(__file__), fp)).read()
+
+
+setup(
+    name="pygicord",
+    author="Davide Tacchini",
+    url="https://github.com/davidetacchini/pygicord",
+    version=version,
+    license="MIT",
+    packages=["pygicord"],
+    keywords=["discord.py", "paginator"],
+    description="A pagination wrapper for discord.py",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=read("requirements.txt"),
+    python_requires=">=3.7.0",
+    extras_require={"dev": ["black", "isort", "flake8"]},
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+)
