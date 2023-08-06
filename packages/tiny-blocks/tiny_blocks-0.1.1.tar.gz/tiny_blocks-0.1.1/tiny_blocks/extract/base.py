4768aa7ba62641c0f55efa30b21bea030238d5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:44 +0000

    Add package tiny-blocks/0.1.1/tiny_blocks-0.1.1.tar.gz

---
+++ packages/tiny-blocks/tiny_blocks-0.1.1.tar.gz/tiny_blocks-0.1.1/tiny_blocks/extract/base.py
@@ -0,0 +1,38 @@
+import logging
+import abc
+from typing import Iterator
+
+import pandas as pd
+from tiny_blocks.base import BaseBlock, KwargsBase
+
+__all__ = ["ExtractBase", "KwargsExtractBase"]
+
+
+logger = logging.getLogger(__name__)
+
+
+class KwargsExtractBase(KwargsBase):
+    """
+    Kwargs Extract Block
+    """
+
+    pass
+
+
+class ExtractBase(BaseBlock, abc.ABC):
+    """
+    Extract Base Block.
+
+    Each extraction Block implement the `get_iter` method.
+    This method return an Iterator of chunked DataFrames
+    """
+
+    @abc.abstractmethod
+    def get_iter(self) -> Iterator[pd.DataFrame]:
+        """
+        Return an iterator of chunked dataframes
+
+        The `chunksize` is defined as kwargs in each
+        extraction block
+        """
+        raise NotImplementedError
