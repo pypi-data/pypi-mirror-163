e511588c9cc2820cbd20c0257ccab3031d5616e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:14 +0000

    Add package dfktools/0.1.4/dfktools-0.1.4.tar.gz

---
+++ packages/dfktools/dfktools-0.1.4.tar.gz/dfktools-0.1.4/src/dfktools/meditation_example.py
@@ -0,0 +1,54 @@
+import logging
+from web3 import Web3
+import sys
+import time
+import meditation.meditation as meditation
+
+if __name__ == "__main__":
+    log_format = '%(asctime)s|%(name)s|%(levelname)s: %(message)s'
+
+    logger = logging.getLogger("DFK-meditation")
+    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(level=logging.INFO, format=log_format, stream=sys.stdout)
+
+    rpc_server = 'https://api.harmony.one'
+    logger.info("Using RPC server " + rpc_server)
+
+    private_key = None  # set private key
+    account_address = '0x2E7669F61eA77F02445A015FBdcFe2DE47083E02'
+    gas_price_gwei = 10
+    tx_timeout_seconds = 30
+    w3 = Web3(Web3.HTTPProvider(rpc_server))
+
+    active_meditations = meditation.get_active_meditations(account_address, rpc_server)
+    logger.info("Pending meditation on address " + str(account_address) + ": "+str(active_meditations))
+
+    level = 1
+    hero_id = 1
+    required_runes = meditation.get_required_runes(level, rpc_server)
+    meditation.start_meditation(1, meditation.stat2id('strength'), meditation.stat2id('endurance'), meditation.stat2id('luck'),
+                                meditation.ZERO_ADDRESS, private_key, w3.eth.getTransactionCount(account_address),
+                                gas_price_gwei, tx_timeout_seconds, rpc_server, logger)
+    hero_meditation = meditation.get_hero_meditation(hero_id, rpc_server)
+    logger.info("Pending meditation "+str(hero_meditation))
+    time.sleep(5)
+    tx_receipt = meditation.complete_meditation(hero_id, private_key, w3.eth.getTransactionCount(account_address),
+                                   gas_price_gwei, tx_timeout_seconds, rpc_server, logger)
+
+    level_up = meditation.parse_meditation_results(tx_receipt, rpc_server)
+                
+    meditation_results_msg = ""
+    for hero_id in level_up:
+        meditation_results_msg += f"""
+                                    ```
+                                    Hero: {hero_id}
+                                        Level: {level_up[hero_id]['new level']}  STAM: +{level_up[hero_id].get("STAM", {}).get("increase", 0)}
+                                        STR: +{level_up[hero_id].get("STR", {}).get("increase", 0)}  DEX: +{level_up[hero_id].get("DEX", {}).get("increase", 0)}
+                                        AGI: +{level_up[hero_id].get("AGI", {}).get("increase", 0)}  VIT: +{level_up[hero_id].get("VIT", {}).get("increase", 0)}
+                                        END: +{level_up[hero_id].get("END", {}).get("increase", 0)}  INT: +{level_up[hero_id].get("INT", {}).get("increase", 0)}
+                                        WIS: +{level_up[hero_id].get("WIS", {}).get("increase", 0)}  LCK: +{level_up[hero_id].get("LCK", {}).get("increase", 0)}
+                                    ```"""
+
+    msg = f"Completed level up: {meditation_results_msg}"
+    logger.info(msg)
+
---
+++ packages/dfktools/dfktools-0.1.4.tar.gz/dfktools-0.1.4/src/dfktools/summoning_example.py
@@ -0,0 +1,52 @@
+import logging
+import sys
+import time
+import summoning.summoning as summoning
+from web3 import Web3
+
+SD = 'serendale'
+CV = 'crystalvale'
+
+if __name__ == "__main__":
+    log_format = '%(asctime)s|%(name)s|%(levelname)s: %(message)s'
+
+    logger = logging.getLogger("DFK-summoning")
+    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(level=logging.INFO, format=log_format, stream=sys.stdout)
+
+    realm = 'serendale'
+
+    if realm == SD:
+        rpc_server = 'https://api.harmony.one'
+        gas_price_gwei = 115 
+        tx_timeout = 30
+        contract_address = summoning.SERENDALE_CONTRACT_ADDRESS
+    else:
+        rpc_server = 'https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc'
+        gas_price_gwei = {'maxFeePerGas': 2, 'maxPriorityFeePerGas': 2} 
+        tx_timeout = 30
+        contract_address = summoning.CRYSTALVALE_CONTRACT_ADDRESS
+
+    logger.info("Using RPC server " + rpc_server)
+
+    # All these will need to be customized for your address
+    address = '0x<YOUR_ADDRESS_HERE>'
+    private_key = '<YOUR_KEY_HERE>'
+    hero_id_1 = 12345
+    hero_id_2 = 67890
+
+    w3 = Web3(Web3.HTTPProvider(rpc_server))
+    nonce = w3.eth.getTransactionCount(address)
+
+    hero_1_tears = 10  # Should be 10 more for every 5 levels attained
+    hero_2_tears = 10 
+
+    summoning.summon_crystal(contract_address, hero_id_1, hero_id_2, hero_1_tears, hero_2_tears, private_key, nonce, gas_price_gwei, tx_timeout, rpc_server, logger)
+
+    crystals = summoning.get_user_crystal_ids(contract_address, address, rpc_server)
+    if crystals:
+        logger.info(f"Waiting 20 seconds before opening crystal {crystals[0]}")
+        time.sleep(20)
+        tx_receipt = summoning.open_crystal(contract_address, crystals[0], private_key, nonce, gas_price_gwei, tx_timeout, rpc_server, logger)
+        hero_id = summoning.parse_opened_crystal(contract_address, tx_receipt, rpc_server)
+        logger.info("Summoned hero: " + str(hero_id))
