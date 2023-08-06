c7d9e91768727ada47670c8e54bf06bc1026c773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:28 +0000

    Add package frostaura/1.0.19/frostaura-1.0.19.tar.gz

---
+++ packages/frostaura/frostaura-1.0.19.tar.gz/frostaura-1.0.19/frostaura/managers/__init__.py
@@ -0,0 +1,4 @@
+'''A convenience importing mechanism for all manager components.'''
+
+from .asset_reporting_manager import IAssetReportingManager
+from .asset_reporting_manager__personal import PersonalAssetReportingManager
