9312f0f34ef952efd90ad302f8d88c1637f6a284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:21 +0000

    Add package pybound/0.2.0/pyBound-0.2.0-py3-none-any.whl

---
+++ packages/pybound/pyBound-0.2.0-py3-none-any.whl/pybound/__init__.py
@@ -0,0 +1,35 @@
+__version__ = '0.2.0'
+
+import sys, os
+from time import sleep
+
+def delete_line(num):
+	CURSOR_UP_ONE = '\x1b[1A' 
+	ERASE_LINE = '\x1b[2K' 
+	for i in range(num):
+		sys.stdout.write(CURSOR_UP_ONE) 
+		sys.stdout.write(ERASE_LINE)
+def clear():
+	os.system('clear')
+def slow_print(*strings, time = 0.045, sep = ' ', end = '\n', file = sys.stdout):	
+	num = len(strings)
+	for string in strings:
+		string = str(string)
+		for character in string:
+			str(character)
+			file.write(character)
+			file.flush()
+			sleep(time)
+		num -= 1
+		if num != 0:
+			file.write(sep)
+			file.flush()	
+	file.flush()
+	file.write(end)
+	file.flush()
+def wait(time = 1):
+	sleep(time)
+def rgb(r, g, b):
+		return "\033[38;2;{};{};{}m".format(r, g, b)
+def rgb_reset():
+	return "\033[38;2;255;255;255m"
