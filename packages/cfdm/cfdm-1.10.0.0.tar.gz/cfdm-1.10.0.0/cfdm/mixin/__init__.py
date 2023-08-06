14b4dc126ea61518710f9c44bee4728908a77c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:52 +0000

    Add package cfdm/1.10.0.0/cfdm-1.10.0.0.tar.gz

---
+++ packages/cfdm/cfdm-1.10.0.0.tar.gz/cfdm-1.10.0.0/cfdm/mixin/__init__.py
@@ -0,0 +1,26 @@
+from .container import Container
+from .properties import Properties
+from .propertiesdata import PropertiesData
+from .propertiesdatabounds import PropertiesDataBounds
+from .coordinate import Coordinate
+
+from .parameters import Parameters
+from .parametersdomainancillaries import ParametersDomainAncillaries
+
+from .netcdf import (
+    NetCDFComponents,
+    NetCDFGlobalAttributes,
+    NetCDFGroupAttributes,
+    NetCDFUnlimitedDimension,
+    NetCDFDimension,
+    NetCDFExternal,
+    NetCDFGeometry,
+    NetCDFHDF5,
+    NetCDFInterpolationSubareaDimension,
+    NetCDFSampleDimension,
+    NetCDFSubsampledDimension,
+    NetCDFUnreferenced,
+    NetCDFVariable,
+)
+
+from .fielddomain import FieldDomain
