9d1a3250aaf4e1d7135626ec53bb24eb7741dbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:59 +0000

    Add package peaqevcore/5.5.0/peaqevcore-5.5.0.tar.gz

---
+++ packages/peaqevcore/peaqevcore-5.5.0.tar.gz/peaqevcore-5.5.0/peaqevcore/hub/hub.py
@@ -0,0 +1,29 @@
+from ..services.hourselection.hourselectionfactory import HourselectionFactory
+from ..services.timer.timer import Timer
+from ..services.chargecontroller.chargecontrollerfactory import ChargeControllerFactory
+from ..services.threshold.thresholdfactory import ThresholdFactory
+from ..services.chargecontroller.chargecontrollerbase import ChargeControllerBase
+from ..services.hourselection.hoursbase import Hours
+from ..services.prediction.prediction import Prediction
+from ..services.production.production import ProductionService
+from ..services.scheduler.scheduler import Scheduler
+from ..services.session.session import SessionPrice
+from ..services.threshold.threshold import ThresholdBase
+from ..services.locale.Locale import LocaleData
+from .hub_sensors import HubSensorsFactory
+from .hub_options import HubOptions
+
+class Hub:
+    def __init__(self, options: HubOptions, domain: str, state_machine):
+        self.options: HubOptions = options
+        self.sensors = HubSensorsFactory.create(self.options)
+        self.timer: Timer = Timer()
+        #self.threshold = ThresholdFactory.create(self.options)
+        #self.prediction = Prediction(self.options)
+        #self.locale: LocaleData(self.options.locale, domain)
+        #chargecontroller: ChargeControllerBase = field(init=False)
+        #hours: Hours = field(init=False)
+
+        self.sensors.setup(state_machine=state_machine, options=options, domain=domain)
+
+
