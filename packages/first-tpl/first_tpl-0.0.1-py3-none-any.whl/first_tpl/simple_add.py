44d94b8652a37f256bff8fdb98a5980d8f9351da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:08 +0000

    Add package first-tpl/0.0.1/first_tpl-0.0.1-py3-none-any.whl

---
+++ packages/first-tpl/first_tpl-0.0.1-py3-none-any.whl/first_tpl/simple_add.py
@@ -0,0 +1,11 @@
+class simple_operators():
+
+    def __init__(self, var1, var2):
+        self.var1 = var1
+        self.var2 = var2
+    
+    def __str__(self):
+        return 'This package does basic operations.'
+
+    def add_operataion(self):
+        return self.var1+self.var2
