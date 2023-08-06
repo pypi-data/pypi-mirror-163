05b1197d29757ca56af6613275e038e5217a4580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:06 +0000

    Add package Mensajes-iraty/6.0/Mensajes_iraty-6.0-py3-none-any.whl

---
+++ packages/Mensajes-iraty/Mensajes_iraty-6.0-py3-none-any.whl/mensajes/adios/despedidas.py
@@ -0,0 +1,6 @@
+def despedir():
+    print("Adiós, me despido desde despedidas.despedir")
+
+class Despedida:
+    def __init__(self) -> None:
+        print("Adiós, me despido  desde Despedida.__init__")
