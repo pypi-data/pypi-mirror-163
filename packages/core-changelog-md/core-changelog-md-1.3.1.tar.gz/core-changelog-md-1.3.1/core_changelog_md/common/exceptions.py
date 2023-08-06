b19624d2ac5af883ad48c498eef66f5ed6e2dc7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:30 +0000

    Add package core-changelog-md/1.3.1/core-changelog-md-1.3.1.tar.gz

---
+++ packages/core-changelog-md/core-changelog-md-1.3.1.tar.gz/core-changelog-md-1.3.1/core_changelog_md/common/exceptions.py
@@ -0,0 +1,10 @@
+class ChangeLogException(Exception): pass
+class UnsupportedTagException(ChangeLogException): pass
+class UnCorrectTitleException(ChangeLogException): pass
+class UnDetectLine(ChangeLogException): pass
+class MissChangeBlock(ChangeLogException): pass
+
+class MissedVersionsSymbolException(ChangeLogException): pass
+class NotDetectVersionException(ChangeLogException): pass
+class VersionOverTextException(ChangeLogException): pass
+class VersionDateConvertException(ChangeLogException): pass
