0ac8ee74207a43e06d059156d8fc036f4d793d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:44 +0000

    Add package databricks-stage/1.0.5/databricks-stage-1.0.5.tar.gz

---
+++ packages/databricks-stage/databricks-stage-1.0.5.tar.gz/databricks-stage-1.0.5/databricks_stage.py
@@ -0,0 +1,20 @@
+import argparse
+import sys
+from pyspark.sql import SparkSession
+
+# Used for high-speed ingest from Databricks. Is meant to be run as part of a Databricks job.
+def run(**kwargs):
+    spark = SparkSession.builder.getOrCreate()
+
+    print("KWARGS", kwargs)
+    print("sys.argv", sys.argv)
+
+    df = spark.sql(f"""{kwargs['sql_query']}""")
+    df.write \
+        .format("csv") \
+        .option("compression", "gzip") \
+        .option("nullValue", "_SISU_NULL") \
+        .option("delimiter", "\x1e") \
+        .mode("overwrite") \
+        .save(f"s3a://{kwargs['access_key_id']}:{kwargs['secret_access_key']}@{kwargs['stage_output_bucket']}/{kwargs['stage_output_prefix']}")
+
