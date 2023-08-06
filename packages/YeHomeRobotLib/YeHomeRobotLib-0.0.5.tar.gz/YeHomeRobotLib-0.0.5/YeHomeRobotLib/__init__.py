692aea5d0de8f7a718149e554a8079c744df0738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:37 +0000

    Add package YeHomeRobotLib/0.0.5/YeHomeRobotLib-0.0.5.tar.gz

---
+++ packages/YeHomeRobotLib/YeHomeRobotLib-0.0.5.tar.gz/YeHomeRobotLib-0.0.5/YeHomeRobotLib/__init__.py
@@ -0,0 +1,13 @@
+"""Main library."""
+
+
+class YeHomeRobotLib:
+    @staticmethod
+    def hi(name):
+        print(f"hi, {name}")
+
+    @staticmethod
+    def to_json(dict_data: dict):
+        return dict_data
+
+
