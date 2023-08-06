eed460f299ed0834e22d49e179c602a915c33279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:26 +0000

    Add package petstore-test/1.0.0/petstore-test-1.0.0.tar.gz

---
+++ packages/petstore-test/petstore-test-1.0.0.tar.gz/petstore-test-1.0.0/swaggerpetstore/models/__init__.py
@@ -0,0 +1,14 @@
+__all__ = [
+    'pet',
+    'api_response',
+    'user',
+    'tag',
+    'order',
+    'category',
+    'o_auth_token',
+    'status_1_enum',
+    'status_enum',
+    'status_2_enum',
+    'o_auth_scope_enum',
+    'o_auth_provider_error_enum',
+]
