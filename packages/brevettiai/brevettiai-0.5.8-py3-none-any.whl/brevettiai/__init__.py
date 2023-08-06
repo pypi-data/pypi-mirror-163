a787adbb2f559d4fd1afabc9ecab28d8a0fcaf73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:40 +0000

    Add package brevettiai/0.5.8/brevettiai-0.5.8-py3-none-any.whl

---
+++ packages/brevettiai/brevettiai-0.5.8-py3-none-any.whl/brevettiai/__init__.py
@@ -0,0 +1,6 @@
+"""
+.. include:: ../README.md
+"""
+from .utils.module import Module
+from .platform.models import Job, JobSettings
+from .platform.models.platform_backend import PlatformBackend, backend
