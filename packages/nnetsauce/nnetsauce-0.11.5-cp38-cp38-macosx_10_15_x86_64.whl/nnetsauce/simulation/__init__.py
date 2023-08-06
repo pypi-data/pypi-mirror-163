2f7229e06c966ce6e2b3a01ca0d5ca5762bbbf1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:12 +0000

    Add package nnetsauce/0.11.5/nnetsauce-0.11.5-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/nnetsauce/nnetsauce-0.11.5-cp38-cp38-macosx_10_15_x86_64.whl/nnetsauce/simulation/__init__.py
@@ -0,0 +1,13 @@
+from .nodesimulation import (
+    generate_sobol2,
+    generate_uniform,
+    generate_hammersley,
+    generate_halton,
+)
+
+__all__ = [
+    "generate_sobol2",
+    "generate_uniform",
+    "generate_hammersley",
+    "generate_halton",
+]
