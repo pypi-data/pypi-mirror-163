dae44b581a432689c0217970649854a49f8b87fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:56 +0000

    Add package python-clickup/1.0.6/python-clickup-1.0.6.tar.gz

---
+++ packages/python-clickup/python-clickup-1.0.6.tar.gz/python-clickup-1.0.6/src/pyclickup/custom_fields/location.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from .base import CustomField
+from ..services import geo_grabber
+from ..utils.types import RawCustomField
+
+
+
+class LocationField(CustomField):
+    TYPE = "location"
+
+    def set_value(self, value: str, raw_field: RawCustomField) -> None:
+        location = geo_grabber.search(value)
+        raw_field['value'] = location.to_dict()
+
+    def get_value(self, raw_field: RawCustomField) -> Optional[str]:
+        value = raw_field.get("value")
+        
+        if value is None:
+            return
+
+        return value['formatted_address']
+
