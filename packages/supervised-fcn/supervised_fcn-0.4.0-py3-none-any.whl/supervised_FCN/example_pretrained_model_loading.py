f088849baae384fe85657a381283947619663da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:23 +0000

    Add package supervised-fcn/0.4.0/supervised_fcn-0.4.0-py3-none-any.whl

---
+++ packages/supervised-fcn/supervised_fcn-0.4.0-py3-none-any.whl/supervised_FCN/example_pretrained_model_loading.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import torch
+from supervised_FCN.models.fcn import FCNBaseline
+
+
+def load_pretrained_FCN(subset_dataset_name: str, in_channels: int = 1):
+    """
+    load a pretrained FCN (Fully Convolutional Network)
+    :param subset_dataset_name:
+    :param in_channels: 1; univariate for the UCR subset datasets.
+    :return:
+    """
+    ucr_summary = pd.read_csv(os.path.join('supervised_FCN', 'datasets', 'DataSummary_UCR.csv'))
+    q = ucr_summary.query(f"Name == '{subset_dataset_name}'")
+    n_classes = q['Class'].iloc[0]
+
+    # build
+    fcn = FCNBaseline(in_channels, n_classes)
+
+    # load
+    ckpt_fname = os.path.join('supervised_FCN', 'saved_models', f'{subset_dataset_name}.ckpt')
+    fcn.load_state_dict(torch.load(ckpt_fname))
+    return fcn
+
+
+# if __name__ == '__main__':
+#     fcn = load_pretrained_FCN('Adiac')
+#     print(fcn)
