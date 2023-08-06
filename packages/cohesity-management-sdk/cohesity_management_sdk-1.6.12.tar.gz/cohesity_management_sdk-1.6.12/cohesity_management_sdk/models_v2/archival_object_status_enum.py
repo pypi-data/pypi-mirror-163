2e223abb94d734124591ae5ab2e4a1f3c2c56c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:29 +0000

    Add package cohesity-management-sdk/1.6.12/cohesity_management_sdk-1.6.12.tar.gz

---
+++ packages/cohesity-management-sdk/cohesity_management_sdk-1.6.12.tar.gz/cohesity_management_sdk-1.6.12/cohesity_management_sdk/models_v2/archival_object_status_enum.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class ArchivalObjectStatusEnum(object):
+
+    """Implementation of the 'archivalObjectStatus' enum.
+
+    TODO: type enum description here.
+
+    Attributes:
+        KINPROGRESS: TODO: type description here.
+        KSUCCESSFUL: TODO: type description here.
+        KFAILED: TODO: type description here.
+        KWAITINGFORNEXTATTEMPT: TODO: type description here.
+        KWARNING: TODO: type description here.
+
+    """
+
+    KINPROGRESS = 'kInProgress'
+
+    KSUCCESSFUL = 'kSuccessful'
+
+    KFAILED = 'kFailed'
+
+    KWAITINGFORNEXTATTEMPT = 'kWaitingForNextAttempt'
+
+    KWARNING = 'kWarning'
+
---
+++ packages/cohesity-management-sdk/cohesity_management_sdk-1.6.12.tar.gz/cohesity_management_sdk-1.6.12/cohesity_management_sdk/models_v2/operation_enum.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class OperationEnum(object):
+
+    """Implementation of the 'Operation' enum.
+
+    The operation clould be poweroff, reboot.
+
+    Attributes:
+        POWEROFF: TODO: type description here.
+        REBOOT: TODO: type description here.
+
+    """
+
+    POWEROFF = 'poweroff'
+
+    REBOOT = 'reboot'
+
