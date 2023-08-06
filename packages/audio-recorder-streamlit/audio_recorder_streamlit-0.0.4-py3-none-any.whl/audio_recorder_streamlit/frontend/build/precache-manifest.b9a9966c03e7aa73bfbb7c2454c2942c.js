cf89d5e405e110149addcf07a1d630e36e75c888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:10 +0000

    Add package audio-recorder-streamlit/0.0.4/audio_recorder_streamlit-0.0.4-py3-none-any.whl

---
+++ packages/audio-recorder-streamlit/audio_recorder_streamlit-0.0.4-py3-none-any.whl/audio_recorder_streamlit/frontend/build/precache-manifest.b9a9966c03e7aa73bfbb7c2454c2942c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb15e76f8f06ea2a53fcd1c18265c9b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2297ed49928b7d09264",
+    "url": "./static/js/2.2e2495b4.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.2e2495b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e0d3951ba4a76d4b7b",
+    "url": "./static/js/main.8796bf83.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.8796bf83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
