9d496b8fa9cd55fe539e367fbdc9b514be1f34a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:29 +0000

    Add package meter-gear/1.2.17/meter-gear-1.2.17.tar.gz

---
+++ packages/meter-gear/meter-gear-1.2.17.tar.gz/meter-gear-1.2.17/setup.py
@@ -0,0 +1,39 @@
+from codecs import open
+from os import path
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+from gear import __version__
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name="meter-gear",
+    version=__version__,
+    description="An adapter between meter-restful and eth-rpc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/meterio/meter-gear",
+    author="Simon Zhang",
+    author_email="zhanghan.simon@gmail.com",
+    license="MIT",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords="meter blockchain ethereum",
+    packages=find_packages("."),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=[x.strip() for x in open('requirements.txt')],
+    entry_points={
+        "console_scripts": [
+            "meter-gear=gear.cli:main",
+        ],
+    }
+)
