50d2a0497fc6d53c0b53d12971db4f131b81652c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:18 +0000

    Add package Subsearch/2.15.47/subsearch-2.15.47-py3-none-any.whl

---
+++ packages/Subsearch/subsearch-2.15.47-py3-none-any.whl/subsearch/data/__video__.py
@@ -0,0 +1,6 @@
+from .__set_data__ import VideoFile
+
+__video_name__ = getattr(VideoFile, "name")
+__video_ext__ = getattr(VideoFile, "ext")
+__video_path__ = getattr(VideoFile, "path")
+__video_directory__ = getattr(VideoFile, "directory")
