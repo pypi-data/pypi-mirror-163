11d38ea912e7f09eb678ab81aa7382d80f046c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:45 +0000

    Add package slurmjobs/1.1.2/slurmjobs-1.1.2.tar.gz

---
+++ packages/slurmjobs/slurmjobs-1.1.2.tar.gz/slurmjobs-1.1.2/slurmjobs/args.py
@@ -0,0 +1,168 @@
+'''Argument Formatters
+'''
+from . import util
+from . import grid
+
+
+class Argument:
+    '''The base-class for all argument formatters.
+    If you want to provide a new argument formatter, 
+    just override this class and change either the:
+
+     * ``format_arg`` method that is used to format positional and key-value pairs, or 
+     * ``format_value`` (which is called in ``format_arg``) to format python values as a string.
+       This is often some form of quoted repr or json formatting.
+
+    
+    '''
+    prefix = suffix = ''
+
+    @classmethod
+    def get(cls, key='fire', *a, **kw) -> 'Argument':
+        '''Return an instance of an argument formatter, based on its name.'''
+        if isinstance(key, cls):
+            return key
+        if isinstance(key, type) and issubclass(key, cls):
+            return key(*a, **kw)
+        return util.subclass_lookup(cls, suffix='argument')[key.lower()](*a, **kw)
+
+    def _format_args(self, *a, **kw):
+        '''This prepares all arguments and returns them as positional and keyword arguments.
+
+        The keyword arguments contain both the key and value flag, as applicable.
+        '''
+        if a and isinstance(a[0], grid._BaseGridItem):
+            a, kw = (a[0].positional + a[1:], dict(a[0], **kw))
+        a = [self.format_arg(v) for v in a]
+        kw = {k: self.format_arg(k, v) for k, v in kw.items()}
+        return a, kw
+
+    def __call__(self, *a, indent=None, **kw):
+        '''Format arguments as a string.
+        
+        Arguments:
+            *a: positional arguments for the CLI.
+            indent (int): The indentation (in spaces) for each argument. If
+                ``indent`` is ``None`` the arguments are inline, otherwise 
+                the arguments are each on their own line.
+            **kw: Keyword arguments for the CLI.
+
+        Returns:
+            str: The command-line arguments formatted and ready to be outputted
+                to a bash command.
+        '''
+        a2, kw2 = self._format_args(*a, **kw)
+        items = (
+            [self.prefix]*bool(self.prefix) + 
+            a2 + [x for xs in kw2.values() if xs for x in xs] + 
+            [self.suffix]*bool(self.suffix))
+
+        indent = '\\\n' + ' '*indent if indent is not None else ''
+        return ' '.join((
+            f'{indent}{" ".join(x) if isinstance(x, list) else x}' 
+            for x in items))
+
+    def format_arg(self, k, v=...):
+        '''Format a key-value pair for the command-line.
+        
+        If it is a positional argument, the value will be in key and the value
+        will be ``...``.
+
+        Arguments:
+            k: The key (or value, for positional arguments).
+            v: The value. If it's a positional argument, it will be ``v=...``
+        '''
+        return [self.format_value(k) if v is ... else self.format_value(v)]
+
+    def format_value(self, v):
+        '''Format a value for the command-line.'''
+        return util.shlex_repr(v)
+
+
+
+class FireArgument(Argument):
+    '''Argument formatting for Python Fire.
+
+    Example: ``python script.py a b --arg1 c --arg2 d``
+    
+    * Docs: https://google.github.io/python-fire/guide/
+    * Github: https://github.com/google/python-fire
+    '''
+    kw_fmt = '--{key}={value}'
+
+    def format_arg(self, k, v=...):
+        if v is ...:
+            return [self.format_value(k)]
+        return [self.kw_fmt.format(key=k, value=self.format_value(v))]
+
+
+
+class ArgparseArgument(Argument):
+    '''Argument formatting for Python's builtin argparse.
+
+    Example: ``python script.py a b --arg1 c --arg2 d``
+    
+    * Docs: https://docs.python.org/3/library/argparse.html
+    '''
+    short_opt, long_opt = '-', '--'
+
+    def format_arg(self, k, v=...):
+        if v is False:
+            return [self.format_key(f'no-{k}')]
+        key = self.format_key(k)
+        if v is True or v is ...:
+            return [key]
+        if v is None:
+            return []
+        if not isinstance(v, (list, tuple, set)):
+            v = [v]
+        # this will all be joined to gether as strings. The extra
+        # list on the outside means that it will appear on the same line
+        # when indentation is requested
+        vs = [self.format_value(x) for x in v]
+        return [ [key] + vs ]
+
+    def format_key(self, k):
+        return '{}{}'.format(self.long_opt if len(k) > 1 else self.short_opt, k)
+
+
+class SacredArgument(FireArgument):
+    '''Formatting for sacred.
+
+    Example: ``python script.py with arg1=a arg2=b``
+    
+    * Docs: https://sacred.readthedocs.io/en/stable/
+    * Github: https://github.com/IDSIA/sacred
+    '''
+    prefix = 'with'
+    kw_fmt = '{key}={value}'
+
+
+class HydraArgument(FireArgument):
+    '''Formatting for hydra.
+
+    Example: ``python script.py  db.user=root db.pass=1234``
+    
+    * Docs: https://hydra.cc/docs/intro/
+    * Github: https://github.com/facebookresearch/hydra
+    '''
+    kw_fmt = '{key}={value}'
+    available_prefixes = ['+', '++', '~', '']
+    # https://hydra.cc/docs/advanced/override_grammar/basic
+
+    def __init__(self, default_prefix='+', **kw):
+        default_prefix = default_prefix or ''
+        if default_prefix not in self.available_prefixes:
+            raise ValueError('invalid hydra prefix: {}. Must be one of {}'.format(
+                default_prefix, self.available_prefixes))
+        self.kw_fmt_prefixed = default_prefix + self.kw_fmt
+        super().__init__(**kw)
+
+    def format_arg(self, k, v=...):
+        if v is ...:
+            return [self.format_value(k)]
+        kw_fmt = (
+            self.kw_fmt
+            if any(k.startswith(pfx) for pfx in self.available_prefixes if pfx)
+            else self.kw_fmt_prefixed)
+        return [kw_fmt.format(key=k, value=self.format_value(v))]
---
+++ packages/slurmjobs/slurmjobs-1.1.2.tar.gz/slurmjobs-1.1.2/slurmjobs/core.py
@@ -0,0 +1,505 @@
+'''Automating Slurm job generation.
+
+Generate a set of `.sbatch` files over a grid of parameters to be searched over. 
+A run script is created which will submit all generated jobs as once.
+
+You can also use `ShellBatch` which excludes the slurm/module references so you can 
+test & run on your local machine or test server.
+
+> NOTE: because NYU switched to Greene which now utilizes singularity, the previous 
+workflow regarding loading modules is not as necessary. Therefore, I will likely be 
+experimenting and changing how things are done to suit the new workflow. If you want 
+to keep the old setup, pin to `<=0.1.2`. (Sorry I should have just did a `0.2` bump 
+off the bat).
+
+
+
+'''
+import os
+import time
+import pprint
+import jinja2
+import pathtrees
+from .grid import *
+from . import util
+from .args import Argument
+
+loader = jinja2.ChoiceLoader([
+    jinja2.PackageLoader('slurmjobs', 'templates')
+])
+env = jinja2.Environment(loader=loader)
+env.filters['prettyjson'] = util.prettyjson
+env.filters['prefixlines'] = util.prefixlines
+env.filters['pprint'] = pprint.pformat
+env.filters['comment'] = lambda x, ns=1, ch='#', nc=1: util.prefixlines(x, ch*nc+' '*ns)
+
+
+class Jobs:
+    '''The base class for Job generation. Sub-class this if you want to provide your own
+
+    .. code-block:: python
+
+        import slurmjobs
+
+        batch = slurmjobs.SlurmBatch(
+            'python train.py',
+            email='mynetid@nyu.edu',
+            conda_env='my_env')
+
+        # generate jobs across parameter grid
+        run_script, job_paths = batch.generate([
+            ('kernel_size', [2, 3, 5]),
+            ('nb_stacks', [1, 2]),
+            ('lr', [1e-4, 1e-3]),
+        ], receptive_field=6)
+
+
+    We use Jinja2 for our script templating. See https://jinja.palletsprojects.com/en/3.0.x/templates/ 
+    for documentation about its syntax and structure.
+    
+    Attributes:
+        options (dict): Template options. Anything here is made available to the template. 
+        template (str): The Jinja2 template for the sbatch script. 
+        job_template (str): The Jinja2 template for the jobs script that launches the sbatch files.
+        cli (str, slurmjobs.args.Argument): The command line argument format your script uses.
+            By default, this uses Fire. See ``args.FireArgument``.
+
+        job_id_arg (str): The argument name to use for passing the job ID. 
+            Set to None to ignore omit the job ID.
+        job_id_key_sep (str): The separator between key and value used in the job ID.
+        job_id_item_sep (str): The separator between key-value items in the job ID. 
+        
+        allowed_job_id_chars (str, list): Characters (other than alphanumeric) allowed in the job ID.
+            Other characters are removed.
+        special_character_replacement (str): A string that can be used in place of 
+            special characters not found in ``allowed_job_id_chars``. Default is empty.
+        key_abbreviations (dict): A mapping from full name to abbreviation for keys in the job ID.
+        abbreviate_length (int): The length to abbreviate keys in the job ID.
+        float_precision (int): The number of decimals to limit float values in the job ID.
+
+
+
+    '''
+    options: dict = {
+        'email': None,
+        'conda_env': None,
+        'run_dir': None,
+        'bashrc': True,
+        'set_e': False,
+    }
+    cli = 'fire'
+    root_dir = 'jobs'
+
+    # make script templating clearer and more simple
+    template = '''{% extends 'job.base.j2' %}
+    '''
+    run_template = '''{% extends 'run.base.j2' %}
+    '''
+
+    job_id_arg = 'job_id'
+    job_id_key_sep = '-'
+    job_id_item_sep = ','
+    job_id_iter_sep = '+'
+    allowed_job_id_chars = ',._-+='
+    special_character_replacement = ''
+    omit_missing_keys_from_job_id = True
+    
+    key_abbreviations = {}
+    abbreviate_length = None
+    float_precision = None
+
+    def __init__(self, command, name=None, cli=None, 
+                 root_dir=None, backup=True, job_id=True, 
+                 template=None, run_template=None,
+                 **options):
+        self.template = template or self.template
+        self.run_template = run_template or self.run_template
+
+        # raise an error if any unrecognized arguments were passed.
+        wrong_options = set(options) - set(self.options)
+        if wrong_options:
+            raise TypeError(
+                f"Unrecognized options: {wrong_options}. If you extended your template to use additional options, "
+                "please give them default values in your Class.options dictionary.")
+        
+        # copy the options dict and add in new values
+        self.options = dict(self.options)
+        for k, v in options.items():
+            dft = self.options[k]
+            if isinstance(dft, dict):
+                v = dict(dft, **(v or {}))
+            self.options[k] = v
+
+        self.command = command
+        self.name = name or util.command_to_name(command)
+        self.cli = Argument.get(self.cli if cli is None else cli)
+        self.job_id_arg = self.job_id_arg if job_id is True else job_id or None
+
+        # paths
+        self.root_dir = root_dir or self.root_dir
+        self.paths = self.get_paths()
+        if 'batch_dir' in self.paths and len(self.paths.batch_dir.glob('*')):
+            if backup:
+                pathtrees.backup(self.paths.batch_dir)
+            else:
+                import shutil
+                assert os.path.abspath(str(self.paths.batch_dir)) != '/', "Ummmmm..... what do you think you're doing... rm -rf / ???"
+                shutil.rmtree(str(self.paths.batch_dir))
+
+    def format_id_key(self, k):
+        '''Format a key in the job ID so it looks nicer. Override this to 
+        provide more sophisticated formatting.
+        '''
+        # key formatting
+        if k in self.key_abbreviations:
+            k = self.key_abbreviations[k]
+        elif self.abbreviate_length:
+            k = k[:self.abbreviate_length]
+        return k
+
+    def format_id_value(self, v):
+        '''Format a value in the job ID so it looks nicer. Override this to 
+        provide more sophisticated formatting.
+        '''
+        if isinstance(v,  (list, tuple)):
+            return self.job_id_iter_sep.join(self.format_id_value(vi) for vi in v)
+        if isinstance(v,  dict):
+            return self.job_id_iter_sep.join(self.format_id_item(ki, vi) for ki, vi in v.items())
+        if self.float_precision is not None and isinstance(v, float):
+            return f'{v:.{self.float_precision}g}'
+        return v
+
+    def format_id_item(self, k, v):
+        '''Formats a key-value pair for the job ID. 
+
+        You can override this to change how each key-value pair in a 
+        job ID is formatted.
+
+        Arguments:
+            k (str): The argument key.
+            v (any): The argument value.
+
+        Returns:
+            Return a tuple if you want it to be joined using ``job_id_key_sep``. Return a string
+            to be used as is. Return None to omit it from the job ID. 
+
+        Be warned that omitting key/value pairs runs the risk of filename collisions which will mean 
+        multiple jobs overwritting each other in the same file.
+        '''
+        k = self.format_id_key(k)
+        v = self.format_id_value(v)
+        return f'{k}{self.job_id_key_sep}{v}'
+
+    def format_job_id(self, args, keys=None, name=None, ignore_keys=None, skip_positional=False):
+        '''Convert a dictionary to a job ID.
+
+        Arguments:
+            args (GridItem): The job dictionary.
+            keys (list, tuple): The keys that we want to include in the job ID.
+            name (str): The job name to include in the job ID. By default
+                this will be the name associated with the Jobs object. Pass 
+                ``False`` to use no name.
+        '''
+        name = name or self.name if name is not False else False
+        # format each key
+        parts = []
+        if not skip_positional:
+            for x in getattr(args, 'positional', ()):
+                formatted = self.format_id_value(x)
+                if formatted is None or formatted == '':
+                    continue
+                parts.append(formatted)
+
+        keys = getattr(args, 'grid_keys', args) if keys is None else keys
+        for k in keys or ():
+            if ignore_keys and k in ignore_keys:
+                continue
+            if self.omit_missing_keys_from_job_id and k not in args:
+                continue
+            formatted = self.format_id_item(k, args.get(k))
+            if formatted is None or formatted == '':
+                continue
+            parts.append(formatted)
+
+        # join them together
+        job_id = self.job_id_item_sep.join(map(str, [name]*bool(name) + parts))
+        job_id = job_id.replace(' ', '').replace('\n', '').replace('\t', '')
+        job_id = ''.join(
+            c if c in self.allowed_job_id_chars or c.isalnum() else self.special_character_replacement
+            for c in job_id)
+        return job_id
+
+    def generate(self, grid_=None, *a, ignore_job_id_keys=None, **kw):
+        '''Generate slurm jobs for every combination of parameters.
+        
+        Arguments:
+            grid_ (Grid, list): The parameter grid.
+            *a: Positional arguments to add to the command.
+            **kw: Additional keyword arguments to pass to the command.
+
+        Returns:
+            tuple:
+
+                str: The path to the run script.
+
+                list[str]: The list of paths for each job file.
+        '''
+        # generate jobs
+        # kw = dict(kw, **(kwargs_ or {})) # for taken keys.
+        grid = Grid.as_grid(grid_)
+
+        used = set()
+        job_paths = []
+        for d in grid:
+            d.positional += a
+            d.update(kw)
+            job_id = self.format_job_id(
+                d, d.grid_keys, name=self.name, 
+                ignore_keys=ignore_job_id_keys)
+            if job_id in used:
+                raise RuntimeError(f"Duplicate job ID: {job_id}")
+            job_paths.append(self.generate_job(job_id, _grid=grid, _args=d))
+
+        # generate run file
+        run_script = self.generate_run_script(job_paths, _grid=grid)
+
+        # store the current timestamp
+        if 'time_generated' in self.paths.paths:
+            self.paths.time_generated.write_text(str(time.time()))
+
+        return run_script, job_paths
+    
+    def generate_job(self, job_id, *a, _args=None, _grid=None, **params):
+        '''Generate a single slurm job file'''
+        # build command
+        paths = self.paths.specify(job_id=job_id)
+        if self.job_id_arg:
+            params[self.job_id_arg] = job_id
+        
+        args = _args
+        if args is None:
+            args = GridItem()
+        args.update(params)
+        args.positional += a
+
+        # generate job file
+        paths.job.parent.mkdir(parents=True, exist_ok=True)
+        with open(paths.job, "w") as f:
+            f.write(env.from_string(self.template).render(
+                job_id=job_id,
+                command=self.command,
+                paths=paths,
+                args=args,
+                cli=self.cli,
+                grid=_grid,
+                **self.options,
+            ).lstrip())
+
+        if 'output' in paths.paths:
+            paths.output.parent.mkdir(parents=True, exist_ok=True)
+
+        return paths.job.format()
+
+    def generate_run_script(self, _job_paths, _grid=None, **kw):
+        '''Generate a job run script that will submit all jobs.'''
+        # Generate run script
+        file_path = self.paths.run
+        with open(file_path, "w") as f:
+            f.write(env.from_string(self.run_template).render(
+                name=self.name,
+                command=self.command,
+                job_paths=_job_paths,
+                paths=self.paths,
+                cli=self.cli,
+                grid=_grid,
+                **self.options, **kw))
+        util.make_executable(file_path)
+        return file_path
+
+    def get_paths(self, **kw) -> pathtrees.Paths:
+        paths = pathtrees.tree(self.root_dir, {'{name}': {
+            '': 'batch_dir',
+            '{job_id}.job.sh': 'job',
+            'run.sh': 'run',
+            # optional
+            'output/{job_id}.log': 'output',
+            'time_generated': 'time_generated',
+        }}).update(name=self.name, **kw)
+        return paths  # type: ignore
+
+
+
+class Shell(Jobs):
+    '''Generate batch jobs to be run without a task scheduler.
+    This will create a grid of scripts that will be run using ``nohup``,
+    a program that will keep running your script even if the parent 
+    process (e.g. your ssh session) dies.
+
+    This is mainly just for doing small tests and whatnot.
+
+    .. code-block:: python
+
+        import slurmjobs
+
+        jobs = Shell('python myscript.py')
+        jobs.generate([
+            ('a', [1, 2]),
+            ('b', [1, 2]),
+        ])
+
+    '''
+
+    options = dict(
+        Jobs.options,
+        background=False,
+    )
+
+    template = '''{% extends 'job.shell.j2' %}
+    '''
+
+
+
+
+class Slurm(Jobs):
+    '''Generate jobs for sbatch. This was used for HPC Prince. 
+    For HPC Greene, please use ``Singularity``.
+
+    This is functionally equivalent to:
+
+    .. code-block:: python
+
+        # sbatch_args
+        #SBATCH --nodes=...
+        #SBATCH --cpus-per-task=...
+        #SBATCH --gres=gpu:...
+        #SBATCH --mail-user=...
+        ...
+
+        {# shell_code_body #}
+    
+    '''
+    # make sbatch args more isomorphic
+    # while also providing helpers for setting cpus to the number of gpus by default (for example)
+    options = dict(
+        Jobs.options,
+        # sbatch arguments
+        # see: https://slurm.schedmd.com/sbatch.html
+        sbatch={
+            'time': '3-0',
+            'mem': '48GB',
+            # 'n_gpus': None,
+            # 'n_cpus': None,
+            # 'nodes': None,
+        },
+        # n_gpus=0,
+        # n_cpus=None,
+        # nodes=None,
+        modules=None,  # no need for modules with singularity
+        bashrc=False,  # disable bashrc by default
+    )
+    template = '''{% extends 'job.sbatch.j2' %}
+    '''
+    run_template = '''{% extends 'run.sbatch.j2' %}
+    '''
+
+    module_presets = {
+        'cuda9': ['cudnn/9.0v7.3.0.29', 'cuda/9.0.176'],
+        'cuda10': ['cuda/10.0.130', 'cudnn/10.0v7.4.2.24'],
+        'cuda10.1': ['cuda/10.1.105', 'cudnn/10.1v7.6.5.32'],
+        'cuda10.2': ['cuda/10.2.89'],
+        'cuda11': ['cuda/11.0.194'],
+        'cuda11.1': ['cuda/11.1.74'],
+        'cuda11.3': ['cuda/11.3.1'],
+        'conda': ['anaconda3/2020.07'],
+        'anaconda': ['anaconda3/2020.07'],
+        'anaconda3': ['anaconda3/2020.07'],
+    }
+
+    def __init__(self, *a, sbatch=None, modules=None, n_gpus=None, n_cpus=None, nv=None, **kw):
+        modules = util.flatten(
+            self.module_presets.get(m, m) for m in (modules or ()))
+        super().__init__(*a, modules=modules, sbatch=sbatch, **kw)
+
+        # handle n_cpus n_gpus
+        sbat: dict = self.options['sbatch']
+        n_cpus = n_cpus if n_cpus is not None else sbat.pop('n_cpus', None)
+        n_gpus = n_gpus if n_gpus is not None else sbat.pop('n_gpus', None)
+        gres = sbat.get('gres')
+        if n_cpus:
+            if sbat.get('cpus-per-task'):
+                raise ValueError("n_cpus specified as well as cpus-per-task")
+            sbat['cpus-per-task'] = n_gpus or 1 if n_cpus is None else n_cpus
+        if n_gpus:
+            if sbat.get('gres'):
+                raise ValueError("n_gpus specified as well as gres. Please choose one or the other.")
+            sbat['gres'] = f'gpu:{n_gpus or 0}'
+            # sbatch['gpus-per-task'] = n_gpus or 0
+        if gres and isinstance(gres, int):
+            sbat['gres'] = f'gpu:{gres}'
+        self.options['nv'] = bool(n_gpus or sbat.get('gres')) if nv is None else nv
+
+    def get_paths(self, **kw):
+        paths = pathtrees.tree(self.root_dir, {'{name}': {
+            '': 'batch_dir',
+            '{job_id}.sbatch': 'job',
+            'run.sh': 'run',
+            'slurm/slurm_%j__{job_id}.log': 'output',
+            'time_generated': 'time_generated',
+        }}).update(name=self.name, **kw)
+        return paths
+
+# alias
+SBatch = Slurm
+
+
+class Singularity(Slurm):
+    '''Generate jobs for sbatch + singularity. Use this for HPC Greene.
+    
+    This is functionally equivalent to:
+
+    .. code-block:: python
+
+        {# sbatch_args #}
+
+        # execute it inside the container
+        singularity exec --overlay ... ... /bin/bash << EOF
+
+        . /ext3/env.sh
+
+        {# shell_code_body #}
+
+        EOF
+
+    To setup a singularity overlay with a anaconda python environment, 
+    please feel free to use https://gist.github.com/beasteers/84cf1eb2e5cc7bc4cb2429ef2fe5209e
+    I'm in the process of integrating it with ``slurmjobs``.
+
+    Source Tutorial: https://sites.google.com/a/nyu.edu/nyu-hpc/services/Training-and-Workshops/tutorials/singularity-on-greene
+
+    
+    '''
+    sif_dir = '/scratch/work/public/singularity/'
+    options = dict(
+        Slurm.options,
+        sif='cuda11.0-cudnn8-devel-ubuntu18.04.sif',
+        overlay='overlay-5GB-200K.ext3',
+        readonly=True,
+        overlays=None,
+        readonly_overlays=None,
+        singularity_init_script='source /ext3/env.sh',
+    )
+    template = '''{% extends 'job.singularity.j2' %}
+    '''
+    def __init__(self, command, overlay, sif, *a, **kw):
+        assert overlay, 'You must specify an overlay file'
+        assert sif, 'You must specify a sif file'
+        super().__init__(command, *a, overlay=overlay, sif=sif, **kw)
+        self.options['sif'] = _inferred_root(self.options['sif'], self.sif_dir)
+
+
+def _inferred_root(fname, root):
+    if root and not os.path.isfile(fname):
+        alt = os.path.join(root, fname)
+        if os.path.isfile(alt):
+            fname = alt
+    return fname
