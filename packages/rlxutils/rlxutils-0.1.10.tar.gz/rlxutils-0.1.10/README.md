159a3f1e7228bb8ccd25612d061867523309e99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:10 +0000

    Add package rlxutils/0.1.10/rlxutils-0.1.10.tar.gz

---
+++ packages/rlxutils/rlxutils-0.1.10.tar.gz/rlxutils-0.1.10/README.md
@@ -0,0 +1,10 @@
+# rlxutils
+
+various utility functions
+
+- verbose `joblib Parallel`
+- easy `matplotlib` grid of subplots
+- measuring elapsed times of code fragments
+- easy execution of shell commands
+- and more
+
---
+++ packages/rlxutils/rlxutils-0.1.10.tar.gz/rlxutils-0.1.10/rlxutils/__init__.py
@@ -0,0 +1,10 @@
+from .rlxutils import *
+import rlxutils.optimization
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
