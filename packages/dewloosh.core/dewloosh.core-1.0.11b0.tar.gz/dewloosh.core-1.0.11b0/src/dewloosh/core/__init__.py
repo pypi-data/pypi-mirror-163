a97fade2c314f414579d4d79c6088be6f88bab27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:26 +0000

    Add package dewloosh.core/1.0.11b0/dewloosh.core-1.0.11b0.tar.gz

---
+++ packages/dewloosh.core/dewloosh.core-1.0.11b0.tar.gz/dewloosh.core-1.0.11b0/src/dewloosh/core/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .wrapping import Wrapper
+from .typing import ishashable, issequence
+
+__version__ = "1.0.11b"
+__description__ = "Common developer utilities and base classes to support other dewloosh packages."
