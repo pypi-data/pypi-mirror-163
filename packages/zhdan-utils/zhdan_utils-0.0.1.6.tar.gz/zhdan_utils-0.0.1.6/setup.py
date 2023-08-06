573d427000ddb9ce0b4eb21459747158ba74398e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:08 +0000

    Add package zhdan-utils/0.0.1.6/zhdan_utils-0.0.1.6.tar.gz

---
+++ packages/zhdan-utils/zhdan_utils-0.0.1.6.tar.gz/zhdan_utils-0.0.1.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='zhdan_utils',
+      version='0.0.1.6',
+      description='Utils include:\n- timer',
+      packages=['zhdan_utils'],
+      author_email='artur.v.zhdan@gmail.com',
+      zip_safe=False)
