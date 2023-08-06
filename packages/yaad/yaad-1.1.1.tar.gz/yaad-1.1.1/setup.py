45c686dccfc632990f71626ed0b16316edfc7f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:33 +0000

    Add package yaad/1.1.1/yaad-1.1.1.tar.gz

---
+++ packages/yaad/yaad-1.1.1.tar.gz/yaad-1.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['yaad']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'yaad',
+    'version': '1.1.1',
+    'description': 'Yet Another Attribute Dict',
+    'long_description': '\n# yaad\n\nYet Another Attribute Dictionary\n',
+    'author': 'Daniel Sullivan',
+    'author_email': 'mumblepins@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/mumblepins/yaad',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/yaad/yaad-1.1.1.tar.gz/yaad-1.1.1/src/yaad/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .attr_dict import AttrDict
+
+__version__: str = "1.1.1"
+
+__all__ = ["AttrDict"]
