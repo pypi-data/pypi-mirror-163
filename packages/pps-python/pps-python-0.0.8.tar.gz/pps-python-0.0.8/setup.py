808f99ec5501a528cd6eae88c88041b6660bd9bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:55 +0000

    Add package pps-python/0.0.8/pps-python-0.0.8.tar.gz

---
+++ packages/pps-python/pps-python-0.0.8.tar.gz/pps-python-0.0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pandas>=1.0.0,<2.0.0", "scikit-learn >= 0.20.2, < 1.0.0","numpy"]
+
+setup(
+    name='pps-python',
+    packages=find_packages(),
+    version='0.0.8',
+    description='Customised Python Functions for calculating Predictive Power Score',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akhil14shukla/ppscore",
+    author='Akhil Shukla (akhil14shukla)',
+    license='GNU v3.0',
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
