bfe6fa8d2d28abe070359ab9c4920aed2db851a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:27 +0000

    Add package abch-woke/0.1.3rc4/abch-woke-0.1.3rc4.tar.gz

---
+++ packages/abch-woke/abch-woke-0.1.3rc4.tar.gz/abch-woke-0.1.3rc4/woke/compile/build_data_model.py
@@ -0,0 +1,33 @@
+from pathlib import Path, PurePath, PurePosixPath, PureWindowsPath
+from typing import Dict, FrozenSet, List
+
+from pydantic import BaseModel, Extra, validator
+
+from woke.compile.solc_frontend import SolcInputSettings, SolcOutputError
+
+
+class BuildInfoModel(BaseModel):
+    class Config:
+        extra = Extra.allow
+        allow_mutation = False
+        arbitrary_types_allowed = True
+        json_encoders = {PurePosixPath: str, PureWindowsPath: str}
+
+
+class CompilationUnitBuildInfo(BuildInfoModel):
+    build_dir: str  # TODO unused
+    sources: Dict[str, Path]
+    contracts: Dict[str, Dict[str, Path]]
+    errors: List[SolcOutputError]
+    source_units: FrozenSet[PurePath]
+    allow_paths: FrozenSet[Path]
+    include_paths: FrozenSet[Path]
+    settings: SolcInputSettings
+
+    @validator("source_units", pre=True, each_item=True)
+    def set_source_units(cls, v):
+        return PurePath(v)
+
+
+class ProjectBuildInfo(BuildInfoModel):
+    compilation_units: Dict[str, CompilationUnitBuildInfo]
