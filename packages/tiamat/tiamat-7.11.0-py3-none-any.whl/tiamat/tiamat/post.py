fab046bf206f266bda8442d14b8d27018d3cc3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:27 +0000

    Add package tiamat/7.11.0/tiamat-7.11.0-py3-none-any.whl

---
+++ packages/tiamat/tiamat-7.11.0-py3-none-any.whl/tiamat/tiamat/post.py
@@ -0,0 +1,40 @@
+import os
+import pathlib
+import shutil
+import sys
+
+
+def report(hub, bname: str):
+    """
+    :param hub: The redistributed pop central hub.
+    :param bname: The name of the build configuration to use from the build.conf file.
+    """
+    opts = hub.tiamat.BUILDS[bname]
+    art = os.path.abspath(os.path.join("dist", opts.name))
+
+    # This is the only print statement from the program, everything else gets logged
+    # through stderr
+    print(art, file=sys.stdout, flush=True)
+
+    # Save the location of the result to an environment variable for CICD or
+    # wrapper scripts
+    os.environ["TIAMAT_BUILD_PACKAGE"] = art
+
+
+def clean(hub, bname: str):
+    """
+    :param hub: The redistributed pop central hub.
+    :param bname: The name of the build configuration to use from the build.conf file.
+    """
+    opts = hub.tiamat.BUILDS[bname]
+    try:
+        shutil.rmtree(opts.venv_dir)
+    except OSError:
+        pathlib.Path(opts.venv_dir).unlink()
+    os.remove(opts.spec)
+    os.remove(opts.req)
+    try:
+        # try to remove pyinstaller warn-*** file
+        os.remove(os.path.join(opts.dir, "build", opts.name, f"warn-{opts.name}.txt"))
+    except FileNotFoundError:
+        pass
