cc507d6db93864f1ac3fc3e23235c90439575755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:26 +0000

    Add package algora-sdk/1.3.8/algora_sdk-1.3.8-py3-none-any.whl

---
+++ packages/algora-sdk/algora_sdk-1.3.8-py3-none-any.whl/algora/api/data/fred/__init__.py
@@ -0,0 +1,2 @@
+from algora.api.data.fred.asynchronous import async_get_series
+from algora.api.data.fred.synchronous import get_series
---
+++ packages/algora-sdk/algora_sdk-1.3.8-py3-none-any.whl/algora/api/data/query/__init__.py
@@ -0,0 +1,2 @@
+from algora.api.data.query.asynchronous import async_query_dataset, async_query_dataset_csv, async_query_timeseries
+from algora.api.data.query.synchronous import query_dataset, query_dataset_csv, query_timeseries
