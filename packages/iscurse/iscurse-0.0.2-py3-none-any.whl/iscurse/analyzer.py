3b01ca5e2d120e378c1f2c0224c631c3202c5d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:55 +0000

    Add package iscurse/0.0.2/iscurse-0.0.2-py3-none-any.whl

---
+++ packages/iscurse/iscurse-0.0.2-py3-none-any.whl/iscurse/analyzer.py
@@ -0,0 +1,73 @@
+import json
+import os
+
+PATH = 'iscurse/data/' 
+afinn = json.load(open(PATH + 'AFINN-pt.json'))
+
+class Analyzer:
+    def __init__(self, text):
+        self.text = text
+        self.score = self._sentiment(text)
+
+    def get_score(self):
+        return self.score
+
+    def get_text(self):
+        return self.text
+    
+    def get_emoji(self):
+        if self.score > 0:
+            return ':)'
+        elif self.score < 0:
+            return ':('
+        else:
+            return ':|'
+        
+    def get_binary(self):
+        if self.score > 0:
+            return 1
+        else:
+            return 0
+    
+    def get_sentiment(self):
+        if self.score > 0:
+            return 'positive'
+        elif self.score < 0:
+            return 'negative'
+        else:
+            return 'neutral'
+    
+    def get_zero_to_five(self):
+        if self.score < -3 and self.score >= -5:
+            return 1
+        elif self.score > -3 and self.score <= -1:
+            return 2
+        elif self.score > -1 and self.score <= 1:
+            return 3
+        elif self.score > 1 and self.score <= 3:
+            return 4
+        elif self.score > 3 and self.score <= 5:
+            return 5
+        else:
+            print('Error: score out of range')
+            print(self.score)
+
+    def get_zero_to_ten(self):
+        return self.get_zero_to_five() * 2
+            
+        
+    def _sentiment(self,text):
+        score = []
+        point  = 0
+        final_score = 0
+        for sent in afinn.keys():
+            if sent in text:
+                point += 1
+                score.append(afinn[sent])
+        
+    
+        score = sum(score)
+        final_score = score/point if point > 0 else 0
+
+        return final_score
+    
