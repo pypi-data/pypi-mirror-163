c70273924335dac9ad648e5bfbb2b24f745682fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:38 +0000

    Add package types-mypy-extensions/0.4.23/types-mypy-extensions-0.4.23.tar.gz

---
+++ packages/types-mypy-extensions/types-mypy-extensions-0.4.23.tar.gz/types-mypy-extensions-0.4.23/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+name = "types-mypy-extensions"
+description = "Typing stubs for mypy-extensions"
+long_description = '''
+## Typing stubs for mypy-extensions
+
+This is a PEP 561 type stub package for the `mypy-extensions` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `mypy-extensions`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/mypy-extensions. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `5477097c6de5bc873a65b7b8749ff41876f5aa69`.
+'''.lstrip()
+
+setup(name=name,
+      version="0.4.23",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/mypy-extensions.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['mypy_extensions-stubs'],
+      package_data={'mypy_extensions-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
