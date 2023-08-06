b635048a7d3a696b48be9601108faad63462753f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:37 +0000

    Add package trainerai/0.1.0/trainerai-0.1.0.tar.gz

---
+++ packages/trainerai/trainerai-0.1.0.tar.gz/trainerai-0.1.0/Setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup 
+
+setup(name="trainerai",
+version="0.1.0",
+description="This is A Package Used To Train Your AI Model With Data.",
+author="Kaushik Shresth",
+author_email="kaushikshresth07@gmail.com",install_requires=['nltk==3.7','numpy==1.22.3','torch==1.11.0'])
