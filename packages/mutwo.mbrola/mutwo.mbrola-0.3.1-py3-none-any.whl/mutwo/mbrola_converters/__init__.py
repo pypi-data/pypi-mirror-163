ccccd39c0d75b2d58890502f1fcc07621abe9b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:16 +0000

    Add package mutwo.mbrola/0.3.1/mutwo.mbrola-0.3.1-py3-none-any.whl

---
+++ packages/mutwo.mbrola/mutwo.mbrola-0.3.1-py3-none-any.whl/mutwo/mbrola_converters/__init__.py
@@ -0,0 +1,6 @@
+from .mbrola import *
+
+__all__ = mbrola.__all__
+
+# Force flat structure
+del mbrola
---
+++ packages/mutwo.mbrola/mutwo.mbrola-0.3.1-py3-none-any.whl/mutwo/mbrola_version/__init__.py
@@ -0,0 +1,7 @@
+MAJOR, MINOR, PATCH = 0, 3, 1
+
+VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
+"""The version of the package ``mutwo.mbrola``."""
+
+
+del MAJOR, MINOR, PATCH
