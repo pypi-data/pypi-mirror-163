132f73c3ffafac7f187d4258dabe67c2d498ea80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:23 +0000

    Add package jacobi/0.3.0/jacobi-0.3.0-py3-none-any.whl

---
+++ packages/jacobi/jacobi-0.3.0-py3-none-any.whl/jacobi/__init__.py
@@ -0,0 +1,4 @@
+"""Fast numerical derivatives for analytic functions with arbitrary round-off error."""
+
+from .core import jacobi, propagate  # noqa
+from ._version import version as __version__  # noqa
