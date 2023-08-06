4edefd86995f3650bbdccedf404cfad7227e175c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:40 +0000

    Add package thundercougarfalconbird/0.0.1/thundercougarfalconbird-0.0.1.tar.gz

---
+++ packages/thundercougarfalconbird/thundercougarfalconbird-0.0.1.tar.gz/thundercougarfalconbird-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['thundercougarfalconbird']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy']
+
+setup_kwargs = {
+    'name': 'thundercougarfalconbird',
+    'version': '0.0.1',
+    'description': 'ode integration rk4 rk runge kutta',
+    'long_description': '![](pics/eddy.png)\n\n# Just one word, `thundercougarfalconbird`\n\n# MIT License\n\n**Copyright (c) 2022 Mom\'s Friendly Robot Company**\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the "Software"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.',
+    'author': 'walchko',
+    'author_email': 'walchko@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://pypi.org/project/thundercougarfalconbird/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
