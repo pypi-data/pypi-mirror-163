60004eb3bdeed4f0c97a370cf7c68fec884d568d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:00 +0000

    Add package lebai/0.8.6/lebai-0.8.6-py3-none-any.whl

---
+++ packages/lebai/lebai-0.8.6-py3-none-any.whl/lebai/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2017-2021 Lebai Robotics
+# Author: kingfree
+# Contact: kingfree@toyama.moe
+
+"""Lebai Robot Python SDK"""
+
+from .type import RobotState, TaskStatus, IODeviceType, CartesianPose, JointPose, Error, RequestError
+from .robot import LebaiRobot
+from .scene import LebaiScene
