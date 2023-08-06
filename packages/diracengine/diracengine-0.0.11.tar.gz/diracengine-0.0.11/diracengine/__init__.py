e9b33f0adba0a2b95bd1a953ea60fe043a49a67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:34 +0000

    Add package diracengine/0.0.11/diracengine-0.0.11.tar.gz

---
+++ packages/diracengine/diracengine-0.0.11.tar.gz/diracengine-0.0.11/diracengine/__init__.py
@@ -0,0 +1,16 @@
+import diracengine.complex as complex
+from diracengine.complex import Complex
+
+import diracengine.complexmatrix as complexmatrix
+from diracengine.complexmatrix import ComplexMatrix
+
+import diracengine.constant as constant
+
+import diracengine.operator as operator
+from diracengine.operator import Operator
+
+import diracengine.state as state
+from diracengine.state import QuantumState
+
+import diracengine.wavefunction as wavefunction
+from diracengine.wavefunction import WaveFunction
---
+++ packages/diracengine/diracengine-0.0.11.tar.gz/diracengine-0.0.11/diracengine/constant.py
@@ -0,0 +1,6 @@
+hbar = 1
+halfhbar = .5 * hbar
+
+c = 299792458
+
+delta = .001
