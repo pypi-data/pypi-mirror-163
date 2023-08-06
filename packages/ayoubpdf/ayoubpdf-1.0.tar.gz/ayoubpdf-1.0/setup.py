1c86b3165f4022019f0d3a5a38c3c864250fc402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:43 +0000

    Add package ayoubpdf/1.0/ayoubpdf-1.0.tar.gz

---
+++ packages/ayoubpdf/ayoubpdf-1.0.tar.gz/ayoubpdf-1.0/setup.py
@@ -0,0 +1,9 @@
+from importlib.resources import read_text
+import setuptools
+from pathlib import Path
+setuptools.setup(
+    name="ayoubpdf",
+    version=1.0,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+)
