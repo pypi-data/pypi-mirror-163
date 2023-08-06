9bf97f619fa9e9ac6e553e45225cbb87f435cc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:18 +0000

    Add package shan/0.0.1/shan-0.0.1-py3-none-any.whl

---
+++ packages/shan/shan-0.0.1-py3-none-any.whl/shan/__init__.py
@@ -0,0 +1,4 @@
+# -*— coding: utf-8 -*-
+# FileName:__init__.py.py
+# Author:SHAN
+# Datetime:2022/8/15 15:12
---
+++ packages/shan/shan-0.0.1-py3-none-any.whl/shan/example.py
@@ -0,0 +1,7 @@
+# -*— coding: utf-8 -*-
+# FileName:example.py
+# Author:SHAN
+# Datetime:2022/8/15 15:12
+
+def add_one(number):
+    return number + 1
