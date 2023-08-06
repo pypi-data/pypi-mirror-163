a5c7a0a37d626a62b67bd221bf65e09fb9345656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:16 +0000

    Add package xueqiudanjuan/0.0.1/xueqiudanjuan-0.0.1.tar.gz

---
+++ packages/xueqiudanjuan/xueqiudanjuan-0.0.1.tar.gz/xueqiudanjuan-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='xueqiudanjuan',
+    version="0.0.1",
+    author='BugMakerH',
+    author_email="BugMakerH@gmail.com",
+    description="This is a package provided by BugMakerH.",
+    long_description="Text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
