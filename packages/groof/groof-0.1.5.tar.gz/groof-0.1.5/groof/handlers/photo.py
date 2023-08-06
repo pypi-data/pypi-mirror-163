d72fad5acf930830218c6d8607fe5d9ce2d94113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:20 +0000

    Add package groof/0.1.5/groof-0.1.5.tar.gz

---
+++ packages/groof/groof-0.1.5.tar.gz/groof-0.1.5/groof/handlers/photo.py
@@ -0,0 +1,30 @@
+from .base import Handler
+from .. import filters
+from ..loader import HANDLERS
+
+
+def photo(
+        user_id: int | list[int] = None,
+        chat_type: str | list[str] = None,
+        state: str = None,
+):
+    _filters = [
+        filters.Photo(),
+        filters.State(state),
+    ]
+
+    if user_id:
+        _filters.append(filters.UserId(user_id))
+
+    if chat_type:
+        _filters.append(filters.ChatType(chat_type))
+
+    def _(func):
+        if isinstance(func, Handler):
+            func = func.func
+
+        handler = Handler(func, _filters)
+        HANDLERS.append(handler)
+        return handler
+
+    return _
---
+++ packages/groof/groof-0.1.5.tar.gz/groof-0.1.5/groof/loader.py
@@ -0,0 +1,20 @@
+import logging
+
+import mongoengine
+import requests
+from envparse import env
+
+from .dispatcher import Dispatcher
+from .handlers.base import Handler
+
+BOT_TOKEN = env('BOT_TOKEN')
+MONGO_DB = env('MONGO_DB')
+MONGO_HOST = env('MONGO_HOST', 'localhost')
+
+logger = logging.getLogger('bot')
+mongoengine.connect(MONGO_DB, host=MONGO_HOST)
+session = requests.Session()
+
+dp = Dispatcher()
+
+HANDLERS: list[Handler] = []
