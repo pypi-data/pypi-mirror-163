f5dc8ae0f338f12c7aecae355755912130a42f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:28 +0000

    Add package weepy/1.0.13/weepy-1.0.13.tar.gz

---
+++ packages/weepy/weepy-1.0.13.tar.gz/weepy-1.0.13/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="weepy",
+	version="1.0.13",
+	scripts=["tools/dev_server.py"],
+	entry_points={
+		'console_scripts': ['weepy=dev_server:server'],
+	},
+	author="Patrik Katrenak",
+	author_email="patrik@katryapps.com",
+	description="Tiny Python ASGI framework",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://gitlab.com/katry/weepy",
+	packages=["weepy"],
+	install_requires=[],
+	extras_require={
+		"orjson": ["orjson"],
+		"ujson": ["ujson"],
+		"dev": ["daemon", "uvicorn[standard]", "orjson", "python-dotenv", "requests", "pytest"]
+	},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+		"Operating System :: OS Independent",
+	],
+	platforms=["any"],
+	python_requires=">=3.9",
+)
---
+++ packages/weepy/weepy-1.0.13.tar.gz/weepy-1.0.13/weepy/__init__.py
@@ -0,0 +1,4 @@
+from .request import Request, HTTPRequest
+from .response import Response, HTTPResponse
+from .route import Route, WSRoute, HTTPRoute
+from .asgi import ASGI
