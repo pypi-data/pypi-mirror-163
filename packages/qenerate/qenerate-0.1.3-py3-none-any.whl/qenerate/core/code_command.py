fada901773566d826197a80ba5e4829bd6887d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:26 +0000

    Add package qenerate/0.1.3/qenerate-0.1.3-py3-none-any.whl

---
+++ packages/qenerate/qenerate-0.1.3-py3-none-any.whl/qenerate/core/code_command.py
@@ -0,0 +1,71 @@
+import json
+import os
+
+from qenerate.core.plugin import Plugin
+from qenerate.plugins.pydantic_v1.plugin import (
+    AnonymousQueryError,
+    InvalidQueryError,
+    PydanticV1Plugin,
+)
+from qenerate.core.feature_flag_parser import FeatureFlagError, FeatureFlagParser
+
+
+plugins: dict[str, Plugin] = {
+    "pydantic_v1": PydanticV1Plugin(),
+}
+
+
+class CodeCommand:
+    @staticmethod
+    def _find_query_files(dir: str) -> list[str]:
+        result: list[str] = []
+        for root, _, files in os.walk(dir):
+            for name in files:
+                if name.endswith(".gql"):
+                    result.append(os.path.join(root, name))
+        return result
+
+    @staticmethod
+    def generate_code(introspection_file_path: str, dir: str):
+        with open(introspection_file_path) as f:
+            introspection = json.loads(f.read())["data"]
+
+        for file in CodeCommand._find_query_files(dir):
+            with open(file, "r") as f:
+                content = f.read()
+                try:
+                    feature_flags = FeatureFlagParser.parse(
+                        query=content,
+                    )
+                    if feature_flags.plugin not in plugins:
+                        print(
+                            f"[Skipping File] Query in {file} specifies "
+                            "unknown plugin: "
+                            f'"# qenerate: plugin={feature_flags.plugin}".'
+                        )
+                        continue
+                    plugin = plugins[feature_flags.plugin]
+                    generated_files = plugin.generate(
+                        query_file=file,
+                        raw_schema=introspection,
+                    )
+                except FeatureFlagError:
+                    print(
+                        f"[Skipping File] Query in {file} does not "
+                        "specify generator plugin via "
+                        '"# qenerate: plugin=<plugin_id>" set.'
+                    )
+                    continue
+                except AnonymousQueryError:
+                    print(
+                        f"[Skipping File] Query in {file} is anonymous. "
+                        "Qenerate does not support anonymous queries."
+                        "Please name the query."
+                    )
+                    continue
+                except InvalidQueryError:
+                    print(f"[Skipping File] Schema validation failed for query {file}.")
+                    continue
+
+                for generated_file in generated_files:
+                    generated_file.save()
