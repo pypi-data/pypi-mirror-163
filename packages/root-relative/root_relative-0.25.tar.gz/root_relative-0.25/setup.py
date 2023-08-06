e5af27f73c8fcc72968a41a343844d80638214fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:01 +0000

    Add package root-relative/0.25/root_relative-0.25.tar.gz

---
+++ packages/root-relative/root_relative-0.25.tar.gz/root_relative-0.25/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='root_relative',
+    version='0.25',
+    packages=['from_root'],
+    url='',
+    license='MIT',
+    author='liubomyr.ivanitskyi',
+    author_email='',
+    description='Module that allow to launch python module without requirements to start from working dir'
+)
