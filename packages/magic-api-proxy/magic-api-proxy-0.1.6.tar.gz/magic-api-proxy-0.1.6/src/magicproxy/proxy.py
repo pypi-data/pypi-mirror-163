0bf59b38e589596227ab752cfb8e2fb60bb82b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:53 +0000

    Add package magic-api-proxy/0.1.6/magic-api-proxy-0.1.6.tar.gz

---
+++ packages/magic-api-proxy/magic-api-proxy-0.1.6.tar.gz/magic-api-proxy-0.1.6/src/magicproxy/proxy.py
@@ -0,0 +1,133 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+import traceback
+from typing import Tuple, Set
+
+import flask
+import requests
+
+import magicproxy
+import magicproxy.types
+from .config import API_ROOT
+from .headers import clean_request_headers, clean_response_headers
+from . import magictoken
+from . import scopes
+from . import queries
+from .magictoken import magictoken_params_validate
+
+logger = logging.getLogger(__name__)
+
+app = flask.Flask(__name__)
+
+query_params_to_clean: Set[str] = set()
+
+custom_request_headers_to_clean: Set[str] = set()
+
+
+@app.route("/__magictoken", methods=["POST", "GET"])
+def create_magic_token():
+    if flask.request.method == "GET":
+        return "magic API proxy for " + API_ROOT + " version " + magicproxy.__version__
+    params = flask.request.json
+    try:
+        magictoken_params_validate(params)
+    except ValueError as e:
+        return str(e), 400
+
+    token = magictoken.create(
+        keys, params["token"], params.get("scopes"), params.get("allowed")
+    )
+
+    return token, 200, {"Content-Type": "application/jwt"}
+
+
+def _proxy_request(
+    request: flask.Request, url: str, headers=None, **kwargs
+) -> Tuple[bytes, int, dict]:
+    clean_headers = clean_request_headers(
+        request.headers, custom_request_headers_to_clean
+    )
+
+    if headers:
+        clean_headers.update(headers)
+
+    print(
+        f"Proxying to {request.method} {url}\nHeaders: {clean_headers}\nQuery: {request.args}\nContent: {request.data!r}"
+    )
+
+    # Make the API request
+    resp = requests.request(
+        url=url,
+        method=request.method,
+        headers=clean_headers,
+        params=dict(request.args),
+        data=request.data,
+        **kwargs,
+    )
+
+    response_headers = clean_response_headers(resp.headers)
+
+    print(resp, resp.headers, resp.content)
+
+    return resp.content, resp.status_code, response_headers
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>", methods=["POST", "GET", "PATCH", "PUT", "DELETE"])
+def proxy_api(path):
+    auth_token = flask.request.headers.get("Authorization")
+    if auth_token is None:
+        return "No authorization token presented", 401
+    # strip out "Bearer " if needed
+    if auth_token.startswith("Bearer "):
+        auth_token = auth_token[len("Bearer ") :]
+
+    try:
+        # Validate the magic token
+        token_info = magictoken.decode(keys, auth_token)
+    except ValueError:
+        return "Not a valid magic token", 400
+
+    # Validate scopes against URL and method.
+    if not scopes.validate_request(
+        flask.request.method, path, token_info.scopes, token_info.allowed
+    ):
+        return (
+            f"Disallowed by API proxy. Allowed scopes: {', '.join(token_info.scopes)}",
+            401,
+        )
+
+    path = queries.clean_path_queries(query_params_to_clean, path)
+
+    response = _proxy_request(
+        request=flask.request,
+        url=f"{API_ROOT}/{path}",
+        headers={"Authorization": f"Bearer {token_info.token}"},
+    )
+
+    try:
+        scopes.response_callback(*response, token_info.scopes)
+    except Exception as e:
+        logger.error("exception in response_callback")
+        logger.error(e)
+        logger.error(traceback.format_exc())
+
+    return response
+
+
+def run_app(host, port):
+    global keys
+    keys = magictoken.Keys.from_env()
+    app.run(host=host, port=port)
---
+++ packages/magic-api-proxy/magic-api-proxy-0.1.6.tar.gz/magic-api-proxy-0.1.6/src/magicproxy/scopes.py
@@ -0,0 +1,110 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+import re
+import types
+from typing import List, Union
+
+from magicproxy.config import SCOPES
+from magicproxy.types import Scope
+
+logger = logging.getLogger(__name__)
+
+
+def is_request_allowed(scope: Scope, method, path):
+    logger.debug(f"validating request {method} {path} on scope {scope}")
+
+    if method != scope.method and scope.method != "*":
+        return False
+
+    if re.match(scope.path, path, re.I):
+        return True
+
+
+def validate_request(
+    method: str,
+    path: str,
+    scopes: List[str] = None,
+    allowed: List[str] = None,
+) -> bool:
+    """Basic scope validation routine.
+
+    Args:
+        method: The request HTTP method.
+        path: The request path.
+        scopes: The list of allowed named scopes.
+        allowed: The list of allowed requests.
+
+    The named scope must be configured in the proxy
+    Or allowed passed through a METHOD path string like this:
+
+        METHOD path
+
+    For example:
+
+        GET /user
+        POST /repos/+?/+?/issues/+?/labels
+
+    Would allow getting the user info and updating labels on issues on a GitHub repo
+    """
+    if allowed is None:
+        allowed = []
+
+    if scopes is None:
+        scopes = []
+
+    if not path.startswith("/"):
+        path = f"/{path}"
+
+    for scope_key in scopes:
+        scope_element: Union[List[Scope], types.ModuleType] = SCOPES[scope_key]
+        if isinstance(scope_element, list):
+            for scope in scope_element:
+                if is_request_allowed(scope, method, path):
+                    return True
+        elif isinstance(scope_element, types.ModuleType):
+            if hasattr(scope_element, "is_request_allowed"):
+                return scope_element.is_request_allowed(method=method, path=path)
+
+    for allowed_item in allowed:
+        allowed_method, allowed_path = allowed_item.split(" ", 1)
+        if is_request_allowed(
+            Scope(method=allowed_method, path=allowed_path), method, path
+        ):
+            return True
+
+    return False
+
+
+def response_callback(content, code, headers, scopes=None):
+    """Response callback, for dynamic proxies
+
+    Args:
+        scopes: The list of allowed named scopes.
+
+    The named scope must be configured in the proxy
+
+    Would allow the proxy to process the response of a request
+    e.g. can allow a DELETE on a resource once its :id: is known in the JSON of the reponse
+    """
+    if scopes is None:
+        scopes = []
+
+    for scope_key in scopes:
+        scope_element: Union[List[Scope], types.ModuleType] = SCOPES[scope_key]
+        if isinstance(scope_element, types.ModuleType):
+            if hasattr(scope_element, "response_callback"):
+                return scope_element.response_callback(
+                    content=content, code=code, headers=headers
+                )
