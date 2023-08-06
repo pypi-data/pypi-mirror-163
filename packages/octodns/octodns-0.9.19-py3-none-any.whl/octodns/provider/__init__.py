32f0ab6ad312f81046369095e832a1582b088eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:44 +0000

    Add package octodns/0.9.19/octodns-0.9.19-py3-none-any.whl

---
+++ packages/octodns/octodns-0.9.19-py3-none-any.whl/octodns/provider/__init__.py
@@ -0,0 +1,18 @@
+#
+#
+#
+
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+
+class ProviderException(Exception):
+    pass
+
+
+class SupportsException(ProviderException):
+    pass
