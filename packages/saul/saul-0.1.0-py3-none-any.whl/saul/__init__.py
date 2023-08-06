e73e43bacadcecb91a218da134fcd2d232b895e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:11 +0000

    Add package saul/0.1.0/saul-0.1.0-py3-none-any.whl

---
+++ packages/saul/saul-0.1.0-py3-none-any.whl/saul/__init__.py
@@ -0,0 +1,7 @@
+name = "saul"
+description = "A simple license generator, based on choosealicense.com."
+__version_info__ = ("0", "1", "0")
+__version__ = ".".join(__version_info__)
+__author_name__ = "Dimitri Kokkonis"
+__author_email__ = "kokkonisd@gmail.com"
+__author__ = f"{__author_name__} ({__author_email__})"
