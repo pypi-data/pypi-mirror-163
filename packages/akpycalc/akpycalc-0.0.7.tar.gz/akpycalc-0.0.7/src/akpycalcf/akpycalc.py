2727c9c773479a346a335d6d9f5e3b379cb8ff7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:07 +0000

    Add package akpycalc/0.0.7/akpycalc-0.0.7.tar.gz

---
+++ packages/akpycalc/akpycalc-0.0.7.tar.gz/akpycalc-0.0.7/src/akpycalcf/akpycalc.py
@@ -0,0 +1,14 @@
+#creating MathOperation module
+#the module provides addition, subtraction, multiplication, and division functions
+
+#all the functions take two numbers as argument
+#creating addition function
+
+#class akpycalc1:
+#    def __init__(self) -> None:
+
+def add(num1,num2):
+    return num1+num2
+
+
+add()
---
+++ packages/akpycalc/akpycalc-0.0.7.tar.gz/akpycalc-0.0.7/test/testpypi.py
@@ -0,0 +1,10 @@
+print('hello')
+
+import akpycalcf.akpycalc as ak
+#from akpycalc.akpycalc1 import akpycalc1
+
+
+one = input('first number')
+two = input('second number')
+
+print("The sum is:",ak.add(int(one),int(two)))
