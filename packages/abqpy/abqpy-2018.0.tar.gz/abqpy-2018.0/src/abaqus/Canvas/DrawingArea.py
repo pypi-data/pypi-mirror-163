15da74b519023ff2f130cadc78538ac4c791e8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:29 +0000

    Add package abqpy/2018.0/abqpy-2018.0.tar.gz

---
+++ packages/abqpy/abqpy-2018.0.tar.gz/abqpy-2018.0/src/abaqus/Canvas/DrawingArea.py
@@ -0,0 +1,25 @@
+import typing
+
+
+class DrawingArea:
+    """The DrawingArea object specifies the location and size of the drawing area used for
+    placement of viewports.
+
+    Notes
+    -----
+    This object can be accessed by:
+
+    .. code-block::
+
+        session.drawingArea
+    """
+
+    #: A Float specifying the width in millimeters.
+    width: float = None
+
+    #: A Float specifying the height in millimeters.
+    height: float = None
+
+    #: A pair of Floats specifying the coordinates of the bottom left hand corner in
+    #: millimeters.
+    origin: typing.Tuple[float, ...] = (0.0, 0.0)
