f1f3199276b7d2f3ef972d9f8900735a74376d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:27 +0000

    Add package root-relative/0.27/root_relative-0.27.tar.gz

---
+++ packages/root-relative/root_relative-0.27.tar.gz/root_relative-0.27/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='root_relative',
+    version='0.27',
+    packages=['from_root'],
+    url='',
+    license='MIT',
+    author='liubomyr.ivanitskyi',
+    author_email='',
+    description='Module that allow to launch python module without requirements to start from working dir',
+    long_description=readme(),
+    long_description_content_type="text/markdown"
+)
