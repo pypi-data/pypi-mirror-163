bb5e2049552d1025b22317134f493d05a8b02867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:37 +0000

    Add package mosaicml-cli/0.2.8a2/mosaicml-cli-0.2.8a2.tar.gz

---
+++ packages/mosaicml-cli/mosaicml-cli-0.2.8a2.tar.gz/mosaicml-cli-0.2.8a2/mcli/serverside/platforms/overrides/__init__.py
@@ -0,0 +1,18 @@
+""" All Platform Overrides and configs """
+# pylint: disable=useless-import-alias
+from mcli.serverside.platforms.overrides.aws import AWSPlatform as AWSPlatform
+from mcli.serverside.platforms.overrides.azure import AzurePlatform as AzurePlatform
+from mcli.serverside.platforms.overrides.cota import COTAPlatform as COTAPlatform
+from mcli.serverside.platforms.overrides.gcp import GCPPlatform as GCPPlatform
+from mcli.serverside.platforms.overrides.microk8s import Microk8sPlatform as Microk8sPlatform
+from mcli.serverside.platforms.overrides.r1z1 import R1Z1Platform as R1Z1Platform
+from mcli.serverside.platforms.overrides.r1z2 import R1Z2Platform as R1Z2Platform
+from mcli.serverside.platforms.overrides.r1z4 import R1Z4Platform as R1Z4Platform
+from mcli.serverside.platforms.overrides.r6z1 import R6Z1Platform as R6Z1Platform
+from mcli.serverside.platforms.overrides.r6z2 import R6Z2Platform as R6Z2Platform
+from mcli.serverside.platforms.overrides.r6z3 import R6Z3Platform as R6Z3Platform
+from mcli.serverside.platforms.overrides.r6z4 import R6Z4Platform as R6Z4Platform
+from mcli.serverside.platforms.overrides.r7z1 import R7Z1Platform as R7Z1Platform
+from mcli.serverside.platforms.overrides.r7z2 import R7Z2Platform as R7Z2Platform
+from mcli.serverside.platforms.overrides.r7z3 import R7Z3Platform as R7Z3Platform
+from mcli.serverside.platforms.overrides.r7z4 import R7Z4Platform as R7Z4Platform
