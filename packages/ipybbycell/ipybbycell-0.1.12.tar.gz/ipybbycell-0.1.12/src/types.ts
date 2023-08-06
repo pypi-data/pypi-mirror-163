ef46dba330cb9bbf402432257a0f9cccc97e7959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:33 +0000

    Add package ipybbycell/0.1.12/ipybbycell-0.1.12.tar.gz

---
+++ packages/ipybbycell/ipybbycell-0.1.12.tar.gz/ipybbycell-0.1.12/src/types.ts
@@ -0,0 +1,43 @@
+import type {
+  IJupyterCell,
+  IJupyterMetadata,
+  IJupyterCellMetadata,
+} from '@boyuai/jupyter-helper/lib/types';
+
+export interface ICell
+  extends Omit<IJupyterCell, '$cellIndex' | 'dryrunOutputs'> {
+  metadata: IJupyterCellMetadata;
+  execution_count: number | null;
+  element: HTMLElement[];
+}
+
+export interface IJupyterNotebook {
+  metadata: IJupyterMetadata;
+  base_url: string;
+  notebook_path: string;
+  events: any;
+  get_cell: (index: number) => ICell;
+  get_cells: () => ICell[];
+  get_selected_cell: () => ICell;
+  get_selected_index: () => number | null;
+  focus_cell: () => void;
+  select: (index: number) => void;
+  insert_cell_below: (type: 'code' | 'markdown') => any;
+  delete_cells: () => void;
+  toJSON: () => Record<string, any>;
+  save_notebook: () => Promise<any>;
+}
+
+export interface IJupyter {
+  notebook: IJupyterNotebook;
+  toolbar: any;
+  keyboard_manager: any;
+  dialog: {
+    modal: (options: {
+      title: string;
+      body: string;
+      sanitize?: boolean;
+      buttons?: Record<string, any>;
+    }) => any; // https://github.com/jupyter/notebook/blob/master/notebook/static/base/js/dialog.js#L40
+  };
+}
