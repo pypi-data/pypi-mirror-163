08fc039fff7fdc7434d5cc6afec364cecabdafd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:04 +0000

    Add package libSDT/0.0.0/libSDT-0.0.0.tar.gz

---
+++ packages/libSDT/libSDT-0.0.0.tar.gz/libSDT-0.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+pkg_name = 'libSDT'
+version = '0.0.0'
+
+setuptools.setup(
+    name=pkg_name,
+    version=version,
+    description='Local packages for development',
+
+    author='hypothesisbase',
+    author_email="support@hypothesisbase.com",
+    url= f'http://pypi.python.org/pypi/{pkg_name}/{version}',
+    license="MIT",
+
+    install_requires=[
+        "bs4",
+        "leveldb",
+        "selenium",
+        "paramiko",
+        "setuptools",
+        "setuptools-rust",
+    ],
+    packages=setuptools.find_packages(),
+    zip_safe=False
+)
