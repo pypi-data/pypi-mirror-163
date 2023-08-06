891f59fdc0f3bbe1e8491488e46486ed9cc79695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:47 +0000

    Add package move-dl/1.0.8/move-dl-1.0.8.tar.gz

---
+++ packages/move-dl/move-dl-1.0.8.tar.gz/move-dl-1.0.8/src/move/__init__.py
@@ -0,0 +1,6 @@
+__license__ = "MIT"
+__version__ = (1, 0, 8)
+__all__ = ["conf", "data", "models", "training", "VAE"]
+
+from move import conf, models, training, data
+from move.models.vae import VAE
