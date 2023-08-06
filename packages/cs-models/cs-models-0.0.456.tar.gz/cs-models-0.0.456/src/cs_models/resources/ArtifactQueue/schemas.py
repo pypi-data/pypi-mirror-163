c520041b0a16ed77979b27700e05a0e998da18a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:37 +0000

    Add package cs-models/0.0.456/cs-models-0.0.456.tar.gz

---
+++ packages/cs-models/cs-models-0.0.456.tar.gz/cs-models-0.0.456/src/cs_models/resources/ArtifactQueue/schemas.py
@@ -0,0 +1,17 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class ArtifactQueueResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    source_type = fields.String(required=True)
+    source_table = fields.String(required=True)
+    source_id = fields.Integer(required=True)
+    artifact_mappings = fields.String(allow_none=True)
+    date = fields.DateTime(allow_none=True)
+    updated_at = fields.DateTime()
