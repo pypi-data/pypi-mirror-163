9695637824a35348621ed97663a987b93610733b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:44 +0000

    Add package eth-multicall/0.2.1/eth_multicall-0.2.1.tar.gz

---
+++ packages/eth-multicall/eth_multicall-0.2.1.tar.gz/eth_multicall-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['eth_multicall']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['web3>=5.30.0,<6.0.0']
+
+setup_kwargs = {
+    'name': 'eth-multicall',
+    'version': '0.2.1',
+    'description': 'Uses Multicall v3 and eth_abi encode + decoder api to fetch multiple json-rpc queries all at once',
+    'long_description': None,
+    'author': 'Nfel',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
