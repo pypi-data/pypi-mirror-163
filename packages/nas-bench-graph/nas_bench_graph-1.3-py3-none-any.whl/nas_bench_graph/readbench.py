61ffefa82bd81fbbad73dfa59da877e6641c2d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:52 +0000

    Add package nas-bench-graph/1.3/nas_bench_graph-1.3-py3-none-any.whl

---
+++ packages/nas-bench-graph/nas_bench_graph-1.3-py3-none-any.whl/nas_bench_graph/readbench.py
@@ -0,0 +1,17 @@
+import pickle
+from .architecture import all_archs
+
+def light_read(dname):
+    f = open("light/{}.bench".format(dname), "rb")
+    bench = pickle.load(f)
+    f.close()
+    return bench
+
+def read(name):
+    f = open(name, "rb")
+    bench = pickle.load(f)
+    f.close()
+    return bench
+
+if __name__ == "__main__":
+    read()
