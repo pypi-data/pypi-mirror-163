34c0992cc846f63ab9ed78b689fd29edf9973d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:07 +0000

    Add package django-nats-nkeys/0.6.4/django_nats_nkeys-0.6.4.tar.gz

---
+++ packages/django-nats-nkeys/django_nats_nkeys-0.6.4.tar.gz/django_nats_nkeys-0.6.4/django_nats_nkeys/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+
+from django_nats_nkeys.settings import nats_nkeys_settings
+
+from organizations.models import (
+    Organization,
+    OrganizationUser,
+    OrganizationOwner,
+    OrganizationInvitation,
+)
+
+NatsOrganization = nats_nkeys_settings.get_nats_account_model()
+NatsOrganizationOwner = nats_nkeys_settings.get_NATS_ORGANIZATION_OWNER_MODEL()
+NatsApp = nats_nkeys_settings.get_nats_organization_app_model()
+NatsOrganizationUser = nats_nkeys_settings.get_nats_user_model()
+
+
+@admin.register(NatsOrganization)
+class NatsOrganizationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NatsApp)
+class NatsOrganizationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NatsOrganizationOwner)
+class NatsOrganizationOwnerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NatsOrganizationUser)
+class NatsAccountOwnerAdmin(admin.ModelAdmin):
+    pass
+
+
+# unregister django organization defaults and register our own
+admin.site.unregister(Organization)
+admin.site.unregister(OrganizationUser)
+admin.site.unregister(OrganizationOwner)
+admin.site.unregister(OrganizationInvitation)
