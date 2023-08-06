fed6c3d2292382d19bd77e024c177f5fd603b686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:44 +0000

    Add package kcli/99.0.202208170824/kcli-99.0.202208170824-py3-none-any.whl

---
+++ packages/kcli/kcli-99.0.202208170824-py3-none-any.whl/kvirt/openshift/apps/advanced-cluster-management/post.sh
@@ -0,0 +1,12 @@
+oc create -n open-cluster-management secret generic open-cluster-management-image-pull-credentials --from-file=.dockerconfigjson={{ pull_secret }}  --type=kubernetes.io/dockerconfigjson
+{% if acm_deploy_baremetal_console %}
+STATUS="Installing"
+while [ "$STATUS" != "Running" ] ; do
+ STATUS=$(oc -n open-cluster-management get multiclusterhub multiclusterhub -o jsonpath='{.status.phase}')
+ sleep 5
+done
+# Enabling Bare metal consoles
+oc -n open-cluster-management patch deploy console-header -p '{"spec":{"template":{"spec":{"containers":[{"name":"console-header","env":[{"name": "featureFlags_baremetal","value":"true"}]}]}}}}'
+DEPLOY_NAME=$(oc -n open-cluster-management get deploy -o name | grep consoleui)
+oc -n open-cluster-management patch ${DEPLOY_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"hcm-ui","env":[{"name": "featureFlags_baremetal","value":"true"}]}]}}}}'
+{% endif %}
