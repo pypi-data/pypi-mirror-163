88786d84ab7c38438c146f3da9c8dcb662dddb9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:47 +0000

    Add package gnb-distributions-analyzer/0.1/gnb_distributions_analyzer-0.1.tar.gz

---
+++ packages/gnb-distributions-analyzer/gnb_distributions_analyzer-0.1.tar.gz/gnb_distributions_analyzer-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gnb_distributions_analyzer',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gnb_distributions_analyzer'],
+      author='Amit Kumar Sharma',
+      author_email='ksamit1110@gmail.com',
+      zip_safe=False)
