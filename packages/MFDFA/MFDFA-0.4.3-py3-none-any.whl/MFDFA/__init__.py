1bd66e99ca89e822606c4086d5f02e87adf764a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:28 +0000

    Add package MFDFA/0.4.3/MFDFA-0.4.3-py3-none-any.whl

---
+++ packages/MFDFA/MFDFA-0.4.3-py3-none-any.whl/MFDFA/__init__.py
@@ -0,0 +1,9 @@
+from .MFDFA import MFDFA
+from .fgn import fgn
+from .emddetrender import detrendedtimeseries, IMFs
+from . import singspect
+
+__name__ = "MFDFA"
+__version__ = "0.4.3"
+__author__ = "Leonardo Rydin Gorjão"
+__copyright__ = "Copyright 2019-2022 Leonardo Rydin Gorjão, MIT License"
