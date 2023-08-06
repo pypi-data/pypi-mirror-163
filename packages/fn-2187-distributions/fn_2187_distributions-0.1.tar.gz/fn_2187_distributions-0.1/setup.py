35bbc7088c849661cb43ed8acc6937022b795d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:15 +0000

    Add package fn-2187-distributions/0.1/fn_2187_distributions-0.1.tar.gz

---
+++ packages/fn-2187-distributions/fn_2187_distributions-0.1.tar.gz/fn_2187_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fn_2187_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['fn_2187_distributions'],
+      author = 'Adesugba Oluwatoba',
+      author_email = 'tobaadesugba@gmail.com',
+      zip_safe=False)
