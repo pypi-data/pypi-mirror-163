71fed3ac97b752457da01f5f98c19e90f64f5dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:44 +0000

    Add package cipher-symmetric/1.0.1/cipher-symmetric-1.0.1.tar.gz

---
+++ packages/cipher-symmetric/cipher-symmetric-1.0.1.tar.gz/cipher-symmetric-1.0.1/README.md
@@ -0,0 +1,37 @@
+# Cipher-symmetric
+
+## The modern python based encryption and decryption utility
+
+Cipher is a python CLI utility for encryption and decryption of strings using the cryptography module of python(symmetric encryption).
+
+Installation:
+
+Linux and Mac:
+```sh
+pip3 install cipher-symmetric
+
+# via git
+pip3 install git+https://github.com/Programming-geek582/encipher.git
+```
+
+Windows:
+```bat
+pip install cipher-symmetric
+
+# via git
+pip install git+https://github.com/Programming-geek582/encipher.git
+```
+
+Usage:
+
+```sh
+# Windows
+cipher or python -m cipher
+
+# Linux and mac
+cipher or python3 -m cipher
+```
+
+Happy encrypting!
+
+***Note: the private key is saved in a weird file format, dont try to mess with that or you wont be able to decrypt the content ever again***
---
+++ packages/cipher-symmetric/cipher-symmetric-1.0.1.tar.gz/cipher-symmetric-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+import cipher
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cipher-symmetric",
+    author=cipher.__author__,
+    author_email=cipher.__email__,
+    version=cipher.__version__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=cipher.__github__,
+    packages=["cipher"],
+    entry_points={"console_scripts": ["cipher=cipher.__main__:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.8",
+    include_package_data=True,
+    exclude=("__pycache__",)
+)
+
